aaa7eeb960abecb760878b81e070089157837a1c
Peter Priestley <peterpriestley@Peters-MacBook-Pro.local>
2016-05-23 08:52:46 +1000

    Added support for Venn Diagrams and basic Rainbow charts

--- analysisscripts/analyseVCF.py
+++ analysisscripts/analyseVCF.py
@@ -1,35 +1,40 @@
 #!/usr/local/bin/python
 
 ROUNDING_PRECISION = 4
+POS_PERCENTILE_BUCKET = 0.1
+CALCULATE_VENN = False
 Path = "/Users/peterpriestley/hmf/70-30sample/"
 VCFFile = "combined.vcf"
+#VCFFile = "CPCT11111111R_CPCT11111111T_merged_somatics_snpEff_dbSNP_Cosmicv76_melted.vcf"
 
-class chromosomeLength():
-    chr1 = 249250622
-    chr10 = 135534749
-    chr11 = 135006518
-    chr12 = 133851897
-    chr13 = 115169880
-    chr14 = 107349541
-    chr15 = 102531394
-    chr16 = 90354755
-    chr17 = 81195212
-    chr18 = 78077250
-    chr19 = 59128985
-    chr2 = 243199374
-    chr20 = 63025522
-    chr21 = 48129897
-    chr22 = 51304568
-    chr3 = 198022431
-    chr4 = 191154277
-    chr5 = 180915261
-    chr6 = 171115068
-    chr7 = 159138664
-    chr8 = 146364023
-    chr9 = 141213432
-    chrMT = 16571
-    chrX = 155270561
-    chrY = 59373567
+#DEFINE CHR LENGTH
+chromosomeLength = {}
+chromosomeLength['1'] = 249250622
+chromosomeLength['10'] = 135534749
+chromosomeLength['11'] = 135006518
+chromosomeLength['12'] = 133851897
+chromosomeLength['13'] = 115169880
+chromosomeLength['14'] = 107349541
+chromosomeLength['15'] = 102531394
+chromosomeLength['16'] = 90354755
+chromosomeLength['17'] = 81195212
+chromosomeLength['18'] = 78077250
+chromosomeLength['19'] = 59128985
+chromosomeLength['2'] = 243199374
+chromosomeLength['20'] = 63025522
+chromosomeLength['21'] = 48129897
+chromosomeLength['22'] = 51304568
+chromosomeLength['3'] = 198022431
+chromosomeLength['4'] = 191154277
+chromosomeLength['5'] = 180915261
+chromosomeLength['6'] = 171115068
+chromosomeLength['7'] = 159138664
+chromosomeLength['8'] = 146364023
+chromosomeLength['9'] = 141213432
+chromosomeLength['MT'] = 16571
+chromosomeLength['X'] = 155270561
+chromosomeLength['Y'] = 59373567
+
 
 class variantType():
     sameAsRef = "Same as Ref"
@@ -49,7 +54,7 @@ class genotype:
     variantCountTumorPrivate = {}
     variantCountSubTypeTumor = {}
     variantCountSubTypeTumorPrivate = {}
-
+    variantFreebayesSNP=0
 
     def __init__(self,caller,ref,alt,inputGenotype):
         altsplit = (ref + ","+ alt).split(',')
@@ -66,6 +71,11 @@ class genotype:
             alleleTumor2 = altsplit[int(inputGenotype[1][2])]
             if len(alleleTumor1) == len(alleleTumor2) and len(alleleTumor1) == len(ref):
                 self.tumorVariantType = variantType.SNP
+                #DEBUG
+                #if len(ref) > 1 and len(altsplit)>2:
+                #    print caller,ref,alt,inputGenotype
+                #    genotype.variantFreebayesSNP += 1
+                #END DEBUG
             else:
                 self.tumorVariantType = variantType.indel
                 if len(alleleTumor1) <= len(ref) and len(alleleTumor2) <= len(ref):
@@ -124,16 +134,16 @@ class somaticVariant:
     variantCountSNPNumberCallers = {}
     variantCountIndelNumberCallers = {}
     variantCountIndelTotal = 0
+    variantCountVenn = {}
+    variantCountVennByChromPos = {}
 
-    def __init__(self, chrom, pos, id, ref, alt, filter, inputGenotypes):
+    def __init__(self, chrom, pos, id, ref, alt, filter, format, inputGenotypes):
 
         if filter == "PASS" or filter == ".":
             tumorCallerCountSNP = 0
             tumorCallerCountIndel = 0
-            #self.chrom = chrom
-            #self.pos = pos
-            #self.id = id
             variantGenotypes = {}
+
 
             for key in inputGenotypes.iterkeys():
                 variantGenotypes[key] = genotype(key, ref, alt, inputGenotypes[key])
@@ -167,6 +177,30 @@ class somaticVariant:
                 if variantGenotype.tumorVariantType == variantType.indel and tumorCallerCountIndel == 1:
                     variantGenotype.markPrivate(caller)
 
+            # VENN
+            if CALCULATE_VENN == True:
+                tumorCallerType = ""
+                posPercentile = str(round(float(pos) / chromosomeLength[chrom] / POS_PERCENTILE_BUCKET,0) * POS_PERCENTILE_BUCKET)
+                if tumorCallerCountSNP > 0:
+                    tumorCallerType = tumorCallerType + "SNP"
+                if tumorCallerCountIndel > 0:
+                    tumorCallerType = tumorCallerType + "Indel"
+                formatSplit = format.split(';')
+                for i in range(len(formatSplit)):
+                    formatItem = formatSplit[i].split('=')
+                    if formatItem[0] == "set":
+                        if somaticVariant.variantCountVenn.has_key(tumorCallerType + " " + formatItem[1]):
+                            somaticVariant.variantCountVenn[tumorCallerType + " " + formatItem[1]] += 1
+                        else:
+                            somaticVariant.variantCountVenn[tumorCallerType + " " + formatItem[1]] = 1
+                        if somaticVariant.variantCountVennByChromPos.has_key(
+                                                                chrom + " " + posPercentile + " " + formatItem[1]):
+                            somaticVariant.variantCountVennByChromPos[
+                                chrom + " " + posPercentile + " " + formatItem[1]] += 1
+                        else:
+                            somaticVariant.variantCountVennByChromPos[
+                                chrom + " " + posPercentile + " " + formatItem[1]] = 1
+
             if tumorCallerCountSNP > 0:
                 somaticVariant.variantCountSNPTotal += 1
 
@@ -186,7 +220,7 @@ def loadVaraintsFromVCF(aPath, aVCFFile,aVariants):
                 myGenotypes['strelka'] = [a[13][:3], a[15][:3]]
                 myGenotypes['varscan'] = [a[14][:3], a[16][:3]]
                 #myGenotypes['melted'] = [a[9][:3], a[9][:3]]
-                aVariants.append(somaticVariant(a[0], a[1], a[2], a[3], a[4], a[6], myGenotypes))
+                aVariants.append(somaticVariant(a[0], a[1], a[2], a[3], a[4], a[6], a[7], myGenotypes))
 
     return 1
 
@@ -194,7 +228,7 @@ def loadVaraintsFromVCF(aPath, aVCFFile,aVariants):
 variants = []
 loadVaraintsFromVCF(Path,VCFFile,variants)
 
-
+print genotype.variantFreebayesSNP
 print "NORMAL VARIANTS"
 for key, value in sorted(genotype.variantCountSubTypeNormal.items()):
     print "%s: %s" % (key, value)
@@ -209,12 +243,16 @@ print "\nIndel 'Truth Set': ",indelTruthSet
 print "SNP 'Truth Set': ",snpTruthSet
 print "\nSENSITIVITY"
 for myVariantType,myTumorCount in sorted(genotype.variantCountTumor.items()):
-            if myVariantType[:3] == 'SNP':
-                print myVariantType,":",round(float(myTumorCount-genotype.variantCountTumorPrivate[myVariantType])/snpTruthSet,ROUNDING_PRECISION)
-            elif myVariantType[:5]  == 'INDEL':
-                print myVariantType,":",round(float(myTumorCount-genotype.variantCountTumorPrivate[myVariantType])/indelTruthSet,ROUNDING_PRECISION)
+    if myVariantType[:3] == 'SNP':
+        print myVariantType,":",round(float(myTumorCount-genotype.variantCountTumorPrivate[myVariantType])/snpTruthSet,ROUNDING_PRECISION)
+    elif myVariantType[:5]  == 'INDEL':
+        print myVariantType,":",round(float(myTumorCount-genotype.variantCountTumorPrivate[myVariantType])/indelTruthSet,ROUNDING_PRECISION)
 print "\nPRECISION"
 for myVariantType,myTumorCount in sorted(genotype.variantCountSubTypeTumorPrivate.items()):
             print myVariantType,":",round(1-float(myTumorCount)/float(genotype.variantCountSubTypeTumor[myVariantType]),ROUNDING_PRECISION)
 print "\nNumber of Callers SNP: ",somaticVariant.variantCountSNPNumberCallers
 print "Number of Callers Indel: ", somaticVariant.variantCountIndelNumberCallers
+for venn,vennCount in sorted(somaticVariant.variantCountVenn.items()):
+    print venn,": ",vennCount
+for vennDecile,vennDecileCount in sorted(somaticVariant.variantCountVennByChromPos.items()):
+    print vennDecile,": ",vennDecileCount
