315ad16bd3fa5db23d6dfcc6d150f660e00a1372
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2016-05-24 16:09:06 +0200

    Samplesheets now do not have to contain string "HMFreg" anymore

--- crunch/sampleSheetParse.sh
+++ crunch/sampleSheetParse.sh
@@ -21,5 +21,5 @@ for sheet in /data1/illumina_data/*/SampleSheet.csv; do
 	## print internal HMF run-name
 	cat $sheet | grep "ExperimentName" | cut -d',' --output-delimiter ": " -f 1,2
 	## print sample-id, sample_name, submission-id, description
-	cat $sheet | grep "HMFreg" | cut -d',' --output-delimiter " " -f2,3,8,9 | sort | uniq; 
+	cat $sheet | sed -e '1,/Lane,Sample_ID/d' | cut -d',' --output-delimiter " " -f2,3,8,9 | sort | uniq; 
 done
