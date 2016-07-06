5d434a3d54961f2e05cdefe7b5897d7ea4515da4
peterpriestley <p.priestley@hartwigmedicalfoundation.nl>
2016-07-06 22:42:02 +1000

    support for ipython

--- analysisscripts/analysePileup.py
+++ analysisscripts/analysePileup.py
@@ -1,8 +1,5 @@
-
-PATH = "/Users/peterpriestley/hmf/sliceCPCT02010267/CPCT02010267.pileup"
-PILEUP_FILENAME = ""
-sorted_coordinates = [(17,55999856),(17,60000147)]
-
+#!/usr/local/bin/python
+import pandas as pd
 
 def readPileup(aPath,aFileName,aPileupCoordinates):
     aPileupCoordinates.reverse()
@@ -12,12 +9,18 @@ def readPileup(aPath,aFileName,aPileupCoordinates):
         for line in f:
             line = line.strip('\n')
             a = [x for x in line.split('\t')]
-            while aPileupCoordinates and (int(a[0])>myCoordinate[0] or (int(a[0])==myCoordinate[0] and int(a[1]) > myCoordinate[1])):
+            while aPileupCoordinates and (int(a[0])>int(myCoordinate[0]) or (int(a[0])==int(myCoordinate[0]) and int(a[1]) > int(myCoordinate[1]))):
                 myCoordinate = aPileupCoordinates.pop()
-            if int(a[0]) == myCoordinate[0] and int(a[1]) == myCoordinate[1]:
+            if int(a[0]) == int(myCoordinate[0]) and int(a[1]) == int(myCoordinate[1]):
                 filteredPileup.append(a)
-    return filteredPileup
+    df = pd.DataFrame(filteredPileup)
+    df.columns = (['chrom', 'pos', 'ref', 'DP','Bases','BaseQ'])
+    return df
 
-pileupOutput = readPileup(PATH,PILEUP_FILENAME,sorted_coordinates)
-print pileupOutput
+if __name__ == "__main__":
+    PATH = "/Users/peterpriestley/hmf/sliceCPCT02010267/"
+    PILEUP_FILENAME = "CPCT02010267.pileup"
+    sorted_coordinates = [(17, 55999856), (17, 60000147)]
+    pileupOutput = readPileup(PATH,PILEUP_FILENAME,sorted_coordinates)
+    print pileupOutput
 
