54acf57c837de21bb93ac8d033f1c9f30ccd31df
peterpriestley <p.priestley@hartwigmedicalfoundation.nl>
2017-02-22 21:36:22 +1100

    added comments

--- analysisscripts/mutPatternsPDFbyTumorType.R
+++ analysisscripts/mutPatternsPDFbyTumorType.R
@@ -18,24 +18,31 @@ searchDir = "/data/cpct/runs/"
 fileNamePattern = "melted.vcf"
 patientlistFile = "/home/peter/tumor_data.csv"
 
+#Load patients and tumor types from ECRF
 tumor_data <- read.csv(file=patientlistFile, header=TRUE, sep=",",strip.white=TRUE)[,1:2]
 my_tumor_list <- split(tumor_data,tumor_data[2])
+
+# Find all somatic vcfs in the health check runs
 allVCFFiles<-list.files(path=searchDir,pattern=fileNamePattern,recursive=TRUE)
+
 for (name in names(my_tumor_list)) {
   if (name != "") {
+    # filter for vcfs with the  tumor type
     vcf_files<-allVCFFiles[grepl((paste(my_tumor_list[[name]][,1],collapse="|")),allVCFFiles)]
     print(paste(name,length(vcf_files)))
-    if (length(vcf_files)>0 & length(vcf_files)<10){   #ONLY DO SMALL COHORTS FOR TESTING
-      print(paste(name,length(vcf_files)))
-      #print(vcf_files)
+    if (length(vcf_files)>0 & length(vcf_files)<10){   ### TEMP CONDITION - ONLY DO SMALL COHORTS FOR TESTING
+      
+      # Load VCFs
       vcf_files<-paste(searchDir,vcf_files,sep="")
       vcfs = read_vcfs_as_granges(vcf_files, tools::file_path_sans_ext(basename(vcf_files)), genome = "hg19")### GENOME??????########
       auto = extractSeqlevelsByGroup(species="Homo_sapiens",style="UCSC",group="auto")
       vcfs = lapply(vcfs, function(x) keepSeqlevels(x, auto))
       
-      # Fit & plot to pdf
+      # Fitting
       fit_res = fit_to_signatures(mut_matrix(vcf_list = vcfs, ref_genome = ref_genome), cancer_signatures)
       select = which(rowSums(fit_res$contribution) > 0.02 * sum(fit_res$contribution))  #2% contribution cutoff
+      
+      # Plot to PDF
       pc<-plot_contribution(fit_res$contribution[select,,drop=FALSE], cancer_signatures[,select], coord_flip = F, mode = "absolute")
       pdf(file=paste("test",name,".pdf",sep=""),width=10)#, width=10, height=2, pointsize=6, useDingbats=FALSE)
       print(pc)
