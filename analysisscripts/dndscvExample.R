3d1d853bc22cf9ba26af9d111d9611eb91fc0681
Peter Priestley <p.priestley@hartwigmedicalfoundation.nl>
2017-09-26 20:20:13 +1000

    Script now looks at all samples from a cohort

--- analysisscripts/dndscvExample.R
+++ analysisscripts/dndscvExample.R
@@ -7,13 +7,14 @@ sample_mutations<-function(dbConnect, cohort)
     "select sv.sampleId, chromosome as chr, position as pos, ref,alt from somaticVariant sv,sample s,patient p ",
     "where sv.sampleId = s.sampleId and s.patientId = p.id and p.primaryTumorLocation='",
     cohort,
-    "' and length(alt) = length(ref) and filter = 'PASS' limit 500000",
+    "' and length(alt) = length(ref) and filter = 'PASS' and gene <> '' limit 3000000",
     sep = "")
-  
+  print(query)
   raw_data = dbGetQuery(dbConnect, query)
 }
 
 dbConnect = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
 mutations<-sample_mutations(dbConnect,'Melanoma')
 dbDisconnect(dbConnect)
-dndscv(mutations)
+output<-dndscv(mutations)
+output
