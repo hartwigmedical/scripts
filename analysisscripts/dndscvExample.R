28e6410031b40ac7c287d0cfc48923fadb733191
Peter Priestley <p.priestley@hartwigmedicalfoundation.nl>
2017-10-25 10:30:56 +1100

    minor changes

--- analysisscripts/dndscvExample.R
+++ analysisscripts/dndscvExample.R
@@ -1,3 +1,4 @@
+### ssh -L 3307:localhost:3306 peter@ext-hmf-datastore
 library(MASS)
 library(dndscv)
 library(MutationalPatterns)
@@ -5,11 +6,11 @@ library(RMySQL)
 library(data.table)
 library("NMF")
 
-sample_mutations_all_snv<-function(dbConnect, cohort)
+sample_mutations_all_snv<-function(dbConnect)
 {
   query = paste(
-    "select sv.sampleId, chromosome as chr, position as pos, ref,alt from somaticVariant sv",
-    "where length(alt) = length(ref) and filter = 'PASS' and gene <> '' limit 20000000",
+    "select sv.sampleId, chromosome as chr, position as pos, ref,alt  from somaticVariant sv where length(alt) = length(ref) and filter = 'PASS' and gene <> ''",
+    "and right(sv.sampleId,1) ='T'",
     sep = "")
   print(query)
   raw_data = dbGetQuery(dbConnect, query)
@@ -21,14 +22,17 @@ sample_mutations<-function(dbConnect, cohort)
     "select sv.sampleId, chromosome as chr, position as pos, ref,alt from somaticVariant sv,sample s,patient p ",
     "where sv.sampleId = s.sampleId and s.patientId = p.id and upper(primaryTumorLocation) like '%",
     cohort,
-    "%' and length(alt) = length(ref) and filter = 'PASS' and gene <> '' limit 10000000",
+    "%' and length(alt) = length(ref) and filter = 'PASS' and gene <> '' limit 20000000 and right(s.sampleId,1) ='T'",
     sep = "")
   print(query)
   raw_data = dbGetQuery(dbConnect, query)
 }
 
+### TO DO - ENSURE WE ONLY GET 1 copy of each tumor!!!!
+
 dbConnect = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
-mutations<-sample_mutations(dbConnect,'BREAST')
+#mutations<-sample_mutations(dbConnect,'BREAST')
+mutations<-sample_mutations_all_snv(dbConnect)
 dbDisconnect(dbConnect)
 output<-dndscv(mutations)
 output
--- analysisscripts/mutSignature.R
+++ analysisscripts/mutSignature.R
@@ -1,3 +1,4 @@
+library(devtools)#; install_github("im3sanger/dndscv")
 library(MutationalPatterns)
 library(RMySQL)
 library(data.table)
@@ -45,6 +46,8 @@ create_empty_signature2<-function() {
   }
   return (DF)
 }
+
+
 
 create_empty_signature<-function() {
   DF <- data.frame(type=character(), context=character(), count=double(), stringsAsFactors=FALSE)
@@ -151,4 +154,25 @@ nmf_res <- extract_signatures(mutation_matrix, rank = 2)
 
 
 ######## RANDOM TESTING UNDER HERE
+library(MASS)
+library(dndscv)
 
+sample_mutations<-function(dbConnect, cohort)
+{
+  query = paste(
+    "select sv.sampleId, chromosome as chr, position as pos, ref,alt from somaticVariant sv,sample s,patient p ",
+    "where sv.sampleId = s.sampleId and s.patientId = p.id and p.primaryTumorLocation='",
+    cohort,
+    "' and length(alt) = length(ref) and filter = 'PASS' and gene <> '' limit 500000",
+    sep = "")
+  
+  raw_data = dbGetQuery(dbConnect, query)
+}
+
+dbConnect = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
+mutations<-sample_mutations(dbConnect,'Melanoma')
+dbDisconnect(dbConnect)
+dndscv(mutations)
+
+paste((cohort),collapse=" ")
+c(cohort)
