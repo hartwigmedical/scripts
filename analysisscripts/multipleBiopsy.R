47f73f8b196e18a5be3c47d3222303595dcb7d4f
Peter Priestley <p.priestley@hartwigmedicalfoundation.nl>
2017-11-10 12:28:18 +1100

    added scatter for multiple biopsy

--- analysisscripts/multipleBiopsy.R
+++ analysisscripts/multipleBiopsy.R
@@ -59,7 +59,7 @@
      "select chromosome,position,ref,alt,length(ref)=length(alt) as isSNV,count(*) as sampleCount,",
      "round(sum(if(sampleId='",multipleBiopsy$sampleId1,"',(greatest(0,adjustedVaf*adjustedCopyNumber)),0)),2) as Sample1Ploidy, ",
      "round(sum(if(sampleId='",multipleBiopsy$sampleId2,"',(greatest(0,adjustedVaf*adjustedCopyNumber)),0)),2) as Sample2Ploidy ",
-     "from hmfpatients_pilot.somaticVariant where filter = 'PASS' and sampleId in (",
+     "from hmfpatients.somaticVariant where filter = 'PASS' and sampleId in (",
      paste(c(shQuote(multipleBiopsy$sampleId1),shQuote(multipleBiopsy$sampleId2)),collapse = ","),
      ") group by 1,2,3,4,5",
      sep = "")
@@ -76,16 +76,18 @@
    p2<-ggplot() + xlim(0,3.5)+ labs(title = multipleBiopsy$sampleId2,x='ploidy')  + 
      geom_histogram(aes(x=Sample2Ploidy),data=subset(variants,sampleCount == 2),fill = "blue", alpha = 0.6,binwidth = binsize) + 
      geom_histogram(aes(x=Sample2Ploidy),data=variants[variants$sampleCount==1 & variants$Sample2Ploidy>0,],fill = "blue", alpha = 0.2,binwidth = binsize)
-   multiplot(p1,p2)
-   ggplot(variants,aes(Sample1Ploidy,Sample2Ploidy))+geom_point()
+   p3<-ggplot(variants,aes(Sample1Ploidy,Sample2Ploidy))+geom_point() +xlim(0,3)+ylim(0,3)+labs(x=multipleBiopsy$SampleId1,y=multipleBiopsy$SampleId2)
+   multiplot(p1,p2,p3,cols=2)
  }
  
  dbConnect = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
  multipleBiopsies<-get_multiple_biopsy_samples(dbConnect)
- #for (i in (1:nrow(multipleBiopsies))) {
- for (i in (1:5)) {
+ for (i in (1:nrow(multipleBiopsies))) {
   variants<-get_multiple_biopsy_somatic_variants(dbConnect,multipleBiopsies[i,])
   chart_multiple_biopsies(variants,multipleBiopsies[i,])
  }
- dbDisconnect(dbConnect)
+
+ 
+ 
+
          
