71f78431df68fa11e58001ae8a9bc2af82a2a5a1
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-11-15 13:44:22 +0100

    Added where clause in gc blood bias test

--- sql/gc_blood_bias_check.sql
+++ sql/gc_blood_bias_check.sql
@@ -1,3 +1,5 @@
 select sampleId,
 sum(if(observedNormalRatio>0.9 and observedNormalRatio<1.1,bafCount,0))/sum(if(observedNormalRatio>0.65 and observedNormalRatio<1.35,bafCount,0)) as QCcheck
-from copyNumberRegion group by 1 order by 2;
+from copyNumberRegion
+where sampleId in ('xxx')
+group by 1 order by 2;
