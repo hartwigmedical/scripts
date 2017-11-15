d5591496c15ecd85ca7ff343c9f0053a44c0a1a2
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-11-15 17:15:32 +0100

    Move view to patient db v1.2

--- sql/clinical/create_view_clinical_overview.sql
+++ sql/clinical/create_view_clinical_overview.sql
@@ -5,9 +5,9 @@ DROP VIEW IF EXISTS clinical;
 CREATE VIEW clinical AS
 
 	SELECT sample.sampleId, sample.arrivalDate as sampleArrivalDate, patient.cpctId AS patientId, patient.hospital, patient.gender, patient.birthYear, patient.registrationDate, biopsy.biopsyDate, 
-	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, patient.deathDate, patient.primaryTumorLocation,   
-	biopsy.biopsyLocation,  treatment.treatmentGiven, treatment.name AS treatment, treatment.type AS treatmentType, firstMatchedTreatmentResponse.measurementDone as responseMeasured,
-	firstMatchedTreatmentResponse.response
+	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, patient.deathDate, patient.primaryTumorCategory as cancerType, 
+    patient.primaryTumorSubcategory as cancerSubType, biopsy.biopsySite, biopsy.biopsyLocation, treatment.treatmentGiven, treatment.name AS treatment, treatment.type AS treatmentType, 
+    firstMatchedTreatmentResponse.measurementDone as responseMeasured, firstMatchedTreatmentResponse.response
 	FROM sample 
 		INNER JOIN patient ON sample.patientId = patient.id
 		LEFT JOIN biopsy ON biopsy.sampleId = sample.sampleId
