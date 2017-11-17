737a206b8c3aabb29451fa387e168adac4a39258
Peter Priestley <p.priestley@hartwigmedicalfoundation.nl>
2017-11-17 21:57:10 +1100

    small analysis improvements

--- analysisscripts/sampleDBAnalysis.R
+++ analysisscripts/sampleDBAnalysis.R
@@ -6,23 +6,16 @@
  library(grid)
  library(devtools)
  library("NMF")
+ library(tidyverse)
 
  ############## GENE PANEL & MAPPINGS ###############
  
  PATH="hmf/repos/scripts/analysisscripts/"
  genePanel<-read.table(paste(PATH,"genePanel.txt",sep=""), sep="\t",col.names="gene")
- tumorMapping<-read.table(paste(PATH,"tumorMappings.txt",sep=""), sep="\t",col.names=c("primaryTumorLocation","category"))
+ tumorMapping<-read.table(paste(PATH,"tumorMappings.txt",sep=""), sep="\t",col.names=c("cancerType","category"))
  geneString<-paste(shQuote(genePanel$gene),collapse=",")
  
- ############## QUERIES #############
- select g.sampleId,minCopyNumber,maxCopyNumber,meanCopyNumber,regions from hmfpatients_pilot.geneCopyNumber g, hmfpatients.purity p where gene = @GENE and g.sampleId = p.sampleId;
- 
- #############
- select gene,c.sampleId,minCopyNumber,maxCopyNumber,meanCopyNumber,regions,purity,ploidy,primaryTumorLocation from copyNumber c, purity p, patient pa 
- where  ;
- ######## SV DISRUPTIONS:
- select v.* from gene_disruption_view v, purity p where gene in (select distinct gene from geneCopyNumber) and v.sampleId = p.sampleId
- and p.qcstatus = 'PASS' and p.status <> 'NO_TUMOR';
+
  
  get_HMF_panel<-function(dbConnect)
  {
@@ -44,21 +37,50 @@
    if (sampleString != ""){
       query=paste(query,"AND sampleId in (",sampleString,")" )
    }
-   print(query)
+
    raw_data = dbGetQuery(dbConnect, query)
  }
 
+ get_gene_copy_number<-function(dbConnect,sampleString="")
+ {
+   query = "SELECT gene,minCopyNumber,regions FROM gene_copy_number"
+   
+   if (sampleString != ""){
+     query=paste(query,"AND sampleId in (",sampleString,")" )
+   }
+   raw_data = dbGetQuery(dbConnect, query)
+ }
  
+ 
+ get_panel_disruptions<-function(dbConnect,geneString,sampleString="")
+ {
+   query = paste("SELECT * FROM gene_disruption_view v ",
+                 "WHERE gene in (",geneString,") ",
+                 sep="")
+   
+   if (sampleString != ""){
+     query=paste(query,"AND sampleId in (",sampleString,")" )
+   }
+   raw_data = dbGetQuery(dbConnect, query)
+ }
+ 
+  
  get_QCpass_samples<-function(dbConnect)
  {
    query = "SELECT sampleId from purity where qcstatus = 'PASS' and status <> 'NO_TUMOR'"
    raw_data = dbGetQuery(dbConnect, query)
  }
  
+ get_sample_purity<-function(dbConnect,sampleId)
+ {
+   query = paste("SELECT purity FROM purity where sampleId = '",sampleId,"'",sep="")
+   raw_data = dbGetQuery(dbConnect, query)
+ }
+ 
  get_sample_data<-function(dbConnect,sampleString="")
  {
    query = paste("SELECT purity.*,hospital,clinical.gender as clinicalGender,",
-          "primaryTumorLocation,biopsyLocation,treatment,biopsyDate,registrationDate,sampleArrivalDate ",
+          "cancerType,biopsyLocation,treatment,biopsyDate,registrationDate,sampleArrivalDate ",
           "FROM purity left join clinical on purity.sampleId = clinical.sampleId ")
    
    if (sampleString != ""){
@@ -67,10 +89,12 @@
    raw_data = dbGetQuery(dbConnect, query)
  }
  
+ 
+ 
  get_multiple_biopsy_samples<-function(dbConnect)
  {
    query = paste(
-     "select left(p.sampleId,12) as patient,primaryTumorLocation,count(*), min(p.sampleId) as sampleId1,max(p.sampleId) as sampleId2,",
+     "select left(p.sampleId,12) as patient,cancerType,count(*), min(p.sampleId) as sampleId1,max(p.sampleId) as sampleId2,",
      "round(min(purity),2) as minPurity,round(max(purity),2) as maxPurity,",
      "round(min(ploidy),2) as minPloidy,round(max(ploidy),2) as maxPloidy ",
      "from purity p left join clinical c on p.sampleId = c.sampleId ",
@@ -79,7 +103,18 @@
    #print(query)
    raw_data = dbGetQuery(dbConnect, query)
  }
- 
+
+ get_somatic_variants<-function(dbConnect, sampleId)
+ {
+   query = paste("select chromosome,position,ref,alt,length(ref)=length(alt) as isSNV,round(greatest(0,adjustedVaf*adjustedCopyNumber),2) as ploidy ",
+     "from hmfpatients.somaticVariant where filter = 'PASS' and sampleId = '",
+     sampleId,
+     "'",
+     sep = "")
+   #print(query)
+   raw_data = dbGetQuery(dbConnect, query)
+ }
+  
  get_multiple_biopsy_somatic_variants<-function(dbConnect, multipleBiopsy)
  {
    query = paste(
@@ -106,6 +141,19 @@
    chrOrder <-c((1:22),"X","Y")
    raw_data$chromosome <- factor(raw_data$chromosome, chrOrder, ordered=TRUE)
    raw_data
+ }
+ 
+ head(CNV)
+ 
+ get_SV_ends<-function(dbConnect, sampleId)
+ {
+   query = paste("select startChromosome as chromosome,startPosition as position,startOrientation as orientation,startAF as AF, type ",
+                 "from structuralVariant where sampleId = '",sampleId,"' ", 
+                 "union ",
+                 "select endChromosome as chromosome,endPosition as position,endOrientation as orientation,endAF as AF, type ",
+                 " from structuralVariant where sampleId = '",sampleId,"' " ,
+                 sep = "")
+   raw_data = dbGetQuery(dbConnect, query)
  }
 
  ############ PLOTTING ###############
@@ -164,9 +212,34 @@
    p2<-ggplot(sampleData,aes(ploidyBucket)) + geom_bar(aes(fill=category))
    multiplot(p1,p2)
  }
+ ######### SINGLE BIOPSY LOGIC  ########## 
+ dbConnect = dbConnect(MySQL(), dbname='hmfpatients_pilot', groups="RAnalysis")
+ sampleId = "DRUP01090005T"
+ variants<-get_somatic_variants(dbConnect,sampleId)
+  ggplot() + xlim(0,3.5)+ labs(title = sampleId,x='') + 
+   geom_histogram(aes(x=ploidy),data=variants,fill = "red", alpha = 0.6,binwidth = 0.1) 
+
+ ######### SINGLE BIOPSY SV PLOIDIES ########## 
+ dbConnect = dbConnect(MySQL(), dbname='hmfpatients_pilot', groups="RAnalysis")
+ sampleId = "DRUP01090005T"
+ PURITY<-as.double(get_sample_purity(dbConnect,sampleId))
+ CNV<-as.data.table(get_copy_number_data(dbConnect,sampleId))
+ CNV[, prevCopyNumber:=c(NA, copyNumber[-.N]), by=chromosome]
+ CNV[, prevInferred:=c(NA, inferred[-.N]), by=chromosome]
+ 
+ SV<-get_SV_ends(dbConnect,sampleId)
+ SV<-merge(x = SV, y = CNV, by.x = c("chromosome","position"),by.y=c("chromosome","start"), all.x = TRUE)
+ SV$ploidy<-ifelse(SV$orientation ==1, -(SV$prevCopyNumber*PURITY+(1-PURITY)*2)*SV$orientation*SV$AF/PURITY ,
+             -(SV$copyNumber*PURITY+(1-PURITY)*2)*SV$orientation*SV$AF/PURITY)#*PURITY#+(1-PURITY)*2)*SV$orientation*SV$AF/PURITY
+ SV$chCopyNumber<-SV$copyNumber-SV$prevCopyNumber
+ ggplot() + xlim(0,3.5)+ labs(title = sampleId,x='') + 
+   geom_histogram(aes(x=ploidy),data=SV,fill = "red", alpha = 0.6,binwidth = 0.1) 
+ ggplot(SV[SV$inferred==0 & SV$prevInferred==0,],aes(ploidy,chCopyNumber))+geom_point()+labs(title="SV Ploidy vs ChCopyNumber Scatter")+ xlim(-4,4)+ylim(-4,4)
+ dbDisconnect(dbConnect)
+ 
  ######### MULTIPLE BIOPSIES LOGIC  ########## 
  
- dbConnect = dbConnect(MySQL(), dbname='hmfpatients_pilot', groups="RAnalysis")
+ dbConnect = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
  multipleBiopsies<-get_multiple_biopsy_samples(dbConnect)
  for (i in (1:nrow(multipleBiopsies))) {
    variants<-get_multiple_biopsy_somatic_variants(dbConnect,multipleBiopsies[i,])
@@ -179,7 +252,7 @@
  dbConnect = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
  sampleString<-paste(shQuote(get_QCpass_samples(dbConnect)$sampleId),collapse=",")
  sampleData<-get_sample_data(dbConnect,sampleString)
- sampleData<-merge(x = sampleData, y = tumorMapping, by = "primaryTumorLocation", all.x = TRUE)
+ sampleData<-merge(x = sampleData, y = tumorMapping, by = "cancerType", all.x = TRUE)
  chart_purity_and_ploidy(sampleData)
  ggplot(sampleData,aes(purity,ploidy))+geom_point()+labs(title="Ploidy vs Purity Scatter")
  
@@ -195,17 +268,23 @@
  
  ########## DRIVERS ############
  dbConnect = dbConnect(MySQL(), dbname='hmfpatients_pilot', groups="RAnalysis")
+ 
  sampleString<-paste(shQuote(get_QCpass_samples(dbConnect)$sampleId),collapse=",")
  geneString<-paste(shQuote(get_HMF_panel(dbConnect)$gene),collapse=",")
+ 
  #SOMATICS
  somatics<-get_panel_somatics(dbConnect,geneString,sampleString)
  dbDisconnect(dbConnect)
-
-
- head(genePanel)
- get_HMF_panel(dbConnect)
+ 
+ #disruptions
+ dbConnect = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
+ disruptions<-get_panel_disruptions(dbConnect,geneString,sampleString)
+ dbDisconnect(dbConnect)
+ 
  #################################
  # SCRATCH
  CNV %>% group_by(segmentStartSupport,segmentEndSupport) %>% summarise(sumCount=sum(observedTumorRatioCount),medCount=median(observedTumorRatioCount),countChromosome=n())
  CNV %>% group_by(chromosome) %>% summarise(sumCount=sum(observedTumorRatioCount),count=n())
  
+ df = data.frame(x = c(1, 14, 3, 21, 11), y = c(102, 500, 40, 101, 189)) 
+ apply(df, 2, function(x) x - c(PURITY)) 
