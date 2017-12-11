169b0c8b07ed1928bdb437c7d463f7a22ea34c54
Jonathan Baber <jonbaber@gmail.com>
2017-12-11 15:07:53 +1000

    better genes

--- analysisscripts/genes.R
+++ analysisscripts/genes.R
@@ -1,13 +1,11 @@
+library(RMySQL)
+library(data.table)
+library(plyr)
 
 ####### Helpers #######
 applySamples<-function(cohort, sampleFunction) {
   result = ddply(cohort, 1, sampleFunction, .drop = FALSE, .progress = "text", .parallel = FALSE)
   return (result)
-}
-
-leftJoin<-function(left, right) {
-  temp = merge(x = left, y = right, by="row.names", all.x=TRUE)
-  return (column_as_names(temp, "Row.names"))
 }
 
 column_as_names<-function(df, .column = "gene", .drop = TRUE) {
@@ -30,7 +28,7 @@ query_gene_panel<-function(dbConnect, panel = "HMF Paper") {
 }
 
 ####### Gene Copy Number #######
-query_gene_copynumber<-function(dbConnect, genes, sample) {
+query_sample_copynumber<-function(dbConnect, genes, sample) {
   geneString = paste("'", genes$gene, "'", collapse = ",", sep = "")
   query = paste(
     "SELECT gene, minCopyNumber, maxCopyNumber ",
@@ -39,63 +37,113 @@ query_gene_copynumber<-function(dbConnect, genes, sample) {
     "  AND gene in (", geneString, ")",
     sep = "")
  
-  return (column_as_names(dbGetQuery(dbConnect, query))) 
+  return (column_as_names(dbGetQuery(dbConnect, query), .drop = FALSE)) 
 }
 
-attach_gene_copynumber<-function(dbConnect, genes, sample) {
-  copynumber = query_gene_copynumber(pilotDB, genes, sample)
-  return (leftJoin(genes, copynumber))
+cohort_gene_copynumber<-function(dbConnect, genes, cohort) {
+  return (applySamples(cohort, function(x) {query_sample_copynumber(dbConnect, genes, x)}))
 }
 
-####### Gene Somatics #######
-query_gene_somatics<-function(dbConnect, genes, sample) {
+####### Somatics #######
+query_position_somatics<-function(dbConnect, genes, sample) {
   geneString = paste("'", genes$gene, "'", collapse = ",", sep = "")
   query = paste(
-    "SELECT gene, ",
-    "       SUM(1) as total,",
-    "       MIN(ROUND(adjustedVAF * adjustedCopyNumber,2)) as minPloidy,",
-    "       MAX(ROUND(adjustedVAF * adjustedCopyNumber, 2)) as maxPloidy,",
-    "       SUM(IF(clonality = 'CLONAL', 1, 0)) AS clonal,",
-    "       SUM(IF(clonality = 'SUBCLONAL', 1, 0)) AS subclonal,",
-    "       SUM(IF(clonality = 'INCONSISTENT', 1, 0)) AS inconsistentClonality,",
-    "       SUM(IF(LOH, 1, 0)) as LOH",
-    "  FROM somaticVariant ",
+    "SELECT sampleId, gene, chromosome, position, adjustedCopyNumber as copyNumber, ",
+    "       ROUND(adjustedVAF * adjustedCopyNumber,2) as ploidy, ",
+    "       clonality, loh", 
+    "  FROM somaticVariant s ",
     " WHERE sampleId = '", sample$sampleId, "'",
     "   AND filter = 'PASS'",
     "   AND effect not in ('non coding exon variant', 'synonymous variant', 'UTR variant', 'sequence feature')",
     "   AND gene in (", geneString, ")",
-    " GROUP BY gene",
     sep = "")
   
-  return (column_as_names(dbGetQuery(dbConnect, query))) 
+  return (dbGetQuery(dbConnect, query)) 
 }
 
-attach_gene_somatics<-function(dbConnect, genes, sample) {
-  somatics = query_gene_somatics(pilotDB, genes, sample)
-  return (leftJoin(genes, somatics))
+sample_position_somatics<-function(dbConnect, genes, sample) {
+  sampleGeneCopyNumbers = query_sample_copynumber(dbConnect, genes, sample)
+  sampleSomatics = query_position_somatics(dbConnect, genes, sample)
+  result = merge(sampleSomatics, sampleGeneCopyNumbers, by="gene",all.x=TRUE)
+  result$biallelic <- ifelse(result$ploidy + 0.5 > result$minCopyNumber | result$loh, 1, 0)
+  result$location <- paste(result$chromosome, result$position, sep = ":")
+  return (result)
 }
 
-####### Sample Drivers #######
-sample_drivers<-function(dbConnect, genes, sample) {
-  genes$sampleId <- sample$sampleId
-  genes = attach_gene_copynumber(pilotDB, genes, sample)
-  genes = attach_gene_somatics(pilotDB, genes, sample)
-  genes = genes[!is.na(genes$total) & (genes$maxPloidy + 0.5 > genes$minCopyNumber | genes$LOH > 0) , ]
-  genes$sampleId <- sample$sampleId
-  return (genes)
+cohort_position_somatics<-function(dbConnect, genes, cohort) {
+  return (applySamples(cohort, function(x) {sample_position_somatics(dbConnect, genes, x)}))
 }
 
-####### Cohort Drivers #######
-cohort_drivers<-function(dbConnect, genes, cohort) {
-  return (applySamples(cohort, function(x) {sample_drivers(pilotDB, genes, x)}))
+cohort_gene_somatics<-function(cohort_position_somatics) {
+  DT = data.table(cohort_position_somatics)
+  cohortGeneSomatics = DT[, .(somatics=length(loh), loh=sum(loh), 
+                                 minSomaticCopyNumber=min(copyNumber), maxSomaticCopyNumber = max(copyNumber),
+                                 minSomaticPloidy=min(ploidy), maxSomaticPloidy=max(ploidy), sumSomaticPloidy=sum(ploidy)), 
+                             by=list(gene, sampleId)]
+  return (cohortGeneSomatics)
 }
 
-############# Load Purple Data
+
+####### Structural Variants #######
+query_sample_structural_variants<-function(dbConnect, genes, sample) {
+  geneString = paste("'", genes$gene, "'", collapse = ",", sep = "")
+  query = paste(
+    "SELECT sampleId, gene, min(copyNumber) as minCopyNumber, max(copyNumber) as maxCopyNumber, min(ploidy) as minPloidy, max(ploidy) as maxPloidy FROM (",
+    "  SELECT s.id, sampleId, startChromosome as chromosome, startPosition as position, startOrientation as orientation, ploidy, gene, adjustedStartCopyNumber as copyNumber ",
+    "    FROM structuralVariant s, structuralVariantBreakend b",
+    "   WHERE b.structuralVariantId = s.id",
+    "     AND isStartEnd",
+    "     AND isCanonicalTranscript",
+    "     AND sampleId = '", sample$sampleId, "'",
+    "   UNION",
+    "  SELECT s.id, sampleId, endChromosome as chromosome, endPosition as position, endOrientation as orientation, ploidy, gene, adjustedEndCopyNumber as copyNumber  ",
+    "    FROM structuralVariant s, structuralVariantBreakend b ",
+    "   WHERE b.structuralVariantId = s.id",
+    "     AND !isStartEnd",
+    "     AND isCanonicalTranscript",
+    "     AND sampleId = '", sample$sampleId, "'",
+    " ) legs ",
+    " WHERE gene in (", geneString, ")",
+    "GROUP BY id, gene, sampleId", sep = "")
+  return (dbGetQuery(dbConnect, query)) 
+}
+
+sample_gene_structual_variants<-function(dbConnect, genes, sample) {
+  queryResult = query_sample_structural_variants(dbConnect, genes, sample)
+  DT = data.table(queryResult)
+  sampleGene = DT[, .(structuralVariants=length(minCopyNumber), 
+                      minSVCopyNumber=min(minCopyNumber), maxSVCopyNumber=max(maxCopyNumber), 
+                      minSVPloidy=min(minPloidy), maxSVPloidy=max(maxPloidy), sumSVPloidy=sum(maxPloidy))
+                  , by=list(gene, sampleId)]
+  return (sampleGene)
+}
+
+cohort_gene_structual_variants<-function(dbConnect, genes, cohort) {
+  return (applySamples(cohort, function(x) {sample_gene_structual_variants(dbConnect, genes, x)}))
+}
+
+############# EXECUTION
 load("~/hmf/purple.RData")
 rm(allSamples)
 rm(backupSamples)
+cohort = cohort[cohort$sampleId %in% c('CPCT02180005T', 'CPCT02080055T', 'CPCT02180008T'), ]
 
 pilotDB = dbConnect(MySQL(), dbname='hmfpatients_pilot', groups="RAnalysis")
 genes = query_gene_panel(pilotDB)
-cohortDrivers = cohort_drivers(pilotDB, genes, cohort[1:5,])
 dbDisconnect(pilotDB)
+rm(pilotDB)
+
+prodDB = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
+
+cohortPositionSomatics = cohort_position_somatics(prodDB, genes, cohort)
+cohortGeneSomatics = cohort_gene_somatics(cohortPositionSomatics)
+cohortGeneCopyNumbers = cohort_gene_copynumber(prodDB, genes, cohort)
+cohortGeneStructuralVariants = cohort_gene_structual_variants(prodDB, genes, cohort)
+cohortGeneComplete = merge(cohortGeneSomatics, cohortGeneStructuralVariants, by=c('gene', 'sampleId'), all=TRUE)
+cohortGeneComplete = merge(cohortGeneComplete, cohortGeneCopyNumbers, by=c('gene', 'sampleId'), all.x=TRUE)
+
+dbDisconnect(prodDB)
+rm(prodDB)
+
+
+
