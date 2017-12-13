ea05e97b3fbeff96f4b325b2b99ef2692d9d9362
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2017-12-13 14:31:33 +0100

    change column name

--- sql/unsupported_cnv_qc_check.sql
+++ sql/unsupported_cnv_qc_check.sql
@@ -1,5 +1,5 @@
 SELECT c.sampleId, 
-round(pow(SUM(IF(start<>1 AND structuralVariantSupport='NONE',1,0)),3)/pow(SUM(IF(start<>1,1,0)),2)/AVG(ploidy),0) AS QCscore
+round(pow(SUM(IF(start<>1 AND segmentStartSupport='NONE',1,0)),3)/pow(SUM(IF(start<>1,1,0)),2)/AVG(ploidy),0) AS QCscore
 FROM copyNumber c INNER JOIN purity p ON c.sampleId = p.sampleId
 WHERE c.sampleId IN ('xxx')
 GROUP BY c.sampleId
