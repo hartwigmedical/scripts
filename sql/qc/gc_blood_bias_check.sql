285925e7393dc0721f75d93e4165a13aec88437f
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-12-14 11:26:39 +0100

    Moved QC sql scripts to separate dir

--- sql/qc/gc_blood_bias_check.sql
+++ sql/qc/gc_blood_bias_check.sql
@@ -1,5 +1,5 @@
-select sampleId,
+SELECT sampleId,
 sum(if(observedNormalRatio>0.9 and observedNormalRatio<1.1,bafCount,0))/sum(if(observedNormalRatio>0.65 and observedNormalRatio<1.35,bafCount,0)) as QCcheck
-from copyNumberRegion
-where sampleId in ('xxx')
-group by 1 order by 2;
+FROM copyNumberRegion
+WHERE sampleId in ('xxx')
+GROUP BY 1 ORDER BY 2;
