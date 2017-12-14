093b3981f4af89f78c38b1e0a40778c8b75c5034
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2017-12-14 11:25:27 +0100

    Updated purple QC check sql script

--- sql/unsupported_cnv_qc_check.sql
+++ sql/unsupported_cnv_qc_check.sql
@@ -1,5 +1,1 @@
-SELECT c.sampleId, 
-round(pow(SUM(IF(start<>1 AND segmentStartSupport='NONE',1,0)),3)/pow(SUM(IF(start<>1,1,0)),2)/AVG(ploidy),0) AS QCscore
-FROM copyNumber c INNER JOIN purity p ON c.sampleId = p.sampleId
-WHERE c.sampleId IN ('xxx')
-GROUP BY c.sampleId
+SELECT qcStatus FROM purity WHERE sampleId IN ('XXX');
