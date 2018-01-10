55f4833e2a01e31520f8322252fa5985324943db
Jonathan Baber <jonbaber@gmail.com>
2018-01-10 16:19:26 +1000

    additional queries and functionality

--- purple/R/common.R
+++ purple/R/common.R
@@ -7,7 +7,29 @@ slookup<-function(df, lookup, on="sampleId") {
   return (sapply(df[[on]], function(x) {lookup[match(x, lookup[[on]]), -c(1)] }))
 }
 
+apply_to_cohort<-function(cohort, sampleFunction) {
+  result = ddply(cohort, 1, sampleFunction, .drop = FALSE, .progress = "text", .parallel = FALSE)
+  return (result)
+}
+
+sample_to_patient_id<-function(sampleId, lookup) {
+  index = match(sampleId, lookup[[1]])
+  if (is.na(index)) {
+    substr(sampleId, 1, 12)
+  } else {
+    lookup[index, c(2)]
+  }
+}
+
+manual_patient_id<-function() {
+  sampleId  = c("CPCT02020192T", "CPCT02030224T", "DRUP01010044T", "DRUP01070024T", "DRUP01050008T")
+  patientId = c("CPCT02020438",  "CPCT02030292",  "DRUP01010044",  "CPCT02070110",  "CPCT02050116")
+  return (data.frame(sampleId, patientId, stringsAsFactors = FALSE))
+}
+
+
 #this is a slow function - don't use!
 dflookup<-function(df, lookup, on="sampleId") {
   return(as.data.frame(t(sapply(df[[on]], function(x) {lookup[match(x, lookup[[on]]), -c(1)] }))))
 }
+
--- purple/R/queries.R
+++ purple/R/queries.R
@@ -41,7 +41,7 @@ query_gene_copy_number<-function(dbConnect, sampleId) {
 
 query_clinical_data<-function(dbConnect) {
   query = paste(
-    "SELECT c.sampleId, c.cancertype, c.birthYear, c.biopsyDate",
+    "SELECT *",
     " FROM clinical c",
     sep = " ")
   return ((dbGetQuery(dbConnect, query)))
@@ -82,14 +82,146 @@ query_msi_sample<-function(dbConnect, sampleId) {
     " FROM somaticVariant ",
     "WHERE filter = 'PASS'",
     "   AND type = 'INDEL'",
-    "   AND repeatCount > 0 ",
     "   AND length(alt) <= 50",
     "   AND length(ref) <= 50",
+    "   AND repeatCount >= 4",
     "   AND sampleId = '", sampleId, "'",
+    "   AND ((length(repeatSequence) BETWEEN 2 AND 4) OR (length(repeatSequence) = 1 AND repeatCount >= 5))",
     " GROUP BY sampleId",
     sep = "")
   score = dbGetQuery(dbConnect, query)
   score$msiScore = score$msiScore/3095
+  score$msiStatus = ifelse(score$msiScore > 0.909, "MSI", "MSS")
   return (score)
+}
+
+query_msi<-function(dbConnect) {
+  query = paste(
+    "SELECT sampleId, count(*) as msiScore ",
+    " FROM somaticVariant ",
+    "WHERE filter = 'PASS'",
+    "   AND type = 'INDEL'",
+    "   AND length(alt) <= 50",
+    "   AND length(ref) <= 50",
+    "   AND repeatCount >= 4",
+    "   AND ((length(repeatSequence) BETWEEN 2 AND 4) OR (length(repeatSequence) = 1 AND repeatCount >= 5))",
+    " GROUP BY sampleId",
+    sep = "")
+  score = dbGetQuery(dbConnect, query)
+  score$msiScore = score$msiScore/3095
+  score$msiStatus = ifelse(score$msiScore > 0.909, "MSI", "MSS")
+  return (score)
+}
+
+query_unsupported_segments<-function(dbConnect, sampleId) {
+  query = paste(
+    "SELECT sampleId, count(*) as unsupportedSegments ",
+    " FROM copyNumber ",
+    "WHERE sampleId = '",sampleId, "'",
+    "  AND segmentStartSupport = 'NONE'",
+    " GROUP BY sampleId",
+    sep = "")
+  return (dbGetQuery(dbConnect, query))
+}
+
+query_structural_variant_overview<-function(dbConnect, sampleId) {
+  query = paste(
+    "SELECT sampleId, type, count(*) as count",
+    " FROM structuralVariant ",
+    "WHERE sampleId = '", sampleId, "'",
+    " GROUP BY sampleId, type",
+    sep = "")
+  result = data.table(dbGetQuery(dbConnect, query))
+  structuralVariants = result[, list(structuralVariants=sum(count)), by=c("sampleId")]
+
+  types = dcast(result, sampleId ~ type, value.var = "count")
+  #colnames(types) <- paste("sv", colnames(types), sep="")
+  #colnames(types)[1] <- "sampleId"
+  structuralVariants = left_join(structuralVariants, types)
+
+  return (structuralVariants)
+}
+
+
+query_somatic_overview_old<-function(dbConnect, sampleId) {
+  query = paste(
+    "SELECT sampleId, clonality, type, count(*) as count, SUM(IF (effect like '%missense%', 1, 0)) AS mutationalLoad",
+    " FROM somaticVariant ",
+    "WHERE filter = 'PASS' ",
+    "   AND sampleId = '", sampleId, "'",
+    " GROUP BY sampleId, clonality, type",
+    sep = "")
+
+  result = data.table(dbGetQuery(dbConnect, query))
+
+  type = result[, list(count=sum(count)), by=c("sampleId","type")]
+  typeLong = dcast(type, sampleId ~ type, value.var="count")
+
+  clonality = result[, list(count=sum(count)), by=c("sampleId","clonality")]
+  clonalityLong = dcast(clonality, sampleId ~ clonality, value.var="count")
+
+  mutationalLoad = result[, list(mutationalLoad=sum(mutationalLoad)), by=c("sampleId")]
+
+  somatics = result[, list(somaticVariants=sum(count)), by=c("sampleId")]
+  somatics$mutationalLoad = mutationalLoad$mutationalLoad
+  somatics = left_join(somatics, typeLong)
+  somatics = left_join(somatics, clonalityLong)
+
+  return (somatics)
+}
+
+query_somatic_overview<-function(dbConnect, sampleId) {
+  query = paste(
+    "SELECT sampleId, clonality, type, count(*) as count, SUM(IF (effect like '%missense%', 1, 0)) AS mutationalLoad",
+    " FROM somaticVariant ",
+    "WHERE filter = 'PASS' ",
+    "   AND sampleId = '", sampleId, "'",
+    " GROUP BY sampleId, clonality, type",
+    sep = "")
+
+  result = data.table(dbGetQuery(dbConnect, query))
+
+  type = result[, list(count=sum(count)), by=c("sampleId","type", "clonality")]
+  typeLong = dcast(type, sampleId ~ type+clonality, value.var="count")
+
+  mutationalLoad = result[, list(mutationalLoad=sum(mutationalLoad)), by=c("sampleId")]
+
+  somatics = result[, list(somaticVariants=sum(count)), by=c("sampleId")]
+  somatics$mutationalLoad = mutationalLoad$mutationalLoad
+  somatics = left_join(somatics, typeLong)
+
+  return (somatics)
+}
+
+query_patient_id_lookup<-function(dbConnect) {
+  query = paste(
+    "SELECT CPCTCNT.patientId AS sampleId, concat('CPCT02',CPCTCNT.itemValue , IF(length(CPCTPN.itemValue) = 2, '00' + CPCTPN.itemValue, IF(left(CPCTPN.itemValue,1) = '-', right(CPCTPN.itemValue, 4), CPCTPN.itemValue ))) as patientId",
+    "  FROM drupEcrf CTCT2YN,  drupEcrf CPCTCNT, drupEcrf CPCTPN ",
+    " WHERE CPCTCNT.patientId = CPCTPN.patientId AND CTCT2YN.patientId = CPCTCNT.patientId ",
+    "   AND CPCTCNT.item = 'FLD.REG.CPCTCNT' AND CPCTCNT.itemValue != ''",
+    "   AND CPCTPN.item = 'FLD.REG.CPCTPN' AND CPCTPN.itemValue != ''",
+    "   AND CTCT2YN.item = 'FLD.REG.CTCT2YN' AND CTCT2YN.itemValue = 'Yes'",
+    sep = "")
+
+  result = dbGetQuery(dbConnect, query)
+  return (rbind(manual_patient_id(), result))
+}
+
+query_whole_genome_duplication<-function(dbConnect, sampleId) {
+  query = paste(
+    "SELECT sampleId, count(*) as duplicatedAutosomes",
+    "  FROM ",
+    " (select sampleId, chromosome, round(sum(bafCount*actualBaf*copyNumber)/sum(bafCount),1) as lwMajorAlleleAvg ",
+    " from copyNumber ",
+    " where sampleId = '", sampleId, "' and chromosome not in ('X', 'Y') ",
+    " group by sampleId, chromosome ",
+    " HAVING lwMajorAlleleAvg > 1.5) a ",
+    " GROUP BY sampleId",
+    sep = "")
+
+  result = dbGetQuery(dbConnect, query)
+  result$WGD <- ifelse(result$duplicatedAutosomes > 10, TRUE, FALSE)
+
+  return (result)
 }
 
