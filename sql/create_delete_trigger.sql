c77240076514c241275d479fb565cc83ce6a7da6
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-01-10 07:41:50 +0100

    Cleaned up SQL scripts

--- sql/create_delete_trigger.sql
+++
@@ -1,5 +0,0 @@
-USE hmfpatients;
-DROP TRIGGER IF EXISTS trigger_sv_deletion;
-CREATE TRIGGER trigger_sv_deletion AFTER DELETE ON structuralVariant
-FOR EACH ROW
-DELETE FROM sv_annotation2 WHERE sv_id = old.id;
--- sql/gene_disruption_PANEL.sql
+++
@@ -1,2 +0,0 @@
-USE hmfpatients;
-SELECT * FROM gene_disruption_view;
