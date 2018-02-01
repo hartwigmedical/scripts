e0eab1d0f1951285c3f3b8de51ce401faebce345
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-02-01 10:08:17 +0100

    Updated gene fusion query

--- sql/reported_gene_fusions.sql
+++ sql/reported_gene_fusions.sql
@@ -1,8 +1,9 @@
-SELECT fiveVariant.sampleId, fiveVariant.id, threeVariant.id, fiveBreakend.gene, fiveBreakend.exonRankUpstream, fiveBreakend.exonRankDownstream, 
-threeBreakend.gene, threeBreakend.exonRankUpstream, threeBreakend.exonRankDownstream, fiveVariant.ploidy FROM structuralVariantFusion
+SELECT fiveVariant.sampleId, fiveVariant.startOrientation, fiveVariant.startChromosome, fiveVariant.startPosition, fiveVariant.endChromosome, fiveVariant.endOrientation, fiveVariant.endPosition,
+fiveBreakend.gene as fiveGene, fiveBreakend.strand as fiveStrand, "1" as firstFiveExon, fiveBreakend.exonRankUpstream as finalFiveExon, 
+threeBreakend.gene as threeGene, threeBreakend.strand as threeStrand, threeBreakend.exonRankDownstream as firstThreeExon, threeBreakend.exonMax as finalThreeExon, fiveVariant.ploidy FROM structuralVariantFusion
 INNER JOIN structuralVariantBreakend AS fiveBreakend ON fiveBreakend.id = structuralVariantFusion.fivePrimeBreakendId 
 INNER JOIN structuralVariant AS fiveVariant ON fiveVariant.id = fiveBreakend.structuralVariantId
 INNER JOIN structuralVariantBreakend AS threeBreakend ON threeBreakend.id = structuralVariantFusion.threePrimeBreakendId 
 INNER JOIN structuralVariant AS threeVariant ON threeVariant.id = threeBreakend.structuralVariantId
 WHERE isReported = 1
-AND fiveVariant.sampleId IN ('XXX');
+AND fiveVariant.sampleId IN  ('XXX');
