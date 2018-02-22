811f95cdd861ad9a5b337bce03cef0c48d0b3a27
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-02-22 09:16:04 +0100

    Simplified MSI query based on new fields.

--- sql/msi_check.sql
+++ sql/msi_check.sql
@@ -1,9 +1,9 @@
-select s.sampleId as Sample, count(*)/3095 as SIndel_PerMb, if(count(*)/3095 > 0.909, "MSI", "MSS" ) as Status from somaticVariant s left join purity on s.sampleId = purity.sampleId
-where filter = 'PASS' and repeatCount > 0 and length(alt) <> length(ref) and length(alt) <= 50 and length(ref) <= 50
-  and (
-(length(repeatSequence) between 2 and 4 ) OR
-(length(repeatSequence) = 1 and repeatCount >= 5)
+select sampleId, count(*)/3095 as indelsPerMb, if(count(*)/3095 > 4, "MSI", "MSS" ) as status from somaticVariant
+where filter = 'PASS' and type = 'INDEL' and repeatCount > 0 and length(alt) <= 50 and length(ref) <= 50
+and (
+	(length(repeatSequence) between 2 and 4 ) OR
+	(length(repeatSequence) = 1 and repeatCount >= 5)
 )
-and s.sampleId IN ('XXX')
-group by s.sampleId
+and sampleId IN ('XXX')
+group by sampleId
 order by 2 desc;
