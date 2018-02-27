aa78ca4497059d33c4fff0b701bf2fbafdf0822a
Jonathan Baber <jonbaber@gmail.com>
2018-02-27 10:29:07 +1000

    create DNDS L matrix

---
+++ purple/R/refCDS.R
@@ -0,0 +1,142 @@
+library(seqinr)
+
+createRefCDS <- function() {
+  nt = c("A","C","G","T")
+  trinucs = paste(rep(nt,each=16,times=1),rep(nt,each=4,times=4),rep(nt,each=1,times=16), sep="")
+  trinucinds = setNames(1:64, trinucs)
+
+  trinucsubs = NULL
+  for (j in 1:length(trinucs)) {
+    trinucsubs = c(trinucsubs, paste(trinucs[j], paste(substr(trinucs[j],1,1), setdiff(nt,substr(trinucs[j],2,2)), substr(trinucs[j],3,3), sep=""), sep=">"))
+  }
+  trinucsubsind = setNames(1:192, trinucsubs)
+
+  ### DNDS
+  data("refcds_hg19", package="dndscv")
+  KRAS = RefCDS[[9224]]
+  AL606500=RefCDS[[940]]
+  RP11 = RefCDS[[14756]]
+
+  krasL = createLmatrix(KRAS, nt, trinucsubsind)
+  AL606500L = createLmatrix(AL606500, nt, trinucsubsind)
+  RP11L = createLmatrix(RP11, nt, trinucsubsind)
+
+  identical(krasL,KRAS$L)
+  identical(AL606500L,AL606500$L)
+  identical(RP11L,RP11$L)
+
+  all(krasL == AL606500$L)
+
+}
+
+createLmatrix <-function(geneRefCDS, nt, trinucsubsind) {
+
+  L = array(0, dim=c(192,4))
+  for (cdsPos in (1:length(geneRefCDS$seq_cds))) {
+    ref = as.character(geneRefCDS$seq_cds[cdsPos])
+    alts = setdiff(nt, ref)
+    for (alt in alts) {
+      altImpact = exonImpactFromCDSPos(cdsPos, ref, alt, geneRefCDS$seq_cds1up, geneRefCDS$seq_cds, geneRefCDS$seq_cds1down)
+      impactInd = impactIndex(altImpact["impact"])
+      if (!is.na(impactInd)) {
+        triInd = trinucsubsind[ paste(altImpact["refTri"],altImpact["altTri"], sep=">") ]
+        L[triInd, impactInd] = L[triInd, impactInd] + 1
+      }
+    }
+  }
+
+  # Splice
+  for (cdsPos in (1:length(geneRefCDS$seq_splice))) {
+    ref = as.character(geneRefCDS$seq_splice[cdsPos])
+    alts = setdiff(nt, ref)
+    for (alt in alts) {
+      refTri = paste(geneRefCDS$seq_splice1up[cdsPos], geneRefCDS$seq_splice[cdsPos], geneRefCDS$seq_splice1down[cdsPos], sep = "")
+      altTri = paste(geneRefCDS$seq_splice1up[cdsPos], alt, geneRefCDS$seq_splice1down[cdsPos], sep = "")
+      triInd = trinucsubsind[ paste(refTri,altTri, sep=">") ]
+      L[triInd, 4] = L[triInd, 4] + 1
+    }
+  }
+
+  return (L)
+}
+
+exonImpactFromCDSPos <-function(cdsPos, ref, alt, seq_cds1up, seq_cds, seq_cds1down) {
+
+  refTri = paste(seq_cds1up[cdsPos], seq_cds[cdsPos], seq_cds1down[cdsPos], sep = "")
+  altTri = paste(seq_cds1up[cdsPos], alt, seq_cds1down[cdsPos], sep = "")
+
+  codonPos = c(ceiling(cdsPos/3)*3-2, ceiling(cdsPos/3)*3-1, ceiling(cdsPos/3)*3)
+  refCodon = as.character(as.vector(seq_cds[codonPos]))
+
+  altCodon = refCodon;
+  altCodon[cdsPos-(ceiling(cdsPos/3)-1)*3] = alt
+
+  refProtein = seqinr::translate(refCodon)
+  altProtein = seqinr::translate(altCodon)
+  impact = impact(refProtein, altProtein)
+
+  return (setNames(c(cdsPos, ref, alt, refTri, altTri, impact), c("cdsPos", "ref", "alt", "refTri", "altTri", "impact")))
+}
+
+## OLD
+exonImpact <-function(position, ref, alt, geneRefCDS) {
+  cdsPos = chr2cds(position, geneRefCDS$intervals_cds, geneRefCDS$strand)
+
+  refTri = paste(geneRefCDS$seq_cds1up[cdsPos], geneRefCDS$seq_cds[cdsPos], geneRefCDS$seq_cds1down[cdsPos], sep = "")
+  altTri = paste(geneRefCDS$seq_cds1up[cdsPos], alt, geneRefCDS$seq_cds1down[cdsPos], sep = "")
+
+  codonPos = c(ceiling(cdsPos/3)*3-2, ceiling(cdsPos/3)*3-1, ceiling(cdsPos/3)*3)
+  refCodon = as.character(as.vector(geneRefCDS$seq_cds[codonPos]))
+
+  altCodon = refCodon;
+  altCodon[cdsPos-(ceiling(cdsPos/3)-1)*3] = alt
+
+  refProtein = seqinr::translate(refCodon)
+  altProtein = seqinr::translate(altCodon)
+  impact = impact(refProtein, altProtein)
+
+  return (c(position, ref, alt, refTri, altTri, impact))
+}
+
+impact <- function(old_aa, new_aa) {
+  # Annotating the impact of the mutation
+  if (new_aa == old_aa){
+    return("Synonymous")
+  } else if (new_aa == "*"){
+    return("Nonsense")
+  } else if (old_aa != "*"){
+    return("Missense")
+  } else if (old_aa=="*") {
+    return ("Stop_loss")
+  }
+}
+
+impactIndex <- function(impact) {
+  if (impact == "Synonymous") {
+    return (1)
+  } else if (impact == "Nonsense") {
+    return (3)
+  } else if (impact == "Missense") {
+    return (2)
+  }
+
+  return (NA)
+}
+
+chr2cds = function(pos,cds_int,strand) {
+  if (strand==1) {
+    return(which(pos==unlist(apply(cds_int, 1, function(x) x[1]:x[2]))))
+  } else if (strand==-1) {
+    return(which(pos==rev(unlist(apply(cds_int, 1, function(x) x[1]:x[2])))))
+  }
+}
+
+
+#library(BSgenome.Hsapiens.UCSC.hg19)
+#genome <- BSgenome.Hsapiens.UCSC.hg19
+
+
+
+
+
+
