dbe6a5fdde9b3b69ba31e14d9ba1ce8d2aa7274a
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-03-06 10:57:13 +0100

    raw data of radiotherapy by treatment after baseline biopsy

---
+++ sql/raw_query_radiotherapyForTherapy.sql
@@ -0,0 +1,40 @@
+select patients.patientId,
+RADIOTHERST, RADIOSITE, RADIOAIM, RADIOTHERAIMST, RADIOIRRAD, RADIODOSE, RADIOSTDTC, RADIOENDTC, RADIORESP
+from 
+	(select distinct patientId from ecrf) patients
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIOTHERST 
+    from ecrf where item = 'FLD.TRTAFTER.RADIOTHERST' group by patientId) therst
+on patients.patientId = therst.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIOSITE 
+    from ecrf where item = 'FLD.TRTAFTER.RADIOSITE' group by patientId) site
+on patients.patientId = site.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIOAIM 
+    from ecrf where item = 'FLD.TRTAFTER.RADIOAIM' group by patientId) aim
+on patients.patientId = aim.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIOTHERAIMST 
+    from ecrf where item = 'FLD.TRTAFTER.RADIOTHERAIMST' group by patientId) aimst
+on patients.patientId = aimst.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIOIRRAD 
+    from ecrf where item = 'FLD.TRTAFTER.RADIOIRRAD' group by patientId) irrad
+on patients.patientId = irrad.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIODOSE
+    from ecrf where item = 'FLD.TRTAFTER.RADIODOSE' group by patientId) dose
+on patients.patientId = dose.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIOSTDTC 
+    from ecrf where item = 'FLD.TRTAFTER.RADIOSTDTC' group by patientId) stdtc
+on patients.patientId = stdtc.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIOENDTC 
+    from ecrf where item = 'FLD.TRTAFTER.RADIOENDTC' group by patientId) endtc
+on patients.patientId = endtc.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as RADIORESP 
+    from ecrf where item = 'FLD.TRTAFTER.RADIORESP' group by patientId) resp
+on patients.patientId = resp.patientId
