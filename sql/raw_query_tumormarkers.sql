5d974706a85ec3c5b2b69ac0df1fd8f62a129a08
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-03-06 10:55:10 +0100

    raw data of tumor markers

---
+++ sql/raw_query_tumormarkers.sql
@@ -0,0 +1,41 @@
+select patients.patientId,
+LBSTAT_TUMORMARKERS,LBDTC_TUMORMARKERS, LBNAM_TUMORMARKERS, LBSEQ_TUMORMARKERS, LBTERM_TUMORMARKERS, LBTERMSP_TUMORMARKERS, 
+LBORRES_TUMORMARKERS, LBORRESU_TUMORMARKERS,LBCAT_TUMORMARKERS
+from 
+	(select distinct patientId from ecrf) patients
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBSTAT_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBSTAT_TUMORMARKERS' group by patientId) lbstat
+on patients.patientId = lbstat.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBDTC_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBDTC_TUMORMARKERS' group by patientId) lbdtc
+on patients.patientId = lbdtc.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBNAM_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBNAM_TUMORMARKERS' group by patientId) lbnam
+on patients.patientId = lbnam.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBSEQ_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBSEQ_TUMORMARKERS' group by patientId) lbseq
+on patients.patientId = lbseq.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBTERM_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBTERM_TUMORMARKERS' group by patientId) lbterm
+on patients.patientId = lbterm.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBTERMSP_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBTERMSP_TUMORMARKERS' group by patientId) lbtermsp
+on patients.patientId = lbtermsp.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBORRES_TUMORMARKERS
+    from ecrf where item = 'FLD.RESPONSE.LBORRES_TUMORMARKERS' group by patientId) lborres
+on patients.patientId = lborres.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBORRESU_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBORRESU_TUMORMARKERS ' group by patientId) lborresu
+on patients.patientId = lborresu.patientId
+left join
+   (select patientId, group_concat(itemValue separator ', ') as LBCAT_TUMORMARKERS 
+    from ecrf where item = 'FLD.RESPONSE.LBCAT_TUMORMARKERS' group by patientId) lbcat
+on patients.patientId = lbcat.patientId
