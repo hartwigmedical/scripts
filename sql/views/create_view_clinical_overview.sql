e49b84bcc30f475222c0edf911c7cbe1ca4701ed
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-03-06 16:23:35 +0100

    Modified view for patient db v2

--- sql/views/create_view_clinical_overview.sql
+++ sql/views/create_view_clinical_overview.sql
@@ -2,13 +2,14 @@ DROP VIEW IF EXISTS clinical;
 
 CREATE VIEW clinical AS
 
-	SELECT sample.sampleId, sample.arrivalDate as sampleArrivalDate, patient.cpctId AS patientId, patient.hospital, patient.gender, patient.birthYear, patient.registrationDate, biopsy.biopsyDate, 
-	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, patient.deathDate, patient.cancerType,
-    patient.cancerSubtype, biopsy.biopsySite, biopsy.biopsyLocation, treatment.treatmentGiven, treatment.name AS treatment, treatment.type AS treatmentType,
-    firstMatchedTreatmentResponse.measurementDone as responseMeasured, firstMatchedTreatmentResponse.response
+	SELECT sample.sampleId, sample.arrivalDate as sampleArrivalDate, patient.cpctId AS patientId, patient.hospital, patient.gender, patient.birthYear,
+	patient.hasSystemicPreTreatment, patient.hasRadiotherapyPreTreatment, patient.preTreatments, patient.informedConsentDate, biopsy.biopsyDate,
+	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, patient.deathDate,
+	patient.cancerType, patient.cancerSubtype, biopsy.biopsySite, biopsy.biopsyLocation, treatment.treatmentGiven, treatment.radiotherapyGiven,
+	treatment.name AS treatment, treatment.type AS treatmentType,
+	firstMatchedTreatmentResponse.measurementDone as responseMeasured, firstMatchedTreatmentResponse.response as firstResponse
 	FROM sample 
 		INNER JOIN patient ON sample.patientId = patient.id
 		LEFT JOIN biopsy ON biopsy.sampleId = sample.sampleId
 		LEFT JOIN treatment on treatment.biopsyId = biopsy.id
 		LEFT JOIN firstMatchedTreatmentResponse ON treatment.id = firstMatchedTreatmentResponse.treatmentId
-		
