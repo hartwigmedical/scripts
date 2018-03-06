77338d295a2ceb662ab89631cda3fce0a2d4f4c3
Peter Priestley <p.priestley@hartwigmedicalfoundation.nl>
2018-03-06 18:31:59 +1100

    improvedCharts

--- analysisscripts/SVSignaturesStory.R
+++ analysisscripts/SVSignaturesStory.R
@@ -97,7 +97,7 @@ breakEnd_Analysis_By_Filter_And_Group<-function(filterString = "",groupByString 
   
 }
 
-cohortSummary<-function(filterString = "",groupByString = "")
+cohortSummary<-function(cluster,filterString = "",groupByString = "")
 {
   summary = (cluster %>% s_filter(filterString) %>% s_group_by(groupByString)
              %>% summarise(count=n(),
@@ -126,17 +126,6 @@ cdfCounts<-function(countsData) {
   multiplot(plotlist=plots,cols=3)
 }
 
-perCancerTypePDF<-function(variantType) {
-  plots <- list()
-  i = 1
-  for (ct in unique(cluster$cancerType)) {
-    summary = (cluster %>% filter(PONCount<2,cancerType==ct,Type==variantType) %>% group_by(LengthBucket) %>% summarise(count=n()))
-    plots[[i]]<-ggplot(data=summary,aes(LengthBucket,count))+geom_point()+scale_x_log10()
-    i=i+1
-  }
-  multiplot(plotlist=plots,cols=5)
-}
-
 
 scatterCounts<-function(countsData,bucket) {
   plots <- list()
@@ -148,6 +137,19 @@ scatterCounts<-function(countsData,bucket) {
     }
   }
   multiplot(plotlist=plots,cols=3)
+}
+
+plot_count_by_bucket_and_type<-function(countsData,bucket,facetWrap,titleString ="") {
+  print(ggplot(data=countsData,aes_string(x=bucket))+geom_line(aes(y=countDEL,colour='DEL'))
+        +geom_line(aes(y=countDUP,colour='DUP'))+geom_line(aes(y=countINV,colour='INV'))+
+          scale_x_log10()+facet_wrap(as.formula(paste("~", facetWrap)))+labs(title = titleString))
+}
+
+signature_by_sample_and_type<-function(filter,signatureName){
+  summary = cohortSummary(cluster,'','SampleId,cancerType,LengthBucket')
+  summary = summary[summary$SampleId %in% filter,]
+  summary$ID = paste(summary$cancerType,summary$SampleId)
+  plot_count_by_bucket_and_type(summary,'LengthBucket','ID',paste('SIGNATURE:',signatureName))
 }
 
 
@@ -197,11 +199,11 @@ cluster = cluster %>% filter(PONCount<2)
 ### 2. Per Sample / Feature Counts
 
 ##### All Samples by sampleID
-summary = cohortSummary('','SampleId')
+summary = cohortSummary(cluster,'','SampleId')
 cdfCounts(summary)
 
 ##### All Samples by Length
-summary = cohortSummary('','LengthBucket')
+summary = cohortSummary(cluster,'','LengthBucket')
 scatterCounts(summary,'LengthBucket')
 
 
@@ -209,55 +211,39 @@ scatterCounts(summary,'LengthBucket')
 ### 3. By Cancer Type
 
 ##### Lengths by cancer Type
-for (variantType in c('INV','DUP','DEL') ) {
-  summary = (cluster %>% filter(Type==variantType,FSStart=='false'|FSEnd=='false') %>% group_by(LengthBucket,cancerType) %>% summarise(count=n()))
-  print(ggplot(data=summary,aes(LengthBucket,count))+geom_point()+scale_x_log10()+facet_wrap(~cancerType)+labs(title=variantType))
-
-}
+summary = cohortSummary(cluster,'','LengthBucket,cancerType')
+plot_count_by_bucket_and_type(summary,'LengthBucket','cancerType')
 
 ##### DEL LENGTHS for FS by cancer Type
-summary = (cluster %>% filter(Type=='DEL',FSStart=='true'|FSEnd=='true') %>% group_by(LengthBucket,cancerType) %>% summarise(count=n()))
-print(ggplot(data=summary,aes(LengthBucket,count))+geom_point()+scale_x_log10()+facet_wrap(~cancerType)+labs(title=variantType))
-
+summary = cohortSummary(cluster,"FSStart=='true'|FSEnd=='true'",'LengthBucket,cancerType')
+plot_count_by_bucket_and_type(summary,'LengthBucket','cancerType')
 
 ################################################################
 ### 4. SIGNATURES
 
-signature_by_sample_and_type<-function(filter,signatureName){
-  summary = cohortSummary('','SampleId,cancerType,LengthBucket')
-  summary = summary[summary$SampleId %in% filter,]
-  summary$ID = paste(summary$cancerType,summary$SampleId)
-  for (variantType in c('INV','DUP','DEL') ) {
-    print(ggplot(data=summary,aes_string('LengthBucket',paste('count',variantType,sep='')))+
-            geom_point()+scale_x_log10()+facet_wrap(~ID)+labs(title=paste(signatureName,"signature for",variantType)))
-  }
-}
-
-
-
 ##### LONG DUPS ############
-filter = cohortSummary('Type=="BND"|LengthBucket>1e5&LengthBucket<5e6','SampleId,cancerType')
+filter = cohortSummary(cluster,'Type=="BND"|LengthBucket>1e5&LengthBucket<5e6','SampleId,cancerType')
 filter$excessDUP = filter$countDUP - 0.5 * filter$countINV -0.5 * filter$countBND
 filter = filter %>% arrange (-excessDUP) %>% filter(row_number() <= 20) %>% .$SampleId
 signature_by_sample_and_type(filter,"Long Dup")
 
 ##### Short DELS ############
-filter = cohortSummary('Type=="BND"|LengthBucket<1e4','SampleId,cancerType')
+filter = cohortSummary(cluster,'Type=="BND"|LengthBucket<1e4','SampleId,cancerType')
 filter = filter %>% arrange (-countDEL) %>% filter(row_number() <= 20) %>% .$SampleId
 signature_by_sample_and_type(filter,"Short DEL")
 
 ##### BRCA RS3 ############
-filter = cohortSummary('Type=="BND"|LengthBucket>4e3,LengthBucket<5e4','SampleId,cancerType')
+filter = cohortSummary(cluster,'Type=="BND"|LengthBucket>4e3,LengthBucket<5e4','SampleId,cancerType')
 filter = filter %>% arrange (-countDUP) %>% filter(row_number() <= 20) %>% .$SampleId
 signature_by_sample_and_type(filter,"BRCA")
 
 ##### High mid length DUP + DEL ############
-filter = cohortSummary('Type=="BND"|LengthBucket>4e3,LengthBucket<5e5','SampleId,cancerType')
+filter = cohortSummary(cluster,'Type=="BND"|LengthBucket>4e3,LengthBucket<5e5','SampleId,cancerType')
 filter = filter %>% filter(countDEL/countDUP>0.5) %>% arrange (-countDUP) %>% filter(row_number() <= 20) %>% .$SampleId
 signature_by_sample_and_type(filter,"Mid Length Dup + DEL")
 
 ##### Ovarian short-mid DEL ############
-filter = cohortSummary('Type=="BND"|LengthBucket>1e4,LengthBucket<5e4','SampleId,cancerType')
+filter = cohortSummary(cluster,'Type=="BND"|LengthBucket>1e4,LengthBucket<5e4','SampleId,cancerType')
 filter = filter %>% filter(countDEL/countDUP>3) %>% arrange (-countDEL) %>% filter(row_number() <= 20) %>% .$SampleId
 signature_by_sample_and_type(filter,"shortish DEL")
 
@@ -267,13 +253,12 @@ filter = filter %>% filter(countINV/(countDUP+countDEL)>0.9) %>% arrange (-count
 signature_by_sample_and_type(filter,"ChromosomalStress")
 
 ##### FS like DEL ############
-filter = cohortSummary('Type=="BND"|LengthBucket>5e4,LengthBucket<5e5','SampleId,cancerType')
-filter = cohortSummary('Type=="BND"|LengthBucket>5e4,LengthBucket<5e5','SampleId,cancerType')
+filter = cohortSummary(cluster,'Type=="BND"|LengthBucket>5e4,LengthBucket<5e5','SampleId,cancerType')
 filter = filter %>% filter(countDEL/countDUP>3)  %>% arrange (-countDEL) %>% filter(row_number() <= 20) %>% .$SampleId
 signature_by_sample_and_type(filter,"FS like DEL")
 
 ##### Esophagus ############
-filter = cohortSummary('cancerType=="Esophagus"','SampleId,cancerType')
+filter = cohortSummary(cluster,'cancerType=="Esophagus"','SampleId,cancerType')
 filter = filter %>% arrange (-count) %>% filter(row_number() <= 20) %>% .$SampleId
 signature_by_sample_and_type(filter,"Esophagus")
 
