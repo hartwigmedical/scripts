94a8f9deca10178740d4b9d9ccb2157fab7070b1
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-03-13 11:54:03 +0100

    Change query to use >= for repeats, to make sure counts add up

--- sql/indel_analysis.sql
+++ sql/indel_analysis.sql
@@ -1,7 +1,7 @@
 SELECT sampleId,
 least(5,greatest(-5,length(alt)-length(ref))) AS indelLength,
 count(*) AS totalCount,
-sum(IF(repeatCount>4,1,0)) AS repeatCount,
+sum(IF(repeatCount>=4,1,0)) AS repeatCount,
 sum(IF(repeatCount<4 AND microhomology <>'',1,0)) AS MHCount,
 sum(IF(repeatCount<4 AND microhomology ='',1,0)) AS nonRepeatnonMHCount  
 FROM somaticVariant WHERE sampleId = 'XXX' AND TYPE = 'INDEL' GROUP BY 1,2;
