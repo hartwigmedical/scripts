f1e2197e158ae5fce14b72849c68e6d1bd5d8b71
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-03-27 14:06:59 +0200

    Fix bug in raw informed consent query

--- sql/rawecrf/raw_informed_consent_query.sql
+++ sql/rawecrf/raw_informed_consent_query.sql
@@ -23,23 +23,23 @@ left join
 on ADPID.patientId = ADTUM.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam81
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM86' group by patientId) TSTAM81
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM81' group by patientId) TSTAM81
 on ADTUM.patientId = TSTAM81.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam82
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM86' group by patientId) TSTAM82
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM82' group by patientId) TSTAM82
 on ADTUM.patientId = TSTAM82.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam83
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM86' group by patientId) TSTAM83
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM83' group by patientId) TSTAM83
 on ADTUM.patientId = TSTAM83.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam84
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM86' group by patientId) TSTAM84
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM84' group by patientId) TSTAM84
 on ADTUM.patientId = TSTAM84.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam85
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM86' group by patientId) TSTAM85
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM85' group by patientId) TSTAM85
 on ADTUM.patientId = TSTAM85.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam86
@@ -47,13 +47,13 @@ left join
 on ADTUM.patientId = TSTAM86.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam87
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM86' group by patientId) TSTAM87
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM87' group by patientId) TSTAM87
 on ADTUM.patientId = TSTAM87.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam88
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM86' group by patientId) TSTAM88
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM88' group by patientId) TSTAM88
 on ADTUM.patientId = TSTAM88.patientId
 left join
     (select patientId, group_concat(itemValue separator ', ') as tstam89
-    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM87' group by patientId) TSTAM89
+    from ecrf where fieldName ='BASELINE.INFORMEDCONSENT.INFORMEDCONSENT.TSTAM89' group by patientId) TSTAM89
 on TSTAM86.patientId = TSTAM89.patientId
