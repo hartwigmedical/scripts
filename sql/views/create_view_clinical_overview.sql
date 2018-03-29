3651945e6c373a81aa7b5a29afd2e974cd48993f
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-03-29 16:21:59 +0200

    Updated clinical view for patient db v2.4

--- sql/views/create_view_clinical_overview.sql
+++ sql/views/create_view_clinical_overview.sql
@@ -2,14 +2,15 @@ DROP VIEW IF EXISTS clinical;
 
 CREATE VIEW clinical AS
 
-	SELECT sample.sampleId, sample.arrivalDate as sampleArrivalDate, patient.cpctId AS patientId, patient.hospital, patient.gender, patient.birthYear,
-	patient.hasSystemicPreTreatment, patient.hasRadiotherapyPreTreatment, patient.preTreatments, patient.informedConsentDate, biopsy.biopsyDate,
-	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, patient.deathDate,
-	patient.cancerType, patient.cancerSubtype, biopsy.biopsySite, biopsy.biopsyLocation, treatment.treatmentGiven, treatment.radiotherapyGiven,
+	SELECT sample.sampleId, sample.arrivalDate as sampleArrivalDate, patient.patientIdentifier AS patientId, baseline.informedConsentDate, baseline.hospital, baseline.gender,
+    baseline.birthYear, baseline.hasSystemicPreTreatment, baseline.hasRadiotherapyPreTreatment, baseline.preTreatments, biopsy.biopsyDate,
+	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, baseline.deathDate,
+	baseline.cancerType, baseline.cancerSubtype, biopsy.biopsySite, biopsy.biopsyLocation, treatment.treatmentGiven, treatment.radiotherapyGiven,
 	treatment.name AS treatment, treatment.type AS treatmentType,
 	firstMatchedTreatmentResponse.measurementDone as responseMeasured, firstMatchedTreatmentResponse.response as firstResponse
-	FROM sample 
+	FROM sample
 		INNER JOIN patient ON sample.patientId = patient.id
+        LEFT JOIN baseline ON patient.id = baseline.patientId
 		LEFT JOIN biopsy ON biopsy.sampleId = sample.sampleId
 		LEFT JOIN treatment on treatment.biopsyId = biopsy.id
-		LEFT JOIN firstMatchedTreatmentResponse ON treatment.id = firstMatchedTreatmentResponse.treatmentId
+		LEFT JOIN firstMatchedTreatmentResponse ON treatment.id = firstMatchedTreatmentResponse.treatmentId;
