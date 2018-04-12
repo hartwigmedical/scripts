003836b4c99dc219c4863a600ddb70654cf33f36
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-04-12 15:57:48 +0200

    Added pretreatment type to clinical view

--- sql/views/create_view_clinical_overview.sql
+++ sql/views/create_view_clinical_overview.sql
@@ -3,7 +3,7 @@ DROP VIEW IF EXISTS clinical;
 CREATE VIEW clinical AS
 
 	SELECT sample.sampleId, sample.arrivalDate as sampleArrivalDate, patient.patientIdentifier AS patientId, baseline.informedConsentDate, baseline.hospital, baseline.gender,
-    baseline.birthYear, baseline.hasSystemicPreTreatment, baseline.hasRadiotherapyPreTreatment, baseline.preTreatments, biopsy.biopsyDate,
+    baseline.birthYear, baseline.hasSystemicPreTreatment, baseline.hasRadiotherapyPreTreatment, baseline.preTreatments, baseline.preTreatmentsType, biopsy.biopsyDate,
 	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, baseline.deathDate,
 	baseline.primaryTumorLocation, baseline.cancerSubtype, biopsy.biopsyType, biopsy.biopsySite, biopsy.biopsyLocation,
 	treatment.treatmentGiven, treatment.radiotherapyGiven, treatment.name AS treatment, treatment.type AS treatmentType,
