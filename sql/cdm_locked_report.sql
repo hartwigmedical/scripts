b64a7e50e18bc91072d65a587b18bc15bb66a35d
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-04-18 14:31:52 +0200

    Update CDM locked rapport

--- sql/cdm_locked_report.sql
+++ sql/cdm_locked_report.sql
@@ -1,36 +1,35 @@
 # Number of CPCT patients
-select patientId, count(sampleId) as countSamples from clinical where sampleId like '%CPCT%'group by patientId ;
+select patientId, count(sampleId) as countSamples from clinical where sampleId like '%CPCT%' group by patientId ;
 
 # Registration date of patient
-select patientIdentifier, registrationDate from baseline
-inner join patient on patient.id=baseline.patientId order by 1;
+select distinct patientId, registrationDate from clinical where patientId like '%CPCT%' group by sampleId;
 
 # Timeline starts with informed consent?
-select * from clinicalFindings where ecrfItem = 'FLD.INFORMEDCONSENT.ICDTC;FLD.BIOPS.BIOPTDT' and
-message ='at least 1 biopsy taken before informed consent date' and formLocked='true';
+select * from clinicalFindings where
+message ='at least 1 biopsy taken before informed consent date' and patientId like '%CPCT%';
 
-select * from clinicalFindings where ecrfitem ='FLD.INFORMEDCONSENT.ICDTC' and message ='informed consent date empty or in wrong format' and formLocked='true';
+select * from clinicalFindings where message ='informed consent date empty or in wrong format' and patientId like '%CPCT%';
 
 # Hospital is known?
-select * from clinicalFindings where message like '%no hospital%' and formLocked='true';
+select * from clinicalFindings where message like '%Hospital could not be determined%' and patientId like '%CPCT%';
 
 # Gender is known?
-select * from clinicalFindings where ecrfItem ='FLD.DEMOGRAPHY.SEX' and message = 'gender empty' and formLocked='true';
+select * from clinicalFindings where message = 'Gender empty' and patientId like '%CPCT%';
 
 # Birthyear is known?
-select * from clinicalFindings where ecrfItem ='FLD.SELCRIT.NBIRTHYEAR;FLD.ELIGIBILITY.BIRTHYEAR;FLD.ELIGIBILITY.BIRTHDTCES' and
-message = 'birth year could not be determined' and formLocked='true';
+select * from clinicalFindings where
+message = 'birth year could not be determined' and patientId like '%CPCT%';
 
 # Can curate the cancer type?
-select * from clinicalFindings where ecrfitem ='FLD.CARCINOMA.PTUMLOC;FLD.CARCINOMA.PTUMLOCS' and message ='primary tumor location empty' and formLocked='true';
-select * from clinicalFindings where ecrfitem ='FLD.CARCINOMA.PTUMLOC;FLD.CARCINOMA.PTUMLOCS' and message ='failed to curate primary tumor location.' and formLocked='true';
+select * from clinicalFindings where message ='primary tumor location empty' and patientId like '%CPCT%';
+select * from clinicalFindings where message ='failed to curate primary tumor location' and patientId like '%CPCT%';
 
 # Systemic pre-therapy is known?
-select * from clinicalFindings where ecrfitem ='FLD.PRETHERAPY.SYSTEMIC' and message = 'pre systemic treatment given empty' and formLocked='true';
+select * from clinicalFindings where message = 'pre systemic treatment given empty' and patientId like '%CPCT%';
 
 # Radiotherapy pre-therapy is known?
-select * from clinicalFindings where ecrfitem ='FLD.PRETHERAPY.RADIOTHER' and message = 'pre radio treatment given empty' and formLocked='true';
+select * from clinicalFindings where message = 'pre radio treatment given empty' and patientId like '%CPCT%';
 
 # Can curate all pre-therapies?
-select * from clinicalFindings where ecrfitem ='FLD.PRETHERAPY.SYSTEMICREG'
-and message = 'failed to curate ecrf drug. Curated list contained no matching entry, or match was ambiguous.' and formLocked='true';
+select * from clinicalFindings where
+message = 'failed to curate ecrf drug. Curated list contained no matching entry, or match was ambiguous' and level = "preTreatmentCuration" and patientId like '%CPCT%';
