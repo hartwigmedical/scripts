b7fb6c3b2b15c0040f635b71a2bf7aa76517ae23
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-04-19 14:29:19 +0200

    Update CDM locked rapport with form locked is true

--- sql/cdm_locked_report.sql
+++ sql/cdm_locked_report.sql
@@ -6,30 +6,30 @@ select distinct patientId, registrationDate from clinical where patientId like '
 
 # Timeline starts with informed consent?
 select * from clinicalFindings where
-message ='at least 1 biopsy taken before informed consent date' and patientId like '%CPCT%';
+message ='at least 1 biopsy taken before informed consent date' and patientId like '%CPCT%' and formLocked = 'true';
 
-select * from clinicalFindings where message ='informed consent date empty or in wrong format' and patientId like '%CPCT%';
+select * from clinicalFindings where message ='informed consent date empty or in wrong format' and patientId like '%CPCT%'  and formLocked = 'true';
 
 # Hospital is known?
-select * from clinicalFindings where message like '%Hospital could not be determined%' and patientId like '%CPCT%';
+select * from clinicalFindings where message like '%Hospital could not be determined%' and patientId like '%CPCT%'  and formLocked = 'true';
 
 # Gender is known?
-select * from clinicalFindings where message = 'Gender empty' and patientId like '%CPCT%';
+select * from clinicalFindings where message = 'Gender empty' and patientId like '%CPCT%'  and formLocked = 'true';
 
 # Birthyear is known?
 select * from clinicalFindings where
-message = 'birth year could not be determined' and patientId like '%CPCT%';
+message = 'birth year could not be determined' and patientId like '%CPCT%'  and formLocked = 'true';
 
 # Can curate the cancer type?
-select * from clinicalFindings where message ='primary tumor location empty' and patientId like '%CPCT%';
-select * from clinicalFindings where message ='failed to curate primary tumor location' and patientId like '%CPCT%';
+select * from clinicalFindings where message ='primary tumor location empty' and patientId like '%CPCT%'  and formLocked = 'true';
+select * from clinicalFindings where message ='failed to curate primary tumor location' and patientId like '%CPCT%'  and formLocked = 'true';
 
 # Systemic pre-therapy is known?
-select * from clinicalFindings where message = 'pre systemic treatment given empty' and patientId like '%CPCT%';
+select * from clinicalFindings where message = 'pre systemic treatment given empty' and patientId like '%CPCT%'  and formLocked = 'true';
 
 # Radiotherapy pre-therapy is known?
-select * from clinicalFindings where message = 'pre radio treatment given empty' and patientId like '%CPCT%';
+select * from clinicalFindings where message = 'pre radio treatment given empty' and patientId like '%CPCT%'  and formLocked = 'true';
 
 # Can curate all pre-therapies?
 select * from clinicalFindings where
-message = 'failed to curate ecrf drug. Curated list contained no matching entry, or match was ambiguous' and level = "preTreatmentCuration" and patientId like '%CPCT%';
+message = 'failed to curate ecrf drug. Curated list contained no matching entry, or match was ambiguous' and level = "preTreatmentCuration" and patientId like '%CPCT%'  and formLocked = 'true';
