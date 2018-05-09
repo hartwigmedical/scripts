2ef19702309bba61f7f25b20767192d2461b5412
Jonathan Baber <jonbaber@gmail.com>
2018-05-09 10:30:45 +1000

    updated copy number overview visualisation

--- analysisscripts/copyNumberOverview.R
+++ analysisscripts/copyNumberOverview.R
@@ -1,7 +1,6 @@
 #install.packages("devtools")
 #devtools::install_github("hadley/multidplyr")
 
-library(RMySQL)
 detach("package:purple", unload=TRUE)
 library(purple)
 library(dplyr)
@@ -18,8 +17,8 @@ isLoh <- function(minAllelePloidy, chromosome, gender) {
   return (result)
 }
 
-isDel <- function(copyNumber, chromosome, gender) {
-  result <- ifelse(isMaleSexChromosome(gender, chromosome), copyNumber < 0.25, copyNumber < 0.5)
+isDel <- function(copyNumber, chromosome, gender, ploidy, cutoff) {
+  result <- ifelse(isMaleSexChromosome(gender, chromosome), copyNumber < cutoff * ploidy / 2, copyNumber < cutoff * ploidy)
   return (result)
 }
 
@@ -29,15 +28,19 @@ isAmp <- function(copyNumber, chromosome, gender, ploidy, cutoff) {
 }
 
 
-load(file = "~/hmf/RData/highestPurityCohort.RData")
+load(file = "~/hmf/RData/reference/highestPurityCohort.RData")
 highestPurityCohort$gender <- ifelse(substr(highestPurityCohort$gender, 1,4) == 'MALE', 'MALE', highestPurityCohort$gender)
 
-load(file = "~/hmf/RData/highestPurityCopyNumbers.RData")
-
-highestPurityCopyNumbers = highestPurityCopyNumbers %>% 
+load(file = "~/hmf/RData/reference/highestPurityCopyNumbers.RData")
+highestPurityCopyNumbers = highestPurityCopyNumbers %>%
   left_join(highestPurityCohort[, c("sampleId", "ploidy", "primaryTumorLocation", "gender")], by = "sampleId") %>%
-  mutate(minAllelePloidy = pmax(0, (1-actualBaf) * copyNumber), loh = isLoh(minAllelePloidy, chromosome, gender)) %>%
-  mutate(del = isDel(copyNumber, chromosome, gender), amp2 = isAmp(copyNumber, chromosome, gender, ploidy, 2), amp3 =isAmp(copyNumber, chromosome, gender, ploidy, 3))  
+  mutate(minAllelePloidy = pmax(0, (1-actualBaf) * copyNumber),
+    loh = isLoh(minAllelePloidy, chromosome, gender),
+    absDel = isDel(copyNumber, chromosome, gender, 1, 0.5),
+    relDel = loh & isDel(copyNumber, chromosome, gender, ploidy, 0.6),
+    amp1_4 = isAmp(copyNumber, chromosome, gender, ploidy, 1.4),
+    amp2_0 = isAmp(copyNumber, chromosome, gender, ploidy, 2),
+    amp3_0 =isAmp(copyNumber, chromosome, gender, ploidy, 3))
 
 highestPurityCopyNumbers$region = GRanges(paste0("chr", highestPurityCopyNumbers$chromosome), ranges = IRanges(start = highestPurityCopyNumbers$start, end = highestPurityCopyNumbers$end))
 
@@ -46,23 +49,24 @@ bins <- tileGenome(seqinfo(Hsapiens), tilewidth=300000, cut.last.tile.in.chrom=T
 length(bins)
 
 ol = as.matrix(findOverlaps(bins, highestPurityCopyNumbers$region, type = "any"))
-olCopyNumbers = cbind(bin = ol[, 1], highestPurityCopyNumbers[ol[, 2], c("sampleId", "primaryTumorLocation", "loh", "del", "amp2", "amp3")])
+olCopyNumbers = cbind(bin = ol[, 1], highestPurityCopyNumbers[ol[, 2], c("sampleId", "primaryTumorLocation", "loh", "absDel", "relDel", "amp1_4", "amp2_0", "amp3_0")])
 
 no_cores <- 7
 cl<-makeCluster(no_cores, type="FORK"); date()
-binSampleSummary = olCopyNumbers %>% partition(bin, sampleId, cluster = cl) %>% 
-  summarise(primaryTumorLocation = first(primaryTumorLocation), loh = any(loh), del = any(del), amp2 = any(amp2), amp3 = any(amp3))  %>%
-  collect() %>% 
+binSampleSummary = olCopyNumbers %>% partition(bin, sampleId, cluster = cl) %>%
+  summarise(primaryTumorLocation = first(primaryTumorLocation),
+            loh = any(loh), absDel = any(absDel), relDel = any(relDel), amp1_4 = any(amp1_4), amp2_0 = any(amp2_0), amp3_0 = any(amp3_0))  %>%
+  collect() %>%
   as_tibble()
 date()
 binCancerTypeSummary = binSampleSummary %>% partition(bin, primaryTumorLocation, cluster = cl) %>%
-  summarise(loh = sum(loh), del = sum(del), amp2 = sum(amp2), amp3 = sum(amp3)) %>%
-  collect() %>% 
+  summarise(loh = sum(loh), absDel = sum(absDel), relDel = sum(relDel), amp1_4 = sum(amp1_4), amp2_0 = sum(amp2_0), amp3_0 = sum(amp3_0)) %>%
+  collect() %>%
   as_tibble()
 date()
 stopCluster(cl)
 
-binSummary = binSampleSummary %>% group_by(bin)  %>%  summarise(primaryTumorLocation = 'All', loh = sum(loh), del = sum(del), amp2 = sum(amp2), amp3 = sum(amp3))
+binSummary = binSampleSummary %>% group_by(bin)  %>%  summarise(primaryTumorLocation = 'All', loh = sum(loh), absDel = sum(absDel), relDel = sum(relDel), amp1_4 = sum(amp1_4), amp2_0 = sum(amp2_0), amp3_0 = sum(amp3_0))
 binSummary = bind_rows(binSummary, binCancerTypeSummary)
 
 primaryTumorLocationCounts = highestPurityCohort %>% group_by(primaryTumorLocation) %>% summarise(nTotal = as.numeric(n()))
@@ -82,14 +86,17 @@ highestPurityCopyNumberSummary$chromosome = substring(as.character(seqnames(high
 highestPurityCopyNumberSummary$start = start(highestPurityCopyNumberSummary$region)
 highestPurityCopyNumberSummary$end = end(highestPurityCopyNumberSummary$region)
 highestPurityCopyNumberSummary$region <- NULL
-highestPurityCopyNumberSummary = highestPurityCopyNumberSummary %>% 
+highestPurityCopyNumberSummary = highestPurityCopyNumberSummary %>%
   mutate(n = ifelse(chromosome == 'Y', nMale, nTotal)) %>%
-  mutate(lohPercentage = loh /n, delPercentage = del / n, amp2Percentage = amp2 / n, amp3Percentage = amp3 / n) %>%
+  mutate(lohPercentage = loh /n, absDelPercentage = absDel / n, relDelPercentage = relDel / n, amp1_4Percentage = amp1_4 / n,  amp2_0Percentage = amp2_0 / n, amp3_0Percentage = amp3_0 / n) %>%
   ungroup() %>%
-  select(chromosome, start, end, primaryTumorLocation, lohPercentage, delPercentage, amp2Percentage, amp3Percentage)
-#save(highestPurityCopyNumberSummary, file = "~/hmf/RData/highestPurityCopyNumberSummary.RData")
+  select(chromosome, start, end, primaryTumorLocation, lohPercentage, absDelPercentage, relDelPercentage, amp1_4Percentage, amp2_0Percentage, amp3_0Percentage)
+save(highestPurityCopyNumberSummary, file = "~/hmf/RData/processed/highestPurityCopyNumberSummary.RData")
 
 
+
+
+load(file = "~/hmf/RData/processed/highestPurityCopyNumberSummary.RData")
 primaryTumorLocations = unique(highestPurityCopyNumberSummary$primaryTumorLocation)
 primaryTumorLocations = primaryTumorLocations[!is.na(primaryTumorLocations)]
 primaryTumorLocations
@@ -97,22 +104,28 @@ for (location in primaryTumorLocations) {
 
   locationString = gsub(" ", "", location, fixed = TRUE)
   locationString = gsub("/", "", locationString, fixed = TRUE)
-  
-  primaryTumorLocationSummary = highestPurityCopyNumberSummary %>% 
+
+  primaryTumorLocationSummary = highestPurityCopyNumberSummary %>%
     filter(primaryTumorLocation == location) %>%
     mutate(chromosome = paste0("hs", chromosome))
-  
-  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, lohPercentage), 
+
+  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, lohPercentage) %>% mutate(lohPercentage = -lohPercentage),
               file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".loh.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
-  
-  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, delPercentage), 
-              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".del.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
-  
-  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, amp2Percentage), 
-              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".amp2.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
-  
-  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, amp3Percentage), 
-              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".amp3.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
+
+  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, absDelPercentage) %>% mutate(absDelPercentage = -absDelPercentage),
+              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".absDel.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
+
+  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, relDelPercentage) %>% mutate(relDelPercentage = -relDelPercentage),
+              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".relDel.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
+
+  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, amp1_4Percentage),
+              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".amp1_4.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
+
+  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, amp2_0Percentage),
+              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".amp2_0.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
+
+  write.table(primaryTumorLocationSummary %>% select(chromosome, start, end, amp3_0Percentage),
+              file = paste0("~/hmf/analysis/copyNumberSummary/", locationString, ".amp3_0.circos"), sep = "\t", row.names = F, col.names = F, quote = F)
 }
 
 
@@ -120,11 +133,11 @@ for (location in primaryTumorLocations) {
 for (location in primaryTumorLocations) {
   locationString = gsub(" ", "", location, fixed = TRUE)
   locationString = gsub("/", "", locationString, fixed = TRUE)
-  
+
   cmd = paste0("sed 's/CANCER/", locationString,"/g' circos.template > ",locationString,".conf\n")
-  #cat(cmd)
-  
-  cmd = paste0("/Users/jon/hmf/tools/circos-0.69-6/bin/circos -nosvg -conf /Users/jon/hmf/analysis/copyNumberSummary/",locationString, ".conf -outputdir /Users/jon/hmf/analysis/copyNumberSummary -outputfile ",locationString,".png\n")
   cat(cmd)
+
+  #cmd = paste0("/Users/jon/hmf/tools/circos-0.69-6/bin/circos -nosvg -conf /Users/jon/hmf/analysis/copyNumberSummary/",locationString, ".conf -outputdir /Users/jon/hmf/analysis/copyNumberSummary -outputfile ",locationString,".png\n")
+  #cat(cmd)
 }
 
