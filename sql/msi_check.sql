fd49149bab034e9ec58e8e61200162a6e1dfa8d8
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-05-12 12:19:50 +0200

    Updated repeat count again

--- sql/msi_check.sql
+++ sql/msi_check.sql
@@ -1,5 +1,5 @@
 select sampleId, count(*)/3095 as indelsPerMb, if(count(*)/3095 > 4, "MSI", "MSS" ) as status from somaticVariant
-where filter = 'PASS' and type = 'INDEL' and repeatCount > 4 and length(alt) <= 50 and length(ref) <= 50
+where filter = 'PASS' and type = 'INDEL' and repeatCount >= 4 and length(alt) <= 50 and length(ref) <= 50
 and (
 	(length(repeatSequence) between 2 and 4 ) OR
 	(length(repeatSequence) = 1 and repeatCount >= 5)
