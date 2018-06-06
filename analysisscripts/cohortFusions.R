02e074dc5d24357e98949fb20b1c94cccf8c5897
Jonathan Baber <jonbaber@gmail.com>
2018-06-06 10:00:56 +1000

    Added whitelist

--- analysisscripts/cohortFusions.R
+++ analysisscripts/cohortFusions.R
@@ -1,3 +1,5 @@
+library(RMySQL)
+library(dplyr)
 
 ### REFERENCE
 load(file = "~/hmf/RData/Reference/allPurity.RData")
@@ -19,7 +21,18 @@ rm(dbEnsemble)
 load(file = "~/hmf/RData/reference/allFusions.RData")
 load(file = "~/hmf/RData/reference/allFusionCodingRegions.RData")
 
+fusionConstraints = read.csv(file = "~/hmf/Resources/3PrimeFusionDomainConstraintsV1.csv", stringsAsFactors = F) %>%
+  filter(!is.na(`Min.Exon`) | !is.na(`Max.Exon`)) %>%
+  select(`3pTranscript` = Transcript, `3pMinExonConstraint` = `Min.Exon`, `3pMaxExonConstraint` = `Max.Exon`)
+
 fusions = allFusions %>%
+  # Exon constraints
+  left_join(fusionConstraints, by = "3pTranscript") %>%
+  filter(is.na(`3pMinExonConstraint`) | `3pExonRankDownstream` >= `3pMinExonConstraint`) %>%
+  filter(is.na(`3pMaxExonConstraint`) | `3pExonRankDownstream` <= `3pMaxExonConstraint`) %>%
+  select(-`3pMinExonConstraint`, -`3pMaxExonConstraint`) %>%
+  
+  # Coding Constraints
   mutate(`5pPosition` = ifelse(`5pIsStartEnd` == 1, startPosition, endPosition)) %>%
   left_join(allFusionCodingRegions %>% select(`5pTranscript` = transcriptId, `5pCodingStart` = coding_start, `5pCodingEnd` = coding_end), by = "5pTranscript")  %>%
   mutate(
@@ -34,7 +47,12 @@ fusions = allFusions %>%
   select(-`3pPosition`, -`3pCodingStart`, -`3pCodingEnd`) %>%
   mutate(intragenic = `5pGene` == `3pGene`) %>%
   filter(`3pPostCoding` == F, is.na(`5pPostCoding`) | `5pPostCoding` == F, !(intragenic & `5pPhase` == -1)) %>%
-  mutate(driver = "Fusion-Coding", driver = ifelse(is.na(`5pPreCoding`) | `5pPreCoding`, "Fusion-UTR", driver), driver = ifelse(intragenic, "Fusion-Intragenic", driver)) %>%
+  
+  # Clean up
+  mutate(
+    driver = "Fusion-Coding", 
+    driver = ifelse(is.na(`5pPreCoding`) | `5pPreCoding`, "Fusion-UTR", driver), 
+    driver = ifelse(intragenic, "Fusion-Intragenic", driver)) %>%
   select(-ends_with("IsStartEnd"),  -ends_with("Coding"), -filter, -intragenic)
 
 load(file = "~/hmf/RData/reference/highestPurityCohort.RData")
