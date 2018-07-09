e93460688c8b98f08eb43b8f684a8f16feceb091
Jonathan Baber <jonbaber@gmail.com>
2018-07-09 10:11:23 +1000

    prepare supplementary table

--- analysisscripts/actionableVisualisation.R
+++ analysisscripts/actionableVisualisation.R
@@ -2,113 +2,107 @@ library(dplyr)
 library(tidyr)
 library(scales)
 
-load(file = '~/hmf/RData/Reference/hpcCancerTypeCounts.RData')
-load(file = '~/hmf/RData/Processed/highestPurityCohortSummary.RData')
-actionableVariants = read.table('~/hmf/resources/actionableVariantsPerSample.tsv',header=T,sep = '\t')
-treatmentTypeFactors = rev(c("A_OnLabel","A_OffLabel","B_OnLabel","B_OffLabel"))
-treatmentTypeColors = setNames(c("#fff7bc","#fee391","#fe9929","#993404"), treatmentTypeFactors)
-treatmentTypeColors = setNames(c("#f0f9e8","#bae4bc","#43a2ca","#0868ac"), treatmentTypeFactors)
-treatmentTypeColors = setNames(rev(c("#2171b5","#6baed6","#bdd7e7","#eff3ff")), treatmentTypeFactors)
+########################################### Prepare Data
+alphabetical_drug <- function(drugs) {
+  result = c()
+  for (i in 1:length(drugs)) {
+    drug = drugs[i]
+    sortedDrugList = sort(unlist(strsplit(drug, " + ", fixed = T)))
+    result[i] <- paste(sortedDrugList, collapse = " + ")
+  }
+  return (result)
+}
 
-#unique(actionableVariants$source)
-#actionableVariants = actionableVariants %>% filter(source == 'oncoKb')
+levelTreatmentFactors = rev(c("A_OnLabel","A_OffLabel","B_OnLabel","B_OffLabel"))
+levelTreatmentColors = setNames(rev(c("#2171b5","#6baed6","#bdd7e7","#eff3ff")), levelTreatmentFactors)
+
+load(file = '~/hmf/RData/Processed/highestPurityCohortSummary.RData')
+actionableVariants = read.table('~/hmf/resources/actionableVariantsPerSample.tsv',header=T,sep = '\t', stringsAsFactors = F) %>%
+  filter(!startsWith(sampleEvent, "Synonymous"), 
+         !gene %in% c('PTEN','KRAS'),
+         !level %in% c('Early trials'),
+         hmfLevel %in% c('A','B')) %>%
+  mutate(
+    drug = ifelse(drug == "Fluvestrant", "Fulvestrant", drug),
+    drug = ifelse(drug == "Ado-trastuzumab Emtansine", "Ado-Trastuzumab Emtansine", drug),
+    drug = ifelse(drug == "AZD4547", "AZD-4547", drug),
+    drug = ifelse(drug == "AZD5363", "AZD-5363", drug),
+    drug = ifelse(drug == "BGJ398", "BGJ-398", drug),
+    drug = alphabetical_drug(drug),
+    treatmentType = ifelse(treatmentType == "On-label", "OnLabel", "OffLabel"),
+    levelTreatment = factor(paste(hmfLevel, treatmentType, sep = "_"), levelTreatmentFactors))
+
+########################################### Supplementary Data
+drugResponse <- function(actionableVariants, response) {
+  actionableVariants %>% 
+    filter(hmfResponse == response) %>%
+    group_by(sampleId,patientCancerType, drug, levelTreatment) %>%
+    count() %>% 
+    group_by(sampleId,patientCancerType, drug) %>%
+    top_n(1, levelTreatment) %>% 
+    select(-n) 
+}
 
 msiResponsive = highestPurityCohortSummary %>% 
   filter(msiStatus == 'MSI', cancerType != 'Other') %>% 
   select(sampleId, patientCancerType = cancerType) %>% 
-  mutate(drug = "MSI", treatmentType = factor("A_OnLabel", treatmentTypeFactors))
+  mutate(drug = "MSI", levelTreatment = factor("A_OnLabel", levelTreatmentFactors))
 
-responsiveDrugs = actionableVariants %>% 
-  filter(
-   !gene %in% c('PTEN','KRAS'),
-    hmfLevel %in% c('A','B'),
-    !level %in% c('Early trials'),
-    hmfResponse %in% c('Responsive')) %>%
-  mutate(
-    treatmentType = ifelse(treatmentType == "On-label", "OnLabel", "OffLabel")) %>%
-  group_by(sampleId,patientCancerType, drug, treatmentType, hmfLevel) %>%
-  count() %>% 
-  unite(treatmentType, hmfLevel, treatmentType) %>%
-  mutate(treatmentType = factor(treatmentType, treatmentTypeFactors)) %>%
-  group_by(sampleId,patientCancerType, drug) %>%
-  top_n(1, treatmentType) %>% 
-  select(-n) %>%
-  bind_rows(msiResponsive)
-
-responsiveDrugGenes = actionableVariants %>% 
-  filter(
-    !gene %in% c('PTEN','KRAS'),
-    hmfLevel %in% c('A','B'),
-    !level %in% c('Early trials'),
-    hmfResponse %in% c('Responsive')) %>%
-  mutate(
-    treatmentType = ifelse(treatmentType == "On-label", "OnLabel", "OffLabel")) %>%
-  group_by(sampleId,patientCancerType, drug, gene, treatmentType, hmfLevel) %>%
-  count() %>% 
-  unite(treatmentType, hmfLevel, treatmentType) %>%
-  mutate(treatmentType = factor(treatmentType, treatmentTypeFactors)) %>%
-  group_by(sampleId,patientCancerType, drug, gene) %>%
-  top_n(1, treatmentType) %>% 
-  select(-n) %>%
-  bind_rows(msiResponsive)
-
-resistantDrugs = actionableVariants %>% 
-  filter(
-    #!gene %in% c('TP53','RB1'),
-    hmfLevel %in% c('A','B'),
-    !level %in% c('Early trials'),
-    hmfResponse %in% c('Resistant')) %>%
-  mutate(
-    treatmentType = ifelse(treatmentType == "On-label","OnLabel", "OffLabel")) %>%
-  group_by(sampleId,patientCancerType, drug, treatmentType, hmfLevel) %>%
-  count() %>% 
-  unite(treatmentType, hmfLevel, treatmentType) %>%
-  mutate(treatmentType = factor(treatmentType, treatmentTypeFactors)) %>%
-  group_by(sampleId,patientCancerType, drug) %>%
-  top_n(1, treatmentType) %>% 
-  select(-n)
+responsiveDrugs = drugResponse(actionableVariants, 'Responsive') %>% bind_rows(msiResponsive)
+resistantDrugs = drugResponse(actionableVariants, 'Resistant')
 
 actionableDrugs = merge(responsiveDrugs,resistantDrugs,by=c('sampleId','patientCancerType','drug'),all=T,suffixes=c('_Response','_Resistance'), fill=0) %>%
   mutate(
-    responsive = !is.na(treatmentType_Response) & is.na(treatmentType_Resistance),
-    resistant = is.na(treatmentType_Response) & !is.na(treatmentType_Resistance),  
-    inconsistent = !is.na(treatmentType_Response) & !is.na(treatmentType_Resistance),
+    responsive = !is.na(levelTreatment_Response) & is.na(levelTreatment_Resistance),
+    resistant = is.na(levelTreatment_Response) & !is.na(levelTreatment_Resistance),  
+    inconsistent = !is.na(levelTreatment_Response) & !is.na(levelTreatment_Resistance),
     status = ifelse(responsive, "Responsive", "Resistance"),
     status = ifelse(inconsistent, "Inconsistent", status)
-    ) 
+  ) %>%
+  filter(responsive)
 
-actionableDrugsSummary = actionableDrugs %>%
-  group_by(sampleId, patientCancerType, drug, status) %>% count() %>%
-  spread(status, n) 
+responsiveVariants = actionableDrugs %>% select(sampleId, drug) %>%
+  left_join(actionableVariants, by = c("sampleId","drug")) %>%
+  mutate(treatmentType = ifelse(treatmentType == "On-label", "OnLabel", "OffLabel")) %>%
+  filter(hmfLevel %in% c('A','B'), !level %in% c('Early trials')) %>%
+  select(sampleId, gene, drug, eventType, sampleEvent, levelTreatment, hmfLevel, treatmentType) %>%
+  group_by(sampleId, gene, drug, eventType, sampleEvent, levelTreatment, hmfLevel, treatmentType) %>%
+  distinct() %>%
+  group_by(sampleId, gene, drug, eventType, sampleEvent) %>%
+  top_n(1, levelTreatment) %>%
+  select(-levelTreatment) %>%
+  group_by(sampleId, gene, eventType, sampleEvent) %>%
+  mutate(variantTreatmentOptions = n()) %>%
+  ungroup()
+save(responsiveVariants, file = "~/hmf/RData/Processed/responsiveVariants.RData")
 
-actionableSampleSummary = actionableDrugs %>%
-  group_by(sampleId, patientCancerType, status) %>% count() %>%
-  spread(status, n) %>% arrange(Inconsistent, Responsive)
-  
 
-drugResponseSummary = actionableDrugs %>% filter(responsive) %>% distinct(sampleId, drug) %>% group_by(drug) %>% count()
-geneResponseSummary = inner_join(responsiveDrugGenes, actionableDrugs %>% filter(responsive) %>% select(sampleId, drug, responsive), by = c("sampleId","drug")) %>%
-  ungroup() %>%
-  distinct(sampleId, gene) %>%
-  group_by(gene) %>%
-  count()
+geneResponseSummary = responsiveVariants %>% ungroup() %>% distinct(sampleId, gene) %>% group_by(gene) %>% count()
+drugResponseSummary = responsiveVariants %>% ungroup() %>% distinct(sampleId, drug) %>% group_by(drug) %>% count()
+#PLATINUM v Platinum Agent ??
+#Binimetinib v Binimetinib (MEK162) v Binimetinib + Ribociclib
+#Alpelisib v Alpelisib + Fulvestrant
+#Buparlisib v Buparlisib + Fulvestrant
+#Dabrafenib v Dabrafenib + Trametinib
+
+########################################### Visualise
+load(file = '~/hmf/RData/Reference/hpcCancerTypeCounts.RData')
 
 actionablePlotData = actionableDrugs %>%
-  filter(responsive) %>%
   group_by(patientCancerType, sampleId) %>%
-  top_n(1, treatmentType_Response) %>%
-  summarise(treatmentType_Response = dplyr::first(treatmentType_Response)) %>%
-  group_by(patientCancerType, treatmentType_Response) %>% 
+  top_n(1, levelTreatment_Response) %>%
+  summarise(levelTreatment_Response = dplyr::first(levelTreatment_Response)) %>%
+  group_by(patientCancerType, levelTreatment_Response) %>% 
   count() %>%
   left_join(hpcCancerTypeCounts %>% select(patientCancerType = cancerType, N), by = "patientCancerType" ) %>%
   mutate(percentage = n/N) %>%
   filter(!is.na(percentage)) %>% 
   arrange(percentage)
 
-actionablePlotDataFactors= actionablePlotData %>% 
-  select(patientCancerType, treatmentType_Response, n, N) %>%
+actionablePlotDataFactors = actionablePlotData %>% 
+  select(patientCancerType, levelTreatment_Response, n, N) %>%
   group_by(patientCancerType) %>% 
-  spread(treatmentType_Response, n, fill = 0) %>%
+  spread(levelTreatment_Response, n, fill = 0) %>%
   mutate(
     APercent = (A_OffLabel + A_OnLabel) / N,
     BPercent = B_OnLabel / N) %>%
@@ -119,8 +113,8 @@ actionablePlotData = actionablePlotData %>%
   mutate(patientCancerType = factor(patientCancerType, actionablePlotDataFactors$patientCancerType))
 
 p1 = ggplot(data = actionablePlotData, aes(x = patientCancerType, y = percentage)) +
-  geom_bar(stat = "identity", aes(fill = treatmentType_Response)) + 
-  scale_fill_manual(values = treatmentTypeColors) +
+  geom_bar(stat = "identity", aes(fill = levelTreatment_Response)) + 
+  scale_fill_manual(values = levelTreatmentColors) +
   ggtitle("") + xlab("") + ylab("% Samples with treatment options") +
   scale_y_continuous(labels = percent, limits = c(0, 1), expand = c(0.02,0)) +
   theme(panel.grid.major.y = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank()) +
@@ -132,6 +126,3 @@ pActionability = plot_grid(p1, labels = "AUTO")
 pActionability
 
 save_plot("~/hmf/RPlot/Figure 8 - Actionable.png", pActionability, base_width = 6, base_height = 6)
-
- 
-
