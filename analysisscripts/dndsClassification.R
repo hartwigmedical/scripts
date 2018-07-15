536891074c5a400672a6581eab80713520909a20
Jonathan Baber <jonbaber@gmail.com>
2018-07-16 08:44:35 +1000

    dnds classification visualisation

--- analysisscripts/dndsClassification.R
+++ analysisscripts/dndsClassification.R
@@ -1,10 +1,12 @@
-#library(IRanges)
-#library(GenomicRanges)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(purple)
 library(ggrepel)
+library(scales)
+library(cowplot)
+theme_set(theme_bw())
+
 
 #### DRIVER TYPE CLASSIFICATION
 load("~/hmf/RData/processed/genePanel.RData")
@@ -38,18 +40,16 @@ tsGenes = genePanel %>% filter(classification == "tsg")
 oncoGenes = genePanel %>% filter(classification == "onco")
 save(tsGenes, oncoGenes, file = "~/hmf/RData/processed/driverGenes.RData")
 
-ggplot(data=genePanelCv,aes(wmis_cv+0.1,wnon_cv+0.1,label=gene_name))+
-  geom_point(aes(colour = factor(classification)))+
-  geom_text_repel(size=2,hjust = 0, nudge_x = 0.01) +
-  scale_x_log10()+
-  scale_y_log10() 
-  
-
-
+#### VISUALISATION
+logisticVisualisation = genePanelCv
+logisticVisualisation$omit <- F
+logisticVisualisation$omit = ifelse(!logisticVisualisation$hmf & !logisticVisualisation$martincorena & !logisticVisualisation$cosmicTsg & logisticVisualisation$cosmicOncogene, T, logisticVisualisation$omit)
+logisticVisualisation$omit = ifelse(!logisticVisualisation$hmf & !logisticVisualisation$martincorena & logisticVisualisation$cosmicTsg & !logisticVisualisation$cosmicOncogene, T, logisticVisualisation$omit)
+logisticVisualisation[genePanel$gene_name == "TERT", "omit"] <- T
+logisticVisualisation = logisticVisualisation %>% filter(!omit)
 
 singleBlue = "#6baed6"
 singleOnco = "#d94701"
-#hotspotColours = setNames(c("#d94701","#fd8d3c", "#fdbe85"), hotspotFactors) #E69F00
 
 g_legend <- function(a.gplot){ 
   tmp <- ggplot_gtable(ggplot_build(a.gplot)) 
@@ -58,39 +58,34 @@ g_legend <- function(a.gplot){
   return(legend)} 
 
 
-genePanelCvNames = genePanelCv %>% filter(gene_name %in% c("KRAS","NRAS","PIK3CA","AR","KIT")| wnon_cv > 30 )
+genePanelCvNames = logisticVisualisation %>% filter(gene_name %in% c("KRAS","NRAS","PIK3CA","AR","KIT")| wnon_cv > 30 )
 
-scatterPlot <- ggplot(genePanelCv, aes(wmis_cv+0.1, wnon_cv+0.1, color=factor(classification)))+ 
+scatterPlot <- ggplot(logisticVisualisation, aes(wmis_cv+0.1, wnon_cv+0.1, color=factor(classification)))+ 
     geom_point() + 
     geom_text_repel(data = genePanelCvNames, aes(wmis_cv+0.1, wnon_cv+0.1, label = gene_name), color = "black", size=3, hjust = 1, nudge_x = 0.01,  direction = "both")+ 
-    scale_x_log10()  +
+    scale_x_log10(breaks = c(0.1, 1, 10, 100))  +
     scale_y_log10()  +
     scale_color_manual(name = "Gene Type", values = c(singleOnco,singleBlue), labels = c("Oncogene", "Tumor Suppressor Gene")) +
     theme(panel.border = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank()) +
     xlab("w_missense") + ylab("w_nonsense") + ggtitle("")
-    #theme(legend.position=c(0,1), legend.justification=c(0,1))
-scatterPlot
 
-xdensity <- ggplot(genePanelCv, aes(wmis_cv+0.1, fill=factor(classification))) + 
+xdensity <- ggplot(logisticVisualisation, aes(wmis_cv+0.1, fill=factor(classification))) + 
     geom_density(alpha=.5, adjust = 6.5) + 
     stat_smooth(aes(y=continuousClassification), fill = singleBlue, method="glm", method.args=list(family="binomial"), se=T) +
-    #scale_y_continuous(trans = "identity", sec.axis = sec_axis(~./2, name = "Relative humidity [%]")) +
     scale_y_continuous(labels = percent, limits = c(0, 1)) +
-    scale_x_log10()  + ylab("TSG Likelihood") + xlab("") + ggtitle("") + 
+    scale_x_log10(breaks = c(0.1, 1, 10, 100))  + ylab("TSG Likelihood") + xlab("") + ggtitle("") + 
     scale_fill_manual(values = c(singleOnco,singleBlue)) + 
     theme(panel.border = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank()) +
     theme(legend.position = "none")
-xdensity
 
-ydensity <- ggplot(genePanelCv, aes(wnon_cv+0.1, fill=factor(classification))) + 
-  geom_density(alpha=.5) + 
+ydensity <- ggplot(logisticVisualisation, aes(wnon_cv+0.1, fill=factor(classification))) + 
+  geom_density(alpha=.5, adjust = 1.4) + 
   stat_smooth(aes(y=continuousClassification), fill = singleBlue, method="glm", method.args=list(family="binomial"), se=T) +
   scale_y_continuous(labels = percent, limits = c(0, 1)) +
-  scale_x_log10()  + ylab("TSG Likelihood") + xlab("") + ggtitle("") + 
+  scale_x_log10(breaks = c(0.1, 1, 10, 100))  +ylab("TSG Likelihood") + xlab("") + ggtitle("") + 
   scale_fill_manual(values = c(singleOnco,singleBlue)) +
   theme(panel.border = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank()) +
   theme(legend.position = "none") + coord_flip()
-ydensity
 
 myLegend = g_legend(scatterPlot)
 scatterPlot = scatterPlot + theme(legend.position="none")
