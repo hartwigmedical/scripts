6d4f1de2fd98a53337731214a13f4fef04a4e2e2
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-07-26 16:08:34 +0200

    Updated queries used for CDM reports

--- sql/cdm_locked_report.sql
+++ sql/cdm_locked_report.sql
@@ -1,30 +1,25 @@
-#  LISC: Number of CPCT patients
-select patientId, count(sampleId) as countSamples from clinical where sampleId like '%CPCT%' group by patientId ;
-
-#  LISC: Registration date of patient
-select distinct patientId, registrationDate, informedConsentDate from clinical where patientId like '%CPCT%' group by sampleId;
+#  LISC: Baseline information
+select patientId, count(sampleId), registrationDate, informedConsentDate from clinical where patientId like '%CPCT%' and not(isnull(registrationDate)) group by 1,3,4;
 
 #  LISC: Timeline starts with informed consent?
-select * from clinicalFindings where
-message ='At least 1 biopsy taken before informed consent date' and patientId like '%CPCT%' and formLocked = 'true';
+select patientId, message, details from clinicalFindings where message ='At least 1 biopsy taken before informed consent date' and patientId like '%CPCT%' and formLocked = 'true'
 
-select * from clinicalFindings where message ='informed consent date empty or in wrong format' and patientId like '%CPCT%'  and formLocked = 'true';
+union select patientId, message, details from clinicalFindings where message ='informed consent date empty or in wrong format' and patientId like '%CPCT%'  and formLocked = 'true';
 
 #  LISC: Hospital is known?
-select * from clinicalFindings where message like '%Hospital could not be determined%' and patientId like '%CPCT%'  and formLocked = 'true';
+select patientId, message, details from clinicalFindings where message like '%Hospital could not be determined%' and patientId like '%CPCT%'  and formLocked = 'true';
 
 #  LISC: Gender is known?
-select * from clinicalFindings where message = 'Gender empty' and patientId like '%CPCT%'  and formLocked = 'true';
+select patientId, message, details from clinicalFindings where message = 'Gender empty' and patientId like '%CPCT%'  and formLocked = 'true';
 
 #  LISC: Birthyear is known?
-select * from clinicalFindings where
-message = 'birth year could not be determined' and patientId like '%CPCT%'  and formLocked = 'true';
+select patientId, message, details from clinicalFindings where message = 'birth year could not be determined' and patientId like '%CPCT%'  and formLocked = 'true';
 
 #  LISC: Can curate the cancer type?
-select * from clinicalFindings where message ='primary tumor location empty' and patientId like '%CPCT%'  and formLocked = 'true';
+select patientId, message, details from clinicalFindings where message ='primary tumor location empty' and patientId like '%CPCT%'  and formLocked = 'true';
 
 #  LISC: Systemic pre-therapy is known?
-select * from clinicalFindings where message = 'pre systemic treatment given empty' and patientId like '%CPCT%'  and formLocked = 'true';
+select patientId, message, details from clinicalFindings where message = 'pre systemic treatment given empty' and patientId like '%CPCT%'  and formLocked = 'true';
 
 #  LISC: Radiotherapy pre-therapy is known?
-select * from clinicalFindings where message = 'pre radio treatment given empty' and patientId like '%CPCT%'  and formLocked = 'true';
+select patientId, message, details from clinicalFindings where message = 'pre radio treatment given empty' and patientId like '%CPCT%'  and formLocked = 'true';
