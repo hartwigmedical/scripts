9c1292367b9bd544589af615e6b99b0d2c7b2afc
Daniel Cameron <daniel.l.cameron@gmail.com>
2018-07-27 00:57:31 +1000

    MIQCQP CN re-estimator now writing LP file

--- analysisscripts/consistentPloidy.R
+++ analysisscripts/consistentPloidy.R
@@ -64,13 +64,15 @@ induced_edge_gr = function (cnv_gr, ...) {
   induced_gr_left = GRanges(
     seqnames=seqnames(cnv_gr[queryHits(hits)]),
     ranges=IRanges(start=end(cnv_gr[queryHits(hits)]), width = 1),
-    strand="+")
+    strand="+",
+    id=paste0("ref", names(cnv_gr[queryHits(hits)])))
   names(induced_gr_left) = paste0("end_", names(cnv_gr[queryHits(hits)]))
   induced_gr_left$cnv_id = names(cnv_gr[queryHits(hits)])
   induced_gr_right = GRanges(
     seqnames=seqnames(cnv_gr[queryHits(hits)]),
     ranges=IRanges(start=end(cnv_gr[queryHits(hits)])+1, width = 1),
-    strand="-")
+    strand="-",
+    id=paste0("ref", names(cnv_gr[queryHits(hits)])))
   names(induced_gr_right) = paste0("start_", names(start_cnv_gr[subjectHits(hits)]))
   induced_gr_right$cnv_id = names(start_cnv_gr[subjectHits(hits)])
   induced_gr_left$partner = names(induced_gr_right)
@@ -153,29 +155,46 @@ cndf = data.frame(
   stringsAsFactors = FALSE)
 
 svdf = data.frame(
-  sv_id = names(sv_gr),
+  sv_id = as.character(sv_gr$id),
+  be_id = names(sv_gr),
+  be_id_partner = sv_gr$partner,
   orientation=as.character(strand(sv_gr)),
   seg_id = sv_gr$cnv_id,
-  partner = sv_gr$partner,
   fragment_count = sv_gr$tumourReferenceFragmentCount,
   stringsAsFactors = FALSE) %>%
   bind_rows(data.frame(
-    sv_id = names(induced_sv_gr),
+    sv_id = induced_sv_gr$id,
+    be_id = names(induced_sv_gr),
+    be_id_partner = induced_sv_gr$partner,
     orientation=as.character(strand(induced_sv_gr)),
     seg_id = induced_sv_gr$cnv_id,
-    partner = induced_sv_gr$partner,
     fragment_count = induced_sv_gr$fragment_count,
     stringsAsFactors = FALSE)
   )
-
+df_cross_product = function(df1, df2, ...) {
+  full_join(df1 %>% mutate(df_cross_product_placeholder=1), df2 %>% mutate(df_cross_product_placeholder=1), by="df_cross_product_placeholder", ...) %>%
+    dplyr::select(-df_cross_product_placeholder)
+}
 # Generate LP file
-writeLPmodel = function(file, cndf, subclones=2) {
+writeLPmodel = function(filename, cndf, svdf, subclones=2, organism_ploidy=1, includeNormal=FALSE) {
   model = list(
-    clones = c("normal", paste0("c", seq(subclones))),
-    alleles = c("J", "n"))
-  model$ascn = expand.grid(allele=paste0("_", model$alleles), subclone = paste0("_", model$clones)) %>%
-      mutate(ordinal = seq(nrow(.)))
-  model$q_ascn = df_cross_product(model$ascn, model$ascn, suffix=c("", "2"))
+    clones = paste0("c", seq(subclones)),
+    alleles = paste0("a", seq(organism_ploidy)))
+  if (includeNormal) {
+    model$clones = c("normal", model$clones)
+    stop("TODO: hard-code normal ploidy")
+  }
+  if (organism_ploidy != 1) {
+    stop("TODO: fix issue with major and minor allele not being able to swap across edges")
+    stop("TODO: restrict SV edges to require non-reference edges to contain only 1 allele.
+         The model can fairly safely assume a somatic SV occurs only on a single allele.")
+  }
+  model$ascn = expand.grid(
+      allele=paste0("_", model$alleles),
+      subclone = paste0("_", model$clones),
+      stringsAsFactors = FALSE) %>%
+    mutate(ordinal = seq(nrow(.)))
+  #model$q_ascn = df_cross_product(model$ascn, model$ascn, suffix=c("", "2"))
 
   # BAF must be minor allele frequency
   cndf = cndf %>% mutate(baf = pmin(baf, 1 - baf))
@@ -193,107 +212,153 @@ writeLPmodel = function(file, cndf, subclones=2) {
     bind_rows(cndf)
   svdf_with_null = bind_rows(svdf,
     cndf %>% dplyr::select(seg_id) %>% mutate(
-      partner=paste0("null_to_start", seg_id),
-      sv_id=paste0("start_to_null", seg_id),
+      sv_id=paste0("start_null", seg_id),
+      be_id=paste0("start_to_null", seg_id),
+      be_id_partner=paste0("null_to_start", seg_id),
       orientation="-",
-      fragment_count=NA),
+      fragment_count=0),
     cndf %>% dplyr::select(seg_id) %>% mutate(
-      partner=paste0("start_to_null", seg_id),
-      sv_id=paste0("null_to_start", seg_id),
+      sv_id=paste0("start_null", seg_id),
+      be_id=paste0("null_to_start", seg_id),
+      be_id_partner=paste0("start_to_null", seg_id),
       seg_id="NULL",
       orientation="*",
-      fragment_count=NA),
+      fragment_count=0),
     cndf %>% dplyr::select(seg_id) %>% mutate(
-      partner=paste0("null_to_end", seg_id),
-      sv_id=paste0("end_to_null", seg_id),
+      sv_id=paste0("end_null", seg_id),
+      be_id=paste0("end_to_null", seg_id),
+      be_id_partner=paste0("null_to_end", seg_id),
       orientation="+",
-      fragment_count=NA),
+      fragment_count=0),
     cndf %>% dplyr::select(seg_id) %>% mutate(
-      partner=paste0("end_to_null", seg_id),
-      sv_id=paste0("null_to_end", seg_id),
+      sv_id=paste0("end_null", seg_id),
+      be_id=paste0("null_to_end", seg_id),
+      be_id_partner=paste0("end_to_null", seg_id),
       seg_id="NULL",
       orientation="*",
-      fragment_count=NA))
+      fragment_count=0))
 
   # need normalisation multipliers between read depth and coverage
-  # and fragment counts and coverage
-  # that is, read counts to effective coverage
-  # for each segment:
-  #   normalisation * sum(purity * ploidy) = observed read depth
-  # and we have a circular definition since the normalisation factor
-  # actually depends on the solution estimate.
-  # need to do an initial estimate: assume ploidy = 2 then re-estimate
+  # this actually varies based on what the total CN estimate is
+  # in this iteration, we've added this as a model variable
+  total_base_coverage = cndf %>% summarise(total=sum(depth * length)) %>% pull(total)
+  max_cn = 128
+  max_ploidy = 128
 
-  # KISS!
-  "Minimise"
-  # segment read depth error
-  cndf %>% mutate(
-      var = paste0("rd_delta_seg", seg_id),
+  objective_segment_cn_abs_diff = cndf %>% mutate(
+      var = paste0("abs_cn_delta_seg_", seg_id),
       weight = sqrt(length)) %>%
     filter(!is.na(weight)) %>%
-    mutate(
-      eqn = paste(weight, var)
-    ) %>%
+    mutate(eqn = paste(weight, var)) %>%
     pull(eqn) %>%
     paste0(collapse = " + ")
-  # segment BAF error
-  # edge fragment count error
-  "Subject To"
-  # sum ploidy = 1
-  paste0("total_ploidy: ", model$ascn %>%
-    mutate(var=paste0("ploidy", subclone, allele)) %>%
+  objective_segment_cn_least_squares = cndf %>% mutate(
+    var = paste0("cn_delta_seg_", seg_id),
+    weight = sqrt(length)) %>%
+    filter(!is.na(weight)) %>%
+    mutate(eqn = paste0(weight, " ", var, " ^ 2")) %>%
+    pull(eqn) %>%
+    paste0(collapse = " + ")
+  objective_segment_baf_least_squares = "TODO"
+  objective_edge_fragment_count_least_squares = "TODO"
+
+  constraint_total_purity = paste0("total_purity: ", model$ascn %>%
+    mutate(var=paste0("purity", subclone, allele)) %>%
     pull(var) %>%
     paste0(collapse = " + ") %>%
     paste(" - 1 = 0", collapse = ""))
+  constraint_subclone_ordering = data.frame(subclone=model$clones) %>%
+    filter(subclone != "normal") %>%
+    mutate(next_subclone = lead(subclone)) %>%
+    filter(!is.na(next_subclone)) %>%
+    mutate(eqn = paste0("subclone_purity_ordering_", row_number(), ": purity_", subclone, " - purity_", next_subclone, " > 0")) %>%
+    pull(eqn)
+  constraint_segment_subclone_allele_ordering = cndf %>%
+    df_cross_product(model$ascn) %>%
+    group_by(seg_id, subclone) %>%
+    arrange(ordinal) %>%
+    mutate(next_allele = lead(allele)) %>%
+    filter(!is.na(next_allele)) %>%
+    ungroup() %>%
+    mutate(eqn = paste0("ploidy_seg", seg_id, subclone, allele, " - ", "ploidy_seg", seg_id, subclone, next_allele, " >= 0")) %>%
+    pull(eqn)
+  constraint_sv_subclone_allele_ordering = svdf_with_null %>%
+    df_cross_product(model$ascn) %>%
+    group_by(sv_id, subclone) %>%
+    mutate(next_allele = lead(allele)) %>%
+    arrange(ordinal) %>%
+    filter(!is.na(next_allele)) %>%
+    ungroup() %>%
+    mutate(eqn = paste0("ploidy_sv", sv_id, subclone, allele, " - ", "ploidy_sv", sv_id, subclone, next_allele, " >= 0")) %>%
+    pull(eqn)
+
   # total cn = sum purity * ploidy
-  cndf %>% mutate(
-      eqn = model$ascn %>%
-        mutate(
-          var1=paste0("purity", subclone),
-          var2=paste0("ploidy_seg", "[SEG_ID]", subclone, allele)) %>%
-        mutate(
-          eqn=paste0(var1, " * ", var2)
-        ) %>%
-        pull(eqn) %>%
-        paste0(collapse = " + ") %>%
-        paste0(" - cn_[SEG_ID] = 0", collapse = "") %>%
-        str_replace_all(stringr::fixed("[SEG_ID]"), seg_id)) %>%
+  constraint_definition_segment_cn_equals_total_purity_ploidy = cndf %>% mutate(
+    eqn = model$ascn %>%
+      mutate(
+        var1=paste0("purity", subclone),
+        var2=paste0("ploidy_seg", "[SEG_ID]", subclone, allele)) %>%
+      mutate(
+        eqn=paste0(var1, " * ", var2)
+      ) %>%
+      pull(eqn) %>%
+      paste0(collapse = " + ") %>%
+      paste0(" - cn_seg_[SEG_ID] = 0", collapse = "") %>%
+      str_replace_all(stringr::fixed("[SEG_ID]"), seg_id)) %>%
     mutate(eqn=paste0("copy_number_seg", seg_id, ": ", eqn)) %>%
     pull(eqn)
   # sum edge ASCN in = segment ASCN (if start != telemore/centromere)
-  cndf %>% filter(!(start %in% c("TELOMERE", "NULL"))) %>%
+  constraint_incoming_edge_ascn_matches_segment_ascn = cndf %>%
+    filter(!(start %in% c("TELOMERE", "NULL"))) %>%
     left_join(svdf_with_null %>% filter(orientation=="-"), by=c("seg_id"), suffix=c("", ".sv")) %>%
     df_cross_product(model$ascn) %>%
-    mutate(eqn = paste("ploidy_sv", sv_id, subclone, allele, sep="")) %>%
+    mutate(eqn = paste0("ploidy_sv", sv_id, subclone, allele)) %>%
     group_by(seg_id, subclone, allele) %>%
     summarise(eqn = paste0(eqn, collapse=" + ")) %>%
     ungroup() %>%
-    mutate(eqn = paste0(eqn, " - ", "ploidy_seg", seg_id, subclone, allele, " = 0"))
+    mutate(eqn = paste0(eqn, " - ", "ploidy_seg", seg_id, subclone, allele, " = 0")) %>%
+    pull(eqn)
   # sum edge ASCN out = segment ASCN (if start != telemore/centromere)
-  cndf %>% filter(!(start %in% c("TELOMERE", "NULL"))) %>%
+  constraint_outgoing_edge_ascn_matches_segment_ascn = cndf %>%
+    filter(!(start %in% c("TELOMERE", "NULL"))) %>%
     left_join(svdf_with_null %>% filter(orientation=="+"), by=c("seg_id"), suffix=c("", ".sv")) %>%
     df_cross_product(model$ascn) %>%
     mutate(eqn = paste("ploidy_sv", sv_id, subclone, allele, sep="")) %>%
     group_by(seg_id, subclone, allele) %>%
     summarise(eqn = paste0(eqn, collapse=" + ")) %>%
     ungroup() %>%
-    mutate(eqn = paste0(eqn, " - ", "ploidy_seg", seg_id, subclone, allele, " = 0"))
-  #TODO: SV_ID must be common for both breakends (need a second id or can we get away with 1 and no partner?)
+    mutate(eqn = paste0(eqn, " - ", "ploidy_seg", seg_id, subclone, allele, " = 0")) %>%
+    pull(eqn)
+    # NULL ASCNs are a special case since they don't have a direction
+    # Extension: we could constrain the NULL copy number based on how confident we are that we have all/most SVs
+  constraint_definition_total_segment_cn = cndf %>%
+    mutate(eqn = paste0(length, " cn_seg_", seg_id)) %>%
+    summarise(eqn = paste0(eqn, collapse = " + ")) %>%
+    pull(eqn) %>%
+    paste0(" - total_segment_cn = 0")
+    # read depth delta definition
+  # estimated cn - normalised observed cn = cn delta
+  constraint_definition_cn_delta = cndf %>%
+    mutate(eqn = paste0("cn_seg_", seg_id, " - ", depth / total_base_coverage,  " total_segment_cn - cn_delta_seg_", seg_id, " = 0")) %>%
+    pull(eqn)
+  bounds_max_segment_cn = cndf %>% mutate(eqn=paste0("cn_seg_", seg_id, " <= ", max_cn)) %>%
+    pull(eqn)
+  bounds_max_ploidy_segment = cndf %>% df_cross_product(model$ascn) %>%
+    mutate(eqn=paste0("ploidy_seg", seg_id, subclone, allele, " <= ", max_ploidy)) %>%
+    pull(eqn)
+  bounds_max_ploidy_sv = svdf_with_null %>% df_cross_product(model$ascn) %>%
+    mutate(eqn=paste0("ploidy_sv", sv_id, subclone, allele, " <= ", max_ploidy)) %>%
+    pull(eqn)
+  # purity <= 1 bounds unncessary as they are covered by constraint_total_purity and the implicit >= 0
+  var_int_ploidy_segment = cndf_with_null %>% df_cross_product(model$ascn) %>%
+    mutate(var=paste0("ploidy_seg", seg_id, subclone, allele)) %>%
+    pull(var) %>%
+    paste0(collapse = " ")
+  var_int_ploidy_sv = svdf_with_null %>% df_cross_product(model$ascn) %>%
+    mutate(var=paste0("ploidy_sv", sv_id, subclone, allele)) %>%
+    pull(var) %>%
+    paste0(collapse = " ")
 
-  # NULL ASCNs are a special case since they don't have a direction
-  # we could constrain the NULL copy number based on how confident we are that we have all/most SVs
-
-  # TODO: since we can get centromeric swaps, we can instead constrain
-  cndf %>% mutate(
-    eqn =
-  )
-
-  # for each segment
-
-  #   sum edge ASCN out = ASCN (if end != telemore/centromere)
-
-  "Bounds"
-  # ploidies <= 1
   # Variables:
   # total cn depth
   # total edge depth
@@ -303,56 +368,37 @@ writeLPmodel = function(file, cndf, subclones=2) {
   # Variables
   # ploidy
 
-  "Integers"
-  # segment total CN
-  # segment ASCN
-  # edge ASCN
+  "General Constraints"
+  general_constraint_definition_abs_cn_delta = cndf %>%
+    mutate(eqn = paste0("abs_cn_delta_seg_", seg_id, " = ABS ( cn_delta_seg_", seg_id, " )")) %>%
+    pull(eqn)
 
-  # General Constraints
-
-  # relative weightings
-  cndf = cndf %>%
-    mutate(
-      read_depth_weight = sqrt(length),
-      baf_weight = baf_count)
-
-  "Minimize"
-   cndf %>% mutate(
-    objname = paste0("OBJ_rd_", name),
-    weight = 1)
-
-   # ploidies sum to 1
-   paste(paste(paste0(c("normal", model$clones), sep="_ploidy"), collapse=" + "), "= 1")
-}
-df_cross_product = function(df1, df2, ...) {
-  full_join(df1 %>% mutate(df_cross_product_placeholder=1), df2 %>% mutate(df_cross_product_placeholder=1), by="df_cross_product_placeholder", ...) %>%
-    dplyr::select(-df_cross_product_placeholder)
-}
-segment_read_depth_objective_function = function(model, cndf, ascn) {
-  # (depth - depth_hat) ^ 2
-  # = depth ^ 2 - 2 * depth_hat * depth + (constant we can ignore)
-  # depth = sum over i in subclones ploidy(i) * (depth(i, major) + depth(i, minor))
-  # + normal_ploidy) * 2
-  quartic_terms = cndf %>% top_n(1) %>%
-    mutate(weight = read_depth_weight) %>%
-    df_cross_product(model$q_ascn) %>%
-    mutate(
-      p1=paste0("ploidy", subclone),
-      p2=paste0("ploidy", subclone2),
-      cn1=paste0(name, subclone, allele),
-      cn2=paste0(name, subclone2, allele2)) %>%
-    dplyr::select(weight, p1, p2, cn1, cn2)
-
-
-  quadradic_terms = cndf %>% mutate(weight = - 2 * read_depth_weight * depth) %>%
-    df_cross_product(model$ascn) %>%
-    mutate(
-      ploidy=paste0("ploidy", subclone),
-      cn=paste0(name, subclone, allele)) %>%
-    dplyr::select(weight, ploidy, allele)
+  fileConn = file(filename)
+  writeLines(c(
+    "MINIMISE",
+    paste0("[ ", objective_segment_cn_least_squares, " ]"),
+    "SUBJECT TO",
+    constraint_total_purity,
+    constraint_subclone_ordering,
+    constraint_segment_subclone_allele_ordering,
+    constraint_sv_subclone_allele_ordering,
+    constraint_definition_segment_cn_equals_total_purity_ploidy,
+    constraint_incoming_edge_ascn_matches_segment_ascn,
+    constraint_outgoing_edge_ascn_matches_segment_ascn,
+    constraint_definition_total_segment_cn,
+    constraint_definition_cn_delta,
+    "Bounds",
+    bounds_max_segment_cn,
+    bounds_max_ploidy_segment,
+    bounds_max_ploidy_sv,
+    "General",
+    var_int_ploidy_segment,
+    var_int_ploidy_sv,
+    "END"),
+    con=fileConn)
+  close(fileConn)
 }
 
-fileConn = file(paste0(currentSampleId, ".model.lp"))
-writeLines(writeLPmodel(cndf, svdf), fileConn)
-close(fileConn)
+writeLPmodel(paste0(currentSampleId, ".model.lp"), cndf, svdf)
+
 
