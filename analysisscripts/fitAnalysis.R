d12a9a1005c88346b9233f19041c6fefd8db26d6
Jonathan Baber <jonbaber@gmail.com>
2018-08-09 09:46:42 +1000

    inconsistent somatics summary

--- analysisscripts/fitAnalysis.R
+++ analysisscripts/fitAnalysis.R
@@ -6,10 +6,14 @@ library(tidyr)
 library(cowplot)
 theme_set(theme_bw())
 
+#### SAVE COHORT
+#save(cohortToRun, cohort, file = "~/hmf/analysis/fit/cohort.RData")
+#load(file = "~/hmf/analysis/fit/cohort.RData")
+
+
 load(file = "~/hmf/RData/Reference/cancerTypeColours.RData")
 chromosomeColours = c(cancerTypeColours, "black")
 chromosomeColours = setNames(chromosomeColours, c(1:22))
-
 
 query_regions <- function(dbConnect, sampleId) {
   query = paste0("select  * from copyNumber where sampleId ='", sampleId, "'");
@@ -17,7 +21,7 @@ query_regions <- function(dbConnect, sampleId) {
 }
 
 query_somatics <- function(dbConnect, sampleId) {
-  query = paste0("select  * from somaticVariant where sampleId ='", sampleId, "'");
+  query = paste0("select  * from somaticVariant where sampleId ='", sampleId, "' and filter = 'PASS'");
   return (dbGetQuery(dbConnect, query))
 }
 
@@ -55,26 +59,48 @@ fitted_region_graph <- function(title, copyNumberRegions) {
   return(p)
 }
 
-somatic_graph <- function(somatics) {
-  
-  inconsistentColours = setNames(c("#6baed6", "#cb181d"), c(F,T))
-  inconsistentAlphas = setNames(c(0.4, 1), c(F,T))
-  
+somatic_enrichment <- function(purity, somatics) {
+  somatics$purity <- purity
   somatics = somatics %>%
-    filter(!chromosome %in% c('X','Y')) %>%
+    filter(!chromosome %in% c('X','Y'), filter == 'PASS') %>%
     mutate(
       majorAllelePloidy = adjustedCopyNumber - minorAllelePloidy,
       somaticPloidy = adjustedVaf * adjustedCopyNumber,
-      distanceFromMajorAllele = somaticPloidy - majorAllelePloidy,
-      distanceFromMinorAllele = somaticPloidy - minorAllelePloidy,
-      distanceFromOne = somaticPloidy - 1,
-      distanceFromAllele = ifelse(abs(distanceFromMajorAllele) < abs(distanceFromMinorAllele), distanceFromMajorAllele, distanceFromMinorAllele),
-      distanceFromAllele = ifelse(abs(distanceFromOne) < abs(distanceFromMajorAllele), distanceFromOne, distanceFromAllele),
       distanceFromWholePloidy = somaticPloidy - round(somaticPloidy),
-      inconsistent = somaticPloidy > majorAllelePloidy,
+      majorAlleleProbability = pmax(majorAllelePloidy, 0) *  purity / (adjustedCopyNumber * purity + 2 * (1 - purity)),
+      maxConceivableAlleleCount = qbinom(0.999, 100000, totalReadCount * majorAlleleProbability / 100000, T),
+      inconsistent = alleleReadCount > maxConceivableAlleleCount,
       CN = round(adjustedCopyNumber),
       CN = ifelse(CN > 5, "6+", CN),
       CN = factor(CN, levels = c("6+", rev(-1:5)), ordered = T))
+  
+  return (somatics)
+}
+
+somatic_summary <- function(somatics) {
+  
+  emptySomaticSummary = data.frame(sampleId = "", consistentSomatics = 0, inconsistentSomatics = 0, stringsAsFactors = F)
+  
+  somatics = somatics %>% 
+    mutate(inconsistent = ifelse(inconsistent, "inconsistentSomatics", "consistentSomatics")) %>% 
+    group_by(sampleId, inconsistent) %>% 
+    count() %>% 
+    spread(inconsistent, n) 
+  
+  result = bind_rows(somatics, emptySomaticSummary)
+  result[is.na(result)] <- 0
+  result = result %>%
+    ungroup() %>%
+    summarise(sampleId = first(sampleId), consistentSomatics = sum(consistentSomatics), inconsistentSomatics = sum(inconsistentSomatics)) %>%
+    mutate(inconsistentSomaticPercentage = round(inconsistentSomatics / (inconsistentSomatics + consistentSomatics), 2))
+  
+  return (result)
+}
+
+somatic_graph <- function(somatics) {
+
+  inconsistentColours = setNames(c("#6baed6", "#cb181d"), c(F,T))
+  inconsistentAlphas = setNames(c(0.4, 1), c(F,T))
   
   p1 = ggplot(somatics) +
     geom_histogram(aes(x = somaticPloidy, fill = CN), binwidth = 0.1, position = "stack") +
@@ -91,19 +117,21 @@ somatic_graph <- function(somatics) {
     xlim(0, 4) + xlab("Ploidy") + ggtitle("Inconsistent Somatics") + ylab("") +
     theme(legend.position = "none", axis.text.y = element_blank()) +
     theme(panel.border = element_blank(), panel.grid.minor.y = element_blank(), axis.ticks = element_blank())
-   
+  
   p3 = plot_grid(p1, p2, nrow = 1, rel_widths = c(1, 1))
   return (p3)
 }
 
 copynumber_graph <- function(copyNumberRegions) {
   
+  distance = copyNumberRegions %>% summarise(distance = sum(as.numeric(end) - as.numeric(start))) %>% pull(distance)
   totalBafCount = sum(copyNumberRegions$bafCount)
   
   copyNumberRegions = copyNumberRegions %>%
     filter(!chromosome %in% c('X','Y'), copyNumber < 10) %>%
     mutate(
       chromosome = factor(chromosome, levels= c(1:22), ordered = T),
+      # weight = (end - start)/distance ) 
       weight = bafCount/totalBafCount ) 
   
   ggplot(copyNumberRegions) +
@@ -124,77 +152,67 @@ env_graph <- function(title, copyNumberRegions, somatics) {
 ### PRODUCTION COHORT
 dbProd = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
 prodCohort = dbGetQuery(dbProd, "SELECT sampleId, purity, ploidy, status, qcStatus FROM purity")
-dbDisconnect(dbProd)
-rm(dbProd)
+dbDisconnect(dbProd); rm(dbProd)
 
 ### PILOT COHORT
 dbPilot = dbConnect(MySQL(), dbname='hmfpatients_pilot', groups="RAnalysis")
 pilotCohort = dbGetQuery(dbPilot, "SELECT sampleId, purity, ploidy, status, qcStatus FROM purity WHERE version = 2.15 and modified > '2018-08-04'")
-dbDisconnect(dbPilot)
-rm(dbPilot)
+dbDisconnect(dbPilot); rm(dbPilot)
 
 #### COMPARE COHORTS
 cohort = left_join(pilotCohort, prodCohort, by = "sampleId", suffix = c(".pilot", ".prod")) %>%
   mutate(
-  changeInPurity = abs(purity.prod - purity.pilot),
-  changeInPloidy = abs(ploidy.prod - ploidy.pilot)) %>%
+    changeInPurity = abs(purity.prod - purity.pilot),
+    changeInPloidy = abs(ploidy.prod - ploidy.pilot)) %>%
   arrange(-changeInPloidy)
-
 
 dbProd = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
 dbPilot = dbConnect(MySQL(), dbname='hmfpatients_pilot', groups="RAnalysis")
-
 cohortToRun = cohort %>% top_n(100, changeInPloidy) %>% mutate(patientId = substr(sampleId, 1, 12)) #%>% pull(patientId)
+
 sample = "CPCT02050047T"
+sample = "CPCT02060129T"
+sample = "CPCT02030352T"
+
+# Initialistaion
+enrichedPilotSomaticsSummary = data.frame()
+enrichedProdSomaticsSummary = data.frame()
 
 for (sample in cohortToRun$sampleId) {
   sampleDetails = cohort %>% filter(sampleId == sample)
   
+  #Collect Data
   prodSomatics = query_somatics(dbProd, sample)
   prodRegions = query_regions(dbProd, sample)
   pilotSomatics = query_somatics(dbPilot, sample)
   pilotRegions = query_regions(dbPilot, sample)
   
-  prod_graphs = env_graph(create_title("Production", sampleDetails$purity.prod, sampleDetails$ploidy.prod, sampleDetails$qcStatus.prod), prodRegions, prodSomatics)
-  pilot_graphs = env_graph(create_title("Pilot", sampleDetails$purity.pilot, sampleDetails$ploidy.pilot, sampleDetails$qcStatus.pilot), pilotRegions, pilotSomatics)
-  complete = plot_grid(prod_graphs, pilot_graphs, ncol = 1)
-  complete
+  # Enrich
+  enrichedProdSomatics = somatic_enrichment(sampleDetails$purity.prod, prodSomatics)
+  enrichedPilotSomatics = somatic_enrichment(sampleDetails$purity.pilot, pilotSomatics)
   
-  save_plot(paste0("/Users/jon/hmf/analysis/fit/", sample, ".png"), complete, base_height = 10, base_width = 20)
+  # Somatic Summary
+  enrichedProdSomaticsSummary = bind_rows(somatic_summary(enrichedProdSomatics), enrichedProdSomaticsSummary)
+  enrichedPilotSomaticsSummary = bind_rows(somatic_summary(enrichedPilotSomatics), enrichedPilotSomaticsSummary)
+
+  # GRAPHS
+  #prod_graphs = env_graph(create_title("Production", sampleDetails$purity.prod, sampleDetails$ploidy.prod, sampleDetails$qcStatus.prod), prodRegions, enrichedProdSomatics)
+  #pilot_graphs = env_graph(create_title("Pilot", sampleDetails$purity.pilot, sampleDetails$ploidy.pilot, sampleDetails$qcStatus.pilot), pilotRegions, enrichedPilotSomatics)
+  #complete = plot_grid(prod_graphs, pilot_graphs, ncol = 1)
+  #save_plot(paste0("/Users/jon/hmf/analysis/fit/", sample, ".png"), complete, base_height = 10, base_width = 20)
 }
 
-
-
-dbDisconnect(dbProd);rm(dbProd)
+dbDisconnect(dbProd); rm(dbProd)
 dbDisconnect(dbPilot); rm(dbPilot)
 
+#View(enrichedProdSomaticsSummary)
+#View(enrichedPilotSomaticsSummary)
+
+#inconsistentSomaticSummary = left_join(enrichedProdSomaticsSummary, enrichedPilotSomaticsSummary, by = "sampleId", suffix = c(".prod",".pilot"))
+#save(inconsistentSomaticSummary, file = "~/hmf/analysis/fit/inconsistentSomaticSummary.RData")
 
 
 
 
 
 
-
-
-####### TESTING
-
-
-
-
-
-copyNumberRegions = prodRegions
-jon = somatics 
-
-
-library(plotly)
-p <- plot_ly(z = ~simpleCopyNumberRegions) %>% add_surface()
-p
-
-jon = simpleCopyNumberRegions %>% filter(majorAllele < 5, minorAllele < 3)
-
-kd <- with(jon, MASS::kde2d (majorAllele, minorAllele, n = 100))
-p <- plot_ly(x = kd$x, y = kd$y, z = kd$z) %>% add_surface()
-p
-
-
-jon =  simpleCopyNumberRegions %>% filter(minorAllele > 1.5,minorAllele < 1.9, majorAllele < 2.5, chromosome == 6 )
