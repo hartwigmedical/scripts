b1df53f2edbc0a24f3f4841cfae49b53df2ee489
Jonathan Baber <jonbaber@gmail.com>
2018-09-04 15:09:03 +1000

    improved somatic modelling

--- analysisscripts/modelSomatics.R
+++ analysisscripts/modelSomatics.R
@@ -30,10 +30,6 @@ estimate_somatic_peak <- function(somatics, peakBinwidth, width) {
   return (as.numeric(peak_x))
 }
 
-save(peak, binwidth, peakSomatics, file = "~/hmf/analysis/somatics/CPCT02010646T.RData")
-
-#somatics = initialSomatics %>% mutate(weight = 1)
-#peak = 0.99
 model_peak <- function(peak, somatics, binwidth) {
   peakOffset = round(peak - round(peak / binwidth) * binwidth, 2)
   
@@ -45,18 +41,28 @@ model_peak <- function(peak, somatics, binwidth) {
   domain = seq(peakOffset, 10 + peakOffset, binwidth)
   for (ploidy in domain) {
     peakSomatics = peakSomatics %>% mutate(
-      lowerBoundAlleleReadCount = floor(pmax(0, ploidy - binwidth/2) / somaticPloidy * alleleReadCount),
-      upperBoundAlleleReadCount = floor((ploidy + binwidth/2) / somaticPloidy * alleleReadCount),
-      peakSliceLikelihood = pbinom(upperBoundAlleleReadCount, totalReadCount, alleleReadCount/totalReadCount) - pbinom(lowerBoundAlleleReadCount, totalReadCount, alleleReadCount/totalReadCount)
+      lowerBoundAlleleReadCount = pmax(0, ploidy - binwidth/2.0) / somaticPloidy * alleleReadCount,
+      lowerBoundAlleleReadCountRounded = round(lowerBoundAlleleReadCount, 0),
+      lowerBoundAddition = lowerBoundAlleleReadCountRounded + 0.5 - lowerBoundAlleleReadCount,
+      
+      upperBoundAlleleReadCount = pmax(0, ploidy + binwidth/2.0) / somaticPloidy * alleleReadCount,
+      upperBoundAlleleReadCountRounded = round(upperBoundAlleleReadCount, 0),
+      upperBoundSubtraction = upperBoundAlleleReadCountRounded + 0.5 - upperBoundAlleleReadCount,
+      
+      peakSliceLikelihood = 
+        pbinom(upperBoundAlleleReadCountRounded, totalReadCount, alleleReadCount/totalReadCount) - 
+        pbinom(lowerBoundAlleleReadCountRounded, totalReadCount, alleleReadCount/totalReadCount) +
+        lowerBoundAddition * dbinom(lowerBoundAlleleReadCountRounded, totalReadCount, alleleReadCount/totalReadCount) -
+        upperBoundSubtraction * dbinom(upperBoundAlleleReadCountRounded, totalReadCount, alleleReadCount/totalReadCount)
     )
     peakSomatics[, paste(ploidy)] <-  round(peakSomatics$peakSliceLikelihood, 2)
   }
- 
+
   peakBucket = peakSomatics[[paste(peak)]]
-  peakSomatics = peakSomatics[, c(-1:-7)]
+  peakSomatics = peakSomatics[, c(-1:-11)]
   normalisePeakSomatics = peakSomaticsWeight * peakSomatics / peakBucket 
   
-  peakDistribution = col62Sums(normalisePeakSomatics)
+  peakDistribution = colSums(normalisePeakSomatics)
   peakDistribution = data.frame(bucket = (names(peakDistribution)), count = round(peakDistribution,2), stringsAsFactors = F)
   peakDistributionNormalised = peakDistribution %>% mutate(bucket = round(as.numeric(bucket) - peakOffset, 3))
   
@@ -94,7 +100,7 @@ remove_peak_from_somatics <- function(peakModel, somatics) {
 
 #minAverageWeight = 0.15; minClonalCutoff = 0.9;
 #enrichedSomatics = filteredSomatics
-model_somatics <- function(enrichedSomatics, binwidth, minAverageWeight = 0.4, minClonalCutoff = 0.9) {
+model_somatics <- function(enrichedSomatics, binwidth, minAverageWeight = 0.4, minClonalCutoff = 0.85, estimateWidth) {
   peaks = list()
   sample = first(enrichedSomatics$sampleId)
 
@@ -114,7 +120,7 @@ model_somatics <- function(enrichedSomatics, binwidth, minAverageWeight = 0.4, m
   while (unexplained > 0.01 & i < 10 & any(somatics$weight > 0)) {
   #for (i in c(1:3)) {
       
-    peakPloidy = estimate_somatic_peak(somatics, 0.01, 15)
+    peakPloidy = estimate_somatic_peak(somatics, 0.01, estimateWidth)
     peakModel = model_peak(peakPloidy, somatics, binwidth)
 
     somatics = remove_peak_from_somatics(peakModel, somatics)
@@ -159,11 +165,21 @@ somatic_enrichment <- function(somatics) {
   return (somatics)
 }
 
-subclonal_regions <- function(peakDistribution) {
-  peakClonality = peakDistribution %>% 
-    mutate(clonality = ifelse(subclonal, "subclonal", "clonal")) %>%
+subclonal_regions <- function(samplePeakDistribution, subclonalCutoff = 0.85) {
+  peakClonality = samplePeakDistribution %>% 
+    mutate(clonality = ifelse(peak < subclonalCutoff, "subclonal", "clonal")) %>%
     group_by(sampleId, bucket, clonality) %>% 
-    summarise(count = sum(count)) %>% spread(clonality, count, fill = 0) %>%
+    summarise(count = sum(count)) %>% spread(clonality, count, fill = 0)
+  
+  if (!"subclonal" %in% names(peakClonality)) {
+    peakClonality$subclonal <- 0
+  }
+  
+  if (!"clonal" %in% names(peakClonality)) {
+    peakClonality$clonal <- 0
+  }
+  
+  peakClonality = peakClonality %>%
     mutate(clonal = round(clonal, 2), subclonal = round(subclonal, 2)) %>%
     mutate(
       total = clonal + subclonal, 
@@ -186,11 +202,14 @@ annotate_somatics_with_clonality <- function(enrichedSomatics, subclonalRegions)
   return (somaticsWithBuckets)
 }
 
-subclonalRegions = subclonal_regions(samplePeakDistribution)
-str(subclonalRegions)
+
+
+
+
+#subclonalRegions = subclonal_regions(samplePeakDistribution)
+#str(subclonalRegions)
 #initialSomatics = somatic_enrichment(somatics)
 #model_somatics(enrichedSomatics, binwidth)
-
 
 
 ### TEST DATA
@@ -208,182 +227,95 @@ theme_set(theme_bw())
 
 ##### FROM RDATA FILES
 #load(file = "~/hmf/RData/reference/allSomatics_p1.RData")
-load(file = "~/hmf/RData/reference/allSomatics_p2.RData")
+#load(file = "~/hmf/RData/reference/allSomatics_p2.RData")
 samples = unique(allSomatics_p2$sampleId)
 peakDistributions = data.frame()
+modifiedSomatics = data.frame()
+
 binwidth = 0.05
-
-subclonalFill = c("red", "black")
-subclonalFill = setNames(subclonalFill, c("TRUE", "FALSE"))
-
+estimateWidth = 10
+i = 0
 for (sample in samples) {
-  sample = "CPCT02010646T"
-  cat("Processing:", sample, "\n")
-  somatics = allSomatics_p2 %>% filter(sampleId == sample) %>% mutate(somaticPloidy = pmax(0, adjustedVaf * adjustedCopyNumber))
+  #sample = samples[3:3]
+  #sample = "CPCT02010618T"
+  
+  i = i + 1
+  cat("Processing:", sample, " (",i,")", "\n")
+  somatics = allSomatics_p2 %>% filter(sampleId == sample) %>% mutate(somaticPloidy = pmax(0, adjustedVaf * adjustedCopyNumber)) %>% 
+    select(-clonalLikelihood, -subclonalLikelihood)
   filteredSomatics = somatics %>% filter(!chromosome %in% c('X','Y'))
   
-  samplePeakDistribution = model_somatics(filteredSomatics,binwidth)
-  samplePeakDistributionTotal = samplePeakDistribution %>% group_by(bucket) %>% summarise(count = sum(count))
-  #sum(samplePeakDistributionTotal$count)
-  #nrow(initialSomatics %>% filter(somaticPloidy > 0, somaticPloidy < 10))
-  
-  p = ggplot() +
-    geom_histogram(data=filteredSomatics, aes(x = somaticPloidy), binwidth = binwidth, alpha = 0.8) +
-    geom_line(data=samplePeakDistributionTotal, aes(x = bucket, y = count), position = "identity", alpha = 0.8) +
-    geom_line(data=samplePeakDistribution, aes(x = bucket, y = count, color = peak), position = "identity") +
-    geom_area(data=samplePeakDistribution %>% filter(subclonal), aes(x = bucket, y = count, color = peak, fill = subclonal), position = "identity",  alpha = 0.3) +
-    ggtitle(paste0(sample, ", binwidth:", binwidth)) + xlab("Ploidy") + ylab("Count") +
-    theme(panel.border = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank()) +
-    xlim(0, 4)
-  
-p
-  #subclonalRegions = subclonal_regions(samplePeakDistribution)
-  #annotatedSomatics = annotate_somatics_with_clonality(somatics, subclonalRegions)
-  #sum(annotatedSomatics$clonalLikelihood) + sum(annotatedSomatics$subclonalLikelihood)
-  
-    save_plot(paste0("/Users/jon/hmf/analysis/somatics/", sample, ".png"), p, base_height = 6, base_width = 8)  
-  
+  samplePeakDistribution = model_somatics(filteredSomatics,binwidth, estimateWidth = estimateWidth)
   peakDistributions = bind_rows(samplePeakDistribution, peakDistributions)
+  
+  #samplePeakDistributionTotal = samplePeakDistribution %>% group_by(bucket) %>% summarise(count = sum(count))
+  #p = ggplot() +
+  #  geom_histogram(data=filteredSomatics, aes(x = somaticPloidy), binwidth = binwidth, alpha = 0.8) +
+  #  geom_line(data=samplePeakDistributionTotal, aes(x = bucket, y = count), position = "identity", alpha = 0.8) +
+  #  geom_line(data=samplePeakDistribution, aes(x = bucket, y = count, color = peak), position = "identity") +
+  #  geom_area(data=samplePeakDistribution %>% filter(subclonal), aes(x = bucket, y = count, color = peak, fill = subclonal), position = "identity",  alpha = 0.3) +
+  #  ggtitle(paste0(sample, ", binwidth:", binwidth, ", estimate width: ", estimateWidth)) + xlab("Ploidy") + ylab("Count") +
+  #  theme(panel.border = element_blank(), panel.grid.minor = element_blank(), axis.ticks = element_blank()) +
+  #  xlim(0, 4)
+  #save_plot(paste0("/Users/jon/hmf/analysis/somatics/", sample, ".png"), p, base_height = 6, base_width = 8)  
+  
+  subclonalRegions = subclonal_regions(samplePeakDistribution)
+  annotatedSomatics = annotate_somatics_with_clonality(somatics, subclonalRegions) %>% select(-bucket, -somaticPloidy) 
+  modifiedSomatics = bind_rows(annotatedSomatics, modifiedSomatics)
 }
 
-rm(allSomatics_p1)
+rm(allSomatics_p2)
+allSomatics_p2 = modifiedSomatics
+save(allSomatics_p2, file = "/Users/jon/hmf/analysis/somatics/RData/allSomatics_p2.RData")
 
 peakDistributions_p2 = peakDistributions
-save(peakDistributions_p2, file = "/Users/jon/hmf/analysis/somatics/peakDistributions_p2.RData")
+save(peakDistributions_p2, file = "/Users/jon/hmf/analysis/somatics/RData/peakDistributions_p2.RData")
+
+head(allSomatics_p1)
+
+### FIX UP ODL
+rm(list=setdiff(ls(), "allSomatics_p1"))
+load(file = "/Users/jon/hmf/analysis/somatics/RData/modifiedSomatics_p2.RData")
+allSomatics_p2 = modifiedSomatics
+rm(modifiedSomatics)
+save(allSomatics_p2, file = "/Users/jon/hmf/analysis/somatics/RData/allSomatics_p2.RData")
 
 
-save(somatics, file = "/Users/jon/hmf/analysis/somatics/CPCT02020689T.RData")
-
-#### FROM DATABASE
-binwidth = 0.1
-dbPaper = dbConnect(MySQL(), dbname='hmfpatients_20180418', groups="RAnalysis")
-cohort = dbGetQuery(dbPaper, "SELECT * FROM purity where qcStatus = 'PASS'")
-distributions = data.frame()
-for (sample in cohort[1:1, "sampleId"]) {
-  sample = "CPCT02010615T"
-  cat("Processing:", sample, "\n")
-  initialSomatics = (dbGetQuery(dbPaper, paste0("SELECT * FROM somaticVariant where filter = 'PASS' and sampleId = '", sample, "'")))
-  enrichedSomatics = somatic_enrichment(initialSomatics)
-  
-  distributions = bind_rows(model_somatics(enrichedSomatics, binwidth), distributions)
-}
-
-sum(distributions$count)
-
-dbDisconnect(dbPaper)
-rm(dbPaper)
+##### APPLY PEAK_DISTRIBUTIONS
+library(multidplyr)
+rm(list=setdiff(ls(), c("allSomatics_p2", "peakDistributions_p2") ))
 
 
+allSomatics_p2 = allSomatics_p2 %>% select(-subclonalLikelihood, -clonalLikelihood)
+head(allSomatics_p2)
+head(peakDistributions_p2)
 
-sublconality = distributions %>% group_by(sampleId, bucket, subclonal) %>% summarise(count = sum(count)) %>% spread(subclonal, count)
-
-
-
-
-
-
+subclonalRegions_p2 = subclonal_regions(peakDistributions_p2, 0.85)
+save(subclonalRegions_p2, file = "/Users/jon/hmf/analysis/somatics/RData/subclonalRegions_p2.RData")
 
 
-
-##### OTHER OPTIONS FOR FINDING PEAKS.. KEEP FOR LATER JUST IN CSE
-
-
-#Option 0
-#peakPloidy = estimate_somatic_peak(somatics, 0.01, 5)
-#peakModel = model_peak(peakPloidy, somatics, binwidth)
-
-
-#Option 2
-#seedPloidy = estimate_somatic_peak(somatics)
-#impact = data.frame()
-#for (j in c(-10:10)) {
-#  currentPloidy = seedPloidy + j * 0.01;
-#  currentModel = model_peak(currentPloidy, somatics, binwidth)
-#  currenImpact = change_in_weights(currentModel, somatics)
-#  impact = bind_rows(impact, currenImpact)
-#}
-#peakPloidy = first(impact %>% mutate(imbalance = abs(imbalance)) %>% arrange(imbalance) %>% pull(peak))
-#peakModel = model_peak(peakPloidy, somatics, binwidth)
-
-#Option 3
-#peakPloidy = estimate_somatic_peak(somatics)
-#peakModel = model_peak(peakPloidy, somatics, binwidth)
-#peakImpact = change_in_weights(currentModel, somatics)
-#repeat {
-#  if (peakImpact$right > peakImpact$left) {
-#    nextPloidy = peakPloidy + 0.01
-#  } else {
-#    nextPloidy = peakPloidy - 0.01
-#  }
-#  
-#  nextModel = model_peak(nextPloidy, somatics, binwidth)
-#  nextImpact = change_in_weights(nextModel, somatics)
-#  if (abs(nextImpact$imbalance) > abs(peakImpact$imbalance)) {
-#    break;
-#  } else {
-#    peakPloidy = nextPloidy;
-#    peakModel = nextModel;
-#    peakImpact = nextImpact;
-#  }
-#}
-
-change_in_weights <- function(peakModel, somatics) {
-  peak = peakModel[["peak"]]
-  offset = peakModel[["offset"]]
-  binwidth = peakModel[["binwidth"]]
-  
-  modelDistribution = peakModel[["distribution"]] %>% filter(peakWeight > 0) %>% select(bucket, weightToRemove = peakWeight)
-  actualDistribution = add_bucket(somatics, binwidth, offset) %>%
-    group_by(bucket) %>%
-    summarise(currentWeight = sum(pmax(0, weight))) %>% 
-    filter(bucket %in% modelDistribution$bucket)
-  
-  combinedDistribtuion = left_join(modelDistribution, actualDistribution, by = "bucket")
-  combinedDistribtuion[is.na(combinedDistribtuion)] <- 0
-  change = combinedDistribtuion %>% 
-    mutate(
-      after = currentWeight - weightToRemove,
-      type = ifelse(as.numeric(bucket) < peak, "left", "right")) %>% 
-    group_by(type) %>% 
-    summarise(value = sum(after)) %>% 
-    spread(type, value) %>%
-    mutate(
-      peak = peak,
-      imbalance = left - right
-    ) %>%
-    select(peak, everything())
-  
-  return (change)
-}
+allSomatics_p2 = allSomatics_p2 %>% mutate(somaticPloidy = pmax(0, adjustedVaf * adjustedCopyNumber))
+date()
+allSomatics_p2 = annotate_somatics_with_clonality(allSomatics_p2, subclonalRegions_p2)
+allSomatics_p2 = allSomatics_p2 %>% select(-bucket, -somaticPloidy) 
+date()
+save(allSomatics_p2, file = "/Users/jon/hmf/RData/Reference/allSomatics_p2.RData")
 
 
 
-totalDistribution = totalDistribution %>% group_by(bucket) %>% summarise(peakWeight = sum(peakWeight)) %>% mutate(peak = "Total")
-peakDistribution = bind_rows(peakDistribution, totalDistribution)
-peakDistribution = peakDistribution %>%  mutate(peak = factor(peak, levels = unique(peakDistribution$peak)))
-
-p = ggplot() + 
-  geom_histogram(data = enrichedSomatics, aes(x = somaticPloidy, fill = CN), binwidth = binwidth, alpha = 0.6) +
-  geom_line(data = peakDistribution, aes(x = bucket, y = peakWeight, color = peak), stat = "identity") +
-  ggtitle(paste0(sample, "", "")) +
-  xlim(0, 5)
-
-jonDistribution = totalDistribution %>% group_by(bucket, clonality) %>% summarise(peakWeight = sum(peakWeight))
-clonalityProportion = jonDistribution %>% group_by(bucket) %>% spread(clonality, peakWeight) %>% 
-  mutate(TOTAL = CLONAL + SUBCLONAL, CLONAL = ifelse(TOTAL == 0, 0, CLONAL / TOTAL), SUBCLONAL = ifelse(TOTAL == 0, 0, SUBCLONAL / TOTAL)) %>% select(-TOTAL) %>%
-  gather(clonality, value, CLONAL, SUBCLONAL)
-
-ggplot()+
-  geom_line(data = jonDistribution, aes(x = bucket, y = peakWeight, color = clonality), position = "stack")
-
-ggplot()+
-  geom_bar(data = jonDistribution, aes(x = bucket, y = peakWeight, fill = clonality), position = "stack", stat = "identity") + xlim(0, 5) 
-
-ggplot() +
-  geom_bar(data = clonalityProportion, width = 0.1,  aes(x = bucket, y = value, fill = clonality), stat = "identity") + xlim(0, 5) 
+load("/Users/jon/hmf/analysis/somatics/RData/peakDistributions_p1.RData")
+subclonalRegions_p1 = subclonal_regions(peakDistributions_p1, 0.85)
+save(subclonalRegions_p1, file = "/Users/jon/hmf/analysis/somatics/RData/subclonalRegions_p1.RData")
 
 
-dev.off()
-p
+load(file = "~/hmf/RData/reference/allSomatics_p1.RData")
+load(file = "~/hmf/analysis/somatics/RData/subclonalRegions_p1.RData")
 
-save_plot(paste0("/Users/jon/hmf/analysis/somatics/", sample, ".png"), p, base_height = 6, base_width = 7)  
+allSomatics_p1 = allSomatics_p1  %>% 
+  select(-subclonalLikelihood, -clonalLikelihood) %>% 
+  mutate(somaticPloidy = pmax(0, adjustedVaf * adjustedCopyNumber))
+
+allSomatics_p1 = annotate_somatics_with_clonality(allSomatics_p1, subclonalRegions_p1) %>% 
+  select(-bucket, -somaticPloidy) 
+
+save(allSomatics_p1, file = "/Users/jon/hmf/RData/Reference/allSomatics_p1.RData")
