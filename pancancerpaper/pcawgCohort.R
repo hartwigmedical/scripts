ba3049e754d6595b22c4a5ab79f83a7e52935c07
Jonathan Baber <jonbaber@gmail.com>
2018-09-06 13:38:16 +1000

    pcawg comparison

---
+++ pancancerpaper/pcawgCohort.R
@@ -0,0 +1,211 @@
+pcawg_structural_variant_summary <- function(dbConnect, cohort) {
+  sampleIdString = paste("'", cohort$sampleId, "'", collapse = ",", sep = "")
+  query = paste(
+    "SELECT sampleId, count(*) as SV",
+    " FROM structuralVariant ",
+    "WHERE sampleId in (",sampleIdString, ") AND (type <> 'BND' OR ploidy >= 0.2) AND (startChromosome <> endChromosome OR endPosition-startPosition>1000)",
+    " GROUP BY sampleId",
+    sep = "")
+  result = dbGetQuery(dbConnect, query)
+  return (result)
+}
+
+load(file = "~/hmf/RData/reference/highestPurityCohort.RData")
+dbProd = dbConnect(MySQL(), dbname='hmfpatients_20180418', groups="RAnalysis")
+pcawgSV = pcawg_structural_variant_summary(dbProd, highestPurityCohort)
+dbDisconnect(dbProd)
+rm(dbProd)
+
+
+load(file = "~/hmf/RData/reference/allSomatics_p1.RData")
+allSomatics_p1 = allSomatics_p1 %>% filter(type != 'MNP' | nchar(ref) == 2)
+somatics_summary_p1 = cohort_somatic_summary(allSomatics_p1)
+rm(allSomatics_p1)
+
+load(file = "~/hmf/RData/reference/allSomatics_p2.RData")
+allSomatics_p2 = allSomatics_p2 %>% filter(type != 'MNP' | nchar(ref) == 2)
+somatics_summary_p2 = cohort_somatic_summary(allSomatics_p2)
+rm(allSomatics_p2)
+
+pcawgSomaticsSummary = rbind(somatics_summary_p1, somatics_summary_p2) %>% filter(sampleId %in% highestPurityCohort$sampleId) %>%
+  mutate(
+    INDEL = TOTAL_INDEL,
+    MNV = TOTAL_MNV,
+    SNV = TOTAL_SNV) %>%
+  select(sampleId, INDEL, MNV, SNV)
+
+filteredHmfMutationalLoad = highestPurityCohort %>% select(sampleId, cancerType) %>%
+  left_join(pcawgSomaticsSummary, by = "sampleId") %>%
+  left_join(pcawgSV, by = "sampleId")
+
+save(filteredHmfMutationalLoad, file = "~/hmf/RData/processed/filteredHmfMutationalLoad.RData")
+
+
+########### PLOT FUNCTIONS
+display_cancer_types <- function(cancerTypes) {
+  for (i in 1:length(cancerTypes)) {
+    if (cancerTypes[i] == "Mesothelioma") {
+      cancerTypes[i]= "Meso- thelioma"
+    }
+    if (cancerTypes[i] == "Esophagus") {
+      cancerTypes[i]= "Esoph- agus"
+    }
+    if (cancerTypes[i] == "Colon/Rectum") {
+      cancerTypes[i]= "Colon/ Rectum"
+    }
+    if (cancerTypes[i] == "Head and neck") {
+      cancerTypes[i]= "Head & Neck"
+    }
+    if (cancerTypes[i] == "Bone/Soft tissue") {
+      cancerTypes[i]= "Bone/Soft Tissue"
+    }
+    if (cancerTypes[i] == "Urinary tract") {
+      cancerTypes[i]= "Urinary Tract"
+    }
+  }
+  
+  return (label_wrap_gen(10)(cancerTypes))
+}
+
+somaticColours = c("#a6611a","#dfc27d","#80cdc1","#018571")
+somaticColours = setNames(somaticColours, c("Adjusted HMF SNV","PCAWG SNV", "PCAWG MNV", "Adjusted HMF MNV"))
+somaticLinetypes = c("solid","dashed","dashed","solid")
+somaticLinetypes = setNames(somaticLinetypes, c("Adjusted HMF SNV","PCAWG SNV", "PCAWG MNV", "Adjusted HMF MNV"))
+
+indelSVColours = c("#d01c8b","#f1b6da","#b8e186","#4dac26")
+indelSVColours = setNames(indelSVColours, c("Adjusted HMF INDEL","PCAWG INDEL", "PCAWG SV", "Adjusted HMF SV"))
+indelSVLinetypes = c("solid","dashed","dashed","solid")
+indelSVLinetypes = setNames(indelSVLinetypes, c("Adjusted HMF INDEL","PCAWG INDEL", "PCAWG SV", "Adjusted HMF SV"))
+
+########### PLOT
+load(file = "~/hmf/RData/processed/highestPurityCohortSummary.RData")
+oldMutationalLoad = highestPurityCohortSummary %>% 
+  select(sampleId, cancerType, ends_with("INDEL"), ends_with("SNV"), ends_with("MNV"), TRL, DEL, INS, INV, DUP) %>%
+  mutate(cancerType = factor(cancerType, levels = cancerTypeFactors)) %>%
+  mutate(
+    INDEL = TOTAL_INDEL,
+    MNV = TOTAL_MNV,
+    SNV = TOTAL_SNV,
+    SV = TRL + DEL + INS + INV + DUP) %>% 
+  select(sampleId, cancerType, INDEL, SNV, MNV, SV)
+
+load(file = "~/hmf/RData/processed/filteredHmfMutationalLoad.RData")
+load(file = '~/hmf/RData/Reference/hpcCancerTypeCounts.RData')
+cancerTypeFactors =  factor(hpcCancerTypeCounts$cancerType, levels = hpcCancerTypeCounts$cancerType)
+
+filteredHmfMutationalLoad = filteredHmfMutationalLoad %>%
+  mutate(
+    SV = (1 - 0.37) * SV,
+    SNV = (1 - 0.07) * SNV,
+    MNV = (1 - 0.21) * MNV,
+    INDEL = (1 - 0.45) * INDEL)
+
+
+hmfMutationalLoad = filteredHmfMutationalLoad
+#hmfMutationalLoad = oldMutationalLoad
+
+pcawgRaw = read.csv("~/hmf/resources/PCAWG_counts.txt", sep = '\t', stringsAsFactors = F)
+pcawg_histology_tier2 = sort(unique(pcawgRaw$histology_tier2))
+
+pcawgCancerTypeMapping = data.frame(histology_tier2 = pcawg_histology_tier2, cancerType = pcawg_histology_tier2, stringsAsFactors = F)
+pcawgCancerTypeMapping[pcawgCancerTypeMapping$histology_tier2 == "Bladder", "cancerType"] = "Urinary tract"
+pcawgCancerTypeMapping[pcawgCancerTypeMapping$histology_tier2 == "Bone/SoftTissue", "cancerType"] = "Bone/Soft tissue"
+pcawgCancerTypeMapping[pcawgCancerTypeMapping$histology_tier2 == "Cervix", "cancerType"] = NA
+pcawgCancerTypeMapping[pcawgCancerTypeMapping$histology_tier2 == "Head/Neck", "cancerType"] = "Head and neck"
+pcawgCancerTypeMapping[pcawgCancerTypeMapping$histology_tier2 == "Myeloid", "cancerType"] = "Other"
+pcawgCancerTypeMapping[pcawgCancerTypeMapping$histology_tier2 == "Lymphoid", "cancerType"] = "Other"
+pcawgCancerTypeMapping[pcawgCancerTypeMapping$histology_tier2 == "Thyroid", "cancerType"] = "Other"
+pcawgCancerTypeMapping = pcawgCancerTypeMapping[!is.na(pcawgCancerTypeMapping$cancerType), ]
+
+pcawgMutationalLoad = pcawgRaw %>% left_join(pcawgCancerTypeMapping, by = "histology_tier2") %>%
+  filter(!is.na(cancerType)) %>% select(PCAWG_SNV = all.SNVs, PCAWG_INDEL = all.Indels, PCAWG_SV = SV.events, age,PCAWG_MNV = all.MNVs,  cancerType) %>%
+  mutate(source = "PCAWG")
+
+combinedMutationalLoad =  hmfMutationalLoad %>% select(sampleId, cancerType, INDEL, SNV, MNV, SV) %>%
+  mutate(source = "HMF") %>%
+  bind_rows(pcawgMutationalLoad) %>%
+  mutate(cancerType = factor(cancerType, levels = cancerTypeFactors)) %>%
+  filter(cancerType != "Other")
+
+combinedMutationalLoad = combinedMutationalLoad %>% 
+  group_by(cancerType) %>% 
+  mutate(
+    medianSNV = median(SNV, na.rm = T), 
+    medianMNV = median(MNV, na.rm = T), 
+    medianPCAWG_SNV = median(PCAWG_SNV, na.rm = T),
+    medianPCAWG_MNV = median(PCAWG_MNV, na.rm = T),
+    medianINDEL = median(INDEL, na.rm = T), 
+    medianSV = median(SV, na.rm = T), 
+    medianPCAWG_INDEL = median(PCAWG_INDEL, na.rm = T),
+    medianPCAWG_SV = median(PCAWG_SV, na.rm = T)
+  ) %>% ungroup()
+
+
+
+p3 = ggplot(data=combinedMutationalLoad) +
+  stat_ecdf(aes(SNV,color='Adjusted HMF SNV',linetype='Adjusted HMF SNV'), geom = "step", pad = FALSE) + geom_segment(aes(x = medianSNV, xend = medianSNV, y = 0.25, yend = 0.75, color='Adjusted HMF SNV'), show.legend = F) + 
+  stat_ecdf(aes(MNV,color='Adjusted HMF MNV',linetype='Adjusted HMF MNV') ,geom = "step", pad = FALSE) + geom_segment(aes(x = medianMNV, xend = medianMNV, y = 0.25, yend = 0.75, color='Adjusted HMF MNV'), show.legend = F) + 
+  stat_ecdf(aes(PCAWG_SNV,color='PCAWG SNV',linetype='PCAWG SNV'), geom = "step", pad = FALSE) + geom_segment(aes(x = medianPCAWG_SNV, xend = medianPCAWG_SNV, y = 0.25, yend = 0.75, color='PCAWG SNV'), show.legend = F) + 
+  stat_ecdf(aes(PCAWG_MNV,color='PCAWG MNV',linetype='PCAWG MNV'), geom = "step", pad = FALSE) + geom_segment(aes(x = medianPCAWG_MNV, xend = medianPCAWG_MNV, y = 0.25, yend = 0.75, color='PCAWG MNV'), show.legend = F) + 
+  scale_x_log10() + 
+  facet_grid(~cancerType, scales = "free_x", labeller = labeller(cancerType = display_cancer_types)) + 
+  scale_colour_manual(name = "Combined", values=somaticColours) + 
+  scale_linetype_manual(name = "Combined", values = somaticLinetypes) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(), 
+        panel.grid.minor.x = element_blank(),
+        #strip.background = element_blank(), 
+        strip.text.x = element_text(size = 9.2),
+        #strip.text = element_blank(), 
+        legend.position="top", legend.title = element_blank()) + 
+  xlab("Somatic Variants") +
+  coord_flip()
+
+p4 = ggplot(data=combinedMutationalLoad) +
+  stat_ecdf(aes(INDEL, color='Adjusted HMF INDEL', linetype = 'Adjusted HMF INDEL'),geom = "step", pad = FALSE) + geom_segment(aes(x = medianINDEL, xend = medianINDEL, y = 0.25, yend = 0.75, color='Adjusted HMF INDEL'), show.legend = F) + 
+  stat_ecdf(aes(SV,color='Adjusted HMF SV',linetype='Adjusted HMF SV'),geom = "step", pad = FALSE) + geom_segment(aes(x = medianSV, xend = medianSV, y = 0.25, yend = 0.75, color='Adjusted HMF SV'), show.legend = F) +
+  stat_ecdf(aes(PCAWG_INDEL,color='PCAWG INDEL',linetype='PCAWG INDEL'),geom = "step", pad = FALSE) + geom_segment(aes(x = medianPCAWG_INDEL, xend = medianPCAWG_INDEL, y = 0.25, yend = 0.75, color='PCAWG INDEL'), show.legend = F) +
+  stat_ecdf(aes(PCAWG_SV,color='PCAWG SV', linetype='PCAWG SV'),geom = "step",  pad = FALSE) + geom_segment(aes(x = medianPCAWG_SV, xend = medianPCAWG_SV, y = 0.25, yend = 0.75, color='PCAWG SV'), show.legend = F) +
+  scale_x_log10() + facet_grid(~cancerType) +
+  scale_colour_manual(name = "Combined", values=indelSVColours) + 
+  scale_linetype_manual(name = "Combined", values = indelSVLinetypes) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(), 
+        panel.grid.minor.x = element_blank(),
+        strip.background = element_blank(), strip.text = element_blank(), legend.position="bottom", legend.title = element_blank()) + 
+  xlab("Somatic Variants") +
+  xlab("INDELs & SVs") + 
+  coord_flip()
+
+pFigure1Revisited = plot_grid(p3, p4, ncol=1, align="v", rel_heights = c(1, 1), labels = "AUTO")
+pFigure1Revisited
+dev.off()
+save_plot("~/hmf/RPlot/Extended Figure 5 - Adjusted TMB Comparison.png", pFigure1Revisited, base_width = 14, base_height = 5)
+#save_plot("~/hmf/RPlot/Extended Figure 10 - OLD.png", pFigure1Revisited, base_width = 14, base_height = 5)
+
+
+########### WILSON COX
+cancerTypes = unique(pcawgMutationalLoad$cancerType)
+result = data.frame(cancerType = cancerTypes, stringsAsFactors = F)
+variantTypes = c("SNV","INDEL","SV","MNV")
+selectedVariant = "SV"
+
+hmfMutationalLoad = filteredHmfMutationalLoad
+#hmfMutationalLoad = oldMutationalLoad
+for (selectedCancerType in cancerTypes) {
+  for (selectedVariant in variantTypes) {
+    pcawgSelected = pcawgMutationalLoad %>% filter(cancerType == selectedCancerType) %>% pull(paste0("PCAWG_", selectedVariant))
+    pcawgSelected = pcawgSelected[!is.na(pcawgSelected)]
+    
+    hmfSelected = hmfMutationalLoad %>% filter(cancerType == selectedCancerType) %>% pull(selectedVariant)
+    hmfSelected = hmfSelected[!is.na(hmfSelected)]
+    
+    w = wilcox.test(pcawgSelected,hmfSelected,conf.int = T)
+    result[result$cancerType == selectedCancerType, selectedVariant] <- w[["p.value"]]
+    result[result$cancerType == selectedCancerType, paste('medianHMF',selectedVariant,sep='_')] <- median(hmfSelected)
+    result[result$cancerType == selectedCancerType, paste('medianPCAWG',selectedVariant,sep='_')] <- median(pcawgSelected)
+    result[result$cancerType == selectedCancerType, paste('diff',selectedVariant,sep='_')] <- w[["estimate"]]
+  }
+}
+
+adjustedHmfWilcoxResult = result
+save(adjustedHmfWilcoxResult, file = "~/hmf/RData/Processed/adjustedHmfWilcoxResult.RData")
+
--- pancancerpaper/pcawgPipeline.R
+++ pancancerpaper/pcawgPipeline.R
@@ -32,32 +32,6 @@ hmfVcfs = c(
 "EGAZ00001222791_EGAZ00001222814_post_processed.vcf.gz",
 "b589d2a2-5bf9-4080-bc99-0ae984dbae43_bbe59385-5f83-43f6-a485-517c860bef6f_post_processed.vcf.gz")
 
-hmfSvVcfs = c(
-  "EGAZ00001225530_EGAZ00001225588_somaticSV_bpi.vcf.gz",
-  "f7f3e156-0ddb-72b9-e040-11ac0d48542c_f7f3e156-0dde-72b9-e040-11ac0d48542c_somaticSV_bpi.vcf.gz",
-  "fc8130df-1cda-cade-e040-11ac0d485dec_fc8130df-1cdd-cade-e040-11ac0d485dec_somaticSV_bpi.vcf.gz",
-  "EGAZ00001225554_EGAZ00001225511_somaticSV_bpi.vcf.gz",
-  "6ff52f43-aae0-432d-ae31-374c22c45fd9_d392ded3-afc8-4c79-b278-40245f18f2f8_somaticSV_bpi.vcf.gz",
-  "fc8130df-1f1e-c8f9-e040-11ac0d485dfc_fc8130df-1f21-c8f9-e040-11ac0d485dfc_somaticSV_bpi.vcf.gz",
-  "EGAZ00001222819_EGAZ00001222815_somaticSV_bpi.vcf.gz",
-  "EGAZ00001225550_EGAZ00001225531_somaticSV_bpi.vcf.gz",
-  "1abec81b-9e0f-40a6-9840-967ee0ab8b54_e84debc4-b47d-48ed-a0d0-2859f0ebf987_somaticSV_bpi.vcf.gz",
-  "EGAZ00001225631_EGAZ00001225528_somaticSV_bpi.vcf.gz",
-  "EGAZ00001222797_EGAZ00001224455_somaticSV_bpi.vcf.gz",
-  "EGAZ00001222825_EGAZ00001222794_somaticSV_bpi.vcf.gz",
-  "d1b992ab-41b0-4d39-992f-c4f8da898576_b75b2663-dcc6-411c-bfcc-574aa33cf388_somaticSV_bpi.vcf.gz",
-  "EGAZ00001222785_EGAZ00001222839_somaticSV_bpi.vcf.gz",
-  "b5c7e4d8-19bb-4253-adeb-abaf1b31aa5d_2a8d63eb-0174-4213-9214-413f391f512c_somaticSV_bpi.vcf.gz",
-  "6b38c35a-7b39-4e96-9237-b02206c5bcc6_f26b1f44-12de-43ba-85bb-bc61741a5a88_somaticSV_bpi.vcf.gz",
-  "dd48a2fc-7c5b-44b5-bf3c-ffc8350445da_8b28f6d2-4b7d-493b-826e-b119a4fb0cb4_somaticSV_bpi.vcf.gz",
-  "EGAZ00001224456_EGAZ00001222832_somaticSV_bpi.vcf.gz",
-  "fc8130df-685d-7677-e040-11ac0d485ddc_fc8130df-6860-7677-e040-11ac0d485ddc_somaticSV_bpi.vcf.gz",
-  "1319bfce-ea65-4181-874d-b0f4c4d4a789_0d0793c1-df1b-4db1-ba36-adcb960cc0f5_somaticSV_bpi.vcf.gz",
-  "6a7fdabe-fd87-481c-b9a3-1f46ad3097ef_c2ec7f57-8510-4bbf-a2e9-dbd9ce8dcad1_somaticSV_bpi.vcf.gz",
-  "EGAZ00001225621_EGAZ00001225598_somaticSV_bpi.vcf.gz",
-  "EGAZ00001222791_EGAZ00001222814_somaticSV_bpi.vcf.gz",
-  "b589d2a2-5bf9-4080-bc99-0ae984dbae43_bbe59385-5f83-43f6-a485-517c860bef6f_somaticSV_bpi.vcf.gz")
-
 hmfSampleIds = gsub("_.*", "", hmfVcfs)
 
 pcawgSnvVcfs = c(
@@ -112,33 +86,7 @@ pcawgIndelVcfs = c(
 "f393bb08-4121-cad8-e040-11ac0d484535.consensus.20161006.somatic.indel.vcf.gz",
 "bbe59385-5f83-43f6-a485-517c860bef6f.consensus.20161006.somatic.indel.vcf.gz")
 
-pcawgSvVcfs = c(
-  "fc8130df-18fe-c74d-e040-11ac0d485df2.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "f7f3e156-0dde-72b9-e040-11ac0d48542c.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc8130df-1cdd-cade-e040-11ac0d485dec.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc8130df-e399-e34d-e040-11ac0c483279.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "d392ded3-afc8-4c79-b278-40245f18f2f8.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc8130df-1f21-c8f9-e040-11ac0d485dfc.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc68c24d-47ad-7961-e040-11ac0c48595c.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc8130df-6bec-7627-e040-11ac0d485e04.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "e84debc4-b47d-48ed-a0d0-2859f0ebf987.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc8130e0-0f1a-b6eb-e040-11ac0c48328f.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "f221cbb5-eefa-187f-e040-11ac0c481708.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc63cbab-d27a-5ebb-e040-11ac0c48724f.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "b75b2663-dcc6-411c-bfcc-574aa33cf388.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc447d4f-2532-c8ea-e040-11ac0c48469f.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "2a8d63eb-0174-4213-9214-413f391f512c.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "f26b1f44-12de-43ba-85bb-bc61741a5a88.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "8b28f6d2-4b7d-493b-826e-b119a4fb0cb4.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "f393bb07-270c-2c93-e040-11ac0d484533.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc8130df-6860-7677-e040-11ac0d485ddc.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "0d0793c1-df1b-4db1-ba36-adcb960cc0f5.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "c2ec7f57-8510-4bbf-a2e9-dbd9ce8dcad1.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "fc8130df-8ec8-5b1e-e040-11ac0d485e06.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "f393bb08-4121-cad8-e040-11ac0d484535.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz",
-  "bbe59385-5f83-43f6-a485-517c860bef6f.pcawg_consensus_1.6.161116.somatic.sv.vcf.gz")
-
-vcfFilenames = data.frame(hmf = hmfVcfs, hmfSv = hmfSvVcfs, pcawfSnv = pcawgSnvVcfs, pcawgIndel = pcawgIndelVcfs, pcawgSv =pcawgSvVcfs, stringsAsFactors = F)
+vcfFilenames = data.frame(hmf = hmfVcfs, pcawfSnv = pcawgSnvVcfs, pcawgIndel = pcawgIndelVcfs, stringsAsFactors = F)
 rm(hmfVcfs, pcawgSnvVcfs, pcawgIndelVcfs, hmfSvVcfs, pcawgSvVcfs)
 
 load_sv_file <- function(directory, fileName) {
@@ -169,19 +117,19 @@ hmf_data_from_file <- function(hmfFile) {
   return (hmf.vcf)
 }
 
-hmf_data_from_db <- function(hmfFile) {
-  sample = gsub("_post_processed.vcf.gz", "", hmfFile)
-  sample = gsub(".*_", "", sample)
+hmf_data_from_db <- function(sample) {
   hmf.vcf = hmfSomaticsDB %>% filter(sampleId == sample) %>%
     select(CHROM = chromosome, POS = position, REF = ref, ALT = alt, FILTER = filter, everything()) %>%
     mutate(
-      ifelse(type == "MNP","MNV", type), 
-      ifelse(type == "SNP","SNV", type),
-      source = "HMF"
-           )
+      type = ifelse(type == "MNP","MNV", type), 
+      type = ifelse(type == "SNP","SNV", type),
+      source = "HMF")
   
   return (hmf.vcf)
 }
+
+#i = 5
+#pcawgSnvFile = vcfFilenames[i, "pcawfSnv"]
 
 combined_data_frame <- function(hmf.vcf, pcawgSnvFile, pcawgIndelFile, hmfDirectory, pcawgDirectory) {
     chromosomeFactors = c(1:22, 'X', 'Y')
@@ -192,17 +140,23 @@ combined_data_frame <- function(hmf.vcf, pcawgSnvFile, pcawgIndelFile, hmfDirect
         mutate(CHROM = factor(CHROM, chromosomeFactors, ordered = T)) %>%
         arrange(CHROM, POS) %>%
         mutate(
-        nextPOS = lead(POS),
-        nextCHROM = lead(CHROM),
         prevPOS = lag(POS),
         prevCHROM = lag(CHROM),
-        nextDistance = ifelse(CHROM == nextCHROM, nextPOS - POS, 0),
+        nextPOS = lead(POS),
+        nextCHROM = lead(CHROM),
+        nextPOS2 = lead(POS, 2),
+        nextCHROM2 = lead(CHROM, 2),
         prevDistance = ifelse(CHROM == prevCHROM, prevPOS - POS, 0),
-        type = ifelse(nextDistance == 1 & prevDistance != -1, "MNV", "SNV"),
+        nextDistance = ifelse(CHROM == nextCHROM, nextPOS - POS, 0),
+        nextDistance2 = ifelse(CHROM == nextCHROM2, nextPOS2 - POS, 0),
+        type = ifelse(prevDistance != -1 & nextDistance == 1, "MNV", "SNV"),
+        type = ifelse(prevDistance != -1 & nextDistance == 1 & nextDistance2 == 2, "MNV_LONG", type),
         type = ifelse(prevDistance == -1, "MNV_BODY", type),
+        type = ifelse(is.na(type), "SNV", type),
         CHROM = as.character(CHROM)
         ) %>%
-        select(-nextPOS, -nextCHROM, -prevPOS, -prevCHROM, -nextDistance, -prevDistance)
+        #select(prevCHROM, prevPOS, CHROM, POS,  nextCHROM, nextPOS, nextCHROM2, nextPOS2, prevDistance, nextDistance, nextDistance2, type)
+        select(-nextPOS, -nextCHROM, -prevPOS, -prevCHROM, -nextDistance, -prevDistance, -nextCHROM2, -nextPOS2, -nextDistance2)
 
 
     pcawg.vcf.indel <- read.vcfR(paste0(pcawgDirectory, pcawgIndelFile), verbose = F)
@@ -217,6 +171,7 @@ combined_data_frame <- function(hmf.vcf, pcawgSnvFile, pcawgIndelFile, hmfDirect
         mutate(
         CHROM = factor(CHROM, chromosomeFactors, ordered = T),
         FILTER = ifelse(is.na(FILTER),"PASS", FILTER)) %>%
+        filter(FILTER == 'PASS') %>%
         select(source, everything())
 
     return (combined)
@@ -243,7 +198,7 @@ filter_count <- function(combined) {
 }
 
 ######### EXECUTE ALGORITHM
-query = "select * from somaticVariant where sampleId not like 'CPCT%'"
+query = "select * from somaticVariant where sampleId not like 'CPCT%' AND filter = 'PASS'"
 dbProd = dbConnect(MySQL(), dbname='landscape_paper_validation', groups="RAnalysis")
 hmfSomaticsDB = dbGetQuery(dbProd, query)
 dbDisconnect(dbProd)
@@ -252,39 +207,89 @@ rm(dbProd, query)
 hmfDirectory = "/Users/jon/hmf/analysis/pcawg/ours/"
 pcawgDirectory = "/Users/jon/hmf/analysis/pcawg/theirs/"
 
-result = data.frame()
-allSomatics = data.frame()
+combinedSnvMnvIndel = data.frame()
 for (i in c(1:nrow(vcfFilenames))) {
-    cat("Processing", i, "of 24")
+    cat("Processing", i, "of 24", "\n")
+
+    sample = vcfFilenames[i, "hmf"]
+    sample = gsub("_post_processed.vcf.gz", "", sample)
+    sample = gsub(".*_", "", sample)
     
-    #### Choose one of these :)
-  i = 1
-    hmfFile = vcfFilenames[i, "hmfSv"]
-    pcawgFile = vcfFilenames[i, "pcawgSv"]
-    
-    
-    hmf.vcf = hmf_data_from_db(vcfFilenames[i, "hmf"]); sample = unique(hmf.vcf$sampleId)
-    #hmf.vcf = hmf_data_from_file(vcfFilenames[i, "hmf"]); sample = first(hmf.vcf$Indiv)
+    hmf.vcf = hmf_data_from_db(sample)
+    #hmf.vcf = hmf_data_from_file(vcfFilenames[i, "hmf"])
     
     combined = combined_data_frame(hmf.vcf, vcfFilenames[i, "pcawfSnv"], vcfFilenames[i, "pcawgIndel"], hmfDirectory, pcawgDirectory)
     combinedWithScope = add_scope(combined) %>% mutate(sampleId = sample)
 
-    allSomatics = bind_rows(combinedWithScope, allSomatics)
-
-    filterCount = filter_count(combinedWithScope %>% filter(type != "MNV_BODY"))
-    result = bind_rows(filterCount, result)
+    combinedSnvMnvIndel = bind_rows(combinedWithScope, combinedSnvMnvIndel)
 }
 
-save(allSomatics, file ="~/hmf/allSomatics.RData")
-save(result, file ="~/hmf/result.RData")
+save(combinedSnvMnvIndel, file = "/Users/jon/hmf/analysis/pcawg/combinedSnvMnvIndel.RData")
 
 
-result$PCAWGSummarised = ifelse(grepl('NORMAL',result$PCAWG),'PON',ifelse(grepl('PASS',result$PCAWG)|is.na(result$PCAWG),result$PCAWG,'OTHER'))
-result$HMFSummarised = ifelse(grepl('PON',result$HMF),'PON',ifelse(grepl('PASS',result$HMF)|is.na(result$HMF),result$HMF,'OTHER'))
-summary=(result %>% filter(HMFSummarised=='PASS'|PCAWGSummarised=='PASS') %>% unite(HMF_PCAWG,HMFSummarised,PCAWGSummarised) %>% group_by(HMF_PCAWG,type,sampleId) %>% summarise(sum=sum(n)) %>% spread(HMF_PCAWG,sum) )
-library(ggplot2)
-ggplot()+geom_point(data=summary %>% filter(isIndel==F),aes(PASS_PASS,PASS_OTHER),color = "red")+
-    geom_point(data=summary %>% filter(isIndel==F),aes(PASS_PASS,PASS_NA),color = "blue") +
-    geom_point(data=summary %>% filter(isIndel==F),aes(PASS_PASS,PASS_PON),color = "green")+
-    geom_point(data=summary %>% filter(isIndel==F),aes(PASS_PASS,NA_PASS),color = "black")# +scale_x_log10()+scale_y_log10()
+######### STATS!!
+load(file = "/Users/jon/hmf/analysis/pcawg/combinedSnvMnvIndel.RData")
+combinedSnvMnvIndel = combinedSnvMnvIndel %>% filter(!type %in% c('MNV_BODY', 'MNV_LONG'), type != 'MNV' | nchar(REF) <= 2) 
+combinedSnvMnvIndel %>% group_by(source, type) %>% count() %>% spread(type, n)
 
+combinedSnvMnvIndel %>% filter(source == 'PCAWG') %>% group_by(type, scope) %>% count() %>% group_by(type) %>% mutate(percent = n / sum(n))
+combinedSnvMnvIndel %>% filter(source == 'PCAWG', t_alt_count > 5) %>% group_by(type, scope) %>% count() %>% group_by(type) %>% mutate(percent = n / sum(n))
+combinedSnvMnvIndel %>% filter(source == 'PCAWG', t_alt_count <= 5) %>% group_by(type, scope) %>% count() %>% group_by(type) %>% mutate(percent = n / sum(n))
+combinedSnvMnvIndel %>% filter(source == 'HMF') %>% group_by(type, scope) %>% count() %>% group_by(type) %>% mutate(percent = n / sum(n))
+
+######### PLOT!!!
+load(file = "/Users/jon/hmf/analysis/pcawg/combinedSnvMnvIndel.RData")
+combinedSnvMnvIndelSummary = combinedSnvMnvIndel %>%
+  filter(!type %in% c('MNV_BODY', 'MNV_LONG'), type != 'MNV' | nchar(REF) <= 2)  %>%
+  group_by(source,sampleId, type) %>% 
+  count() %>% 
+  spread(source,n, fill = 0)
+
+load(file = "/Users/jon/hmf/analysis/pcawg/combinedSV.RData")
+combinedSVSummary = combinedSV %>% 
+  mutate(source = ifelse(source == "hmf", "HMF", "PCAWG")) %>%
+  filter(startChromosome!=endChromosome|endPosition-startPosition>1000) %>%  
+  group_by(source,sampleId) %>% 
+  count() %>% 
+  spread(source,n, fill = 0) %>%
+  mutate(type = 'SV')
+
+combinedSummary = bind_rows(combinedSnvMnvIndelSummary, combinedSVSummary)
+
+lm_eqn = function(m) {
+  
+  l <- list(a = format(coef(m)[1], digits = 3),
+            b = format(abs(coef(m)[2]), digits = 3),
+            r2 = format(summary(m)$r.squared, digits = 3));
+  
+  if (coef(m)[2] >= 0)  {
+    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
+  } else {
+    eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(r)^2~"="~r2,l)    
+  }
+  
+  as.character(as.expression(eq));                 
+}
+
+singleBlue = "#6baed6"
+singleRed = "#d94701"
+
+create_plot <- function(data, xLab, yLab) {
+  ggplot(data=data,aes(PCAWG,HMF)) + 
+    geom_smooth(method = 'lm', colour = singleBlue, se = F, alpha = 0.2) +
+    geom_point(color = "black") +
+    geom_text(aes(x = xLab, y = yLab, label = lm_eqn(lm(PCAWG ~ HMF, data))), parse = TRUE, size = 3, hjust = 0) +
+    xlab("PCAWG Pipeline") + ylab("HMF Pipeline") +# ggtitle("Structural Variants") +
+    scale_x_continuous(expand = c(0.02,0.02)) + scale_y_continuous(expand = c(0.02,0.02)) +
+    theme(panel.border = element_blank(), axis.ticks = element_blank(), panel.grid.minor = element_blank())
+}
+
+
+p1 = create_plot(combinedSummary %>% filter(type == 'SNV'), 10000, 30000) + xlab("") + ggtitle("SNV")
+p2 = create_plot(combinedSummary %>% filter(type == 'MNV'), 25, 100) + xlab("") + ylab("") + ggtitle("MNV")
+p3 = create_plot(combinedSummary %>% filter(type == 'INDEL'), 5000, 30000) + ggtitle("INDEL")
+p4 = create_plot(combinedSummary %>% filter(type == 'SV'), 100, 400) + ylab("") + ggtitle("SV") + ylim(0, 500)
+
+pTotal = plot_grid(p1,p2,p3,p4, nrow = 2, labels = "AUTO")
+pTotal
+save_plot("~/hmf/RPlot/Extended Figure 4 - Pipeline Comparison.png", pTotal, base_width = 8, base_height = 8)
