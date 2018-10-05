359f2dd6a882085625810a90564f405a623d2e2e
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-10-05 15:49:07 +0200

    DEV-419: for TMB update size ref genome

--- sql/TMB_check.sql
+++ sql/TMB_check.sql
@@ -1,3 +1,3 @@
-select sampleId, count(*)/3095 as TMB, if (count(*)/3095 > 10, "High", "Low") as status from somaticVariant where  filter in ('PASS')
+select sampleId, count(*)/2859 as TMB, if (count(*)/2859 > 10, "High", "Low") as status from somaticVariant where  filter in ('PASS')
 group by sampleId
 order by 2 desc;
