5667f6a2efc552602e2b6ff30924866d6f5eab51
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2018-11-16 14:16:24 +0100

    Fix R code

--- chord/extractHRDsample.R
+++ chord/extractHRDsample.R
@@ -3,7 +3,7 @@
 library('devtools')
 args <- commandArgs(TRUE)
 
-load_all(args[1] + '/mutSigExtractor')
+load_all(paste(args[1], '/mutSigExtractor'))
 setwd(args[2])
 sample = extractSigsForHrdClassifier(args[4], args[4], args[5], args[3])
 write.table(sample, file = paste(args[3], "_sample_mut_signatures.txt", sep = ""))
--- chord/predictHRDsample.R
+++ chord/predictHRDsample.R
@@ -2,7 +2,7 @@
 library(randomForest)
 args <- commandArgs(TRUE)
 
-base_dir <- args[1] + '/'
+base_dir <- paste(args[1], '/')
 setwd(args[2])
 hmf_patients_mut_sigs <- read.table(args[4])
 
