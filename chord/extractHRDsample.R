7e6cbd4c31a8fec80133bbf2feb5aa03077464cf
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2018-11-19 15:00:29 +0100

    add output to contexts

--- chord/extractHRDsample.R
+++ chord/extractHRDsample.R
@@ -5,5 +5,5 @@ args <- commandArgs(TRUE)
 
 load_all(paste(args[1], '/mutSigExtractor', sep = ""))
 setwd(args[2])
-sample = extractSigsForHrdClassifier(args[4], args[4], args[5], args[3], args[6])
+sample = extractSigsForHrdClassifier(args[4], args[4], args[5], args[3], args[6], args[7])
 write.table(sample, file = paste(args[3], "_sample_mut_signatures.txt", sep = ""))
