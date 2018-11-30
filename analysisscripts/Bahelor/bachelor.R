9456521f0b4b885909326d7b46f905ab1b3e223c
Peter Priestley <p.priestley@hartwigmedicalfoundation.nl>
2018-11-30 11:05:33 +1100

    minor updates of display

--- analysisscripts/Bahelor/bachelor.R
+++ analysisscripts/Bahelor/bachelor.R
@@ -4,7 +4,7 @@ load('~/hmf/RData/Processed/highestPurityCohortSummary.RData')
 
 ### SUSPICIOUS BUT SUGGEST TO KEEP #####
 #   GJB2 p.Gly12fs rs80338939 (33) - present in 0.9% of germline, gene seems to be mainly associated with hearing.  BUT frequently biallleic  => Keep?
-#   BLM  15:91306246 & 15:91306246 (STOP - 3 instances each)
+#   BLM  15:91306246 & 15:91306246 (STOP - 3 instances each) - Enriched in CNS
 #   COL7A1 3:48621017 - Enriched in Prostate cancer
 #   DOCK8 9:271626 Splice Acceptor (5 instances) - but 2 biallelic
 
@@ -14,19 +14,33 @@ load('~/hmf/RData/Processed/highestPurityCohortSummary.RData')
 #   KRAS  12:25368442 (STOP GAINED 2 instances) - (codon 168/189) => probably filter?
 #   TSHR p.Pro68Ser (4 instances) - assocaited with Hypothyroidism, congenital, nongoitrous => probably filter?
 
-View(merge(bachelorLatest %>% mutate(refSampleVaf=RefCount/RefReadDepth) %>% 
-       group_by(SampleId,Gene) %>% mutate(netFS=sum(stri_length(Ref)-stri_length(Alt)) %% 3) %>% ungroup() %>%  #check Frameshift per sample
-       group_by(Gene,Chromosome,Position,Ref,Alt) %>% mutate(medianRefSampleVaf=median(refSampleVaf),medianNetFS=median(netFS)) %>% ungroup() %>%  #check median vaf
-       filter(!(ClinvarSignificance %in% c('Benign/Likely_benign','Benign','Likely_benign')),
-              !Effects=='missense_variant'|!(ClinvarDiagnosis %in% c('PI_Z|PI_Z(AUGSBURG)|PI_Z(TUN)|Inborn_genetic_diseases|Alpha-1-antitrypsin_deficiency|FRAXE|not_provided','PI_M(PROCIDA)|Alpha-1-antitrypsin_deficiency')),  #Non cancer related pathogenic
-              !(Gene=='GJB2' & HgvsProtein %in% c('p.Leu90Pro')),   #Associated with deafness
-              !DbsnpId=='rs111033566',   #very common SNP in PRSS1 (47% of ExAc)
-              WorstCodingEffect!='SPLICE'|Type!='INDEL',
-              Filter!='ARTEFACT',
-              medianRefSampleVaf>0.2,medianRefSampleVaf<0.8,
-              !grepl('frameshift',Effects)|medianNetFS!=0),
-       highestPurityCohortSummary %>% select(sampleId,primaryTumorLocation),by.x='SampleId',by.y='sampleId',all.x=T) %>%
+filteredBachelor = merge(bachelorLatest %>% mutate(refSampleVaf=RefCount/RefReadDepth) %>% 
+                           group_by(SampleId,Gene) %>% mutate(netFS=sum(stri_length(Ref)-stri_length(Alt)) %% 3) %>% ungroup() %>%  #check Frameshift per sample
+                           group_by(Gene,Chromosome,Position,Ref,Alt) %>% mutate(medianRefSampleVaf=median(refSampleVaf),medianNetFS=median(netFS)) %>% ungroup() %>%  #check median vaf
+                           filter(!(ClinvarSignificance %in% c('Benign/Likely_benign','Benign','Likely_benign')),
+                                  !Effects=='missense_variant'|!(ClinvarDiagnosis %in% c('PI_Z|PI_Z(AUGSBURG)|PI_Z(TUN)|Inborn_genetic_diseases|Alpha-1-antitrypsin_deficiency|FRAXE|not_provided','PI_M(PROCIDA)|Alpha-1-antitrypsin_deficiency')),  #Non cancer related pathogenic
+                                  !(Gene=='GJB2' & HgvsProtein %in% c('p.Leu90Pro')),   #Associated with deafness
+                                  !DbsnpId=='rs111033566',   #very common SNP in PRSS1 (47% of ExAc)
+                                  WorstCodingEffect!='SPLICE'|Type!='INDEL',
+                                  Filter!='ARTEFACT',
+                                  medianRefSampleVaf>0.2,medianRefSampleVaf<0.8,
+                                  !grepl('frameshift',Effects)|medianNetFS!=0),
+                         highestPurityCohortSummary %>% select(sampleId,primaryTumorLocation),by.x='SampleId',by.y='sampleId',all.x=T) 
+
+View(filteredBachelor)
+
+# By Variant & Biallleic
+View(filteredBachelor %>%
        group_by(Gene,CodonInfo,ClinvarDiagnosis,ClinvarSignificance,Chromosome,Position,Effects,Ref,Alt,HgvsProtein,DbsnpId,CosmicId,medianRefSampleVaf,Biallelic) %>% count() %>% 
        arrange(-n)  %>% spread(Biallelic,n))
 
-  
+# By Gene & Biallleic
+View(filteredBachelor %>%
+       group_by(Gene,Biallelic) %>% count() %>% 
+       arrange(-n)  %>% spread(Biallelic,n))
+
+# By Gene & PrimaryTumorLocation
+View(filteredBachelor %>%
+       group_by(Gene,primaryTumorLocation) %>% count() %>% 
+       arrange(-n)  %>% spread(primaryTumorLocation,n))
+
