424fa8cee0e4b4caee4a4a5a7cc5802f8dfe16cd
Daniel Cameron <d-cameron@github.com>
2018-12-11 16:51:49 +1100

    Added virushostdb SQL import script creation.

--- virus/generate_viral_reference.sh
+++ virus/generate_viral_reference.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 HOMOSAPIENS_TAX_ID=9606
 
-wget ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.genomic.fna.gz
-wget ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.tsv
-gunzip virushostdb.genomic.fna.gz
-
+# if [[ ! -f virushostdb.genomic.fna ]] ; then
+	# wget ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.genomic.fna.gz
+	# gunzip virushostdb.genomic.fna.gz
+# fi
+# if [[ ! -f virushostdb.tsv ]] ; then
+	# wget ftp://ftp.genome.jp/pub/db/virushostdb/virushostdb.tsv
+# fi
 
 HUMAN_VIRUS_REFSEQ=$(cut -f 4,8 virushostdb.tsv  | grep $HOMOSAPIENS_TAX_ID | cut -f 1 | tr '\n' ',' | tr -d '\t ')
-# filterbyname.sh is part of BBMap
-filterbyname.sh  in=virushostdb.genomic.fna out=human_virus.fa names=$HUMAN_VIRUS_REFSEQ include=t
-RepeatMasker -no_is -pa $(nproc) -s -noint -norna -species human -html human_virus.fa
+# # filterbyname.sh is part of BBMap
+# filterbyname.sh  in=virushostdb.genomic.fna out=human_virus.fa names=$HUMAN_VIRUS_REFSEQ include=t
+# RepeatMasker -no_is -pa $(nproc) -s -noint -norna -species human -html human_virus.fa
 
-rm human_virus.fa
-ln -s human_virus.fa.masked human_virus.fa
+#rm human_virus.fa
+#ln -s human_virus.fa.masked human_virus.fa
 
-samtools faidx human_virus.fa
-bwa index hg19_virus.fa
+#samtools faidx human_virus.fa
+#bwa index human_virus.fa
 
-for REFSEQ in $(echo $HUMAN_VIRUS_REFSEQ | tr , " ") ; do 
-	echo "$REFSEQ,$(grep $REFSEQ virushostdb.tsv | cut -f 2,3,11,12)" >> virus_refseq_to_human_readable.tsv
-done
+Rscript extract.R
