04d8d66f425aae324e70b8993b13fd91b558db4c
Jonathan Baber <jonbaber@gmail.com>
2018-12-19 09:18:36 +1000

    Add all tert promoters to hotspots

--- analysisscripts/createHotspots.R
+++ analysisscripts/createHotspots.R
@@ -2,30 +2,13 @@
 
 library(tidyr)
 library(dplyr)
+library(RMySQL)
 
 pasteNA <- function(...) {
   source = paste(..., sep =",")
   source = gsub("NA,", "", source)
   source = gsub(",NA", "", source)
   return (source)
-}
-
-mnv_to_svn <- function(mnvs) {
-  snvs = data.frame(chromosome = character(), position = integer(), ref = character(), alt = character(), stringsAsFactors = F)
-  for (i in 1:nrow(mnvs)) {
-    
-    mnvEntry = mnvs[i, ]
-    j = nchar(mnvEntry$ref)
-    
-    chromosome = rep(mnvEntry$chromosome, j)
-    position = mnvEntry$position + c(0:(j-1))
-    ref = strsplit(mnvEntry$ref, "")[[1]]
-    alt = strsplit(mnvEntry$alt, "")[[1]]
-    
-    snvs = rbind(snvs, data.frame(chromosome, position, alt, ref, stringsAsFactors = F))
-  }
-  
-  return (snvs)
 }
 
 chromosomeLevels = c(1:22, "X","Y")
@@ -47,23 +30,20 @@ civic = civic %>%
   select(chromosome = CHROMOSOME, position = POSITION, ref = REF, alt = ALT, civicGene = GENE) %>%
   group_by(chromosome, position, ref, alt) %>% distinct(chromosome, position, ref, alt) 
 
-tert = data.frame(chromosome = "5", position = 1295250, ref = "G", alt = "A", stringsAsFactors = F)
+dbProd = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
+query = "SELECT distinct chromosome, position, ref, alt FROM somaticVariant where gene = 'TERT' and position in (1295242,1295228,1295250) AND canonicalEffect = 'upstream gene variant'"
+tert = dbGetQuery(dbProd, query)
+dbDisconnect(dbProd)
+rm(dbProd)
 
 hotspots = merge(cgi, onco, by = c("chromosome", "position", "ref", "alt"), all = T)
 hotspots = merge(hotspots, civic, by = c("chromosome", "position", "ref", "alt"), all = T)
 hotspots = merge(hotspots, tert, by = c("chromosome", "position", "ref", "alt"), all = T)
 
-mnvInd = nchar(hotspots$ref) > 1 & nchar(hotspots$ref) == nchar(hotspots$alt)
-mnvs = hotspots[mnvInd, ]
-snvs = mnv_to_svn(mnvs)
-
-hotspots = hotspots[!mnvInd, ]
-hotspots = rbind(hotspots, snvs)
 hotspots = hotspots %>% distinct(chromosome, position, ref, alt) %>%
   mutate(chromosome = factor(chromosome, chromosomeLevels)) %>%
   arrange(chromosome, position)
 
-rm(cgi,civic,onco,tert, mnvs, mnvInd, snvs)
 write.table(hotspots, file = "~/hmf/resources/Hotspot.tsv", sep = "\t", col.names = F, quote = F, row.names = F)
 
 #bgzip Hotspot.tsv
