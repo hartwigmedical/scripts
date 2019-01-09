5561b258dfe2d334a544fe19b9532053a6acc442
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-01-09 11:15:40 +0100

    add logging

--- chord/extractHRDsample.R
+++ chord/extractHRDsample.R
@@ -3,6 +3,12 @@
 library('devtools')
 args <- commandArgs(TRUE)
 
+cat(args[1] , "/n")
+cat(args[2] , "/n")
+cat(args[3] , "/n")
+cat(args[4] , "/n")
+cat(args[5] , "/n")
+
 load_all(paste(args[1], '/mutSigExtractor', sep = ""))
 setwd(args[2])
 sample = extractSigsForHrdClassifier(args[4], args[4], args[5], args[3], "gridss")
