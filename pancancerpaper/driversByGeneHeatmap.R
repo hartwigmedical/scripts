7326b291e2123c751440ffea53676c9e83376910
Jonathan Baber <jonbaber@gmail.com>
2019-01-09 16:41:41 +1000

    added germline heatmap

--- pancancerpaper/driversByGeneHeatmap.R
+++ pancancerpaper/driversByGeneHeatmap.R
@@ -3,7 +3,9 @@ library(circlize)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(cowplot)
 
+load(file = "~/hmf/RData/Processed/germlineCatalog.RData")
 load("~/hmf/RData/processed/hpcDriversByGene.RData")
 load("~/hmf/RData/processed/driverGenes.RData")
 load(file = '~/hmf/RData/Reference/hpcCancerTypeCounts.RData')
@@ -11,8 +13,12 @@ load(file = "~/hmf/RData/reference/simplifiedDrivers.RData")
 
 tsgDrivers = c("Del","FragileDel","Indel","Missense","Multihit","Nonsense","Splice")
 oncoDrivers = c("Amp","Del","Fusion","Indel","Missense","Promoter")
+germlineDrivers = c("Del", "Indel","Missense", "Multihit","Nonsense", "Splice","Synonymous")
 tsgDriverColours = simplifiedDriverColours[tsgDrivers]
 oncoDriverColours = simplifiedDriverColours[oncoDrivers]
+germlineDriverColours = simplifiedDriverColours[germlineDrivers]
+
+germlineDriversByGene = germlineDriverCatalog %>% filter(highConfidenceGenePanel, is.na(variantLostInTumor) | !variantLostInTumor)
 
 hpcDriversByGene = hpcDriversByGene  %>% 
   filter(driverLikelihood > 0) %>%
@@ -27,17 +33,24 @@ hpcDriversByGene = hpcDriversByGene  %>%
 sortedTsgGenes = hpcDriversByGene %>% filter(type == 'TSG') %>% group_by(gene) %>% summarise(n = sum(driverLikelihood)) %>% ungroup() %>% top_n(30, n) %>% arrange(-n)
 sortedOncoGenes = hpcDriversByGene %>% filter(type == 'ONCO') %>% group_by(gene) %>% summarise(n = sum(driverLikelihood)) %>% ungroup() %>% top_n(30, n) %>% arrange(-n)
 sortedCancerTypes = hpcDriversByGene %>% group_by(cancerType) %>% summarise(n = sum(driverLikelihood)) %>% arrange(-n)
+sortedGermlineGenes = germlineDriversByGene %>% group_by(gene) %>% count() %>% arrange(-n)
 
 oncoDriversByGene = hpcDriversByGene %>% filter(type == 'ONCO', gene %in% sortedOncoGenes$gene) %>% mutate(driver = factor(driver, simplifiedDrivers))
 tsgDriversByGene = hpcDriversByGene %>% filter(type == 'TSG', gene %in% sortedTsgGenes$gene)  %>% mutate(driver = factor(driver, simplifiedDrivers))
+germlineDriversByGene = germlineDriversByGene %>% 
+  mutate(
+    driver = ifelse(mutationClass %in% c("Frameshift", "Inframe"), "Indel", mutationClass),
+    driver = factor(driver, simplifiedDrivers)) %>% mutate(driverLikelihood = 1)
 
-main_heatmap_data <- function(sourceData, cancerTypeSamples) {
+main_heatmap_data <- function(sourceData) {
   heatmapData = sourceData %>%
     group_by(cancerType, gene) %>% summarise(n = sum(driverLikelihood)) %>%
-    left_join(hpcCancerTypeCounts %>% select(cancerType, cancerTypeSamples = N), by = "cancerType") %>%
+    full_join(hpcCancerTypeCounts %>% select(cancerType, cancerTypeSamples = N), by = "cancerType") %>%
     mutate(proportion = n / cancerTypeSamples) %>%
     select(-n, -cancerTypeSamples) %>%
-    spread(cancerType, proportion, fill = 0)
+    spread(cancerType, proportion, fill = 0) %>% 
+    filter(!is.na(gene))
+  
   rownames(heatmapData) <- heatmapData$gene
   heatmapData$gene <- NULL
   return (heatmapData)
@@ -49,6 +62,7 @@ samples_annotation <- function(sourceData) {
     mutate(proportion = round(n / sum(hpcCancerTypeCounts$N), 3)) %>% select(-n) 
   rownames(annotationData1) <- annotationData1$gene
   annotationData1$gene <- NULL
+  annotationData1$non <- 0
   return (annotationData1)
 }
 
@@ -82,16 +96,36 @@ biallelic_annotation <- function(sourceData) {
   return (biallelicData)
 }
 
+wildtypelost_annotation <- function(sourceData) {
+  wildData = sourceData %>% 
+    mutate(wildTypeLostInTumor = ifelse(is.na(wildTypeLostInTumor), F, wildTypeLostInTumor)) %>%
+    group_by(gene, wildTypeLostInTumor) %>% 
+    summarise(n = sum(driverLikelihood)) %>% 
+    mutate(wildTypeLostInTumor = ifelse(wildTypeLostInTumor, "lost", "notLost")) %>%
+    spread(wildTypeLostInTumor, n, fill = 0) %>%
+    mutate(lostPercentage = lost / (lost + notLost) ) %>%  
+    select(gene, lostPercentage) %>%
+    mutate(notLostPercentage = 1 - lostPercentage)
+  
+  rownames(wildData) <- wildData$gene
+  wildData$gene <- NULL
+  return (wildData)
+}
 
-tsgHeatmapData = main_heatmap_data(tsgDriversByGene, cancerTypeSamples)
+
+tsgHeatmapData = main_heatmap_data(tsgDriversByGene)
 tsgSamplesAnnotationData = samples_annotation(tsgDriversByGene)
 tsgDriversAnnotationData = driver_annotation(tsgDriversByGene)
 tsgBiallelicAnnotationData = biallelic_annotation(tsgDriversByGene)
 
-oncoHeatmapData = main_heatmap_data(oncoDriversByGene, cancerTypeSamples)
+oncoHeatmapData = main_heatmap_data(oncoDriversByGene)
 oncoSamplesAnnotationData = samples_annotation(oncoDriversByGene)
 oncoDriversAnnotationData = driver_annotation(oncoDriversByGene)
 
+germlineHeatmapData = main_heatmap_data(germlineDriversByGene)
+germlineSamplesAnnotationData = samples_annotation(germlineDriversByGene)
+germlineDriversAnnotationData = driver_annotation(germlineDriversByGene)
+germlineWildtypeLostData = wildtypelost_annotation(germlineDriversByGene)
 
 heat_map_text <- function(value) {
   value = round(100*value, 1)
@@ -101,13 +135,63 @@ heat_map_text <- function(value) {
   return ("")
 }
 
+germlineMat = (data.matrix(germlineHeatmapData)) 
+germlineHeatmap = Heatmap(
+  column_title = "  ",
+  germlineMat, 
+  row_names_gp = gpar(fontsize = 9),
+  column_names_gp = gpar(fontsize = 9),
+  row_order = sortedGermlineGenes$gene,
+  column_order = sortedCancerTypes$cancerType,
+  cluster_rows = FALSE, cluster_columns = FALSE, show_heatmap_legend = FALSE,
+  col = colorRamp2(c(0, 0.33, 0.66, 0.1), c("#f7fcf5","#bae4b3","#74c476","#238b45")), 
+  cell_fun = function(j, i, x, y, w, h, col) {
+    myColor = "black"
+    if (germlineMat[i, j] > 0.15) {
+      myColor = "white"
+    }
+    grid.text(heat_map_text(germlineMat[i, j]), x, y, gp=gpar(fontsize=8, col = myColor))
+  }
+)
+
+
+
+germlineSamplesAnnotation = rowAnnotation(
+  `% Samples` = row_anno_barplot(baseline = "min", germlineSamplesAnnotationData, axis = T, axis_side = "top", ylim = c(0,0.1), gp = gpar(fill = "#bc80bd"), border = F), 
+  width = unit(2, "cm"),
+  show_annotation_name = T,
+  annotation_name_gp  = gpar(fontsize = 7),
+  annotation_name_rot = 0
+)
+
+germlineDriversAnnotation = rowAnnotation(
+  Drivers = row_anno_barplot(germlineDriversAnnotationData, axis = T, axis_side = "top", ylim = c(0,1), gp = gpar(fill = germlineDriverColours), border = F), 
+  width = unit(3, "cm"),
+  show_annotation_name = T,
+  annotation_name_gp  = gpar(fontsize = 7),
+  annotation_name_rot = 0
+)
+
+
+germlineWildTypeLostAnnotationColours = setNames(c("#ccebc5","white"), c("lostPercentage", "notLostPercentage"))
+germlineWildTypeLostAnnotation = rowAnnotation(
+  show_annotation_name = T,
+  annotation_name_rot = 0,
+  annotation_name_gp  = gpar(fontsize = 7),
+  `% Wild Type Lost` = row_anno_barplot(germlineWildtypeLostData, axis = T, axis_side = "top", ylim = c(0,1), gp = gpar(fill = germlineWildTypeLostAnnotationColours), border = F), 
+  width = unit(2, "cm")
+)
+
+
+pGermline = germlineHeatmap + germlineSamplesAnnotation + germlineDriversAnnotation + germlineWildTypeLostAnnotation
+pGermline
 
 oncoMat = (data.matrix(oncoHeatmapData))
 oncoHeatmap = Heatmap(
   column_title = "  ",
   oncoMat, 
   row_names_gp = gpar(fontsize = 9),
-  column_names_gp = gpar(fontsize = 9),
+  column_names_gp = gpar(fontsize = 0),
   row_order = sortedOncoGenes$gene,
   column_order = sortedCancerTypes$cancerType,
   cluster_rows = FALSE, cluster_columns = FALSE, show_heatmap_legend = FALSE,
@@ -126,6 +210,7 @@ oncoSamplesAnnotation = rowAnnotation(
   `% Samples` = row_anno_barplot(oncoSamplesAnnotationData, axis = T, axis_side = "top", ylim = c(0,0.2), gp = gpar(fill = "#bc80bd"), border = F), 
   width = unit(2, "cm"),
   show_annotation_name = T,
+  annotation_name_gp  = gpar(fontsize = 7),
   annotation_name_rot = 0
 )
 
@@ -133,6 +218,7 @@ oncoDriversAnnotation = rowAnnotation(
   Drivers = row_anno_barplot(oncoDriversAnnotationData, axis = T, axis_side = "top", ylim = c(0,1), gp = gpar(fill = oncoDriverColours), border = F), 
   width = unit(3, "cm"),
   show_annotation_name = T,
+  annotation_name_gp  = gpar(fontsize = 7),
   annotation_name_rot = 0
 )
 
@@ -140,21 +226,18 @@ oncoDriversAnnotationIndex = rowAnnotation(
   df = data.frame(Driver = simplifiedDrivers),
   col = list(Driver = simplifiedDriverColours),
   width = unit(0, "cm"),
-  annotation_legend_param = list(title = "")
+  annotation_legend_param = list(title = "", width = unit(2, "cm"), labels_gp = gpar(fontsize = 7))
 )
 
 pOnco = oncoHeatmap + oncoSamplesAnnotation + oncoDriversAnnotation + oncoDriversAnnotationIndex
-png(file = "~/hmf/RPlot/Figure 3A - Onco.png", width = 10, height = 7, units = "in", res = 217)
 pOnco
-dev.off()
-
 
 tsgMat = (data.matrix(tsgHeatmapData))
 tsgHeatmap = Heatmap(
   column_title = "  ",
   tsgMat, 
   row_names_gp = gpar(fontsize = 9),
-  column_names_gp = gpar(fontsize = 9),
+  column_names_gp = gpar(fontsize = 0),
   row_order = sortedTsgGenes$gene,
   column_order = sortedCancerTypes$cancerType,
   cluster_rows = FALSE, cluster_columns = FALSE, show_heatmap_legend = FALSE,
@@ -172,12 +255,14 @@ tsgSamplesAnnotation = rowAnnotation(
   `% Samples` = row_anno_barplot(tsgSamplesAnnotationData, axis = T, axis_side = "top", ylim = c(0,0.6), gp = gpar(fill = "#bc80bd"), border = F), 
   width = unit(2, "cm"),
   show_annotation_name = T,
+  annotation_name_gp  = gpar(fontsize = 7),
   annotation_name_rot = 0
 )
 
 tsgBiallelicAnnotationColours = setNames(c("#ccebc5","white"), c("biallelicPercentage", "nonBiallelicPercentage"))
 tsgBiallelicAnnotation = rowAnnotation(
   show_annotation_name = T,
+  annotation_name_gp  = gpar(fontsize = 7),
   annotation_name_rot = 0,
   `% Biallelic` = row_anno_barplot(tsgBiallelicAnnotationData, axis = T, axis_side = "top", ylim = c(0,1), gp = gpar(fill = tsgBiallelicAnnotationColours), border = F), 
   width = unit(2, "cm")
@@ -187,25 +272,23 @@ tsgDriversAnnotation = rowAnnotation(
   Drivers = row_anno_barplot(tsgDriversAnnotationData, axis = T, axis_side = "top", ylim = c(0,1), gp = gpar(fill = tsgDriverColours), border = F), 
   width = unit(3, "cm"),
   show_annotation_name = T,
+  annotation_name_gp  = gpar(fontsize = 7),
   annotation_name_rot = 0
 )
 
-tsgDriversAnnotationIndex = rowAnnotation(
-  df = data.frame(Driver = tsgDrivers),
-  col = list(Driver = tsgDriverColours),
-  width = unit(0, "cm"),
-  annotation_legend_param = list(title = "")
-  
-)
-
 pTSG = tsgHeatmap + tsgSamplesAnnotation + tsgDriversAnnotation + tsgBiallelicAnnotation
-tsgHeatmap + tsgSamplesAnnotation + tsgBiallelicAnnotation + tsgDriversAnnotation + tsgDriversAnnotationIndex
-
-png(file = "~/hmf/RPlot/Figure 3B - TSG.png", width = 10, height = 7, units = "in", res = 217)
 pTSG
-dev.off()
 
-####################
+p_tsg_grob = grid.grabExpr(draw(pTSG))
+p_onco_grob = grid.grabExpr(draw(pOnco))
+p_germline_gro= grid.grabExpr(draw(pGermline))
+
+p_final = plot_grid(p_tsg_grob, p_onco_grob, p_germline_gro, labels = "AUTO", ncol = 1, rel_heights = c(1, 1, 0.9))
+save_plot("~/hmf/RPlot/Figure 3 - Driver Map.png", p_final, base_width = 14, base_height = 19)
+
+
+
+#################
 ### Drivers per sample by driver Type
 #### TO DO: Make colours consistent
 head(hpcDriversByGene)
