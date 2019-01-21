3125ce3e35d8eaebaf353182d379401b238d0579
Daniel Cameron <d-cameron@github.com>
2019-01-21 16:56:13 +1100

    Rewrote PON script - reduced peak memory usage - batched updating of PON - explicit tracking of PON samples - using exact matching of calls

--- gridss/create_gridss_pon.R
+++ gridss/create_gridss_pon.R
@@ -8,17 +8,16 @@ argp = add_argument(argp, "--pondir", default=NA, help="Directory to write PON t
 argp = add_argument(argp, "--scriptdir", default=ifelse(sys.nframe() == 0, "./", dirname(sys.frame(1)$ofile)), help="Path to libgridss.R script")
 argp = add_argument(argp, "--cache-only", flag=TRUE, help="Only generate cache objects for input files. Useful for parallel processing of inputs.")
 argp = add_argument(argp, "--normalordinal", type="integer", default=1, help="Ordinal of normal sample in the VCF")
+argp = add_argument(argp, "--batchsize", default=100, help="Number of VCFs to process")
 argp = add_argument(argp, "--input", nargs=Inf, help="Input VCFs normal")
-# argv = parse_args(argp, argv=c("--pondir", "~/pon/", "--scriptdir", "/data/common/repos/scripts/gridss/", "--input", "a.vcf", "b.vcf"))
+# argv = parse_args(argp, argv=c("--pondir", "D:/hartwig/pon/", "--scriptdir", "D:/hartwig/scripts/gridss", "--batchsize", "1", "--input", "D:/hartwig/down/COLO829R_COLO829T.gridss.vcf", "D:/hartwig/down/DRUP01010047R_DRUP01010047T.gridss.vcf"))
 argv = parse_args(argp)
 
 if (!dir.exists(argv$pondir)) {
   write("PON directory not found", stderr())
-  write(usage, stderr())
   q(save="no", status=1)
 }
-vcf_list = argv$input
-if (!any(file.exists(vcf_list))) {
+if (!all(file.exists(argv$input))) {
   write("VCF input file not found", stderr())
   q(save="no", status=1)
 }
@@ -27,6 +26,7 @@ if (file.exists(libgridssfile)) {
   tmpwd = getwd()
   setwd(argv$scriptdir)
   library(tidyverse, quietly=TRUE)
+  library(readr, quietly=TRUE)
   library(stringr, quietly=TRUE)
   library(rtracklayer, quietly=TRUE)
   library(R.cache, quietly=TRUE)
@@ -38,6 +38,11 @@ if (file.exists(libgridssfile)) {
   print(argp)
   stop(msg)
 }
+
+samplefile = paste(argv$pondir, "gridss_pon_samples.txt", sep="/")
+befile = paste(argv$pondir, "gridss_pon_single_breakend.bed", sep="/")
+beimpfile = paste(argv$pondir, "gridss_pon_single_breakend_imprecise.bed", sep="/")
+bpfile = paste(argv$pondir, "gridss_pon_breakpoint.bedpe", sep="/")
 
 setCacheRootPath(paste0(argv$pondir, "/Rcache"))
 options("R.cache.compress"=TRUE)
@@ -74,91 +79,114 @@ load_germline_pon_calls = function(vcf_file, sampleId) {
   write(paste("End load", vcf_file), stderr())
   return(result)
 }
-full_bp = list()
-full_be = list()
-for (vcf_file in vcf_list) {
-  sampleId = str_replace(str_replace(basename(vcf_file), ".gridss.vcf.gz", ""), ".gridss.vcf", "")
-  calls = load_germline_pon_calls(vcf_file, sampleId)
-  full_bp[[sampleId]] = calls$bp
-  full_be[[sampleId]] = calls$be
+load_pon = function() {
+  for (f in c(samplefile, befile, beimpfile, bpfile)) {
+    if (!file.exists(f)) {
+      write(paste("Missing pon file", f, "creating new empty file."), stderr())
+      file.create(f)
+    }
+  }
+  pon = list(
+    samples = read_tsv(samplefile, col_names=c("sample"), col_types=c("c")),
+    bedf = read_tsv(befile, col_names=c("chrom", "chromStart", "chromEnd", "name", "score", "strand"), col_types=c("ciicic")),
+    beimpdf = read_tsv(beimpfile, col_names=c("chrom", "chromStart", "chromEnd", "name", "score", "strand"), col_types=c("ciicic")),
+    bpdf = read_tsv(bpfile, col_names=c("chrom1", "start1", "end1", "chrom2", "start2", "end2", "name", "score", "strand1", "strand2", "IMPRECISE"), col_types=c("ciiciiciccl")))
+  return(pon)
 }
-if (argv$cache_only) {
-  write("Caching complete ", stderr())
-  q(save="no", status=0)
+save_pon = function(pon) {
+  withr::with_options(c(scipen = 10), write.table(pon$samples, file=samplefile, quote=FALSE, sep='\t', row.names=FALSE, col.names=FALSE))
+  withr::with_options(c(scipen = 10), write.table(
+    pon$bedf %>% arrange(chrom, chromStart, chromEnd, strand),
+    file=befile, quote=FALSE, sep='\t', row.names=FALSE, col.names=FALSE))
+  withr::with_options(c(scipen = 10), write.table(
+    pon$beimpdf %>% arrange(chrom, chromStart, chromEnd, strand),
+    file=beimpfile, quote=FALSE, sep='\t', row.names=FALSE, col.names=FALSE))
+  withr::with_options(c(scipen = 10), write.table(
+    pon$bpdf %>% arrange(chrom1, start1, end1, chrom2, start2, end2, strand1, strand2),
+    file=bpfile, quote=FALSE, sep='\t', row.names=FALSE, col.names=FALSE))
 }
-
-bpdf = bind_rows(lapply(full_bp, function(x) {
+as_beddf = function(begr) {
   data.frame(
-    seqnames = as.character(seqnames(x)),
-    start = start(x),
-    end = end(x),
-    strand = as.character(strand(x)),
-    name = names(x),
+    chrom = as.character(seqnames(begr)),
+    chromStart = start(begr) - 1,
+    chromEnd = end(begr),
+    name = ".",
+    score = 1,
+    strand = as.character(strand(begr)),
+    stringsAsFactors=FALSE)
+}
+as_bedpedf = function(bpgr) {
+  lowergr = bpgr[str_detect(names(bpgr), "o$")]
+  uppergr = bpgr[lowergr$partner]
+  data.frame(
+    chrom1=as.character(GenomeInfoDb::seqnames(lowergr)),
+    start1=start(lowergr) - 1,
+    end1=end(lowergr),
+    chrom2=as.character(GenomeInfoDb::seqnames(uppergr)),
+    start2=start(uppergr) - 1,
+    end2=end(uppergr),
+    name=".",
     score=1,
-    partner = x$partner,
-    IMPRECISE = x$IMPRECISE,
-    stringsAsFactors=FALSE
-  )})) %>%
-  # preferentially call the precise call with the largest homology
-  # name included purely to ensure stable sort order
-  arrange(IMPRECISE, desc(end - start), name)
-bpgr = as(bpdf, "GRanges")
-names(bpgr) = bpdf$name
-hits = findBreakpointOverlaps(bpgr, bpgr) %>%
-  group_by(queryHits) %>%
-  summarise(n=n())
-bpdf$score[hits$queryHits] = hits$n
-bedpe = bpdf %>% mutate(
-    chrom1=seqnames,
-    start1=start,
-    end1=end,
-    chrom2=as.character(GenomeInfoDb::seqnames(partner(bpgr))),
-    start2=BiocGenerics::start(partner(bpgr)),
-    end2=BiocGenerics::end(partner(bpgr)),
-    score=score,
-    strand1=strand,
-    strand2=as.character(BiocGenerics::strand(partner(bpgr))),
-    IMPRECISE=IMPRECISE) %>%
-  mutate(
-    # switch from 1-based [ ] to BED 0-based [ ) indexing
-    start1=start1 - 1,
-    start2=start2 - 1) %>%
-  group_by(chrom1, start1, end1, strand1, IMPRECISE, chrom2, start2, end2, strand2) %>%
-  summarise(score=max(score)) %>%
-  filter(score >= gridss.pon.min_samples) %>%
-  filter(chrom1 < chrom2 | (chrom1 == chrom2 & start1 <= start2)) %>%
-  arrange(chrom1, start1, chrom2, start2) %>%
-  mutate(name=".") %>%
-  dplyr::select(chrom1, start1, end1, chrom2, start2, end2, name, score, strand1, strand2, IMPRECISE)
-withr::with_options(c(scipen = 10), write.table(bedpe, paste(argv$pondir, "gridss_pon_breakpoint.bedpe", sep="/"), quote=FALSE, sep='\t', row.names=FALSE, col.names=FALSE))
+    strand1=as.character(strand(lowergr)),
+    strand2=as.character(strand(uppergr)),
+    IMPRECISE=lowergr$IMPRECISE,
+    stringsAsFactors=FALSE)
+}
+merge_pon_be = function(...) {
+  bind_rows(...) %>%
+    group_by(chrom, chromStart, chromEnd, strand) %>%
+    summarise(score=sum(score)) %>%
+    ungroup() %>%
+    mutate(name=".") %>%
+    dplyr::select(chrom, chromStart, chromEnd, name, score, strand)
+}
+merge_pon_bp = function(...) {
+  bind_rows(...) %>%
+    group_by(chrom1, start1, end1, chrom2, start2, end2, strand1, strand2, IMPRECISE) %>%
+    summarise(score=sum(score)) %>%
+    ungroup() %>%
+    mutate(name=".") %>%
+    dplyr::select(chrom1, start1, end1, chrom2, start2, end2, name, score, strand1, strand2, IMPRECISE)
+}
+merge_pon_calls = function(pon, samples, bedf, beimpdf, bpdf) {
+  assertthat::assert_that(!any(pon$samples$samples %in% samples))
+  pon$samples = bind_rows(pon$samples, samples)
+  pon$bedf = merge_pon_be(pon$bedf, bedf)
+  pon$beimpdf = merge_pon_be(pon$beimpdf, beimpdf)
+  pon$bpdf = merge_pon_bp(pon$bpdf, bpdf)
+  return(pon)
+}
 
-bedf = bind_rows(lapply(full_be, function(x) {
-  data.frame(
-    seqnames = seqnames(x),
-    start = start(x),
-    end = end(x),
-    strand = strand(x),
-    vcf = x$vcf,
-    IMPRECISE = x$IMPRECISE
-  )})) %>%
-  arrange(vcf, seqnames, start, desc(end - start))
-begr = GRanges(
-  seqnames=bedf$seqnames,
-  ranges=IRanges(
-    start=bedf$start,
-    end=bedf$end),
-  strand=bedf$strand)
-bedf$score = countOverlaps(begr, begr)
-bedf = bedf %>%
-  group_by(seqnames, start, end, strand, IMPRECISE) %>%
-  summarise(score=max(score)) %>%
-  filter(score >= gridss.pon.min_samples)
-ponbe = GRanges(
-    seqnames=bedf$seqnames,
-    ranges=IRanges(
-      start=bedf$start,
-      end=bedf$end),
-    strand=bedf$strand,
-    score=bedf$score,
-    IMPRECISE=bedf$IMPRECISE)
-export(ponbe, con=paste(argv$pondir, "gridss_pon_single_breakend.bed", sep="/"), format="bed")
+###
+# Start script
+###
+if (!argv$cache_only) {
+  pon = load_pon()
+}
+for (chunk_files in split(argv$input, ceiling(seq_along(argv$input)/argv$batchsize))) {
+  full_bp = list()
+  full_be = list()
+  samples = list()
+  for (vcf_file in chunk_files) {
+    sampleId = str_replace(str_replace(basename(vcf_file), ".gridss.vcf.gz", ""), ".gridss.vcf", "")
+    if (!is.null(pon$samples$samples) & sampleId %in% pon$samples$samples) {
+      write(paste("Skipping", sampleId, "already in PON."), stderr())
+    } else {
+      calls = load_germline_pon_calls(vcf_file, sampleId)
+      samples[[sampleId]] = data.frame(samples=sampleId, stringsAsFactors=FALSE)
+      full_bp[[sampleId]] = calls$bp
+      full_be[[sampleId]] = calls$be
+    }
+  }
+  if (!argv$cache_only && length(full_bp) > 0) {
+    write(paste("Updating PON with", length(full_bp), "new samples"), stderr())
+    pon = merge_pon_calls(
+      pon,
+      bind_rows(samples),
+      bind_rows(lapply(full_be, function(x) {as_beddf(x[!x$IMPRECISE])})),
+      bind_rows(lapply(full_be, function(x) {as_beddf(x[x$IMPRECISE])})),
+      bind_rows(lapply(full_bp, as_bedpedf)))
+    save_pon(pon)
+  }
+}
+write("Complete", stderr())
