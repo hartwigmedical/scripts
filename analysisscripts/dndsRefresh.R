f3b30c7dd89507adcc12e3551eb3a6c3e11b2532
Jonathan Baber <jonbaber@gmail.com>
2019-04-17 14:28:40 +1000

    biallelic

--- analysisscripts/dndsRefresh.R
+++ analysisscripts/dndsRefresh.R
@@ -4,9 +4,11 @@ library(RMySQL)
 library(dplyr)
 library(tidyr)
 library(dndscv)
-
+library(GenomicRanges)
 
 ####### DB
+#somaticQuery = paste0("select * from somaticVariant where filter = 'PASS' and gene <> '' and worstCodingEffect != 'NONE' AND sampleId in (",sampleIdString, ")")
+
 dbProd = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis", host = "127.0.0.1")
 dbDisconnect(dbProd)
 rm(dbProd)
@@ -16,11 +18,30 @@ entireCohort = purple::query_cohort(dbProd)
 highestPurityCohort = purple::highest_purity_cohort(entireCohort)
 save(highestPurityCohort, file = "~/hmf/analysis/dnds/highestPurityCohort.RData")
 
-sampleIdString = paste("'", highestPurityCohort$sampleId, "'", collapse = ",", sep = "")
-somaticQuery = paste0("select * from somaticVariant where filter = 'PASS' and gene <> '' and worstCodingEffect != 'NONE' AND sampleId in (",sampleIdString, ")")
-exonicSomatics = dbGetQuery(dbProd, somaticQuery)
-save(exonicSomatics, file = "~/hmf/analysis/dnds/exonicSomatics.RData")
+sampleIdString1 = paste("'", highestPurityCohort[1:1740, ]$sampleId, "'", collapse = ",", sep = "")
+somaticQuery1 = paste0("select * from somaticVariant where filter = 'PASS' AND sampleId in (",sampleIdString1, ")")
+somatics1 = dbGetQuery(dbProd, somaticQuery1)
+save(somatics1, file = "~/hmf/analysis/dnds/somatics1.RData")
 
+sampleIdString2 = paste("'", highestPurityCohort[1741:3483, ]$sampleId, "'", collapse = ",", sep = "")
+somaticQuery2 = paste0("select * from somaticVariant where filter = 'PASS' AND sampleId in (",sampleIdString2, ")")
+somatics2 = dbGetQuery(dbProd, somaticQuery2)
+save(somatics2, file = "~/hmf/analysis/dnds/somatics2.RData")
+
+load(file = "~/hmf/RData/reference/HmfRefCDS.RData")
+exonic_somatics <- function(somatics, gr_genes) {
+  gr_muts = GRanges(somatics$chromosome, IRanges(somatics$position,somatics$position + nchar(somatics$ref) - 1))
+  ol = as.matrix(findOverlaps(gr_muts, gr_genes, type="any", select="all"))
+  return (somatics[unique(ol[, 1]), ])
+}
+
+exonicSomatics1 = exonic_somatics(somatics1, gr_genes)
+exonicSomatics2 = exonic_somatics(somatics2, gr_genes)
+exonicSomatics = rbind(exonicSomatics1, exonicSomatics2)
+save(exonicSomatics, file = "~/hmf/analysis/dnds/exonicSomatics.RData")
+rm(exonicSomatics1, exonicSomatics2, gr_genes, RefCDS)
+
+sampleIdString = paste("'", highestPurityCohort$sampleId, "'", collapse = ",", sep = "")
 somaticCountsQuery = paste0("select sampleId, type, count(*) as total from somaticVariant where filter = 'PASS' and sampleId in (",sampleIdString, ") group by sampleId, type")
 somaticCounts = dbGetQuery(dbProd, somaticCountsQuery)
 somaticCounts = somaticCounts %>% mutate(type = paste0("TOTAL_", type)) %>% spread(type, total)
@@ -28,7 +49,7 @@ save(somaticCounts, file = "~/hmf/analysis/dnds/somaticCounts.RData")
 
 ####### DNDS HmfRefCDSCv 
 load(file = "~/hmf/analysis/dnds/highestPurityCohort.RData")
-load(file = "~/hmf/analysis/dnds/exonicSomatics_question.RData")
+load(file = "~/hmf/analysis/dnds/exonicSomatics.RData")
 
 somatics = exonicSomatics %>% 
   filter(type == "INDEL" | type == "SNP", repeatCount <= 8) %>%
@@ -45,10 +66,25 @@ rownames(cv) = newgenes
 kc = newgenes[oldgenes %in% known_cancergenes]
 
 output = dndscv(somatics, refdb=refdb, kc=kc, cv=cv, stop_loss_is_nonsense = TRUE)
-#save(output, file = "~/hmf/analysis/dnds/dndsOutput.RData")
-
 HmfRefCDSCv  <- output$sel_cv
 save(HmfRefCDSCv, file = "~/hmf/analysis/dnds/HmfRefCDSCv.RData")
+
+
+biallelicSomatics = exonicSomatics %>% 
+  filter(type == "SNP", repeatCount <= 8, biallelic) %>%
+  select(sampleId, chr = chromosome, pos = position, ref, alt)
+
+biallelicOutput = dndscv(biallelicSomatics, refdb=refdb, kc=kc, cv=cv, stop_loss_is_nonsense = TRUE)
+HmfRefCDSCvBiallelic  <- biallelicOutput$sel_cv
+save(HmfRefCDSCvBiallelic, file = "~/hmf/analysis/dnds/HmfRefCDSCvBiallelic.RData")
+
+nonBiallelicSomatics = exonicSomatics %>% 
+  filter(type == "SNP", repeatCount <= 8, !biallelic) %>%
+  select(sampleId, chr = chromosome, pos = position, ref, alt)
+
+nonBiallelicOutput = dndscv(nonBiallelicSomatics, refdb=refdb, kc=kc, cv=cv, stop_loss_is_nonsense = TRUE)
+HmfRefCDSCvNonBiallelic  <- nonBiallelicOutput$sel_cv
+save(HmfRefCDSCvNonBiallelic, file = "~/hmf/analysis/dnds/HmfRefCDSCvNonBiallelic.RData")
 
 
 ####### DNDS UNFILTERED ANNOTATED MUTATIONS
@@ -60,6 +96,7 @@ save(dndsUnfilteredAnnotatedMutations, file = "~/hmf/analysis/dnds/exonicSomatic
 
 
 ############################################   HIGHEST PURITY COHORT ############################################
+load(file = "~/hmf/analysis/dnds/highestPurityCohort.RData")
 load(file = "~/hmf/analysis/dnds/somaticCounts.RData")
 somaticCounts[is.na(somaticCounts)] <- 0
 somaticCounts = somaticCounts %>%
@@ -73,12 +110,25 @@ load(file = "~/hmf/analysis/dnds/HmfRefCDSCv.RData")
 load(file = "~/hmf/analysis/dnds/exonicSomaticsDndsAnnotated.RData")
 load(file = "~/hmf/analysis/dnds/exonicSomatics.RData")
 HmfRefCDSCv$cancerType <- "All"
+
+####### JON JON
+#dndsExpectedDriversPerGene = dnds_expected_drivers(HmfRefCDSCv, dndsUnfilteredAnnotatedMutations, somatics)
+#mutations = dnds_annotate_somatics(dndsUnfilteredAnnotatedMutations, somatics) %>%
+#  mutate(patient = substr(sampleId, 1,12), pHGVS = "") %>%
+#  select(-patient) 
+#mutations = mutations %>% filter(gene %in% genePanel$gene_name, impact != "")
+
+tsgMutations = tsg_mutations(mutations %>% filter(gene %in% tsGenes$gene_name))
+tsgDriverRates = dnds_driver_rates(HmfRefCDSCv, tsgMutations)
+
+
+############################################CODE RUNS FROM HERE  ############################################
 somatics = exonicSomatics %>%
   mutate(subclonalLikelihood = 0) %>%
   select(sampleId, chromosome, position, ref, alt, type, worstCodingEffect, canonicalCodingEffect, hotspot, biallelic, subclonalLikelihood, repeatCount) %>%
   mutate(shared = F)
-dndsExpectedDriversPerGene = dnds_expected_drivers(HmfRefCDSCv, dndsUnfilteredAnnotatedMutations, somatics)
 
+dndsExpectedDriversPerGene = dnds_expected_drivers(HmfRefCDSCv, dndsUnfilteredAnnotatedMutations, somatics)
 save(dndsExpectedDriversPerGene, file = "~/hmf/analysis/dnds/dndsExpectedDriversPerGene.RData")
 
 mutations = dnds_annotate_somatics(dndsUnfilteredAnnotatedMutations, somatics) %>%
@@ -146,5 +196,66 @@ onco = oncoDriverLikelihood %>% left_join(oncoDriverLikelihoodAdjustment, by = "
          dndsDriverLikelihoodIndel = 0, pDriverIndel = 0, pVariantNonDriverFactorIndel = 0) 
 write.table(onco, "~/hmf/analysis/dnds/DndsDriverLikelihoodOnco.tsv", quote = F, row.names = F, sep = "\t")
 
+##### IMPACT
+load(file = "~/hmf/RData/processed/driverGenes.RData")
+genePanel = bind_rows(oncoGenes, tsGenes)
 
+load(file = "~/hmf/Rdata/Processed/HmfRefCDSCv.RData")
+Old = HmfRefCDSCv %>% filter(cancerType == 'All', gene_name %in% genePanel$gene_name)
+
+load(file = "~/hmf/analysis/dnds/HmfRefCDSCv.RData")
+New = HmfRefCDSCv %>% filter(gene_name %in% genePanel$gene_name)
+New$change = round((New$wmis_cv - Old$wmis_cv) * New$n_mis, 2)
+save(New, Old, file = "~/hmf/Rdata/Processed/ImpactOfNewData.RData")
+
+#### KRAS
+
+load(file = "~/hmf/analysis/dnds/exonicSomaticsDndsAnnotated.RData")
+dndsUnfilteredAnnotatedMutations %>% filter(gene == 'KRAS', impact == 'Synonymous')
+
+load(file = "~/hmf/RData/processed/hpcExonicSomaticsDndsAnnotated.RData")
+dndsUnfilteredAnnotatedMutations %>% filter(gene == 'KRAS', impact == 'Synonymous')
+
+
+
+load(file = "~/hmf/RData/Processed/hpcDndsTsgDriverRates.RData")
+
+
+load(file = "~/hmf/analysis/dnds/OldDataNewLogicHmfRefCDSCv.RData")
+OldDataNewLogic = HmfRefCDSCv
+
+load(file = "~/hmf/Rdata/Processed/HmfRefCDSCv.RData")
+Old = HmfRefCDSCv
+
+load(file = "~/hmf/analysis/dnds3/HmfRefCDSCv.RData")
+NewDataNewLogic = HmfRefCDSCv
+
+load(file = "~/hmf/analysis/dnds2/HmfRefCDSCv.RData")
+Updated = HmfRefCDSCv
+
+Old %>% filter(gene_name == "RB1", cancerType == 'All')
+OldDataNewLogic %>% filter(gene_name == "RB1")
+NewDataNewLogic %>% filter(gene_name == "RB1")
+Updated %>% filter(gene_name == "RB1")
+
+
+load(file = "~/hmf/RData/Processed/hpcDndsTsgDrivers.RData")
+
+####OLD DATA
+load(file = "~/hmf/RData/reference/highestPurityCohort.RData")
+load(file = "~/hmf/RData/reference/hpcExonicSomatics.RData")
+exonicSomatics = hpcExonicSomatics
+
+
+load(file = "~/hmf/Rdata/Processed/HmfRefCDSCv.RData")
+load(file = "~/hmf/RData/processed/hpcExonicSomaticsDndsAnnotated.RData")
+load(file = "~/hmf/RData/processed/driverGenes.RData")
+
+
+
+load(file = "~/hmf/RData/processed/highestPurityCohortSummary.RData")
+somaticCounts = highestPurityCohortSummary %>% select(sampleId, ends_with("SNV"), ends_with("INDEL"))
+somaticCounts = somaticCounts %>%
+  mutate(sample_SNV = TOTAL_SNV, sample_INDEL = TOTAL_INDEL) %>%
+  select(starts_with("sample"))
 
