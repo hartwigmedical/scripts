51490885296224d56e171a58c19d90deab4a8c33
Daniel Cameron <d-cameron@github.com>
2019-04-26 21:57:44 +1000

    Integrated somatic simple TI comparison

--- analysisscripts/germlineTIs/process_candidates.R
+++ analysisscripts/germlineTIs/process_candidates.R
@@ -17,7 +17,7 @@ vcf_to_breakpoints = function(file, sampleId=str_match(file, "([^/\\\\]*).gridss
   return(gr)
 }
 
-simple_ti_candidates = function(gr, insert_site_max_gap=35, template_insert_max_length=10000) {
+  = function(gr, insert_site_max_gap=35, template_insert_max_length=10000) {
   gr$repeatClass = grrm$repeatClass[ findOverlaps(gr, grrm, select="first", ignore.strand=TRUE)]
   gr$repeatType = grrm$repeatType[ findOverlaps(gr, grrm, select="first", ignore.strand=TRUE)]
   pgr = partner(gr)
@@ -53,10 +53,11 @@ simple_ti_candidates = function(gr, insert_site_max_gap=35, template_insert_max_
   # Get logic from BEALN annotation code
   df = hits %>% mutate(
       asmlinked=elementNROWS(intersect(gr[queryHits]$asmlinks, gr[subjectHits]$asmlinks)) > 0,
+      sampleId=gr[queryHits]$sampleId,
       vcfid1=names(gr)[subjectHits],
       vcfid2=names(gr)[queryHits],
-      hom1=as.integer(info(vcf[names(gr)[subjectHits]])$HOMLEN),
-      hom2=as.integer(info(vcf[names(gr)[queryHits]])$HOMLEN),
+      hom1=gr$HOMLEN[subjectHits],
+      hom2=gr$HOMLEN[queryHits],
       rcins1=gr$repeatClass[subjectHits],
       rcins2=gr$repeatClass[queryHits],
       rcti1=pgr$repeatClass[subjectHits],
@@ -101,25 +102,54 @@ source("../analysisscripts/libSvAnalyser.R")
 dbProd = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis")
 svTable = query_somatic_structuralVariants(dbProd)
 somatic_raw_gr = to_sv_gr(svTable, FALSE)
+somatic_raw_gr$HOMLEN = str_length(somatic_raw_gr$homseq)
 somatic_raw_gr = somatic_raw_gr[!is.na(somatic_raw_gr$partner)]
-somatic_raw_gr$asmlinks = setdiff(CharacterList(paste(somatic_raw_gr$linkedBy, partner(somatic_raw_gr)$linkedBy, sep=",")), ".")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$linkedBy, "/[0-9]+", "")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$asmlinkedBy, "[.]", "")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$asmlinkedBy, "dsb[0-9]+", "")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$asmlinkedBy, "bpbeins[0-9]+", "")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$asmlinkedBy, "eqv[0-9]+", "")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$asmlinkedBy, "inv[0-9]+", "")
+somatic_raw_gr$asmlinkedBy = paste(somatic_raw_gr$asmlinkedBy, partner(somatic_raw_gr)$asmlinkedBy, sep=",")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$asmlinkedBy, ",,", ",")
+somatic_raw_gr$asmlinkedBy = str_replace_all(somatic_raw_gr$asmlinkedBy, "^,$", "")
+somatic_raw_gr$asmlinks = setdiff(CharacterList(str_split(somatic_raw_gr$asmlinkedBy, ",")), "")
+somatic_raw_grl = lapply(unique(somatic_raw_gr$sampleId), function(x) somatic_raw_gr[somatic_raw_gr$sampleId == x])
+somatic_fulldf = bind_rows(lapply(somatic_raw_grl, simple_ti_candidates))
+somatic_fullgr = unlist(GRangesList(somatic_raw_grl))
 
 
-somatic_list_gr = lapply(unique(somatic_raw_gr$sampleId))
-
-fullgr = unlist(GRangesList(fullgr))
+# Ensure uniqueness by adding sample to identifiers
 names(fullgr) = paste0(fullgr$sampleId, names(fullgr))
 fullgr$partner = paste0(fullgr$sampleId, fullgr$partner)
 fulldf$uid1 = paste0(fulldf$sampleId, fulldf$vcfid1)
 fulldf$uid2 = paste0(fulldf$sampleId, fulldf$vcfid2)
+fulldf$source = "germline"
+fullgr$source = "germline"
+
+names(somatic_fullgr) = paste0(somatic_fullgr$sampleId, names(somatic_fullgr))
+somatic_fullgr$partner = paste0(somatic_fullgr$sampleId, somatic_fullgr$partner)
+somatic_fulldf$uid1 = paste0(somatic_fulldf$sampleId, somatic_fulldf$vcfid1)
+somatic_fulldf$uid2 = paste0(somatic_fulldf$sampleId, somatic_fulldf$vcfid2)
+somatic_fullgr$source = "somatic"
+somatic_fulldf$source = "somatic"
+somatic_fullgr$vcfId = somatic_fullgr$beid
+
+# filter to only rows that are in the df
+other_beid = function(x) { ifelse(str_detect(x, "h$"), str_replace(x, "h$", "o"), str_replace(x, "o$", "h")) }
+merged_gr = c(
+  fullgr[names(fullgr) %in% c(fulldf$uid1, fulldf$uid2, other_beid(c(fulldf$uid1, fulldf$uid2)))],
+  somatic_fullgr[names(somatic_fullgr) %in%
+    c(somatic_fulldf$uid1, somatic_fulldf$uid2,
+      other_beid(c(somatic_fulldf$uid1, somatic_fulldf$uid2)))])
 
 # can't use findBreakpointOverlaps() because there's too many pairwise hits
-pfullgr = partner(fullgr)
-merged = as.data.frame(fullgr) %>%
+pgr = partner(merged_gr)
+merged = as.data.frame(merged_gr) %>%
   dplyr::mutate(
-    seqnames2=as.character(seqnames(pfullgr)),
-    start2=start(pfullgr),
-    strand2=as.character(strand(pfullgr))) %>%
+    seqnames2=as.character(seqnames(pgr)),
+    start2=start(pgr),
+    strand2=as.character(strand(pgr))) %>%
   group_by(seqnames, seqnames2, start, start2, strand, strand2) %>%
   mutate(uid=paste0(sampleId, vcfId)) %>%
   summarise(
@@ -129,13 +159,13 @@ merged = as.data.frame(fullgr) %>%
     n=n()) %>%
   ungroup() %>%
   mutate(id=paste0("merged", row_number()))
-mlookup = dplyr::select(merged, id, uids) %>%
+mlookup = merged %>%
+  dplyr::select(id, uids) %>%
   separate_rows(uids, sep=";")
-fulldf = fulldf %>%
+merged_df = bind_rows(fulldf, somatic_fulldf) %>%
   left_join(mlookup, by=c("uid1"="uids")) %>%
   left_join(mlookup, by=c("uid2"="uids"), suffix=c("1", "2"))
-
-fulldf = fulldf %>%
+merged_df = merged_df %>%
   mutate(
     repins1=str_extract(rcins1, "^[^/]*"),
     repins2=str_extract(rcins2, "^[^/]*"),
@@ -145,13 +175,34 @@ fulldf = fulldf %>%
     repti=ifelse(repti1 == "", repti2, repti1))
 
 
+merged_repeats = function(x) {
+  ifelse(x == "", x, c(
+    "DNA"="DNA",
+    "LINE"="LINE",
+    "SINE"="SINE",
+    "Low_complexity"="Low complexity/simple repeat",
+    "Simple_repeat"="Low complexity/simple repeat",
+    "LTR"="LTR",
+    "LTR?"="LTR",
+    "Other"="Other",
+    "RNA"="Other",
+    "Unknown"="Other",
+    "Satellite"="Other",
+    "snRNA"="Other",
+    "srpRNA"="Other",
+    "tRNA"="Other",
+    "scRNA"="Other",
+    "rRNA"="Other")[x] )
+}
+
+
 # TODO:
 # 0) Generated merged VCF by adding a new annotation
 # 1) add TI top level repeat class and %coverage
 # 2) dedup variants common across multiple samples
 
-dedupdf = fulldf %>%
-  group_by(id1, id2, repins1, repins2, repti1, repti2, repins, repti) %>%
+dedupdf = merged_df %>%
+  group_by(id1, id2, repins1, repins2, repti1, repti2, repins, repti, source) %>%
   summarise(
     insdellen=mean(insdellen),
     tilen=mean(tilen),
@@ -164,8 +215,8 @@ ggplot(dedupdf) +
   aes(x=insdellen, fill=factor(pmin(count, 10), levels=1:10, labels=c(1:9, "10+"))) +
   geom_histogram(bins=70) +
   scale_x_continuous(limits=c(-35, 35)) +
-  facet_wrap(. ~ repti) +
-  labs(title="Germline templated insertions", fill="Samples", x="Overlap at insertion site(bp)")
+  facet_grid( merged_repeats(repti) ~ source) +
+  labs(title="Templated insertions", fill="Samples", x="Overlap at insertion site(bp)")
 
 ggplot(dedupdf) +
   aes(x=count) +
@@ -196,5 +247,5 @@ ggplot(dedupdf) +
   aes(x=tilen) +
   geom_histogram(bins=70) +
   scale_x_log10() +
-  facet_wrap(. ~ repti)
+  facet_grid(source ~ repti)
 
