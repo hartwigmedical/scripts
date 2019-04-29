84f2c6a4e84a91bf659c045e9e3deda977e97e3b
Jonathan Baber <jonbaber@gmail.com>
2019-04-29 11:43:40 +1000

    add tumor locations

--- svpaper/SvCohort.R
+++ svpaper/SvCohort.R
@@ -19,3 +19,29 @@ save(entireCohort, highestPurityCohort, multipleBiopsyCohort, file = "~/hmf/anal
 load(file = "~/hmf/analysis/svPaper/cohort.RData")
 hpcCopyNumbers = purple::query_copy_number(dbProd, highestPurityCohort)
 save(hpcCopyNumbers,file = "~/hmf/analysis/svPaper/hpcCopyNumbers.RData")
+
+
+### Primary Tumor Location
+load(file = "~/hmf/RData/reference/allClinicalData.RData")
+
+allClinicalDataDatabase = purple::query_clinical_data(dbProd)
+
+cohortCancerTypes = allClinicalDataDatabase %>% 
+  select(sampleId, primaryTumorLocation) %>% 
+  filter(!sampleId %in% allClinicalData$sampleId) %>%
+  bind_rows(allClinicalData %>% select(sampleId, primaryTumorLocation)) %>%
+  left_join(entireCohort %>% select(sampleId, patientId), by = "sampleId") %>%
+  filter(!is.na(patientId)) %>% 
+  group_by(patientId) %>% 
+  mutate(valid = length(unique(primaryTumorLocation)) ==1) 
+
+save(cohortCancerTypes,file = "~/hmf/analysis/svPaper/cohortCancerTypes.RData")
+
+
+jon = entireCohort %>% filter(qcStatus !=  'PASS')
+load(file = "~/hmf/analysis/svPaper/cohort.RData")
+
+
+load( file = "~/hmf/RData/reference/highestPurityCohort.RData")
+highestPurityCohort %>% filter(qcStatus != 'PASS' | purity < 0.20)
+highestPurityCohort %>% filter(sampleId == 'CPCT02030224T')
