fcc847e0a2aa503d475d5e470028a8f8fa2dfef6
Jonathan Baber <jonbaber@gmail.com>
2019-06-05 08:58:20 +1000

    updated patient mapping

--- analysisscripts/patientMapping.R
+++ analysisscripts/patientMapping.R
@@ -25,11 +25,9 @@ sample_mapping <- function(amberBafs, percentCutoff = 0.6) {
   return (sampleMapping)
 }
 
-patient_mapping <- function(sampleMapping, existingMappings) {
-  existingSamples = existingMappings %>% filter(type == 'Sample') %>% select(sampleId = sourceId) %>%
-    mutate(match = T)
-  
-  existingPatientMappings = existingMappings %>% filter(type == 'Patient') %>% select(sourceId, targetId) %>% mutate(new = F)
+patient_mapping <- function(sampleMapping, existingSamples, existingMappings) {
+  existingSamples$match = T
+  existingMappings$match = T
   
   patientMapping = sampleMapping %>%
     left_join(existingSamples %>% select(sample1 = sampleId, sample1Prior = match), by = "sample1") %>%
@@ -37,12 +35,23 @@ patient_mapping <- function(sampleMapping, existingMappings) {
     mutate(sample1Prior = ifelse(is.na(sample1Prior), F, T), sample2Prior = ifelse(is.na(sample2Prior), F, T)) %>%
     mutate(patient1 = substr(sample1, 1, 12), patient2 = substr(sample2, 1, 12)) %>%
     filter(patient1 != patient2) %>%
+    left_join(existingMappings %>% select(patient1 = sourceId, patient2 = targetId, patient1Prior = match), by = c("patient1", "patient2")) %>%
+    left_join(existingMappings %>% select(patient1 = targetId, patient2 = sourceId, patient2Prior = match), by = c("patient1", "patient2")) %>%
+    mutate(patient1Prior = ifelse(is.na(patient1Prior), F, T), patient2Prior = ifelse(is.na(patient2Prior), F, T)) %>%
     mutate(
+      # First sort alphabetically
       sourceId = ifelse(patient1 > patient2, patient1, patient2),
       targetId = ifelse(patient1 > patient2, patient2, patient1),
+      
+      # Next, favour existing samples
       xorSamples = xor(sample1Prior, sample2Prior),
       sourceId = ifelse(xorSamples, ifelse(sample1Prior, patient2, patient1), sourceId),
-      targetId = ifelse(xorSamples, ifelse(sample1Prior, patient1, patient2), targetId)
+      targetId = ifelse(xorSamples, ifelse(sample1Prior, patient1, patient2), targetId),
+      
+      # Finally, favour existing mappings
+      xorPatients = xor(patient1Prior, patient2Prior),
+      sourceId = ifelse(xorPatients, ifelse(patient1Prior, patient1, patient2), sourceId),
+      targetId = ifelse(xorPatients, ifelse(patient1Prior, patient2, patient1), targetId)
     ) %>%
     group_by(sourceId, targetId) %>% summarise(matchPercent = max(matchPercent)) %>%
     group_by(targetId) %>% mutate(target_n = n()) %>%
@@ -51,21 +60,24 @@ patient_mapping <- function(sampleMapping, existingMappings) {
     filter(row_number() == 1) %>%
     select(sourceId, targetId, matchPercent)
   
-  patientMapping = patientMapping %>% left_join(existingPatientMappings, by = c("sourceId", "targetId"))
+  patientMapping = patientMapping %>% left_join(existingMappings %>% select(sourceId, targetId, existing = match), by = c("sourceId", "targetId"))
   patientMapping[is.na(patientMapping)] <- T
   
-  return (patientMapping %>% arrange(-new, matchPercent))
+  return (patientMapping %>% arrange(existing, matchPercent))
 }
 
 ######################## Step 1 - Regenerate patient mappings
 dbProd = dbConnect(MySQL(), dbname='hmfpatients', groups="RAnalysis", host = "127.0.0.1")
 amberBafs = dbGetQuery(dbProd, "SELECT * FROM amber")
-existingMappings = dbGetQuery(dbProd, "SELECT * FROM idMapping")
+existingSamples = dbGetQuery(dbProd, "SELECT * FROM sampleMapping")
+existingMappings = dbGetQuery(dbProd, "SELECT * FROM patientMapping")
+
+#existingMappings = existingMappings %>% filter(sourceId != 'CPCT02010981T')
 dbDisconnect(dbProd)
 rm(dbProd)
 
 sampleMapping = sample_mapping(amberBafs)
-patientMapping = patient_mapping(sampleMapping, existingMappings)
+patientMapping = patient_mapping(sampleMapping, existingSamples, existingMappings)
 
 amberSamples = unique(amberBafs$sampleId)
 write.table(amberSamples, file = "~/hmf/resources/idgenerator/samples_20190604.csv", quote = F, row.names = F, col.names = F, sep = ",")
@@ -80,29 +92,19 @@ write.table(patientMapping %>% select(sourceId, targetId), file = "~/hmf/resourc
 # -patient_mapping_file ~/hmf/resources/idgenerator/patient_mapping_20190604.csv
 # -out ~/hmf/resources/idgenerator/sample_hashes_20190604.csv
 # -mapping_out ~/hmf/resources/idgenerator/remapping_20190604.csv
+# -anonymize_out ~/hmf/resources/idgenerator/anonymized_20190604.csv
 
 ######################## Step 3 - Include hash in build
 # cp ~/hmf/resources/idgenerator/sample_hashes_20190604.csv ~/hmf/repos/hmftools/hmf-id-generator/src/main/resources/sample_hashes.csv
 
-######################## Step 4 - Run Anonymise Ids
-# com.hartwig.hmftools.idgenerator.HmfIdApplicationKt
-# -anonymize_ids
-# -password xxxxx
-# -sample_ids_file ~/hmf/resources/idgenerator/samples_20190604.csv
-# -patient_mapping_file ~hmf/resources/idgenerator/patient_mapping_20190604.csv
+######################## Step 4 - Load into database
+# TRUNCATE patientMapping;
+# TRUNCATE sampleMapping;
+# LOAD DATA LOCAL INFILE '~/hmf/resources/idgenerator/patient_mapping_20190604.csv' INTO TABLE patientMapping  FIELDS TERMINATED BY ',' IGNORE 0 LINES (sourceId, targetId) SET modified = CURRENT_TIMESTAMP;
+# LOAD DATA LOCAL INFILE '~/hmf/resources/idgenerator/remapping_20190604.csv' INTO TABLE patientMapping  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (sourceId, targetId) SET modified = CURRENT_TIMESTAMP;
+# LOAD DATA LOCAL INFILE '~/hmf/resources/idgenerator/anonymized_20190604.csv' INTO TABLE sampleMapping FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES  (sampleId, hmfId) SET modified = CURRENT_TIMESTAMP;
 
-######################## Step 5 - Copy output
-# Need to copy the relevant run output without header to ~/hmf/resources/idgenerator/anonymized_20190604.csv
-
-######################## Step 6 - Load into database
-# Note we do not delete type = 'Sample' so we can have a history. 
-
-#delete from idMapping where type in ('Patient', 'Remapping');
-#LOAD DATA LOCAL INFILE '~/hmf/resources/idgenerator/patient_mapping_20190604.csv' IGNORE INTO TABLE idMapping  FIELDS TERMINATED BY ',' IGNORE 0 LINES (sourceId, targetId) SET modified = CURRENT_TIMESTAMP, type = 'Patient';
-#LOAD DATA LOCAL INFILE '~/hmf/resources/idgenerator/anonymized_20190604.csv' IGNORE INTO TABLE idMapping FIELDS TERMINATED BY ',' IGNORE 0 LINES  (sourceId, targetId) SET modified = CURRENT_TIMESTAMP, type = 'Sample';
-#LOAD DATA LOCAL INFILE '~/hmf/resources/idgenerator/remapping_20190604.csv' IGNORE INTO TABLE idMapping  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 0 LINES (sourceId, targetId) SET modified = CURRENT_TIMESTAMP, type = 'Remapping';
-
-######################## Step 7 - Store files
+######################## Step 5 - Store files
 # copy all new files to datastore, eg
 # scp *20190604.csv hmf-datastore:/data/common/dbs/idgenerator/
 
