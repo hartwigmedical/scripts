2f85b8dd8d6e6d4731527958c25162ce6236e1a3
Jonathan Baber <jonbaber@gmail.com>
2019-07-29 10:00:45 +1000

    make pie chart smaller

--- pancancerpaper/actionableVisualisation.R
+++ pancancerpaper/actionableVisualisation.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(tidyr)
 library(scales)
 library(cowplot)
-theme_set(theme_bw() + theme(axis.text=element_text(size=5),axis.title=element_text(size=6), legend.text = element_text(size=5)))
+theme_set(theme_bw() + theme(axis.text=element_text(size=5),axis.title=element_text(size=5), legend.text = element_text(size=5)))
 
 ########################################### Prepare Data
 alphabetical_drug <- function(drugs) {
@@ -134,15 +134,16 @@ actionablePlotData = actionablePlotData %>%
 p1 = ggplot(data = actionablePlotData, aes(x = cancerType, y = percentage)) +
   geom_bar(stat = "identity", aes(fill = response)) + 
   scale_fill_manual(values = levelTreatmentColors, guide = guide_legend(reverse = TRUE)) +
-  ggtitle("") + xlab("") + ylab("% with treatment options") +
+  xlab("") + ylab("% with treatment options") +
   scale_y_continuous(labels = percent, limits = c(0, 1), expand = c(0.02,0)) +
   theme(panel.grid.major.y = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank()) +
-  theme(legend.position = "bottom", legend.title = element_blank(),  legend.key.size = unit(0.2, "cm")) +
+  theme(plot.margin = margin(2,3,0,0,unit = "pt")) +
+  theme(legend.position = c(0.75, 0.1), legend.title = element_blank(),  legend.key.size = unit(0.2, "cm"), legend.margin = margin(0,0,0,0,unit = "pt")) +
   theme(axis.ticks = element_blank()) +
-  guides(fill = guide_legend(nrow = 2)) + 
+  guides(fill = guide_legend(ncol = 1, reverse = T)) + 
   theme(legend.spacing.x = unit(4, 'pt')) +
   coord_flip()
-
+p1
 
 ########################################### Figure 8 part 2 - Actionable Pie
 library(ggforce) 
@@ -152,46 +153,52 @@ actionableDriverColours = simplifiedDriverColours[c("Amplification", "Fusion", "
 names(actionableDriverColours) <- c("Amplification", "Fusion", "Indel","SNV","MSI","MNV")
 
 load(file = "~/hmf/RData/Processed/responsiveVariants.RData")
+eventTypeFactor = c("SNV","MSI","Amplification","MNV","Indel","Fusion")
 responseChartData = responsiveVariants %>% 
+  mutate(eventType = ifelse(eventType == "INDEL", "Indel", eventType)) %>%
+  mutate(eventType = factor(eventType, eventTypeFactor, ordered = T)) %>% arrange(eventType) %>%
   group_by(eventType) %>% 
   dplyr::count() %>% 
   ungroup() %>% 
-  mutate(share = n / sum(n)) %>%
-  mutate(
-    eventType = ifelse(eventType == "INDEL", "Indel", eventType)
-  )
+  mutate(share = n / sum(n))
 
-responseChartData = responseChartData %>%
+responseChartData = responseChartData %>% 
   mutate(end = 2 * pi * cumsum(share)/sum(share),
          start = lag(end, default = 0),
          middle = 0.5 * (start + end),
          hjust = ifelse(middle > pi, 1, 0),
          vjust = ifelse(middle < pi/2 | middle > 3 * pi/2, 0, 1))
 
-responseChartData$xOffset <- ifelse(responseChartData$eventType == "MSI", -0.1, 0)
-responseChartData$yOffset <- ifelse(responseChartData$eventType == "MSI", -0.1, 0)
+responseChartData$xOffset <- ifelse(responseChartData$eventType == "XX", -0.1, 0)
+responseChartData$yOffset <- ifelse(responseChartData$eventType == "XX", -0.1, 0)
 
 pie = ggplot(responseChartData) + 
-  geom_arc_bar(aes(x0 = 0, y0 = 0, r0 = 0, r = 1, start = start, end = end, fill = eventType), size = 0.000001) +
-  geom_text(aes(x = 1.05 * sin(middle) + xOffset, y = 1.05 * cos(middle) + yOffset, label = paste0(round(responseChartData$share*100, 1), "%"), hjust = hjust, vjust = vjust), size = 4.5 * 25.4 / 72) +
+  geom_arc_bar(aes(x0 = 0, y0 = 0, r0 = 0, r = 1, start = start, end = end, fill = eventType), size = 0.01, alpha = 1) +
+  geom_text(aes(x = 1.17 * sin(middle) + xOffset, y = 1.17 * cos(middle) + yOffset, label = paste0(round(responseChartData$share*100, 1), "%"), hjust = 0.5, vjust = 0.5), size = 5* 25.4 / 72) +
   coord_fixed() +
-  scale_x_continuous(limits = c(-1.5, 1.4), name = "", breaks = NULL, labels = NULL) +
-  scale_y_continuous(limits = c(-1, 1), name = "", breaks = NULL, labels = NULL) +
-  theme(panel.grid.major.y = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), legend.position = "bottom",  legend.key.size = unit(0.2, "cm"), legend.title = element_blank()) +
-  theme(legend.spacing.x = unit(4, 'pt')) +
-  scale_fill_manual(values = actionableDriverColours, name = "Variant Type")
+  scale_x_continuous(name = "", breaks = NULL, labels = NULL, limits = c(-1.3, 1.3)) +
+  scale_y_continuous( name = "", breaks = NULL, labels = NULL) +
+  theme(
+    panel.grid.major.y = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), 
+    legend.position = c(0.5, -0.05),  legend.key.size = unit(0.2, "cm"), legend.title = element_blank(), legend.background = element_blank()) +
+  theme(legend.spacing.x = unit(2, 'pt')) +
+  theme(plot.margin = margin(t = 2,0,0,00,unit = "pt")) +
+  scale_fill_manual(values = actionableDriverColours, name = "Variant Type") +
+  guides(fill = guide_legend(nrow = 2)) 
+pie
 
-pActionability = plot_grid(p1, pie, nrow = 1, labels = "auto", rel_widths = c(3, 3), label_size = 8)
+pActionability = plot_grid(p1, pie, nrow = 1, labels = "auto", rel_widths = c(4, 4), label_size = 8)
 pActionability
 
-ggplot2::ggsave("~/hmf/RPlot/Figure 5.png", pActionability, width = 89, height = 70, units = "mm", dpi = 300)
+#ggplot2::ggsave("~/hmf/RPlot/Figure 5.pdf", pActionability, width = 94, height = 50, units = "mm", dpi = 300)
+ggplot2::ggsave("~/hmf/RPlot/Figure 5.png", pActionability, width = 94, height = 50, units = "mm", dpi = 300)
 #convert -density 300 ~/hmf/RPlot/Figure\ 5.png ~/hmf/RPlot/Figure\ 5.pdf
 #ggplot2::ggsave("~/hmf/RPlot/Figure 5b.pdf", pActionability, width = 89, height = 70, units = "mm", dpi = 300)
 
 
-pdf(file = "~/hmf/RPlot/Figure 5.pdf", width = 89/10/2.54, height = 70/10/2.54)
-pActionability
-dev.off()
+#pdf(file = "~/hmf/RPlot/Figure 5.pdf", width = 89/10/2.54, height = 70/10/2.54)
+#pActionability
+#dev.off()
 
 pActionability
 save_plot("~/hmf/RPlot/Figure 8 - Actionable.png", pActionability, base_width = 16, base_height = 8)
