ede03775a8fe00b3fb90b222492dd2e7bcd230ec
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2019-08-23 18:20:26 +0200

    work in progress

--- datarequest/collectDatarequestMetadata.R
+++ datarequest/collectDatarequestMetadata.R
@@ -6,33 +6,50 @@ suppressPackageStartupMessages(library(jsonlite))
 
 DB_NAME = 'hmfpatients'
 DB_GROUP = 'RAnalysis'
-SAMPLE_TO_SET_MAPPING_FILE = '/data/data_archive/datarequests/procedure/sampleid2setname.tsv'
+SAMPLE_TO_SET_MAPPING_FILE = '/data/data_archive/datarequests/procedure/files/sampleid2setname.tsv'
+LOG_FILE = "./log.txt"
 
 main <- function(configFile) {
     
-    cat( sprintf("[INFO] Using config (%s) and database (%s)\n", configFile, DB_NAME) )
+    dateTime <- Sys.time()
+    write( sprintf("Date: %s", dateTime), file=LOG_FILE, append=FALSE)
+
+    catInfo( sprintf("Using config (%s) and database (%s)", configFile, DB_NAME) )
     config <- parseConfig(configFile)
     id2set <- parseMappingTsv(SAMPLE_TO_SET_MAPPING_FILE)
     
     db = dbConnect(MySQL(), dbname = DB_NAME, groups = DB_GROUP)
-    samplesRaw = queryDatarequestBase(db)
-    samplesFlt = queryDatarequestFiltered(db, config)
-    dbDisconnect(db)
+    samplesRaw = queryDatarequestBase(db, logFile)
+    samplesFlt = queryDatarequestFiltered(db, config, logFile)
     
     catInfo( sprintf("Adding Set names by sampleId") )
     samplesRaw <- addSetNames(samplesRaw, id2set)
-    samplesFlt <- addSetNames(samplesFlt, id2set)
+    samplesFlt <- addSetNames(samplesFlt, id2set)    
+    sIds <- samplesFlt$sampleId
+    pIds <- samplesFlt$patientId
 
-    outPathJsnRaw = "./metadata_raw.json"
-    outPathTsvRaw = "./metadata_raw.tsv"
-    outPathJsnFlt = "./metadata_filtered.json"
-    outPathTsvFlt = "./metadata_filtered.tsv"
+    catInfo( sprintf("Retrieving PostBiopsyTreatment responses") )
+    postResp <- queryPostBiopsyResponsesBySample(db, sIds, logFile)
+    postDrug <- queryPostBiopsyDrugsBySample(db, sIds, logFile)
 
-    writeOutputFile(samplesRaw, outPathJsnRaw, "json")
-    writeOutputFile(samplesRaw, outPathTsvRaw, "tsv")
+    catInfo( sprintf("Retrieving PreBiopsyTreatment drugs") )
+    preDrug <- queryPreBiopsyDrugsByPatient(db, pIds, logFile)
+
+    ## cleanup
+    dbDisconnect(db)
     
-    writeOutputFile(samplesFlt, outPathJsnFlt, "json")
-    writeOutputFile(samplesFlt, outPathTsvFlt, "tsv")    
+    ## output to files
+    writeOutputFile(samplesRaw, "./metadata_raw.json", "json")
+    writeOutputFile(samplesRaw, "./metadata_raw.tsv", "tsv")
+    writeOutputFile(samplesFlt, "./metadata_filtered.json", "json")
+    writeOutputFile(samplesFlt, "./metadata_filtered.tsv", "tsv")
+    writeOutputFile(  postResp, "./postBiopsyTreatmentResponses.json", "json")
+    writeOutputFile(  postResp, "./postBiopsyTreatmentResponses.tsv", "tsv")
+    writeOutputFile(  postDrug, "./postBiopsyTreatmentDrugs.json", "json")
+    writeOutputFile(  postDrug, "./postBiopsyTreatmentDrugs.tsv", "tsv")
+    writeOutputFile(   preDrug, "./preBiopsyTreatmentDrugs.json", "json")
+    writeOutputFile(   preDrug, "./preBiopsyTreatmentDrugs.tsv", "tsv")
+    catInfo(sprintf("Done. See log for exact queries used (%s)", LOG_FILE))
     
 }
 
@@ -47,8 +64,6 @@ parseMappingTsv <- function( filePath ) {
     catInfo( sprintf("Parsing file (%s)", filePath) )
     data <- fread(filePath, header=FALSE)
     colnames(data) <- c("sampleId", "setName")
-    catInfo( "Data inspection:" )
-    print(head(data))
     return(data)
 }
 
@@ -101,25 +116,27 @@ writeOutputFile <- function( obj, filePath, fileType ) {
     } else {
         stop( sprintf("[EXIT] Unknown output file type (%s)", fileType) )
     }
-    cat( sprintf("[INFO] Written output file of type %s with %s objects (%s)\n", fileType, objNrow, filePath) )
+    cat( sprintf("[INFO] Written output file with %s objects of type %s (%s)\n", objNrow, fileType, filePath) )
 }
 
 queryDatabase <- function(db, query) {
     raw_data = dbGetQuery(db, query)
-    DT = data.table(raw_data, key="sampleId")
+    first_column = colnames(raw_data)[1]
+    DT = data.table(raw_data, key=first_column)
     return(DT)
 }
 
-queryDatarequestBase <- function(db) {
+queryDatarequestBase <- function(db, logFile) {
     query = paste0(
         "SELECT datarequestBase.*, metric.refMeanCoverage, metric.tumorMeanCoverage",
         " FROM datarequestBase LEFT JOIN metric ON datarequestBase.sampleId = metric.sampleId"
     )
+    log(title="queryDatarequestBase", msg=query)
     DT = queryDatabase(db, query)
     return(DT)
 }
 
-queryDatarequestFiltered <- function(db, config) {
+queryDatarequestFiltered <- function(db, config, logFile) {
     conditional_string <- createSqlConditionsStringFromFilters(config$FILTERS)
     ## sampleId column name present multiple times due to JOIN
     conditional_string <- gsub( " sampleId IN", " datarequestFiltered.sampleId IN", conditional_string)
@@ -130,10 +147,67 @@ queryDatarequestFiltered <- function(db, config) {
         " FROM datarequestFiltered LEFT JOIN metric ON datarequestFiltered.sampleId = metric.sampleId",
         conditional_string
     )
-    catInfo( sprintf("Final query (filtered): %s", query) )
+    log(title="queryDatarequestFiltered", msg=query)
         
     DT = queryDatabase(db, query)
     return(DT)
+}
+
+queryPostBiopsyResponsesBySample <- function(dbConnect, sampleIds, logFile) {
+    inClauseString <- paste0( paste0( '"', sampleIds, '"'), collapse=',' )
+    query = paste(
+    'SELECT DISTINCT',
+        'c.sampleId AS "sampleId",',
+        'tr.responseDate, tr.response,',
+        't.startDate, t.endDate, t.name, t.type, t.mechanism',
+    'FROM',
+    'treatmentResponse AS tr',
+        'LEFT JOIN treatment AS t ON tr.treatmentId = t.id',
+        'LEFT JOIN biopsy    AS b ON t.biopsyId = b.id',
+        'LEFT JOIN clinical  AS c ON c.sampleId = b.sampleId',
+    'WHERE',
+        'c.informedConsentDate > "2016-04-20"',
+        'AND t.treatmentGiven = "Yes"',
+        'AND tr.measurementDone = "Yes"',
+        'AND c.sampleId IN (', inClauseString, ')',
+    'ORDER BY',
+        'c.sampleId, tr.responseDate',
+    sep = ' ')
+    log( title="FinalPostResponsesBySampleQuery", msg=query)
+    return(queryDatabase(dbConnect, query))
+}
+
+queryPostBiopsyDrugsBySample <- function(dbConnect, sampleIds, logFile) {
+    inClauseString <- paste0( paste0( '"', sampleIds, '"'), collapse=',' )
+    query = paste(
+    'SELECT DISTINCT c.sampleId AS "sampleId", d.startDate, d.endDate, d.name, d.type, d.mechanism',
+    'FROM drug AS d',
+        'LEFT JOIN treatment AS t ON d.treatmentId = t.id',
+        'LEFT JOIN biopsy    AS b ON t.biopsyId    = b.id',
+        'LEFT JOIN clinical  AS c ON c.sampleId    = b.sampleId',
+    'WHERE c.informedConsentDate > "2016-04-20"',
+        'AND t.treatmentGiven = "Yes"',
+        'AND not isnull(d.startDate)',
+        'AND c.sampleId IN (', inClauseString, ')',
+    'ORDER BY c.sampleId',
+    sep = ' ')
+    log( title="FinalPostDrugsByPatientQuery", msg=query)
+    return(queryDatabase(dbConnect, query))
+}
+
+queryPreBiopsyDrugsByPatient <- function(dbConnect, patientIds, logFile) {
+    inClauseString <- paste0( paste0( '"', patientIds, '"'), collapse=',' )
+    query = paste( 'SELECT DISTINCT p.patientIdentifier AS "patientId", d.startDate, d.endDate, d.name, d.type, d.mechanism',
+    'FROM preTreatmentDrug AS d',
+        'LEFT JOIN patient  AS p ON d.patientId = p.id',
+        'LEFT JOIN clinical AS c ON c.patientId = p.patientIdentifier',
+    'WHERE c.informedConsentDate > "2016-04-20"',
+        'AND not isnull(d.startDate)',
+        'AND c.patientId IN (', inClauseString, ')',
+    'ORDER BY p.patientIdentifier',
+    sep = ' ')
+    log( title="FinalPreDrugsByPatientQuery", msg=query)
+    return(queryDatabase(dbConnect, query))
 }
 
 catInfo <- function( msg ) {
@@ -141,6 +215,14 @@ catInfo <- function( msg ) {
 }
 catError <- function( msg ) {
     stop(sprintf("[EXIT] %s\n", msg))
+}
+
+log <- function( title, msg ){
+    write( 
+        sprintf( "-----\n%s\n%s", title, msg), 
+        LOG_FILE, 
+        append=TRUE 
+    )
 }
 
 spec = matrix(c(
@@ -161,5 +243,6 @@ if ( length(commandArgs(TRUE)) < 1 || !is.null(opt$help) ) {
 }
 
 main(opt$config)
+
 
 
