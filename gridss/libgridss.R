949313c2f90c87afaaf7b1808e431534efc16beb
Daniel Cameron <d-cameron@github.com>
2019-08-25 23:01:54 +1000

    Fixed bug caused by R format() left padding printed integers with spaces

--- gridss/libgridss.R
+++ gridss/libgridss.R
@@ -772,7 +772,7 @@ align_breakpoints <- function(vcf, align=c("centre"), is_higher_breakend=names(v
                                                   partner_alt[,5],
                                                   partner_alt[,3],
                                                   ":",
-                                                  format(partner_pos, scientific=FALSE),
+                                                  format(partner_pos, trim=TRUE, scientific=FALSE),
                                                   partner_alt[,5],
                                                   str_pad("", stringr::str_length(partner_alt[,6]), pad="N"))), "CharacterList")
   info(vcf)$CIRPOS = NULL # TODO: remove CIRPOS from GRIDSS entirely
