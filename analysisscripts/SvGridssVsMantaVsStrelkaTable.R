c60424a2f9fb4d3ac5a61df21fbd5f95c2762fcc
Jonathan Baber <jonbaber@gmail.com>
2019-09-09 13:07:54 +1000

    probe data

--- analysisscripts/SvGridssVsMantaVsStrelkaTable.R
+++ analysisscripts/SvGridssVsMantaVsStrelkaTable.R
@@ -11,12 +11,6 @@ patientMapping = dbGetQuery(dbProd, "SELECT sampleId, hmfId from sampleMapping")
 dbDisconnect(dbProd)
 rm(dbProd)
 
-unique(probeResult$callset)
-unique(probeResult$scope)
-
-probeResult %>% filter(callset == 'Gridss') %>% group_by(scope)  %>% summarise(n = n()) 
-probeResult %>% filter(callset == 'Strelka') %>% group_by(scope)  %>% summarise(n = n()) 
-
 supplementaryTable =  left_join(probeResult, patientMapping, by = "sampleId") %>%
   mutate(
     gridss = callset == 'Gridss', 
@@ -32,3 +26,27 @@ supplementaryTable =  left_join(probeResult, patientMapping, by = "sampleId") %>
 
 
 write.csv(supplementaryTable, file = "/Users/jon/hmf/analysis/mantaVgridss/probeSupplementaryTable.csv", row.names = F)
+
+
+
+
+load(file = "/Users/jon/hmf/analysis/mantaVgridss/scopedData.RData")
+library(BSgenome.Hsapiens.UCSC.hg19)
+library(seqinr)
+dups = gridss %>% filter(type == 'DUP', sampleId == 'XXXXX') %>%
+  mutate(len = endPosition - startPosition + 1) %>% filter (len <= 200 ) %>%
+  mutate(
+    dup = as.character(getSeq(BSgenome.Hsapiens.UCSC.hg19, paste0("chr", startChromosome), startPosition, endPosition)),
+    repeatContext = substr(dup, 1, 2), 
+    repeatCount = floor(nchar(dup) / 2),
+    tmp = strrep(repeatContext, 12),
+    repeatCount = ifelse(substr(dup, 1, nchar(tmp)) == tmp, repeatCount, 0),
+    msi = repeatCount > 0) %>%
+  select(-tmp, -dup) %>%
+  filter(msi)
+
+dupSummary = dups %>% mutate(isRepeated = repeatCount > 0) %>% group_by(sampleId, isRepeated) %>% count() %>% spread(isRepeated, n)
+dupSummary
+
+probeMsi = dups
+save(probeMsi, file = "~/hmf/tmp/probeMsi.RData")
