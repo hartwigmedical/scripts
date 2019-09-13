68597bdf9544716be76f96201ac365c921f046e4
Jonathan Baber <jonbaber@gmail.com>
2019-09-13 15:44:33 +1000

    features and replication

---
+++ svpaper/SvFeatures.R
@@ -0,0 +1,45 @@
+library(tidyr)
+library(dplyr)
+
+svData = read.csv(file = "/Users/jon/hmf/analysis/fusions/SVA_SVS.csv")
+
+beData = rbind(
+  svData %>% mutate(LnkLen = LnkLenStart, IsFoldback = Type == 'INV' & FoldbackLnkStart == FoldbackLnkEnd & FoldbackLnkStart > 0, RepeatClass, RepeatType, FoldbackLnk=FoldbackLnkStart,LocTopType=LocTopTypeStart,Chr=ChrStart,Pos=PosStart,Orient=OrientStart,IsStart=T,LE=LEStart, replication = RepOriginStart, Len=PosEnd - PosStart + 1, PosMid = round(PosStart  + (PosEnd - PosStart)/2)),
+  svData %>% filter(ChrEnd != 0) %>% mutate(LnkLen = LnkLenEnd, IsFoldback = Type == 'INV' & FoldbackLnkStart == FoldbackLnkEnd & FoldbackLnkStart > 0, RepeatClass, RepeatType, FoldbackLnk=FoldbackLnkEnd,LocTopType=LocTopTypeEnd,Chr=ChrEnd,Pos=PosEnd,Orient=OrientEnd,IsStart=F, LE=LEEnd, replication = RepOriginEnd, Len=PosEnd - PosStart + 1, PosMid = round(PosStart  + (PosEnd - PosStart)/2))) %>%
+  select(SampleId,Id,IsStart,ClusterId,Type,ResolvedType,RepeatType,RepeatClass,FoldbackLnk,LocTopType,Chr,Pos,Orient,LE,ClusterCount,Len,replication, IsFoldback, LnkLen) 
+  # %>% filter(replication >= 0.06, replication <= 0.8)
+
+lengthLabels = c("Short", "Medium", "Long", "VeryLong") 
+lengthBreaks = c(0, 500, 10000, 500000, 1e100)
+dupLabels = paste0(lengthLabels, "Dup")
+delLabels = paste0(lengthLabels, "Del")
+featureLevels = c(dupLabels, delLabels, "TiSource", "CentromericSGL", "PeriCentromericSGL", "TelomericSGL", "LeftFoldback", "RightFoldback", "LineInsertion", "LineSource")
+
+dupBreakends = beData %>% filter(ResolvedType == 'DUP', ClusterCount == 1) %>% 
+  mutate(feature = cut(Len, lengthBreaks, labels = paste0(lengthLabels, "Dup"), ordered_result = F))
+delBreakends = beData %>% filter(ResolvedType == 'DEL', ClusterCount == 1) %>% 
+  mutate(feature = cut(Len, lengthBreaks, labels = paste0(lengthLabels, "Del"), ordered_result = F))
+sglBreakends = beData %>% filter(ResolvedType == 'SGL') %>%
+  mutate(
+    feature = ifelse(RepeatClass == 'Satellite/centr', "CentromericSGL", NA), 
+    feature = ifelse(RepeatType == 'HSATII', "PeriCentromericSGL", feature),
+    feature = ifelse(RepeatType %in% c('(CCCTAA)n',''), "TelomericSGL", feature)) %>%
+  filter(!is.na(feature))
+lineBreakends = beData %>% filter(ResolvedType == 'LINE') %>%
+  mutate(
+    feature = ifelse(LE == "None", "LineInsertion", NA), 
+    feature = ifelse(LE != "None", "LineSource", feature)) %>%
+  filter(!is.na(feature))
+tiBreakends = beData %>% filter(LocTopType=='TI_ONLY', LnkLen < 1000) %>% 
+  mutate(feature = "TiSource") 
+foldbackBreakends = beData %>% filter(IsFoldback) %>%
+  mutate(feature = ifelse(Orient == 1, "LeftFoldback", "RightFoldback"))
+
+featuredBreakends = bind_rows(dupBreakends, delBreakends) %>% bind_rows(sglBreakends) %>% bind_rows(lineBreakends) %>% bind_rows(tiBreakends) %>% bind_rows(foldbackBreakends)
+featuredBreakends = featuredBreakends %>% mutate(feature = factor(feature, levels = featureLevels, ordered = T))
+
+save(featuredBreakends, file = "/Users/jon/hmf/analysis/svPaper/featuredBreakends.RData")
+
+
+
+
--- svpaper/SvReplication.R
+++ svpaper/SvReplication.R
@@ -1,6 +1,7 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(GenomicRanges)
 
 singleBlue = "#6baed6"
 
@@ -27,10 +28,10 @@ normalise_replication <- function(df, averageReplication) {
   dfHist = df %>%
     filter(replication > 0) %>%
     mutate(bucket = replication_bucket(replication)) %>%
-    group_by(ResolvedType, bucket) %>%
+    group_by(feature, bucket) %>%
     summarise(count = n()) %>%
     filter(!is.na(bucket)) %>% 
-    group_by(ResolvedType) %>% 
+    group_by(feature) %>% 
     mutate(
       actualDensity = count / sum(count))
   
@@ -45,11 +46,11 @@ normalise_replication <- function(df, averageReplication) {
     select(-count)
   
   # Verify all 1s
-  dfHist %>% group_by(ResolvedType) %>% summarise(totalDensity = sum(actualDensity))
+  dfHist %>% group_by(feature) %>% summarise(totalDensity = sum(actualDensity))
   sum(refHist$expectedDensity)
   
   normalisedHist = dfHist %>% inner_join(refHist, by = "bucket") %>%
-    group_by(ResolvedType) %>%
+    group_by(feature) %>%
     mutate(
       ratio = actualDensity / expectedDensity,
       sumProduct = sum(ratio * actualDensity),
@@ -62,10 +63,9 @@ plot_replication <- function(df) {
   ggplot(df) +
     geom_bar(aes(x = bucket, y = factor), stat = "identity", width = 1, fill = singleBlue, position = "stack") +
     scale_x_discrete(breaks = unique(df$bucket)[seq(1,37, 9)], labels = (seq(1,37, 9) * 0.02 + 0.05)) +
-    facet_wrap( ~ResolvedType, nrow = 3) + 
+    facet_wrap( ~feature, nrow = 4) + 
     xlab("Replication") + ylab("Factor")
 }
-
 
 averageReplication_1k = read.table(file = "~/hmf/analysis/svPaper/heli_rep_origins.bed", sep = '\t', header = F, stringsAsFactors = F) %>% 
   mutate(chromosome = substring(V1, 4), start = V2 + 1, end = V3, replication = V4) %>%
@@ -79,102 +79,10 @@ ol = as.matrix(findOverlaps(replicationRegions, binRegions, type = "any"))
 averageReplication_1k = averageReplication_1k[ol[, 1], ]
 
 
-#allData = svData %>% 
-#  select(ResolvedType, RepOriginStart, RepOriginEnd, PosStart, PosEnd, ClusterCount, FoldbackLnkStart, FoldbackLnkEnd, LocTopTypeStart, LocTopTypeEnd, LEStart, LEEnd) %>% 
-#  gather(type, replication, c(2,3)) %>%
-#  filter(replication >= 0.06, replication <= 0.8)
+load(file = "/Users/jon/hmf/analysis/svPaper/featuredBreakends.RData")  
+replicationBreakends = featuredBreakends %>% filter(replication >= 0.06, replication <= 0.8)
+replicationBreakendNormalised = normalise_replication(replicationBreakends, averageReplication_1k)
+plot_replication(replicationBreakendNormalised)
 
-load(file = "/Users/jon/hmf/analysis/svPaper/svData.RData")
-beData = rbind(
-  svData %>% mutate(IsFoldback = FoldbackLnkStart == FoldbackLnkEnd, FoldbackLnk=FoldbackLnkStart,LocTopType=LocTopTypeStart,Chr=ChrStart,Pos=PosStart,Orient=OrientStart,IsStart=T,LE=LEStart, replication = RepOriginStart, Len=PosEnd - PosStart + 1),
-  svData %>% mutate(IsFoldback = FoldbackLnkStart == FoldbackLnkEnd, FoldbackLnk=FoldbackLnkEnd,LocTopType=LocTopTypeEnd,Chr=ChrEnd,Pos=PosEnd,Orient=OrientEnd,IsStart=F, LE=LEEnd, replication = RepOriginEnd, Len=PosEnd - PosStart + 1)) %>%
-  select(SampleId,Id,IsStart,ClusterId,Type,ResolvedType,FoldbackLnk,LocTopType,Chr,Pos,Orient,LE,ClusterCount,Len,replication, IsFoldback) %>%
-  filter(replication >= 0.06, replication <= 0.8)
-
-unique(allData$ResolvedType)
-
-resolveTypeDF = beData %>% mutate(ResolvedType = ifelse(ResolvedType %in% c('DEL','DUP','LINE','COMPLEX'),as.character(ResolvedType), "Other"))
-resolveTypeNormalised = normalise_replication(resolveTypeDF, averageReplication_1k)
-plot_replication(resolveTypeNormalised)
-
- 
-
-delDF = beData %>% filter(ResolvedType == 'DEL', ClusterCount == 1) %>%
-  mutate(
-    length = Len,
-    label = cut(length, c(0, 500, 10000, 500000, 1e100), labels = c("Short", "Medium", "Long", "VeryLong")),
-    ResolvedType = paste0(label, "Del"),
-    ResolvedType = factor(ResolvedType, levels = c('ShortDel', "MediumDel", "LongDel", "VeryLongDel"), ordered = T) )
-delNormalised = normalise_replication(delDF, averageReplication_1k)
-plot_replication(delNormalised)
-
-dupDF = beData %>% filter(ResolvedType == 'DUP', ClusterCount == 1) %>%
-  mutate(
-    length = Len,
-    label = cut(length, c(0, 500, 10000, 500000, 1e100), labels = c("Short", "Medium", "Long", "VeryLong")),
-    ResolvedType = paste0(label, "Dup"),
-    ResolvedType = factor(ResolvedType, levels = c('ShortDup', "MediumDup", "LongDup", "VeryLongDup"), ordered = T) )
-dupNormalised = normalise_replication(dupDF, averageReplication_1k)
-plot_replication(dupNormalised)
-
-
-featuresDF = beData %>% mutate(
-  ResolvedType = as.character(ResolvedType),
-  ResolvedType = ifelse(ResolvedType == 'INV' & IsFoldback, "Foldback", ResolvedType),
-  ResolvedType = ifelse(LocTopType=='TI_ONLY', "TISource", ResolvedType),
-  ResolvedType = ifelse(ResolvedType == 'LINE' & LE == "None", "LineInsertion", ResolvedType)) %>%
-  filter(ResolvedType %in% c("Foldback", "TISource","LineInsertion"))
-featuresNormalised = normalise_replication(featuresDF, averageReplication_1k)
-plot_replication(featuresNormalised)
-
-pdf("~/hmf/analysis/svPaper/plot/replication.pdf")
-plot_replication(resolveTypeNormalised)
-plot_replication(delNormalised)
-plot_replication(dupNormalised)
-plot_replication(featuresNormalised)
-dev.off()
-
-########## JUNK?
-
-verification = normalisedHist %>% group_by(ResolvedType) %>% summarise(total = sum(factor * count / sum(count)))
-replicationFactor = normalisedHist %>% select(ResolvedType, replicationBucket = bucket, replicationFactor = factor)
+replicationFactor = replicationBreakendNormalised %>% select(feature, replicationBucket = bucket, replicationFactor = factor)
 save(replicationFactor, file = "~/hmf/analysis/svPaper/replicationFactor.RData")
-
-
-types = unique(svData$ResolvedType)
-for (type in types) {
-  cat("Processing", type, "\n")
-  replicationPlot = plot_replication(normalisedHist %>% filter(grepl(type, ResolvedType)))
-  filename = paste0("~/hmf/analysis/svPaper/plot/replication", type, ".png")
-  ggsave(filename, replicationPlot, width = 183, height = 161, units = "mm", dpi = 300)
-}
-
-all = plot_replication(normalisedHist)
-all
-
-delReplicationPlot = plot_replication(normalisedHist %>% filter(grepl("Del", subtype)))
-delReplicationPlot
-ggsave("~/hmf/analysis/svPaper/plot/delReplication.png", delReplicationPlot, width = 183, height = 120, units = "mm", dpi = 300)
-
-dupReplicationPlot = plot_replication(normalisedHist %>% filter(grepl("Dup", subtype)))
-ggsave("~/hmf/analysis/svPaper/plot/dupReplication.png", dupReplicationPlot, width = 183, height = 120, units = "mm", dpi = 300)
-
-
-simplePlot = plot_replication(normalisedHist %>% filter(grepl("Dup", subtype) | grepl("Del", subtype)))
-ggsave("~/hmf/analysis/svPaper/plot/simpleReplication.png", simplePlot, width = 183, height = 161, units = "mm", dpi = 300)
-ggsave("~/hmf/analysis/svPaper/plot/simpleReplication.pdf", simplePlot, width = 183, height = 161, units = "mm", dpi = 300)
-
-featurePlot = plot_replication(normalisedHist %>% filter(grepl("Fold", subtype) | grepl("ShortTI", subtype)))
-ggsave("~/hmf/analysis/svPaper/plot/featureReplication.png", featurePlot, width = 183, height = 161, units = "mm", dpi = 300)
-ggsave("~/hmf/analysis/svPaper/plot/featureReplication.pdf", featurePlot, width = 183, height = 161, units = "mm", dpi = 300)
-
-
-load(file = "/Users/jon/hmf/analysis/svPaper/svData.RData")
-df = svData %>% filter(isShortTI, !ResolvedType %in% c("COMPLEX", "LINE")) %>% filter(xor(LocTopTypeStart=='TI_ONLY', LocTopTypeEnd=='TI_ONLY')) %>%
-  mutate(
-    TIReplication = ifelse(LocTopTypeStart=='TI_ONLY', RepOriginStart, RepOriginEnd), 
-    NonTIReplication = ifelse(LocTopTypeStart=='TI_ONLY', RepOriginEnd, RepOriginStart))
-
-ggplot(df) +
-    geom_point(aes(x = TIReplication, y = NonTIReplication, color = ResolvedType), size = 0.3) + ggtitle("ShortTI TI Vs Non TI Replication" ) 
-
