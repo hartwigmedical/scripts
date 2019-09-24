4f67455ff2d5d600ef6f72c895073de5332ce7b5
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-09-24 09:22:45 +0200

    Add adjusted vaf to comparison query

--- sql/genomic/compare_snv_indel_between_2_databases.sql
+++ sql/genomic/compare_snv_indel_between_2_databases.sql
@@ -1,5 +1,5 @@
-SELECT min(pipeline) as pipeline, chromosome, position, ref, alt, count(*) FROM
-(SELECT id, 'Truth' AS pipeline, chromosome, position, ref, alt FROM reference_validation_sets.somaticVariant WHERE sampleId = 'XXX' AND filter = "PASS"
+SELECT min(pipeline) as pipeline, chromosome, position, ref, alt, count(*), min(adjustedVaf) FROM
+(SELECT id, 'Truth' AS pipeline, chromosome, position, ref, alt, adjustedVaf FROM reference_validation_sets.somaticVariant WHERE sampleId = 'XXX' AND filter = "PASS"
 UNION
-SELECT id, 'New' AS pipeline, chromosome, position, ref, alt FROM pipeline_v5_validation.somaticVariant WHERE sampleId = 'XXX' AND filter = "PASS") AS a
+SELECT id, 'New' AS pipeline, chromosome, position, ref, alt, adjustedVaf FROM pipeline_v5_validation.somaticVariant WHERE sampleId = 'XXX' AND filter = "PASS") AS a
 GROUP BY 2,3,4,5 HAVING count(*) = 1;
