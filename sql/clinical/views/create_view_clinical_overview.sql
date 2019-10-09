adc1dbc9b8110f1c0bb802c4172c5fb63f17d909
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-10-09 13:12:46 +0200

    Add concatenated treatments to clinical overview

--- sql/clinical/views/create_view_clinical_overview.sql
+++ sql/clinical/views/create_view_clinical_overview.sql
@@ -6,11 +6,14 @@ CREATE OR REPLACE VIEW clinical AS
     baseline.preTreatmentsMechanism, biopsy.biopsyDate,
 	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, baseline.deathDate,
 	baseline.primaryTumorLocation, baseline.cancerSubtype, biopsy.biopsyType, biopsy.biopsySite, biopsy.biopsyLocation,
-	treatment.treatmentGiven, treatment.radiotherapyGiven, treatment.name AS treatment, treatment.type AS treatmentType, treatment.mechanism AS treatmentMechanism,
+	treatment.treatmentGiven, treatment.radiotherapyGiven, treatment.name AS treatment,
+	treatment.type AS consolidatedTreatmentType, GROUP_CONCAT(drug.type SEPARATOR '|') as concatenatedTreatmentType,
+	treatment.mechanism AS consolidatedTreatmentMechanism, GROUP_CONCAT(drug.mechanism SEPARATOR '|') as concatenatedTreatmentMechanism,
 	firstMatchedTreatmentResponse.measurementDone AS responseMeasured, firstMatchedTreatmentResponse.response AS firstResponse
 	FROM sample
 		INNER JOIN patient ON sample.patientId = patient.id
         LEFT JOIN baseline ON patient.id = baseline.patientId
 		LEFT JOIN biopsy ON biopsy.sampleId = sample.sampleId
 		LEFT JOIN treatment ON treatment.biopsyId = biopsy.id
+		LEFT JOIN drug on drug.treatmentId = treatment.id
 		LEFT JOIN firstMatchedTreatmentResponse ON treatment.id = firstMatchedTreatmentResponse.treatmentId;
