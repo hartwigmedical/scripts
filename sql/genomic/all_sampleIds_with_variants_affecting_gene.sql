f941bc9733934e9d85040cc17c78bf0c8cc9b31d
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-11-07 11:03:26 +0100

    Reformatted some queries

--- sql/genomic/all_sampleIds_with_variants_affecting_gene.sql
+++ sql/genomic/all_sampleIds_with_variants_affecting_gene.sql
@@ -1,16 +1,13 @@
-SELECT count(distinct datarequestFiltered.sampleId) FROM hmfpatients.datarequestFiltered
-INNER JOIN
-((SELECT sampleId FROM hmfpatients.somaticVariant
-WHERE gene = 'XXX' AND (worstCodingEffect = 'MISSENSE' OR worstCodingEffect = 'NONSENSE_OR_FRAMESHIFT' OR worstCodingEffect = 'SPLICE'))
-UNION
-(SELECT geneCopyNumber.sampleId FROM hmfpatients.geneCopyNumber
-INNER JOIN
-(SELECT sampleId, ploidy FROM hmfpatients.purity) as b on geneCopyNumber.sampleId = b.sampleId
-WHERE gene = 'XXX' AND minCopyNumber > (ploidy*3))
-UNION
-(SELECT sampleId FROM geneCopyNumber WHERE gene = 'XXX' AND minCopynumber < 0.5)
-UNION
-(SELECT sampleId FROM svBreakend WHERE gene = 'XXX' AND disruptive = 1)
-UNION
-(SELECT sampleId from svFusion WHERE name like '%XXX%' AND phased = 1) )
-as a on datarequestFiltered.sampleId = a.sampleId;
+SELECT count(distinct sampleId) FROM datarequestFiltered
+WHERE sampleId IN
+(
+		SELECT sampleId FROM somaticVariant
+		WHERE gene = 'XXX' AND worstCodingEffect IN ('MISSENSE', 'NONSENSE_OR_FRAMESHIFT', 'SPLICE')
+    UNION
+        SELECT geneCopyNumber.sampleId FROM geneCopyNumber INNER JOIN purity ON geneCopyNumber.sampleId = purity.sampleId
+        WHERE gene = 'XXX' AND minCopyNumber > (ploidy*3)
+    UNION
+        SELECT sampleId FROM geneCopyNumber WHERE gene = 'XXX' AND minCopynumber < 0.5
+    UNION
+        SELECT sampleId FROM svBreakend WHERE gene = 'XXX' AND disruptive = 1
+)
