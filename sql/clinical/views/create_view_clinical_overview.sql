e423ba691320b5557b0e9a6fe6ed2175b7e1a136
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-11-07 11:26:33 +0100

    Updated clinical view to add HMF IDs and fix up sort order

--- sql/clinical/views/create_view_clinical_overview.sql
+++ sql/clinical/views/create_view_clinical_overview.sql
@@ -1,17 +1,20 @@
 CREATE OR REPLACE VIEW clinical AS
 
-SELECT sample.sampleId, sample.arrivalDate as sampleArrivalDate, patient.patientIdentifier AS patientId,
-	baseline.registrationDate, baseline.informedConsentDate, baseline.hospital, baseline.gender,
-    baseline.birthYear, baseline.hasSystemicPreTreatment, baseline.hasRadiotherapyPreTreatment, baseline.preTreatments, baseline.preTreatmentsType,
-    baseline.preTreatmentsMechanism, biopsy.biopsyDate,
-	treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate, firstMatchedTreatmentResponse.responseDate, baseline.deathDate,
-	baseline.primaryTumorLocation, baseline.cancerSubtype, biopsy.biopsyType, biopsy.biopsySite, biopsy.biopsyLocation,
-	treatment.treatmentGiven, treatment.radiotherapyGiven, treatment.name AS treatment,
-	treatment.type AS consolidatedTreatmentType, biopsyDrugs.treatmentType AS concatenatedTreatmentType,
+SELECT sampleMapping.hmfId as hmfSampleId, left(sampleMapping.hmfId, 9) as hmfPatientId,
+    sample.sampleId, patient.patientIdentifier AS patientId, sample.arrivalDate as sampleArrivalDate,
+    baseline.registrationDate, baseline.informedConsentDate, baseline.deathDate,
+    baseline.primaryTumorLocation, baseline.cancerSubtype, baseline.hospital, baseline.gender, baseline.birthYear,
+    baseline.hasSystemicPreTreatment, baseline.hasRadiotherapyPreTreatment,
+    baseline.preTreatments, baseline.preTreatmentsType, baseline.preTreatmentsMechanism,
+    biopsy.biopsyDate, biopsy.biopsyType, biopsy.biopsySite, biopsy.biopsyLocation,
+	treatment.treatmentGiven, treatment.radiotherapyGiven, treatment.startDate AS treatmentStartDate, treatment.endDate AS treatmentEndDate,
+    treatment.name AS treatment, treatment.type AS consolidatedTreatmentType, biopsyDrugs.treatmentType AS concatenatedTreatmentType,
 	treatment.mechanism AS consolidatedTreatmentMechanism, biopsyDrugs.treatmentMechanism as concatenatedTreatmentMechanism,
-	firstMatchedTreatmentResponse.measurementDone AS responseMeasured, firstMatchedTreatmentResponse.response AS firstResponse
+    firstMatchedTreatmentResponse.measurementDone AS responseMeasured, firstMatchedTreatmentResponse.responseDate,
+    firstMatchedTreatmentResponse.response AS firstResponse
 FROM sample
     INNER JOIN patient ON sample.patientId = patient.id
+    LEFT JOIN sampleMapping on sample.sampleId = sampleMapping.sampleId
     LEFT JOIN baseline ON patient.id = baseline.patientId
     LEFT JOIN biopsy ON biopsy.sampleId = sample.sampleId
     LEFT JOIN treatment ON treatment.biopsyId = biopsy.id
