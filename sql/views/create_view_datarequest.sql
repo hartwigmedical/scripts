8bc80f61b12c49650c399920d2134e48d8838d7b
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-11-07 12:01:08 +0100

    Move datarequest view into views dir and make all views consistent in terms of formatting

--- sql/views/create_view_datarequest.sql
+++ sql/views/create_view_datarequest.sql
@@ -6,4 +6,6 @@ FROM
 	clinical INNER JOIN purity ON purity.sampleId = clinical.sampleId
 WHERE 
 	qcStatus = 'PASS' AND status <> 'NO_TUMOR' AND purity > 0.195 AND 
-	((clinical.sampleId LIKE '%CPCT%' AND informedConsentDate > '2016-04-20') OR clinical.sampleId LIKE '%WIDE%' OR clinical.sampleId LIKE '%DRUP%');
+	((clinical.sampleId LIKE '%CPCT%' AND informedConsentDate > '2016-04-20')
+	    OR clinical.sampleId LIKE '%WIDE%'
+	    OR clinical.sampleId LIKE '%DRUP%');
