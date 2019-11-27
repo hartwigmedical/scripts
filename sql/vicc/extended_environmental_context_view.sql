4e039e2f82d20f9fcbb8f82e6d66323a300a8ad2
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2019-11-27 09:44:45 +0100

    Add drug view for VICC

--- sql/vicc/extended_environmental_context_view.sql
+++ sql/vicc/extended_environmental_context_view.sql
@@ -1,5 +1,13 @@
-select environmentalContext.*, taxonomy.*, approvedCountries from environmentalContext
-left join taxonomy on environmentalContext.id = taxonomy.environmentContextId
-left join
-	(select environmentContextId, GROUP_CONCAT(approvedCountryName SEPARATOR ",") as approvedCountries from approvedCountry group by 1)
-	approvedCountries on approvedCountries.environmentContextId = environmentalContext.id;
+CREATE OR REPLACE VIEW viccDrug AS
+
+SELECT
+	association.viccEntryId,
+	environmentalContext.term, environmentalContext.description, environmentalContext.source, environmentalContext.usanStem,
+    environmentalContext.toxicity, environmentalContext.idEnvironmentalContext AS identifier, approvedCountries,
+    taxonomy.kingdom, taxonomy.directParent, taxonomy.class, taxonomy.subClass, taxonomy.superClass
+FROM association
+INNER JOIN environmentalContext ON environmentalContext.associationId = association.id
+LEFT JOIN taxonomy ON environmentalContext.id = taxonomy.environmentalContextId
+LEFT JOIN
+	(SELECT environmentalContextId, GROUP_CONCAT(approvedCountryName SEPARATOR ",") AS approvedCountries FROM approvedCountry GROUP BY 1)
+	approvedCountries ON approvedCountries.environmentalContextId = environmentalContext.id;
