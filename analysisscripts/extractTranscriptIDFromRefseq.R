32938a439a474baa8bac7a3f541cd9cdcfa5ece9
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2019-12-03 14:49:53 +0100

    DEV-810: get more info from refseq

--- analysisscripts/extractTranscriptIDFromRefseq.R
+++ analysisscripts/extractTranscriptIDFromRefseq.R
@@ -5,7 +5,7 @@ library("biomaRt")
 grch37 = useMart(biomart = "ENSEMBL_MART_ENSEMBL", host = "grch37.ensembl.org", path = "/biomart/martservice", dataset = "hsapiens_gene_ensembl")
 refSeqNKI = c("XXXX")
 
-getBM(attributes = c('refseq_mrna', 'ensembl_transcript_id', 'external_gene_name'),
+getBM(attributes = c('external_gene_name', 'refseq_mrna', 'ensembl_transcript_id', 'chromosome_name', 'band'),
 filters = 'refseq_mrna',
 values = refSeqNKI,
 mart = grch37)
