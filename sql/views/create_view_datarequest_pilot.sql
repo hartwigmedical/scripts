e091f508da14d11859b9e9c9860caf4ec853a9f5
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-01-13 16:08:14 +0100

    DEV-1105: refactor

--- sql/views/create_view_datarequest_pilot.sql
+++ sql/views/create_view_datarequest_pilot.sql
@@ -5,7 +5,7 @@ SELECT
 FROM
 	clinical
 	INNER JOIN purity ON purity.sampleId = clinical.sampleId
-	INNER JOIN metric ON metricQC.sampleId = clinical.sampleId
+	INNER JOIN metric ON metric.sampleId = clinical.sampleId
 WHERE
 	blacklisted = 0 AND qcStatus = 'PASS' AND metricQC = 1 AND status <> 'NO_TUMOR' AND purity > 0.195 AND
 	((clinical.sampleId LIKE '%CPCT%' AND informedConsentDate > '2016-04-20')
