0f13959c37ba4c20524d4919978625e523445cb8
Sandra vd Broek <vandenbroek.alexandra@gmail.com>
2020-01-23 13:48:12 +0100

    Update script to check patients blacklisted before preparing a Datarequest

--- datarequest/scripts/check_patients_blacklisted_sqlDRselection.sh
+++ datarequest/scripts/check_patients_blacklisted_sqlDRselection.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#put SQL selectie query file path at $1 (for all use: /data/common/repos/scripts/datarequest/selection/all_patients_eligible_datarequests.sql)
+#put SQL selectie query file path at $1 (for all use: all_patients_eligible_datarequests.sql, otherwise: DRXXX.sql)
 
-execute_sql_on_prod $1 | sort | uniq > temp_datarequest.tsv
+execute_sql_on_prod /data/common/repos/scripts/datarequest/selection/$1 | sort | uniq > temp_datarequest.tsv
 awk -F"\t" 'FNR > 1 {print $1}' temp_datarequest.tsv | sort | uniq > testfile.tmp && mv testfile.tmp temp_datarequest.tsv
 
 wc -l /data/lims/patient_blacklist.tsv
