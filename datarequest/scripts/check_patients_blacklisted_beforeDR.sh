295033dc6e3c4f901f440788ea07883b25484284
Sandra vd Broek <vandenbroek.alexandra@gmail.com>
2020-01-23 13:34:24 +0100

    Update script to check patients blacklisted before preparing a Datarequest

--- datarequest/scripts/check_patients_blacklisted_beforeDR.sh
+++ datarequest/scripts/check_patients_blacklisted_beforeDR.sh
@@ -2,14 +2,12 @@
 
 #put SQL selectie query file path at $1 (for all use: /data/common/repos/scripts/datarequest/selection/all_patients_eligible_datarequests.sql)
 
-mkdir temp_blacklist_check
-
-execute_sql_on_prod $1 | sort | uniq > temp_blacklist_check/temp_datarequest.tsv
-awk -F"\t" 'FNR > 1 {print $1}' temp_blacklist_check/temp_datarequest.tsv | sort | uniq > testfile.tmp && mv testfile.tmp temp_blacklist_check/temp_datarequest.tsv
+execute_sql_on_prod $1 | sort | uniq > temp_datarequest.tsv
+awk -F"\t" 'FNR > 1 {print $1}' temp_datarequest.tsv | sort | uniq > testfile.tmp && mv testfile.tmp temp_datarequest.tsv
 
 wc -l /data/lims/patient_blacklist.tsv
-wc -l temp_blacklist_check/temp_datarequest.tsv
+wc -l temp_datarequest.tsv
 echo 'Overlap between two files:'
-comm -1 -2 <(sort /data/lims/patient_blacklist.tsv) <(sort temp_blacklist_check/temp_datarequest.tsv) | wc -l
+comm -1 -2 <(sort /data/lims/patient_blacklist.tsv) <(sort temp_datarequest.tsv) | wc -l
 
-rm -r temp_blacklist_check
+rm temp_datarequest.tsv
