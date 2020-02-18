f518d309d50bd10f8c7fb671399d7e21ccdc36e0
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2020-02-18 13:29:51 -0500

    Add script to pass QC for flowcell
    Calls the SBP API patch on the flowcell to change its status, followed by a recalculate. The recalculate
    will QC all the samples and fastq, not taking into account the flowcell.

---
+++ sbpext/pass_qc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+api_url=$1
+api_dir=$2
+flowcell_id=$3
+
+api_crt="${api_dir}/api.crt"
+api_key="${api_dir}/api.key"
+
+## Generic function to use api
+hmfapi () {
+  echo "$@" 1>&2
+  http --ignore-stdin --cert="${api_crt}" --cert-key="${api_key}" "$@"
+}
+
+flowcell="$(hmfapi GET ${api_url}/hmf/v1/flowcells?flowcell_id=${flowcell_id})"
+
+if [ $(echo "${flowcell}" | jq length) -eq 1 ]; then
+    api_id=$(echo "${flowcell}" | jq -r .[0].id);
+    hmfapi PATCH ${api_url}/hmf/v1/flowcells/${api_id} undet_rds_p_pass=True > /dev/null;
+    hmfapi POST ${api_url}/hmf/v1/flowcells/${api_id}/recalculate
+fi
