a0af2a793a1025166f0e5a85a81f34e88deb54b2
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2020-02-28 14:57:57 +0100

    Add tml and tmb to comparison queries

--- sql/genomic/compare_purity_between_2_databases.sql
+++ sql/genomic/compare_purity_between_2_databases.sql
@@ -1,5 +1,9 @@
-SELECT "Truth" as pipeline, version, sampleId, gender, status, qcStatus, purity, normFactor, score, somaticPenalty, ploidy, diploidProportion, polyclonalProportion, wholeGenomeDuplication, minPurity, maxPurity, minDiploidProportion, maxDiploidProportion, msIndelsPerMb, msStatus
+SELECT "Truth" as pipeline, version, sampleId, gender, status, qcStatus, purity, normFactor, score,
+somaticPenalty, ploidy, diploidProportion, polyclonalProportion, wholeGenomeDuplication, minPurity, maxPurity, minDiploidProportion, maxDiploidProportion,
+msIndelsPerMb, msStatus, tmbPerMb, tmbStatus, tml, tmlStatus
 FROM reference_validation_sets.purity WHERE sampleId = 'XXX'
 UNION
-SELECT "New" as pipeline, version, sampleId, gender, status, qcStatus, purity, normFactor, score, somaticPenalty, ploidy, diploidProportion, polyclonalProportion, wholeGenomeDuplication, minPurity, maxPurity, minDiploidProportion, maxDiploidProportion, msIndelsPerMb, msStatus
+SELECT "New" as pipeline, version, sampleId, gender, status, qcStatus, purity, normFactor, score,
+somaticPenalty, ploidy, diploidProportion, polyclonalProportion, wholeGenomeDuplication, minPurity, maxPurity, minDiploidProportion, maxDiploidProportion,
+msIndelsPerMb, msStatus, tmbPerMb, tmbStatus, tml, tmlStatus
 FROM pipeline_v5_validation.purity WHERE sampleId = 'XXX';
