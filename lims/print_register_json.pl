e674f627aa80f918f1d01659872bb4cb0f772436
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2020-04-30 15:04:21 +0200

    DEV-1240: changes to prepare for removal of sbpext user

--- lims/print_register_json.pl
+++ lims/print_register_json.pl
@@ -28,7 +28,6 @@ my $NO_PIPELINE_PRIO = 100;
 my $YES_PIPELINE_PRIO = 99;
 
 my $LIMS_IN_FILE = '/data/lims/lims.json';
-my $OUTPUT_DIR = '/data/ops/api/prod/'
 my $JSON_BASE_DIR = '/data/ops/api/prod/set_jsons';
 my $JSON_DONE_DIR = '/data/ops/api/prod/set_jsons/registered';
 my $SHALLOW_RUNS_DIR = '/data/data_archive/shallow_seq_pipelines';
