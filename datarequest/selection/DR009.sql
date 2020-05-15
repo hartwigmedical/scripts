340d3556a780f84c4460998a983b59d8fbe5022f
Daphne van Beek <d.vanbeek@hartwigmedicalfoundation.nl>
2020-05-15 14:24:01 +0200

    Corrected patient selection for DR009

--- datarequest/selection/DR009.sql
+++ datarequest/selection/DR009.sql
@@ -1,6 +1,19 @@
 SELECT
-DISTINCT patientId AS '#patientId'
+DISTINCT(dr.patientId) AS '#patientId'
 FROM
-datarequest
-WHERE concatenatedTreatmentType LIKE "%Immuno%"
+datarequest as dr
+  INNER JOIN
+  patient AS p on dr.patientId = p.patientIdentifier
+  INNER JOIN
+  baseline AS b ON b.patientId = p.id
+  LEFT JOIN
+  drug AS d ON d.patientId = p.id
+  INNER JOIN
+  treatment AS t ON d.treatmentId = t.id
+WHERE
+d.type LIKE '%immuno%'
+AND
+t.treatmentGiven = "Yes"
+AND NOT
+isnull(t.startDate)
 ORDER BY 1;
