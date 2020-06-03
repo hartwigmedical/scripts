cb826ac8ac473c4d346080db42aaf613d83fe2f3
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-06-03 14:26:32 +0200

    DEV-1316: Order by sample ID

--- sql/genomic/wide_germline_carriership.sql
+++ sql/genomic/wide_germline_carriership.sql
@@ -1,3 +1,4 @@
 SELECT sampleId, chromosome, position, filter, type, ref, alt, gene, transcript, codingEffect, hgvsCoding, hgvsProtein, refStatus
 FROM germlineVariant
-WHERE sampleId LIKE '%WIDE%';
+WHERE sampleId LIKE '%WIDE%'
+ORDER by sampleId;
