63ac645da833dd6bff615d7c2632a39d56eccbc2
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-06-03 13:39:47 +0200

    DEV-1316: Simplified querying scripts and added sql for WIDE germline carriership

---
+++ sql/genomic/wide_germline_carriership.sql
@@ -0,0 +1,3 @@
+SELECT sampleId, chromosome, position, filter, type, ref, alt, gene, transcript, codingEffect, hgvsCoding, hgvsProtein, refStatus
+FROM germlineVariant
+WHERE sampleId LIKE '%WIDE%';
