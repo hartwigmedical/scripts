722afa85aecdf86e6d63878ae78176342cfb9f76
Ned Leitch <ned.leitch@gmail.com>
2020-07-13 13:32:11 -0400

    DEV-1374 Remove arrival_date script
    We have re-implemented in Java.

--- turquoise/upload_all.sh
+++ turquoise/upload_all.sh
@@ -2,7 +2,6 @@
 #
 # Wrapper to allow all scripts to be run from cron easily
 
-/data/common/repos/scripts/turquoise/arrival_date.sh && \
 /data/common/repos/scripts/turquoise/report_date.sh && \
-gsutil cp ${HOME}/.turquoise/*.json gs://turquoise-events-prod/
-
+gsutil cp ${HOME}/.turquoise/*.json gs://turquoise-events-prod/ && \
+gsutil cp /data/ops/lims/prod/lims.json gs://turquoise-lims-prod/
