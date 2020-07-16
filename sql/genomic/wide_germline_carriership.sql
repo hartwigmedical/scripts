3a0fba9aeec11494d3e18bfd0ef018444c05a9a4
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-07-16 19:40:29 +0200

    Reenable WIDE carriership overview

--- sql/genomic/wide_germline_carriership.sql
+++ sql/genomic/wide_germline_carriership.sql
@@ -1,4 +1,4 @@
 SELECT sampleId, chromosome, position, filter, type, ref, alt, gene, transcript, codingEffect, hgvsCoding, hgvsProtein, refStatus
 FROM germlineVariant
-WHERE sampleId LIKE '%WIDE%'
+WHERE sampleId LIKE '%WIDE%' and reported = 1
 ORDER by sampleId;
