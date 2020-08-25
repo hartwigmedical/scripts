51207b49fd23aada60d0af0aa54e33f3f70340a2
Sandra vd Broek <vandenbroek.alexandra@gmail.com>
2020-08-25 11:54:38 +0200

    Update check_gcp_bucket script

---
+++ datarequest/scripts/check_gcp_hmf_dr_bucket.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+print_usage(){
+    echo "-----"
+    echo " Descr: Checks created GCP bucket for DR"
+    echo " Usage: $(basename $0) -b <bucket_name> -e <gcp_mail>"
+    echo " Exmpl: $(basename $0) -b 'hmf-dr-XXX' -e 'john@doe.com'"
+    echo "-----"
+    exit 1
+}
+
+while getopts ':b:s:m:e:' flag; do
+    case "${flag}" in
+        i) bucket_name=${OPTARG} ;;
+        e) gcp_mail=${OPTARG} ;;
+        *) print_usage
+        exit 1 ;;
+    esac
+done
+
+if [[ -z "${bucket_name}" || -z "${gcp_mail}" ]]; then
+    print_usage
+fi
+
+echo ""
+echo "[INFO] Files in the bucket" ${bucket_name}":"
+gsutil du -h gs://${bucket_name}/
+echo ""
+
+echo "[INFO] Persmissions of the bucket"  ${bucket_name}":"
+gsutil -u hmf-share iam get gs://${bucket_name}/
+echo ""
+
+email_in_persmissions=$( gsutil -u hmf-share iam get gs://${bucket_name}/ | grep ${gcp_mail} )
+if [[ ${email_in_persmissions} == "" ]]; then
+    echo "[ERROR] Account(s) not added to IAM GCP bucket. Please add manually in the GUI (role = storage object viewer)."
+else echo "[INFO] Permissions set correctly."
+fi
+echo ""
+
