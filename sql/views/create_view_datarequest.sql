b6f131596c5ccfff25222b83e55be8e1c1d6b1dd
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-10-22 11:02:11 +0200

    Simplify datarequest view thanks to new purple

--- sql/views/create_view_datarequest.sql
+++ sql/views/create_view_datarequest.sql
@@ -1,12 +1,11 @@
 CREATE OR REPLACE VIEW datarequest AS
 
 SELECT
-	clinical.*, purity AS tumorPurity, qcStatus AS purpleQC, fitMethod AS purpleFitMethod,
-	version AS purpleVersion, metric.sufficientCoverage AS sufficientCoverage
+	clinical.*, purity AS tumorPurity, qcStatus AS purpleQC, version AS purpleVersion, metric.sufficientCoverage AS sufficientCoverage
 FROM
 	clinical
 	INNER JOIN purity ON purity.sampleId = clinical.sampleId
 	INNER JOIN metric ON metric.sampleId = clinical.sampleId
 WHERE
-	blacklisted = 0 AND qcStatus = 'PASS' AND sufficientCoverage = 1 AND fitMethod <> 'NO_TUMOR' AND purity > 0.195 AND
+	blacklisted = 0 AND qcStatus = 'PASS' AND sufficientCoverage = 1 AND
 	(clinical.sampleId LIKE '%CPCT%' OR clinical.sampleId LIKE '%WIDE%' OR clinical.sampleId LIKE '%DRUP%');
