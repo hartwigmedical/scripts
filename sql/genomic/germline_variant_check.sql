899e2d5c72b2ae8dbee153c672d88237bdf8593d
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2020-11-09 17:47:16 +0100

    Use one X

--- sql/genomic/germline_variant_check.sql
+++ sql/genomic/germline_variant_check.sql
@@ -6,4 +6,4 @@ OR (pathogenic = 'CLINVAR_CONFLICTING' AND filter = 'PASS')
 OR (gene IN ('BMPR1A','EPCAM','FH','FLCN','MUTYH','NTHL1','POLD1','SDHA','SDHAF2','SDHB','SDHC','SDHD') AND biallelic = 0 AND reported = 1)			#these genes are not in gene panel AND require biallelic reporting. We should check somaticVariant in case the WT allele is not already lost.
 OR (gene IN ('NTHL1','MUTYH') AND reported = 1 AND refStatus = 'HOM')                                                                                   #these genes should only be marked with patient notification in case of refStatus = homozygous
 OR (gene IN ('CDK4','RET','MET','KIT') AND reported = 1 AND pathogenic = 'UNANNOTATED' AND codingEffect IN ('NONSENSE_OR_FRAMESHIFT','SPLICE')))                          #are unannotated frameshift/nonsense/splice in 'oncogenes' pathogenic?
-AND sampleId IN ('XXX'); 
+AND sampleId IN ('X');
