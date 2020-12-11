0cbf74fc74b2eeb5e99cb0d8352483ef42a47607
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2020-12-11 15:29:09 +0100

    use lims file

--- lims/query_lims_pilot.pl
+++ lims/query_lims_pilot.pl
@@ -15,7 +15,7 @@ use 5.010.000;
 ## -----
 my $DATETIME = localtime;
 my $SCRIPT = basename $0;
-my $LIMS_JSON = '/data/experiments/patient_reporter_version_7_19_verification/lims';
+my $LIMS_JSON = '/data/experiments/patient_reporter_version_7_19_verification/lims/lims.json';
 my $NONREPORTABLE_TSV = '/data/ops/lims/prod/samples_without_pdf_report.tsv';
 my @PDF_DIRS = ( '/data/cpct/final_reports', '/data/core/final_reports', '/data/cpct/old_cpct_reports' );
 my $pdfdirs_string = join( ", ", sort @PDF_DIRS );
