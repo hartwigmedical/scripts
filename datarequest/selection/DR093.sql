2b1551f9fe6cc60c7abafe399c242de92372029b
Sandra vd Broek <vandenbroek.alexandra@gmail.com>
2021-01-05 10:19:22 +0100

    updated selection for DR093

--- datarequest/selection/DR093.sql
+++ datarequest/selection/DR093.sql
@@ -2,5 +2,5 @@ SELECT
     DISTINCT patientId AS '#patientId'
 FROM
     datarequest
-WHERE primaryTumorLocation = 'Nervous system' AND cancerSubtype = 'Glioblastoma Multiforme' AND (treatment like '%Temozolomide%')
+WHERE primaryTumorLocation = 'Nervous system' AND primaryTumorSubType = 'Glioblastoma Multiforme' AND (treatment like '%Temozolomide%')
 ORDER BY 1;
