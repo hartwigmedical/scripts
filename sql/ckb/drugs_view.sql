242d8a1d7827cfd5ac63685efe980c2b9952db0f
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-02-23 13:42:41 +0100

    Add 2 more views on CKB data

---
+++ sql/ckb/drugs_view.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE VIEW drugs AS (
+
+SELECT drug.createDate, drugName, tradeName, casRegistryNum, ncitId, group_concat(DISTINCT drugClass) AS drugClasses
+FROM drug INNER JOIN drugClass ON drugClass.drugId = drug.id
+GROUP BY 1,2,3,4,5);
