e9555b4d97f8b7037fd6466c2761d105a43234f0
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-02-25 10:00:44 +0100

    Updated drug query

--- sql/ckb/drugs_view.sql
+++ sql/ckb/drugs_view.sql
@@ -1,5 +1,12 @@
 CREATE OR REPLACE VIEW drugs AS (
 
-SELECT drug.createDate, drugName, tradeName, casRegistryNum, ncitId, group_concat(DISTINCT drugClass) AS drugClasses
-FROM drug LEFT JOIN drugClass ON drugClass.drugId = drug.id
-GROUP BY 1,2,3,4,5);
+SELECT
+    drug.createDate, drugName, tradeName, group_concat(DISTINCT term) AS terms, group_concat(DISTINCT synonym) AS synonyms,
+    group_concat(DISTINCT drugClass) AS drugClasses,
+    casRegistryNum, ncitId, description, group_concat(DISTINCT pubmedId) AS pubmeds
+FROM drug
+LEFT JOIN drugClass ON drugClass.drugId = drug.id
+LEFT JOIN drugTerm ON drugTerm.drugId = drug.id
+LEFT JOIN drugSynonym ON drugSynonym.drugId = drug.id
+LEFT JOIN drugReference ON drugReference.drugId = drug.id AND NOT(isnull(pubmedId))
+GROUP BY 1,2,3,7,8,9);
