0f535543c439f40c82fb5d251ee2e566caa8242d
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-03-01 16:19:59 +0100

    Modification of trial view CKB

--- sql/ckb/trials_view.sql
+++ sql/ckb/trials_view.sql
@@ -4,11 +4,11 @@ SELECT DISTINCT
 	profileName AS tumorProfile, clinicalTrial.updateDate AS trialUpdateDate, clinicalTrial.nctId, title, phase, recruitment, gender,
 	group_concat(DISTINCT ageGroup) AS ageGroups,
 	group_concat(DISTINCT country) AS countries,
-    group_concat(DISTINCT name) as cancerType
+    group_concat(DISTINCT name) as cancerTypes
 FROM ckbEntry
 INNER JOIN clinicalTrial ON clinicalTrial.ckbEntryId = ckbEntry.id
-LEFT JOIN indicationClinicalTrial ON indicationClinicalTrial.clinicalTrialId=clinicalTrial.id
-LEFT JOIN indication ON indication.id=indicationClinicalTrial.indicationId
+LEFT JOIN indicationClinicalTrial ON indicationClinicalTrial.clinicalTrialId = clinicalTrial.id
+LEFT JOIN indication ON indication.id = indicationClinicalTrial.indicationId
 LEFT JOIN ageGroup ON ageGroup.clinicalTrialId = clinicalTrial.id
 LEFT JOIN location ON location.clinicalTrialId = clinicalTrial.id
 GROUP BY 1,2,3,4,5,6,7);
