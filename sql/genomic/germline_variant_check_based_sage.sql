21623a03e7f3f6ab9b9ae059b2fec84e76c426c9
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-03-01 16:09:41 +0100

    germline variant check based sage reportage

--- sql/genomic/germline_variant_check_based_sage.sql
+++ sql/genomic/germline_variant_check_based_sage.sql
@@ -1,11 +1,7 @@
-SELECT * FROM germlineVariant2
-WHERE (
-(pathogenic = 'UNANNOTATED' AND canonicalCodingEffect IN ('NONSENSE_OR_FRAMESHIFT','SPLICE') AND reported = 0)						                    #also check for filter here
-OR (pathogenic IN ('CLINVAR_PATHOGENIC','CLINVAR_LIKELY_PATHOGENIC','WHITE_LIST') AND reported = 0)
-OR (pathogenic = 'CLINVAR_CONFLICTING' AND filter = 'PASS')
-OR (gene IN ('BMPR1A','EPCAM','FH','FLCN','POLD1','SDHA','SDHAF2','SDHB','SDHC','SDHD') AND biallelic = 0 AND reported = 1)			#these genes are not in gene panel AND require biallelic reporting. We should check somaticVariant in case the WT allele is not already lost.
-OR (gene IN ('NTHL1','MUTYH') AND reported = 1 AND germlineGenotype = 'HOM')                                                                                   #these genes should only be marked with patient notification in case of refStatus = homozygous
-OR (gene IN ('CDK4','RET','MET','KIT') AND reported = 1 AND pathogenic = 'UNANNOTATED' AND canonicalCodingEffect IN ('NONSENSE_OR_FRAMESHIFT','SPLICE'))
-OR (sampleId IN (SELECT sampleId FROM germlineVariant WHERE gene = 'MUTYH' AND reported = 1 GROUP BY sampleId HAVING count(sampleId)>1))
-OR (sampleId IN (SELECT sampleId FROM germlineVariant WHERE gene = 'NTHL1' AND reported = 1 GROUP BY sampleId HAVING count(sampleId)>1)))
-AND sampleId IN ("COREDB010034T","COREDB010039T","CORELR020039T","CORELR110013T","CORELR110017T","DRUP01070178T","WIDE01011233T");
+select * from germlineVariant
+where sampleId in("XXX") and reported and (sampleId, gene, hgvsCoding)
+not in (select sampleId, gene, canonicalHgvsCodingImpact from germlineVariant2 where sampleId in("XXX") and reported);
+
+select * from germlineVariant2
+where sampleId in("XXX") and reported and (sampleId, gene, canonicalHgvsCodingImpact)
+not in (select sampleId, gene, hgvsCoding from germlineVariant where sampleId in("XXX") and reported);
