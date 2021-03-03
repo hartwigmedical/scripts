0ad762fc03702c44e24bf11e4471d8a4a486ad34
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-03-03 09:18:21 +0100

    remove pilot db

--- sql/ckb/check_new_ckbEntry_dump.sql
+++ sql/ckb/check_new_ckbEntry_dump.sql
@@ -1,3 +1,3 @@
 # [date] --> could be eg. 2021-02-05
 
-select * from ckb_db_pilot.ckbEntry where createDate > "[date]" order by createDate desc;
+select * from ckbEntry where createDate > "[date]" order by createDate desc;
