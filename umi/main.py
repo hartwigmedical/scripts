72969a15dff4707b1d8820d8be89ada03e979b68
David Koetsier <david.f.koetsier@gmail.com>
2021-03-04 09:37:09 +0100

    Make umi undup use warnings instead of erroring out

--- umi/main.py
+++ umi/main.py
@@ -4,7 +4,7 @@ import logging
 import sys
 from collections import defaultdict
 
-from typing import List, NamedTuple, Set, Dict, Optional, DefaultDict
+from typing import List, NamedTuple, Set, Dict, Optional, DefaultDict, Tuple
 
 import pysam as pysam
 
@@ -44,12 +44,13 @@ class PairedReadTracker(object):
     def __init__(self) -> None:
         self.__read_positions_to_paired_read_summaries = defaultdict(list)
 
-    def add(self, reads: Set[SingleReadSummary]) -> None:
+    def add(self, reads: Set[SingleReadSummary], warnings: DefaultDict[Tuple[str, ...], List[str]]) -> None:
         # Order of arguments does not matter
-        umis = {read.explicit_umi for read in reads if read.explicit_umi is not None}
-        if len(umis) == 0:
-            umis = {read.query_name.split(":")[-1] for read in reads}
+        # umis = {read.explicit_umi for read in reads if read.explicit_umi is not None}
+        # if len(umis) == 0:
+        #     umis = {read.query_name.split(":")[-1] for read in reads}
 
+        umis = {read.query_name.split(":")[-1] for read in reads}
         if len(umis) == 1:
             umi = umis.pop()
         elif len(umis) == 0:
@@ -62,8 +63,16 @@ class PairedReadTracker(object):
         read_summaries = {
             PairedReadSummary(read.query_name, umi, worst_mapping_quality, read.is_duplicate) for read in reads
         }
-        assert len(read_summaries) == 1, f"Read summaries are not the same"
-        read_summary = read_summaries.pop()
+        if len(read_summaries) == 1:
+            read_summary = read_summaries.pop()
+        else:
+            read_is_duplicate = any(read.is_duplicate for read in reads)
+            read_summaries = {
+                PairedReadSummary(read.query_name, umi, worst_mapping_quality, read_is_duplicate) for read in reads
+            }
+            assert len(read_summaries) == 1, f"Read summaries are still not the same: {reads}"
+            warnings[tuple(sorted(list({read.query_name for read in reads})))].append(f"Not all reads agree on whether they are duplicate: {reads}")
+            read_summary = read_summaries.pop()
 
         read_positions = frozenset({
             ReadPosition(read.reference_name, read.reference_start, read.is_reverse, read.is_unmapped) for read in reads
@@ -71,7 +80,7 @@ class PairedReadTracker(object):
 
         self.__read_positions_to_paired_read_summaries[read_positions].append(read_summary)
 
-    def get_reads_to_be_undupped(self) -> Set[str]:
+    def get_reads_to_be_undupped(self, warnings: DefaultDict[Tuple[str, ...], List[str]]) -> Set[str]:
         reads_to_be_undupped = set()
         for read_summaries in self.__read_positions_to_paired_read_summaries.values():
             umi_to_summaries = defaultdict(list)
@@ -85,16 +94,20 @@ class PairedReadTracker(object):
                     best_quality_pair = sorted(summaries_grouped_by_umi, key=lambda x: x.worst_mapping_quality, reverse=True)[0]
                     reads_to_be_undupped.add(best_quality_pair.query_name)
                 else:
-                    assert len(non_duplicate_read_pair_summaries) == 1, (
-                        f"Multiple non duplicate reads for same umi at same locations.\n"
-                        f"{non_duplicate_read_pair_summaries}"
-                    )
+                    if len(non_duplicate_read_pair_summaries) != 1:
+                        warning_msg = (
+                            f"Multiple non duplicate reads for same umi at same locations.\n"
+                            f"{non_duplicate_read_pair_summaries}"
+                        )
+                        warnings[tuple(sorted(list({summary.query_name for summary in non_duplicate_read_pair_summaries})))].append(warning_msg)
 
         return reads_to_be_undupped
 
 
 def main(input_bam_path: str, output_bam_path: str) -> None:
     print("START UNDUP")
+    warnings = defaultdict(list)
+
     bam_file = pysam.AlignmentFile(input_bam_path, "rb")
 
     query_name_to_single_read_summaries: DefaultDict[str] = defaultdict(list)
@@ -137,23 +150,30 @@ def main(input_bam_path: str, output_bam_path: str) -> None:
     for grouped_read_summaries in query_name_to_single_read_summaries.values():
         if not reads_match(grouped_read_summaries):
             grouped_reads_string = "\n".join(str(summary) for summary in grouped_read_summaries)
-            error_msg = f"Reads don't match perfectly:\n{grouped_reads_string}"
-            raise ValueError(error_msg)
+            warning_msg = f"Reads don't match perfectly:\n{grouped_reads_string}"
+            warnings[(grouped_read_summaries[0].query_name,)].append(warning_msg)
 
-        tracker.add(grouped_read_summaries)
+        tracker.add(grouped_read_summaries, warnings)
 
-    reads_to_be_undupped = tracker.get_reads_to_be_undupped()
-    print(reads_to_be_undupped)
+    reads_to_be_undupped = tracker.get_reads_to_be_undupped(warnings)
+    # print(reads_to_be_undupped)
 
     bam_file = pysam.AlignmentFile(input_bam_path, "rb")
     with pysam.AlignmentFile(output_bam_path, "wb", header=bam_file.header) as outf:
         for read in bam_file:
             if read.query_name in reads_to_be_undupped:
                 if read.flag % 2048 < 1024:
-                    error_msg = f"Impossible bit flag for read to be undupped: {read.flag}\n{read}"
-                    raise ValueError(error_msg)
-                read.flag = read.flag - 1024
+                    warning_msg = f"Impossible bit flag for read to be undupped: {read.flag}\n{read}"
+                    warnings[(read.query_name,)].append(warning_msg)
+                else:
+                    read.flag = read.flag - 1024
             outf.write(read)
+
+    warning_keys = sorted(list(warnings.keys()), key=lambda x: (len(x), x))
+    print("\n\n".join([f"{key}:\n" + "\n".join(warnings[key]) for key in warning_keys]))
+
+    if warnings:
+        raise ValueError("There was at least one warning")
 
     print("DONE WITH UNDUP")
 
