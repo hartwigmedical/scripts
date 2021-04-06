9ab1ca65d6b895555b1168e9f67fcf9ae7fb03e0
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-04-06 09:16:54 +0200

    Added view for HPC

---
+++ sql/views/create_view_hpc.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE VIEW hpc AS
+
+SELECT SUBSTRING_INDEX(group_concat(purity.sampleId ORDER BY purity DESC, id ASC SEPARATOR ','), ",", 1) AS sampleId
+FROM purity
+    INNER JOIN amberPatient ON purity.sampleId = amberPatient.sampleId
+    INNER JOIN metric ON purity.sampleId = metric.sampleId
+WHERE qcStatus = 'PASS' and sufficientCoverage = 1
+GROUP BY patientId;
