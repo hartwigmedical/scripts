0ba7e5373d9f3388ea1848599b4918dae9c229b5
David Koetsier <david.f.koetsier@gmail.com>
2021-04-12 17:06:04 +0200

    Handle case where filtered vcf is empty

--- pgx/src/vcf_reader.py
+++ pgx/src/vcf_reader.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Tuple
+from typing import Dict, Any, Tuple, Optional
 
 import allel
 
@@ -33,7 +33,8 @@ class VcfReader(object):
         return cls.__get_call_data_from_variants(variants, panel)
 
     @classmethod
-    def __get_variants_from_filtered_vcf(cls, filtered_vcf: str) -> Dict[str, Any]:
+    def __get_variants_from_filtered_vcf(cls, filtered_vcf: str) -> Optional[Dict[str, Any]]:
+        # variants is None precisely when filtered vcf file has no variants
         try:
             variants = allel.read_vcf(filtered_vcf, fields=cls.FIELD_NAMES, transformers=allel.ANNTransformer())
         except IOError:
@@ -41,58 +42,60 @@ class VcfReader(object):
         return variants
 
     @classmethod
-    def __get_call_data_from_variants(cls, variants: Dict[str, Any], panel: Panel) -> Grch37CallData:
+    def __get_call_data_from_variants(cls, variants: Optional[Dict[str, Any]], panel: Panel) -> Grch37CallData:
         match_on_rsid = 0
         match_on_location = 0
         filtered_calls = set()
-        for i, rs_ids_string in enumerate(variants[cls.RS_IDS_FIELD_NAME]):
-            chromosome = str(variants[cls.CHROMOSOME_FIELD_NAME][i])
-            position = int(variants[cls.POSITION_FIELD_NAME][i])
-            reference_allele = str(variants[cls.REF_ALLELE_FIELD_NAME][i])
+        if variants is not None:
+            for i, rs_ids_string in enumerate(variants[cls.RS_IDS_FIELD_NAME]):
+                chromosome = str(variants[cls.CHROMOSOME_FIELD_NAME][i])
+                position = int(variants[cls.POSITION_FIELD_NAME][i])
+                reference_allele = str(variants[cls.REF_ALLELE_FIELD_NAME][i])
 
-            rs_ids = cls.__get_rs_ids_from_string(rs_ids_string)
-            relevant_coordinates = cls.__get_relevant_coordinates(chromosome, position, reference_allele)
+                rs_ids = cls.__get_rs_ids_from_string(rs_ids_string)
+                relevant_coordinates = cls.__get_relevant_coordinates(chromosome, position, reference_allele)
 
-            rs_id_match_to_panel_exists = any(panel.contains_rs_id(rs_id) for rs_id in rs_ids)
-            coordinate_match_to_panel_exists = any(
-                panel.contains_rs_id_with_grch37_coordinate(coord) for coord in relevant_coordinates
-            )
-            if rs_id_match_to_panel_exists or coordinate_match_to_panel_exists:
-                if rs_id_match_to_panel_exists:
-                    match_on_rsid += 1
-                if coordinate_match_to_panel_exists:
-                    match_on_location += 1
-                if variants[f"{cls.FILTER_FIELD_NAME}_PASS"][i]:
-                    filter_type = Filter.PASS
-                else:
-                    continue
-                alts = [str(allele) for allele in variants[cls.ALT_ALLELE_FIELD_NAME][i]]
-                variant_annotation = str(variants[f"{cls.ANNOTATION_FIELD_NAME}_HGVS_c"][i])
-                gene = str(variants[f"{cls.ANNOTATION_FIELD_NAME}_Gene_Name"][i])
-                genotype = variants[cls.GENOTYPE_FIELD_NAME][i][0].tolist()
-                if genotype == [0, 1]:
-                    alleles = (reference_allele, alts[0])
-                elif genotype == [1, 1]:
-                    alleles = (alts[0], alts[0])
-                elif genotype == [1, 2]:
-                    alleles = (alts[0], alts[1])
-                elif genotype == [0, 0]:
-                    alleles = (reference_allele, reference_allele)
-                    variant_annotation = REF_CALL_ANNOTATION_STRING
-                else:
-                    error_msg = f"Genotype not found: {genotype}"
-                    raise ValueError(error_msg)
-
-                call = Grch37Call(
-                    GeneCoordinate(chromosome, position),
-                    reference_allele,
-                    alleles,
-                    gene,
-                    rs_ids,
-                    variant_annotation,
-                    filter_type,
+                rs_id_match_to_panel_exists = any(panel.contains_rs_id(rs_id) for rs_id in rs_ids)
+                coordinate_match_to_panel_exists = any(
+                    panel.contains_rs_id_with_grch37_coordinate(coord) for coord in relevant_coordinates
                 )
-                filtered_calls.add(call)
+                if rs_id_match_to_panel_exists or coordinate_match_to_panel_exists:
+                    if rs_id_match_to_panel_exists:
+                        match_on_rsid += 1
+                    if coordinate_match_to_panel_exists:
+                        match_on_location += 1
+                    if variants[f"{cls.FILTER_FIELD_NAME}_PASS"][i]:
+                        filter_type = Filter.PASS
+                    else:
+                        # Ignore all calls with filter != PASS
+                        continue
+                    alts = [str(allele) for allele in variants[cls.ALT_ALLELE_FIELD_NAME][i]]
+                    variant_annotation = str(variants[f"{cls.ANNOTATION_FIELD_NAME}_HGVS_c"][i])
+                    gene = str(variants[f"{cls.ANNOTATION_FIELD_NAME}_Gene_Name"][i])
+                    genotype = variants[cls.GENOTYPE_FIELD_NAME][i][0].tolist()
+                    if genotype == [0, 1]:
+                        alleles = (reference_allele, alts[0])
+                    elif genotype == [1, 1]:
+                        alleles = (alts[0], alts[0])
+                    elif genotype == [1, 2]:
+                        alleles = (alts[0], alts[1])
+                    elif genotype == [0, 0]:
+                        alleles = (reference_allele, reference_allele)
+                        variant_annotation = REF_CALL_ANNOTATION_STRING
+                    else:
+                        error_msg = f"Genotype not found: {genotype}"
+                        raise ValueError(error_msg)
+
+                    call = Grch37Call(
+                        GeneCoordinate(chromosome, position),
+                        reference_allele,
+                        alleles,
+                        gene,
+                        rs_ids,
+                        variant_annotation,
+                        filter_type,
+                    )
+                    filtered_calls.add(call)
 
         print("[INFO] Matches on RS id: " + str(match_on_rsid))
         print("[INFO] Matches on location: " + str(match_on_location))
