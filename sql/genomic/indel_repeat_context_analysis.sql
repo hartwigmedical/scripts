1bd0091f82b476a283a37d5b7b9348bf4285bcbb
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-04-26 14:58:27 +0200

    Formatting

--- sql/genomic/indel_repeat_context_analysis.sql
+++ sql/genomic/indel_repeat_context_analysis.sql
@@ -3,6 +3,6 @@ least(5,greatest(-5,length(alt)-length(ref))) AS indelLength,
 count(*) AS totalCount,
 sum(IF(repeatCount>=4,1,0)) AS repeatCount,
 sum(IF(repeatCount<4 AND microhomology <>'' AND microhomology <> '.',1,0)) AS MHCount,
-sum(IF(repeatCount<4 AND (microhomology ='' OR microhomology='.'),1,0)) AS nonRepeatnonMHCount  
+sum(IF(repeatCount<4 AND (microhomology ='' OR microhomology='.'),1,0)) AS nonRepeatNonMHCount
 FROM somaticVariant WHERE sampleId = 'WIDE01010200T' AND type = 'INDEL' AND filter = 'PASS' GROUP BY 1,2;
 
