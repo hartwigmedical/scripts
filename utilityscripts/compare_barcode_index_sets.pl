9d3ec71059574695f164d20a9533aa0c2ae56a74
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2021-04-30 12:22:59 +0200

    use hmf indexes by default and help msg updated

--- utilityscripts/compare_barcode_index_sets.pl
+++ utilityscripts/compare_barcode_index_sets.pl
@@ -7,25 +7,46 @@ use Getopt::Long;
 use File::Basename;
 use 5.010.000;
 
-use constant MIN_DISTANCE => 2;
+my $MIN_DISTANCE = 2;
+my $script_name = basename $0;
+my $script_dir = dirname $0;
+my $out_file = "index_comparison.tsv";
+my $fail_str = 'FAIL';
+my $success_str = 'OK';
 
 my $HELP =<<HELP;
+
   Description
-    Takes two TSV dual barcode index collection TSV files (columns: name1, name2, seq1, seq2)
-    and determines the i7 and i5 distance for every dual index in file1 against every in file2.
+    Compares two dual barcode index collections (format: name1<tab>name2<tab>seq1<tab>seq2)
+    and determines the i7 and i5 distance for every possible combination. Result for each is
+    '$fail_str' in case both i5 and i7 distances are lower than $MIN_DISTANCE, and otherwise '$success_str'.
+  Usage
+    $script_name <tsv-with-external-indexes> <tsv-with-hmf-indexes>
+    $script_name <tsv-with-external-indexes>
+  Example TSV input
+    #i7name i5name i7seq i5seq
+    IDT8_i7_1 IDT8_I5_1 CTGATCGT GCGCATAT
+  Example output (written to $out_file)
+    #result dist_s1 dist_s2 dist_total i1_t1 i1_s1 i1_t2 i1_s2 i2_t1 i2_s1 i2_t2 i2_s2
+    OK 5 3 8 F498_LCH588 CTGCGGAT CZ-0379-LL_A AGATAACC IDT8_i7_384 CGACCATT IDT8_I5_384 TGATAGGC
+
 HELP
-print $HELP and exit(0) if scalar @ARGV > 0 and ($ARGV[0] eq '-h' or $ARGV[0] eq '--help');
+print $HELP and exit(0) if scalar @ARGV == 0 or $ARGV[0] eq '-h' or $ARGV[0] eq '--help';
 
-my $file1 = "index_customer.tsv";
-my $file2 = "index_hmf.tsv";
-my $out_file = "table.tsv";
+my $file1 = "./indexes_customer.tsv";
+my $file2 = "$script_dir/indexes_hmf.tsv";
+$file1 = $ARGV[0] if $ARGV[0];
+$file2 = $ARGV[1] if $ARGV[1];
 
-say "Comparing $file1 with $file2";
+say "[INFO] Comparing $file1 with $file2";
 my $indexes1 = read_indexes_tsv($file1);
 my $indexes2 = read_indexes_tsv($file2);
 
 my @header = qw(result dist_s1 dist_s2 dist_total i1_t1 i1_s1 i1_t2 i1_s2 i2_t1 i2_s1 i2_t2 i2_s2);
 open(my $OUT_FH, '>', $out_file) or die $!;
+say $OUT_FH '##' . " Input file1: $file1";
+say $OUT_FH '##' . " Input file2: $file2";
+say $OUT_FH '##' . " Minimal distance: $MIN_DISTANCE";
 say $OUT_FH '#' . join("\t", @header);
 foreach my $index1 (@$indexes1){
     foreach my $index2 (@$indexes2){
@@ -33,7 +54,7 @@ foreach my $index1 (@$indexes1){
     }
 }
 close $OUT_FH;
-say "Done. See $out_file for complete result.";
+say "[INFO] Finished with $script_name (see $out_file for result).";
 
 sub compare{
     my ($index1, $index2, $out_fh) = @_;
@@ -50,16 +71,18 @@ sub compare{
 
     my $tag = join("_", $i1_t1, $i1_t2, "vs", $i2_t1, $i2_t2);
 
-    die "Cannot compare $i1_s1 with $i2_s1 (reason: different size)\n" unless length($i1_s1) eq length($i2_s1);
-    die "Cannot compare $i1_s2 with $i2_s2 (reason: different size)\n" unless length($i1_s2) eq length($i2_s2);
+    die "Impossible to compare $i1_s1 with $i2_s1 (reason: different size)\n" unless length($i1_s1) eq length($i2_s1);
+    die "Impossible to compare $i1_s2 with $i2_s2 (reason: different size)\n" unless length($i1_s2) eq length($i2_s2);
 
-    my $result = "OK";
+    my $result = "";
     my $distance1 = calc_distance_between_two_seq($i1_s1, $i2_s1);
     my $distance2 = calc_distance_between_two_seq($i1_s2, $i2_s2);
 
-    if ($distance1 < MIN_DISTANCE and $distance2 < MIN_DISTANCE){
+    if ($distance1 < $MIN_DISTANCE and $distance2 < $MIN_DISTANCE){
         warn "[FAIL] distance too small for $tag ($distance1 and $distance2)\n";
-        $result = "FAIL";
+        $result = $fail_str;
+    }else{
+        $result = $success_str;
     }
     my @fields = join("\t", $i1_t1, $i1_s1, $i1_t2, $i1_s2, $i2_t1, $i2_s1, $i2_t2, $i2_s2);
     say $out_fh join("\t", $result, $distance1, $distance2, $distance1+$distance2, @fields);
@@ -73,7 +96,7 @@ sub calc_distance_between_two_seq{
 
 sub read_indexes_tsv{
     my ($file) = @_;
-    say "Reading file $file";
+    say "[INFO] Reading file $file";
     my @indexes = ();
     open my $fh, '<', $file or die "Unable to open file ($file): $!\n";
     while ( <$fh> ) {
