4b8442c243c048fc359b7c2b111dfefb25d1a432
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-05-04 11:47:45 +0200

    Add contamination check to datarequest

--- sql/views/create_view_datarequest.sql
+++ sql/views/create_view_datarequest.sql
@@ -7,5 +7,5 @@ FROM
 	INNER JOIN purity ON purity.sampleId = clinical.sampleId
 	INNER JOIN metric ON metric.sampleId = clinical.sampleId
 WHERE
-	blacklisted = 0 AND qcStatus = 'PASS' AND sufficientCoverage = 1 AND
+	blacklisted = 0 AND qcStatus = 'PASS' AND sufficientCoverage = 1 AND contamination <= 0.1 AND
 	(clinical.sampleId LIKE '%CPCT%' OR clinical.sampleId LIKE '%WIDE%' OR clinical.sampleId LIKE '%DRUP%');
