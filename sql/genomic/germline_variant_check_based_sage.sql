33d541df31e3063aa801f5e55f2eaea1938f25ad
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-06-04 16:47:41 +0200

    DEV-1979: update to v3.60

--- sql/genomic/germline_variant_check_based_sage.sql
+++
@@ -1,7 +0,0 @@
-select * from germlineVariant
-where sampleId in("XXX") and reported and (sampleId, gene, hgvsCoding)
-not in (select sampleId, gene, canonicalHgvsCodingImpact from germlineVariant2 where sampleId in("XXX") and reported);
-
-select * from germlineVariant2
-where sampleId in("XXX") and reported and (sampleId, gene, canonicalHgvsCodingImpact)
-not in (select sampleId, gene, hgvsCoding from germlineVariant where sampleId in("XXX") and reported);
