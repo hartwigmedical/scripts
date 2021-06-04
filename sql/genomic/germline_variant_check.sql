f092771b49d3df8dcfeab12eef3781076fa4a7d8
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2021-06-04 17:34:49 +0200

    DEV-1979: use germlineVariant table

--- sql/genomic/germline_variant_check.sql
+++ sql/genomic/germline_variant_check.sql
@@ -1,8 +1,8 @@
-SELECT * FROM germlineVariant2
+SELECT * FROM germlineVariant
 WHERE (
 (gene IN (SELECT gene FROM driverGenePanel WHERE reportGermlineVariant=1 OR reportGermlineHotspot=1) AND pathogenic AND filter <> 'PASS')
 OR (gene IN (SELECT gene FROM driverGenePanel WHERE reportGermlineVariant=1 OR reportGermlineHotspot=1) AND pathogenic AND filter = 'PASS' AND NOT reported)
 OR (gene IN (SELECT gene FROM driverGenePanel WHERE reportGermlineVariant=1 OR reportGermlineHotspot=1) AND pathogenicity = 'CONFLICTING' AND filter = 'PASS')
-OR (sampleId IN (SELECT sampleId FROM germlineVariant2 WHERE gene = 'MUTYH' AND pathogenic GROUP BY sampleId HAVING count(sampleId)>1))
-OR (sampleId IN (SELECT sampleId FROM germlineVariant2 WHERE gene = 'NTHL1' AND pathogenic GROUP BY sampleId HAVING count(sampleId)>1)))
+OR (sampleId IN (SELECT sampleId FROM germlineVariant WHERE gene = 'MUTYH' AND pathogenic GROUP BY sampleId HAVING count(sampleId)>1))
+OR (sampleId IN (SELECT sampleId FROM germlineVariant WHERE gene = 'NTHL1' AND pathogenic GROUP BY sampleId HAVING count(sampleId)>1)))
 AND sampleId IN ('XXX');
