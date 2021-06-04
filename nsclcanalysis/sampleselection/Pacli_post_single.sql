92b1925a6bc6a0b4dca25457e9f91772fb077e8c
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-06-04 14:32:56 +0200

    fix script

--- nsclcanalysis/sampleselection/Pacli_post_single.sql
+++ nsclcanalysis/sampleselection/Pacli_post_single.sql
@@ -18,58 +18,56 @@ posttreatmentLine9, posttreatmentLine9startDate, dateDiff(t0.dateBiopsy,posttrea
 FROM
 (select n.sampleId, driver, qcStatus, n.dateBiopsy from nsclcSingleSample n inner join hmfpatients.purity p on n.sampleId=p.sampleId where (qcStatus NOT LIKE '%WARN_LOW_PURITY%' AND qcStatus <> 'FAIL_NO_TUMOR')) t0
 left join 
-(select sampleId, pretreatmentLine1, pretreatmentLine1PdDate, pretreatmentLine1StopReason from nsclcSingleSample where preTreatmentLine1 like '%Osi%') t1
+(select sampleId, pretreatmentLine1, pretreatmentLine1PdDate, pretreatmentLine1StopReason from nsclcSingleSample where preTreatmentLine1 like '%Pacli%') t1
 on t0.sampleId=t1.sampleId
 left join (
-select sampleId, pretreatmentLine2, pretreatmentLine2PdDate, pretreatmentLine2StopReason from nsclcSingleSample where preTreatmentLine2 like '%Osi%') t2
+select sampleId, pretreatmentLine2, pretreatmentLine2PdDate, pretreatmentLine2StopReason from nsclcSingleSample where preTreatmentLine2 like '%Pacli%') t2
 on t0.sampleId=t2.sampleId
 left join (
-select sampleId, pretreatmentLine3, pretreatmentLine3PdDate, pretreatmentLine3StopReason from nsclcSingleSample where preTreatmentLine3 like '%Osi%') t3
+select sampleId, pretreatmentLine3, pretreatmentLine3PdDate, pretreatmentLine3StopReason from nsclcSingleSample where preTreatmentLine3 like '%Pacli%') t3
 on t0.sampleId=t3.sampleId
 left join (
-select sampleId, pretreatmentLine4, pretreatmentLine4PdDate, pretreatmentLine4StopReason from nsclcSingleSample where preTreatmentLine4 like '%Osi%') t4
+select sampleId, pretreatmentLine4, pretreatmentLine4PdDate, pretreatmentLine4StopReason from nsclcSingleSample where preTreatmentLine4 like '%Pacli%') t4
 on t0.sampleId=t4.sampleId
 left join (
-select sampleId, pretreatmentLine5, pretreatmentLine5PdDate, pretreatmentLine5StopReason from nsclcSingleSample where preTreatmentLine5 like '%Osi%') t5
+select sampleId, pretreatmentLine5, pretreatmentLine5PdDate, pretreatmentLine5StopReason from nsclcSingleSample where preTreatmentLine5 like '%Pacli%') t5
 on t0.sampleId=t5.sampleId
 left join (
-select sampleId, pretreatmentLine6, pretreatmentLine6PdDate, pretreatmentLine6StopReason from nsclcSingleSample where preTreatmentLine6 like '%Osi%') t6
+select sampleId, pretreatmentLine6, pretreatmentLine6PdDate, pretreatmentLine6StopReason from nsclcSingleSample where preTreatmentLine6 like '%Pacli%') t6
 on t0.sampleId=t6.sampleId
 left join (
-select sampleId, pretreatmentLine7, pretreatmentLine7PdDate, pretreatmentLine7StopReason from nsclcSingleSample where preTreatmentLine7 like '%Osi%') t7
+select sampleId, pretreatmentLine7, pretreatmentLine7PdDate, pretreatmentLine7StopReason from nsclcSingleSample where preTreatmentLine7 like '%Pacli%') t7
 on t0.sampleId=t7.sampleId
 left join (
-select sampleId, pretreatmentLine8, pretreatmentLine8PdDate, pretreatmentLine8StopReason from nsclcSingleSample where preTreatmentLine8 like '%Osi%') t8
+select sampleId, pretreatmentLine8, pretreatmentLine8PdDate, pretreatmentLine8StopReason from nsclcSingleSample where preTreatmentLine8 like '%Pacli%') t8
 on t0.sampleId=t8.sampleId
 left join 
-(select sampleId, posttreatmentLine1, posttreatmentLine1startDate, posttreatmentLine1PdDate, posttreatmentLine1StopReason from nsclcSingleSample where posttreatmentLine1 like '%Osi%') p1
+(select sampleId, posttreatmentLine1, posttreatmentLine1startDate, posttreatmentLine1PdDate, posttreatmentLine1StopReason from nsclcSingleSample where posttreatmentLine1 like '%Pacli%') p1
 on t0.sampleId=p1.sampleId
 left join (
-select sampleId, posttreatmentLine2, posttreatmentLine2startDate, posttreatmentLine2PdDate, posttreatmentLine2StopReason from nsclcSingleSample where posttreatmentLine2 like '%Osi%') p2
+select sampleId, posttreatmentLine2, posttreatmentLine2startDate, posttreatmentLine2PdDate, posttreatmentLine2StopReason from nsclcSingleSample where posttreatmentLine2 like '%Pacli%') p2
 on t0.sampleId=p2.sampleId
 left join (
-select sampleId, posttreatmentLine3, posttreatmentLine3startDate, posttreatmentLine3PdDate, posttreatmentLine3StopReason from nsclcSingleSample where posttreatmentLine3 like '%Osi%') p3
+select sampleId, posttreatmentLine3, posttreatmentLine3startDate, posttreatmentLine3PdDate, posttreatmentLine3StopReason from nsclcSingleSample where posttreatmentLine3 like '%Pacli%') p3
 on t0.sampleId=p3.sampleId
 left join (
-select sampleId, posttreatmentLine4, posttreatmentLine4startDate, posttreatmentLine4PdDate, posttreatmentLine4StopReason from nsclcSingleSample where posttreatmentLine4 like '%Osi%') p4
+select sampleId, posttreatmentLine4, posttreatmentLine4startDate, posttreatmentLine4PdDate, posttreatmentLine4StopReason from nsclcSingleSample where posttreatmentLine4 like '%Pacli%') p4
 on t0.sampleId=p4.sampleId
 left join (
-select sampleId, posttreatmentLine5, posttreatmentLine5startDate, posttreatmentLine5PdDate, posttreatmentLine5StopReason from nsclcSingleSample where posttreatmentLine5 like '%Osi%') p5
+select sampleId, posttreatmentLine5, posttreatmentLine5startDate, posttreatmentLine5PdDate, posttreatmentLine5StopReason from nsclcSingleSample where posttreatmentLine5 like '%Pacli%') p5
 on t0.sampleId=p5.sampleId
 left join (
-select sampleId, posttreatmentLine6, posttreatmentLine6startDate, posttreatmentLine6PdDate, posttreatmentLine6StopReason from nsclcSingleSample where posttreatmentLine6 like '%Osi%') p6
+select sampleId, posttreatmentLine6, posttreatmentLine6startDate, posttreatmentLine6PdDate, posttreatmentLine6StopReason from nsclcSingleSample where posttreatmentLine6 like '%Pacli%') p6
 on t0.sampleId=p6.sampleId
 left join (
-select sampleId, posttreatmentLine7, posttreatmentLine7startDate, posttreatmentLine7PdDate, posttreatmentLine7StopReason from nsclcSingleSample where posttreatmentLine7 like '%Osi%') p7
+select sampleId, posttreatmentLine7, posttreatmentLine7startDate, posttreatmentLine7PdDate, posttreatmentLine7StopReason from nsclcSingleSample where posttreatmentLine7 like '%Pacli%') p7
 on t0.sampleId=p7.sampleId
 left join (
-select sampleId, posttreatmentLine8, posttreatmentLine8startDate, posttreatmentLine8PdDate, posttreatmentLine8StopReason from nsclcSingleSample where posttreatmentLine8 like '%Osi%') p8
+select sampleId, posttreatmentLine8, posttreatmentLine8startDate, posttreatmentLine8PdDate, posttreatmentLine8StopReason from nsclcSingleSample where posttreatmentLine8 like '%Pacli%') p8
 on t0.sampleId=p8.sampleId
 left join (
-select sampleId, posttreatmentLine9, posttreatmentLine9startDate, posttreatmentLine9PdDate, posttreatmentLine9StopReason from nsclcSingleSample where posttreatmentLine9 like '%Osi%') p9
+select sampleId, posttreatmentLine9, posttreatmentLine9startDate, posttreatmentLine9PdDate, posttreatmentLine9StopReason from nsclcSingleSample where posttreatmentLine9 like '%Pacli%') p9
 on t0.sampleId=p9.sampleId
 WHERE 
-(posttreatmentLine1 like '%Osi%' or posttreatmentLine2 like '%Osi%' or posttreatmentLine3 like '%Osi%' or posttreatmentLine4 like '%Osi%' or posttreatmentLine5 like '%Osi%' or posttreatmentLine6 like '%Osi%' or posttreatmentLine7 like '%Osi%' or posttreatmentLine8 like '%Osi%' or posttreatmentLine9 like '%Osi%')
+(posttreatmentLine1 like '%Pacli%' or posttreatmentLine2 like '%Pacli%' or posttreatmentLine3 like '%Pacli%' or posttreatmentLine4 like '%Pacli%' or posttreatmentLine5 like '%Pacli%' or posttreatmentLine6 like '%Pacli%' or posttreatmentLine7 like '%Pacli%' or posttreatmentLine8 like '%Pacli%' or posttreatmentLine9 like '%Pacli%')
 AND t0.sampleId not like 'CORE%';
-
-
