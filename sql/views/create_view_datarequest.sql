508dd11a650022c4d2230ef04c10fe1363b948eb
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-06-21 08:54:23 +0200

    DEV-2026: Add snpcheck to datarequest view

--- sql/views/create_view_datarequest.sql
+++ sql/views/create_view_datarequest.sql
@@ -6,6 +6,7 @@ FROM
 	clinical
 	INNER JOIN purity ON purity.sampleId = clinical.sampleId
 	INNER JOIN metric ON metric.sampleId = clinical.sampleId
+    INNER JOIN snpcheck ON snpcheck.sampleId = clinical.sampleId
 WHERE
-	blacklisted = 0 AND qcStatus = 'PASS' AND sufficientCoverage = 1 AND contamination <= 0.1 AND
+	blacklisted = 0 AND purity.qcStatus = 'PASS' AND metric.sufficientCoverage = 1 AND snpcheck.isPass = 1 AND
 	(clinical.sampleId LIKE '%CPCT%' OR clinical.sampleId LIKE '%WIDE%' OR clinical.sampleId LIKE '%DRUP%' OR clinical.sampleId LIKE '%ACTN%');
