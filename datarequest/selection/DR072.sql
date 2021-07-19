c37e16de31e2938dc720cfcfa7c51cc7c64c4390
SandravdBroek <vandenbroek.alexandra@gmail.com>
2021-07-19 14:16:22 +0200

    temporary extra filter for selection since shifting DR tooling to GCP

--- datarequest/selection/DR072.sql
+++ datarequest/selection/DR072.sql
@@ -2,5 +2,5 @@ SELECT
     DISTINCT patientId AS '#patientId'
 FROM
     datarequest
-WHERE primaryTumorLocation = 'Colorectum' or primaryTumorLocation = 'Lung' or primaryTumorLocation = 'Breast'
+WHERE (primaryTumorLocation = 'Colorectum' or primaryTumorLocation = 'Lung' or primaryTumorLocation = 'Breast') AND purpleVersion < 2.54
 ORDER BY 1;
--- datarequest/selection/DR189.sql
+++ datarequest/selection/DR189.sql
@@ -2,4 +2,5 @@ SELECT
     DISTINCT patientId AS '#patientId'
 FROM
     datarequest
+WHERE purpleVersion < 2.54
 ORDER BY 1;
