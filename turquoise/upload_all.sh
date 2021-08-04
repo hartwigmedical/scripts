2d63f90422bb11bc41e40cee066e5bc5864ffef9
Ned Leitch <ned.leitch@gmail.com>
2021-08-04 16:30:05 -0400

    DEV-2155 Improve messages for Turquoise events

--- turquoise/upload_all.sh
+++ turquoise/upload_all.sh
@@ -2,6 +2,9 @@
 #
 # Wrapper to allow all scripts to be run from cron easily
 
+echo "Beginning upload of Turquoise events"
 /data/common/repos/scripts/turquoise/report_date.sh && \
 gsutil cp ${HOME}/.turquoise/reported.json gs://turquoise-events-prod/ && \
 gsutil cp /data/ops/lims/prod/lims.json gs://turquoise-lims-prod/
+echo "Upload successful"
+
