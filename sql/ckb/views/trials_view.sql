70c449e9f7f1cc0a5a581160a3ac0c38c24100d5
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-09-16 10:46:36 +0200

    Formatting

--- sql/ckb/views/trials_view.sql
+++ sql/ckb/views/trials_view.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW trials AS (
 
 SELECT DISTINCT
-	clinicalTrial.id, profileName AS tumorProfile, clinicalTrial.updateDate AS trialUpdateDate, clinicalTrial.nctId, title, phase, recruitment, gender
+	clinicalTrial.id, profileName AS tumorProfile, clinicalTrial.updateDate AS trialUpdateDate,
+	clinicalTrial.nctId, title, phase, recruitment, gender
 FROM ckbEntry INNER JOIN clinicalTrial ON clinicalTrial.ckbEntryId = ckbEntry.id);
