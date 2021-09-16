ae369a5810fd3d3a0ac3df75a98524de0c1d0d39
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-09-16 08:23:09 +0200

    Simplified trial query in CKB to make it lot faster

--- sql/ckb/views/trials_therapies_view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW trialsTherapies AS (
-
-SELECT DISTINCT
-    clinicalTrial.updateDate AS clinicalTrialUpdateDate, nctId, therapy.updateDate AS therapyUpdateDate, therapyName, description
-FROM clinicalTrial
-LEFT JOIN therapyClinicalTrial ON clinicalTrial.id = therapyClinicalTrial.clinicalTrialId
-LEFT JOIN therapy ON therapyClinicalTrial.therapyId = therapy.id);
