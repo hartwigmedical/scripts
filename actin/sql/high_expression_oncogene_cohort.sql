b7e28e3b909ba1f34ce4319607eeb8ef61a6e884
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-10-08 16:50:14 +0200

    Refine added queries for ACTIN explorative RNA gene expression analyses

--- actin/sql/high_expression_oncogene_cohort.sql
+++ actin/sql/high_expression_oncogene_cohort.sql
@@ -1,4 +1,4 @@
-SELECT a.*, IF(driver IS NOT NULL, group_concat(driver," (",round(driverLikelihood,2),")"), "") AS hasDriverInGene, round(g.maxCopyNumber,2) as maxCopyNumber, purity
+SELECT a.*, IF(driver IS NOT NULL, group_concat(driver," (",round(driverLikelihood,2),")"), "") AS hasDriverInGene,  round(g.minCopyNumber,2) AS minCopyNumber, round(g.maxCopyNumber,2) AS maxCopyNumber, purity
 FROM
 (SELECT primaryTumorLocation, primaryTumorSubLocation, primaryTumorType, primaryTumorSubType, g.* FROM geneExpression g INNER JOIN datarequest dr ON g.sampleId=dr.sampleId
 WHERE g.gene IN
@@ -13,4 +13,4 @@ INNER JOIN geneCopyNumber g ON g.sampleId=a.sampleId AND g.gene=a.gene
 INNER JOIN purity p ON a.sampleId=p.sampleId
 WHERE percentileCohort > 0.95
 GROUP BY gene, sampleId
-ORDER BY gene, tpm DESC;
+ORDER BY gene, percentileCohort DESC;
--- actin/sql/low_expression_tsg.sql
+++ actin/sql/low_expression_tsg.sql
@@ -2,4 +2,4 @@ SELECT * FROM geneExpression g
 WHERE gene IN (SELECT gene FROM driverGenePanel WHERE likelihoodType='TSG')
 AND (percentileCohort < 0.05 OR percentileCancer < 0.1)
 AND sampleId in ('XXX')
-ORDER BY percentileCohort DESC;
+ORDER BY percentileCohort;
