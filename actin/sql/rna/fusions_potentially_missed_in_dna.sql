87eb1713a120616ebd9036b75686ac936eda03ba
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-10-11 14:30:14 +0200

    Use capital letters as appropriate

--- actin/sql/rna/fusions_potentially_missed_in_dna.sql
+++ actin/sql/rna/fusions_potentially_missed_in_dna.sql
@@ -2,13 +2,13 @@ SELECT primaryTumorLocation, cuppaTumorLocation, b.reportedType, dnaFusionCount,
 FROM
 (SELECT * FROM rnaFusion WHERE name IN (SELECT name FROM svFusion WHERE reportedType <> 'NONE'))
 	AS a
-LEFT JOIN (SELECT distinct name, reportedType, count(*) as dnaFusionCount FROM svFusion WHERE reportedType <> 'NONE' GROUP BY 1,2) AS b
+LEFT JOIN (SELECT distinct name, reportedType, count(*) AS dnaFusionCount FROM svFusion WHERE reportedType <> 'NONE' GROUP BY 1,2) AS b
     ON a.name=b.name
 LEFT JOIN svFusion
-    ON a.sampleId=svFusion.sampleId and b.name=svFusion.name
+    ON a.sampleId=svFusion.sampleId AND b.name=svFusion.name
 LEFT JOIN clinical
-    on a.sampleId=clinical.sampleId
+    ON a.sampleId=clinical.sampleId
 LEFT JOIN cuppa
-    on a.sampleId=cuppa.sampleId
+    ON a.sampleId=cuppa.sampleId
 WHERE svFusion.sampleId IS NULL AND a.sampleId IN ('XXX')
 ORDER BY b.reportedType, a.name, a.sampleId;
