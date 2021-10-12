c1561152c761df1b4604d7b992bed727cbab1472
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-10-12 10:42:16 +0200

    Add query for RNA annotation of DNA somatic variants

---
+++ sql/rna/somatic_variants_annotated_with_rna.sql
@@ -0,0 +1,11 @@
+SELECT a.*, group_concat(likelihoodMethod) as driversInGene, purity
+FROM
+	(SELECT sampleId,chromosome,position,gene,ref,alt,canonicalCodingEffect,alleleReadCount/totalReadCount AS DNAVaf,rnaAlleleReadCount/rnaTotalReadCount AS
+	RNAVaf,alleleReadCount,totalReadCount,rnaAlleleReadCount,rnaTotalReadCount,copyNumber,reported
+	FROM hmfpatients_pilot.somaticVariant)
+    AS a
+	LEFT JOIN driverCatalog d ON a.sampleId=d.sampleId AND a.gene=d.gene
+    LEFT JOIN purity p ON a.sampleId=p.sampleId
+WHERE a.sampleId IN ('XXX') AND reported
+GROUP BY a.sampleId, a.gene
+ORDER BY a.sampleId, a.gene;
