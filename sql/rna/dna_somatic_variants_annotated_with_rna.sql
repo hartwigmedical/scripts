d90df596a01312578150499421417a199bbae98a
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-10-13 10:16:04 +0200

    Fix bug in query

--- sql/rna/dna_somatic_variants_annotated_with_rna.sql
+++ sql/rna/dna_somatic_variants_annotated_with_rna.sql
@@ -1,11 +1,12 @@
-SELECT a.*, group_concat(likelihoodMethod) AS driversInGene, purity
+SELECT a.*, driversInGene, purity
 FROM
 	(SELECT sampleId,chromosome,position,gene,ref,alt,canonicalCodingEffect,alleleReadCount/totalReadCount AS DNAVaf,rnaAlleleReadCount/rnaTotalReadCount AS
 	RNAVaf,alleleReadCount,totalReadCount,rnaAlleleReadCount,rnaTotalReadCount,copyNumber,reported
 	FROM hmfpatients_pilot.somaticVariant)
     AS a
-	LEFT JOIN driverCatalog d ON a.sampleId=d.sampleId AND a.gene=d.gene
-    LEFT JOIN purity p ON a.sampleId=p.sampleId
+    LEFT JOIN (SELECT sampleId, gene, group_concat(likelihoodMethod) AS driversInGene FROM driverCatalog d GROUP BY 1,2) AS b
+		ON a.sampleId=b.sampleId AND a.gene=b.gene
+    LEFT JOIN purity p
+		ON a.sampleId=p.sampleId
 WHERE a.sampleId IN ('XXX') AND reported
-GROUP BY a.sampleId, a.gene
 ORDER BY a.sampleId, a.gene;
