0dfd59ce47d385e7ba6b1ad2f4c551e5c1489ba1
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-10-19 12:02:32 +0200

    Update low expression RNA analysis query

--- sql/rna/low_expression_tsg.sql
+++ sql/rna/low_expression_tsg.sql
@@ -1,5 +1,5 @@
 SELECT * FROM geneExpression g
 WHERE gene IN (SELECT gene FROM driverGenePanel WHERE likelihoodType='TSG')
-AND (percentileCohort < 0.05 OR percentileCancer < 0.1)
+AND (percentileCohort < 0.05 AND percentileCancer < 0.05)
 AND sampleId in ('XXX')
 ORDER BY percentileCohort;
--- sql/rna/low_expression_tsg_cancer_type.sql
+++ sql/rna/low_expression_tsg_cancer_type.sql
@@ -4,12 +4,12 @@ FROM
 WHERE (primaryTumorLocation, g.gene) IN
 	(SELECT primaryTumorLocation, gene FROM geneExpression g INNER JOIN clinical c ON g.sampleId=c.sampleId
 	WHERE gene IN (SELECT gene FROM driverGenePanel WHERE likelihoodType='TSG')
-	AND percentileCancer < 0.1
+	AND percentileCancer < 0.05
 	AND g.sampleId IN ('XXX'))
 ) AS a
 LEFT JOIN driverCatalog d ON d.sampleId=a.sampleId AND d.gene=a.gene
 INNER JOIN geneCopyNumber g ON g.sampleId=a.sampleId AND g.gene=a.gene
 INNER JOIN purity p ON a.sampleId=p.sampleId
-WHERE percentileCancer < 0.1 AND a.sampleId IN (SELECT sampleId FROM rnaStatistics WHERE qcStatus='PASS')
+WHERE percentileCancer < 0.05 AND a.sampleId IN (SELECT sampleId FROM rnaStatistics WHERE qcStatus='PASS')
 GROUP BY gene, sampleId
 ORDER BY gene, percentileCancer;
