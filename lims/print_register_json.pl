6fd0cc916250b39f93b4be96b0bc63a4ca973b40
David Koetsier <david.f.koetsier@gmail.com>
2021-10-29 16:50:04 +0200

    Fix bug by using string comparison

--- lims/print_register_json.pl
+++ lims/print_register_json.pl
@@ -339,10 +339,10 @@ sub processSample{
             my $new_name_ref = $patient . 'R';
             my $ready_new_ref_sample_count = `hmf_api_get 'samples?name=$new_name_ref' | jq 'select(.[].status == "Ready") | length'`;
             my $new_barcode_ref;
-            if ( $ready_new_ref_sample_count == 1 ){
+            if ( $ready_new_ref_sample_count eq "1" ){
                 $new_barcode_ref = `hmf_api_get 'samples?name=$new_name_ref' | jq 'select(.[].status == "Ready")[0].barcode' | tr -d '"\n'`;
                 push( @warn_msg, "DOUBLE CHECK JSON for $barcode ($name): OtherRef flag is set in LIMS. Reusing existing REF barcode ($new_barcode_ref)" );
-            } elsif ( $ready_new_ref_sample_count == 0 ) {
+            } elsif ( $ready_new_ref_sample_count eq "" ) {
                 $new_barcode_ref = $barcode_ref . "_c2f" . $date;
                 push( @warn_msg, "DOUBLE CHECK JSON for $barcode ($name): OtherRef flag is set in LIMS. Adding suffix to create new REF barcode ($new_barcode_ref)" );
             } else {
