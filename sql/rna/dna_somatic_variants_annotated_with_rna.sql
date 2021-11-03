272d558b507abbf75758950ef475fb312c48fac6
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2021-11-03 15:24:21 +0100

    Update query

--- sql/rna/dna_somatic_variants_annotated_with_rna.sql
+++ sql/rna/dna_somatic_variants_annotated_with_rna.sql
@@ -1,8 +1,8 @@
 SELECT a.*, driversInGene, purity
 FROM
-	(SELECT sampleId,chromosome,position,gene,ref,alt,canonicalCodingEffect,alleleReadCount/totalReadCount AS DNAVaf,rnaAlleleReadCount/rnaTotalReadCount AS
+	(SELECT filter,sampleId,chromosome,position,gene,ref,alt,canonicalCodingEffect,alleleReadCount/totalReadCount AS DNAVaf,rnaAlleleReadCount/rnaTotalReadCount AS
 	RNAVaf,alleleReadCount,totalReadCount,rnaAlleleReadCount,rnaTotalReadCount,round(variantCopyNumber,1) AS variantCopyNumberDna,copyNumber,reported
-	FROM hmfpatients_pilot.somaticVariant)
+	FROM somaticVariant)
     AS a
     LEFT JOIN (SELECT sampleId, gene, group_concat(likelihoodMethod) AS driversInGene FROM driverCatalog d GROUP BY 1,2) AS b
 		ON a.sampleId=b.sampleId AND a.gene=b.gene
