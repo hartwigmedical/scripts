a0d1c0650c7f1adffc47a9353639e8940801c6eb
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2021-12-02 16:31:53 +0100

    DEV-2410 support different sample names in pipeline verification

--- validation/counts_variants.sql
+++ validation/counts_variants.sql
@@ -1,8 +1,8 @@
 ## Check variant counts
 SELECT 
-(SELECT COUNT(*) FROM pipeline_v5_validation.somaticVariant WHERE sampleId = "COLO829v003T" AND filter = "PASS") AS NewSom,
-(SELECT COUNT(*) FROM reference_validation_sets.somaticVariant WHERE sampleId = "COLO829v003T" AND filter = "PASS") AS TruthSom,
-(SELECT COUNT(*) FROM pipeline_v5_validation.structuralVariant WHERE sampleId = "COLO829v003T" and filter <> "PON") AS NewSv,
-(SELECT COUNT(*) FROM reference_validation_sets.structuralVariant WHERE sampleId = "COLO829v003T" and filter <> "PON") AS TruthSv,
-(SELECT COUNT(*) FROM pipeline_v5_validation.svBreakend WHERE sampleId = "COLO829v003T") AS NewSvbe,
-(SELECT COUNT(*) FROM reference_validation_sets.svBreakend WHERE sampleId = "COLO829v003T") AS TruthSvbe;
+(SELECT COUNT(*) FROM VARIABLE_NEW_DB_SCHEMA.somaticVariant WHERE sampleId = "VARIABLE_NEW_SAMPLE_ID" AND filter = "PASS") AS NewSom,
+(SELECT COUNT(*) FROM VARIABLE_TRUTH_DB_SCHEMA.somaticVariant WHERE sampleId = "VARIABLE_TRUTH_SAMPLE_ID" AND filter = "PASS") AS TruthSom,
+(SELECT COUNT(*) FROM VARIABLE_NEW_DB_SCHEMA.structuralVariant WHERE sampleId = "VARIABLE_NEW_SAMPLE_ID" and filter <> "PON") AS NewSv,
+(SELECT COUNT(*) FROM VARIABLE_TRUTH_DB_SCHEMA.structuralVariant WHERE sampleId = "VARIABLE_TRUTH_SAMPLE_ID" and filter <> "PON") AS TruthSv,
+(SELECT COUNT(*) FROM VARIABLE_NEW_DB_SCHEMA.svBreakend WHERE sampleId = "VARIABLE_NEW_SAMPLE_ID") AS NewSvbe,
+(SELECT COUNT(*) FROM VARIABLE_TRUTH_DB_SCHEMA.svBreakend WHERE sampleId = "VARIABLE_TRUTH_SAMPLE_ID") AS TruthSvbe;
