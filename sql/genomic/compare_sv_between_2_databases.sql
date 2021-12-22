3a2ec30d4396921430782fa28a4532d502f819cf
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2021-12-22 15:49:08 +0100

    Fix SV compare script

--- sql/genomic/compare_sv_between_2_databases.sql
+++ sql/genomic/compare_sv_between_2_databases.sql
@@ -1,5 +1,8 @@
-SELECT min(pipeline), startChromosome, startPosition, endChromosome, endPosition,TYPE, endPosition-startPosition AS len, count(*), max(ploidy), max(qualScore) FROM
-(SELECT id, 'Truth' AS pipeline, startChromosome, startPosition, endChromosome, endPosition, TYPE, ploidy, qualScore FROM reference_validation_sets.structuralVariant WHERE sampleId = 'XXX' AND filter <> "PON"
-UNION
-SELECT id, 'New' AS pipeline, startChromosome, startPosition, endChromosome, endPosition, TYPE, ploidy, qualScore FROM pipeline_v5_validation.structuralVariant WHERE sampleId = 'XXX' AND filter <> "PON") AS a
+SELECT min(pipeline), startChromosome, startPosition, endChromosome, endPosition,TYPE, endPosition-startPosition AS len, count(*), max(junctionCopyNumber), max(qualScore)
+FROM
+	(SELECT id, 'Truth' AS pipeline, startChromosome, startPosition, endChromosome, endPosition, TYPE, junctionCopyNumber, qualScore
+	FROM reference_validation_sets.structuralVariant WHERE sampleId = 'XXX' AND filter <> "PON"
+	UNION
+	SELECT id, 'New' AS pipeline, startChromosome, startPosition, endChromosome, endPosition, TYPE, junctionCopyNumber, qualScore
+	FROM pipeline_v5_validation.structuralVariant WHERE sampleId = 'XXX' AND filter <> "PON") AS a
 GROUP BY 2,3,4,5,6,7 HAVING count(*) = 1;
