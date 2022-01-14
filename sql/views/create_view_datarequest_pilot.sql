15280f954e3f6a28a49deeaf22827e8a000678ae
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-01-14 16:43:42 +0100

    update sql views for upgrade to v3.66 patient db

--- sql/views/create_view_datarequest_pilot.sql
+++ sql/views/create_view_datarequest_pilot.sql
@@ -1,12 +1,11 @@
 CREATE OR REPLACE VIEW datarequest AS
 
 SELECT
-	clinical.*, metric.sufficientCoverage AS sufficientCoverage
+	clinical_purity.*, metric.sufficientCoverage AS sufficientCoverage
 FROM
-	clinical
-	INNER JOIN metric ON metric.sampleId = clinical.sampleId
-    INNER JOIN snpcheck ON snpcheck.sampleId = clinical.sampleId
-    INNER JOIN purity on purity.sampleId = clinical.sampleId
+	clinical_purity
+	INNER JOIN metric ON metric.sampleId = clinical_purity.sampleId
+    INNER JOIN snpcheck ON snpcheck.sampleId = clinical_purity.sampleId
 WHERE
-	blacklisted = 0 AND purity.qcStatus = 'PASS' AND metric.sufficientCoverage = 1 AND snpcheck.isPass = 1 AND inHMFDatabase =1 AND outsideEU =1 AND
-	(clinical.sampleId LIKE '%CPCT%' OR clinical.sampleId LIKE '%WIDE%' OR clinical.sampleId LIKE '%DRUP%' OR clinical.sampleId LIKE '%ACTN%');
+	blacklisted = 0 AND purpleQC = 'PASS' AND metric.sufficientCoverage = 1 AND snpcheck.isPass = 1 AND inHMFDatabase =1 AND outsideEU =1 AND
+	(clinical_purity.sampleId LIKE '%CPCT%' OR clinical_purity.sampleId LIKE '%WIDE%' OR clinical_purity.sampleId LIKE '%DRUP%' OR clinical_purity.sampleId LIKE '%ACTN%');
