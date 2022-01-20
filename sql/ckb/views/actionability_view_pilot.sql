d9636bb55a652de7a0639e93e7fed5773ee136d7
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-01-20 12:09:27 +0100

    DEV-2434: update actionability view

--- sql/ckb/views/actionability_view_pilot.sql
+++ sql/ckb/views/actionability_view_pilot.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE VIEW actionability AS (
 
 SELECT
     ckbEntry.id as ckbEntryId, therapy.createDate as therapyCreateDate, therapy.updateDate as therapyUpdateDate,
-    profileName AS tumorProfile, therapyName AS treatment, treatmentApproachDrugClass.drugClass as drugClass,
-    name AS cancerType, termId AS cancerTypeId,evidenceType, responseType, ampCapAscoEvidenceLevel, approvalStatus,
-    efficacyEvidence, description,group_concat(DISTINCT pubmedId) as pubmeds
+    profileName AS tumorProfile, therapyName AS treatment, drugClass, name AS cancerType, termId AS cancerTypeId,
+    evidenceType, responseType, ampCapAscoEvidenceLevel, approvalStatus, efficacyEvidence, description,
+    group_concat(DISTINCT pubmedId) as pubmeds
 FROM ckbEntry
 INNER JOIN evidence ON evidence.ckbEntryId = ckbEntry.id
 LEFT JOIN evidenceReference on evidenceReference.evidenceId = evidence.id
@@ -12,7 +12,6 @@ INNER JOIN therapyEvidence ON therapyEvidence.evidenceId = evidence.id
 INNER JOIN therapy ON therapyEvidence.therapyId = therapy.id
 INNER JOIN indicationEvidence ON indicationEvidence.evidenceId = evidence.id
 INNER JOIN indication ON indicationEvidence.indicationId = indication.id
-INNER JOIN treatmentApproachEvidence ON treatmentApproachEvidence.treatmentApproachEvidenceId = evidence.id
-INNER JOIN treatmentApproach ON treatmentApproachEvidence.treatmentApproachEvidenceId = treatmentApproach.id
-INNER JOIN treatmentApproachDrugClass ON treatmentApproachDrugClass.treatmentApproachId = treatmentApproach.id
-GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14);
+LEFT JOIN treatmentApproachEvidence ON treatmentApproachEvidence.evidenceId = evidence.id
+LEFT JOIN treatmentApproachDrugClass ON treatmentApproachDrugClass.treatmentApproachId=treatmentApproachEvidence.treatmentApproachEvidenceId
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13, 14);
