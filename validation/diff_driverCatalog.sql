b2902c6388eb55be435b0f72bce49ebc1b555376
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2022-04-22 18:41:50 +0200

    add driverCatalog diff

---
+++ validation/diff_driverCatalog.sql
@@ -0,0 +1,41 @@
+SELECT
+    MIN(pipeline),
+    chromosome,
+    chromosomeBand,
+    gene,
+    transcriptId,
+    category,
+    driver,
+    biallelic,
+    COUNT(*),
+    MIN(driverLikelihood)
+FROM
+    (SELECT
+        'OnlyInTruth' AS pipeline,
+            chromosome,
+            chromosomeBand,
+            gene,
+            transcriptId,
+            category,
+            driver,
+            driverLikelihood,
+            biallelic
+    FROM
+        VARIABLE_TRUTH_DB_SCHEMA.driverCatalog
+    WHERE
+        sampleId = 'VARIABLE_TRUTH_SAMPLE_ID' UNION SELECT
+        'OnlyInNew' AS pipeline,
+            chromosome,
+            chromosomeBand,
+            gene,
+            transcriptId,
+            category,
+            driver,
+            driverLikelihood,
+            biallelic
+    FROM
+        VARIABLE_NEW_DB_SCHEMA.driverCatalog
+    WHERE
+        sampleId = 'VARIABLE_NEW_SAMPLE_ID') AS a
+GROUP BY chromosome , chromosomeBand , gene , transcriptId , category , driver , biallelic
+HAVING COUNT(*) != 2;
