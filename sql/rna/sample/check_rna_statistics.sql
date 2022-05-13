a787cdb0152b7aac33b5550190fc4d4b425f5296
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-05-13 09:46:51 +0200

    Update RNA queries

--- sql/rna/sample/check_rna_statistics.sql
+++ sql/rna/sample/check_rna_statistics.sql
@@ -2,7 +2,8 @@ SELECT sampleId, readLength, qcStatus, totalFragments, duplicates,
 totalFragments-duplicates AS nonDuplicates,
 duplicates/totalFragments AS duplicateRate,
 splicedPercent, unsplicedPercent, alternateSplicePercent, chimericPercent,
-round((splicedPercent+chimericPercent+alternateSplicePercent)*totalFragments,0) AS geneExpressionFragments,
+round(totalFragments*splicedPercent,0) AS splicedFragments,
+round(totalFragments*alternateSplicePercent,0) AS alternateSpliceFragments,
 round(totalFragments*chimericPercent,0) AS chimericFragments,
 fragmentLengthPct05, fragmentLengthPct50, fragmentLengthPct95, enrichedGenePercent, medianGCRatio
 FROM rnaStatistics
