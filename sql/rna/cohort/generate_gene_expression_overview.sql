2701aa6d9cdf6c0fce2667e0e3c0e4cc98fa225c
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-05-17 12:01:23 +0200

    Update RNA query

--- sql/rna/cohort/generate_gene_expression_overview.sql
+++ sql/rna/cohort/generate_gene_expression_overview.sql
@@ -1,5 +1,6 @@
-SELECT qcStatus AS "RNA QC status", primaryTumorLocation AS "Primary tumor location", g.sampleId AS "Sample ID", gene AS "Gene", round(tpm,3) AS "TPM", round(medianTpmCancer,2) AS "Median TPM for cancer type", round(percentileCancer,2) AS "Percentile for cancer type", round(medianTpmCohort,2) AS "Median TPM for database", round(percentileCohort,2) AS "Percentile for database"
+SELECT purity AS "Tumor purity", r.qcStatus AS "RNA QC status", primaryTumorLocation AS "Primary tumor location", g.sampleId AS "Sample ID", gene AS "Gene", round(tpm,3) AS "TPM", round(medianTpmCancer,2) AS "Median TPM for cancer type", round(percentileCancer,2) AS "Percentile for cancer type", round(medianTpmCohort,2) AS "Median TPM for database", round(percentileCohort,2) AS "Percentile for database"
 FROM geneExpression g
 INNER JOIN clinical c ON g.sampleId=c.sampleId
 INNER JOIN rnaStatistics r ON g.sampleId=r.sampleId
+INNER JOIN purity p ON g.sampleId=p.sampleId
 WHERE g.sampleId LIKE 'XXX%' AND gene IN ('XXX') ORDER BY g.sampleId;
