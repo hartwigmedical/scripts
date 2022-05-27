664d1fb99e103783bb84de3746bfd5089b887c92
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-05-27 15:35:31 +0200

    update query

--- sql/rna/sample/novel_splice_junction_novel_exon_intron.sql
+++ sql/rna/sample/novel_splice_junction_novel_exon_intron.sql
@@ -1,6 +1,6 @@
 SELECT geneId, transcriptId, a.* FROM
 (SELECT * FROM novelSpliceJunction
-WHERE type IN ('NOVEL_INTRON','NOVEL_EXON') AND gene IN (SELECT gene FROM driverGenePanel) AND fragmentCount>5 AND cohortFrequency<5
+WHERE type IN ('NOVEL_INTRON','NOVEL_EXON') AND gene IN (SELECT gene FROM driverGenePanel) AND fragmentCount>5 AND cohortFrequency<10
 AND sampleId IN ('XXX')) AS a
 INNER JOIN canonicalTranscript
 ON a.gene=canonicalTranscript.gene;
