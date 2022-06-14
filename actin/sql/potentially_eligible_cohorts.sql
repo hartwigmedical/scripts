6a63ca59e0b843fe9a268fda9f4fc726fc480143
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-06-14 08:54:38 +0200

    Remove obsolete query

--- actin/sql/potentially_eligible_cohorts.sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT sampleId, trialId, trialAcronym, cohortDescription
-FROM trialEvaluation
-WHERE sampleId IN ('XXX')
-AND ((isEligibleTrial AND NOT trialHasCohorts AND trialOpen) OR (isEligibleTrial AND isEligibleCohort AND cohortOpen AND NOT cohortBlacklist));
