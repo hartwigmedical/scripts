c1099fa5badde36ce1e9314a556ca0c651ac2a01
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-06-27 14:39:54 +0200

    revert back

--- validation/diff_protect.sql
+++ validation/diff_protect.sql
@@ -1,27 +1,73 @@
 SELECT
     MIN(pipeline) AS pipeline,
-    gene, transcript, isCanonical, event, eventIsHighDriver, germline, reported, treatment, onLabel, level, direction,
-    source, sourceEvent, sourceUrls, evidenceType, rangeRank, evidenceUrls, COUNT(*)
+    gene,
+    transcript,
+    isCanonical,
+    event,
+    eventIsHighDriver,
+    germline,
+    reported,
+    treatment,
+    onLabel,
+    level,
+    direction,
+    source,
+    sourceEvent,
+    sourceUrls,
+    evidenceType,
+    rangeRank,
+    evidenceUrls,
+    COUNT(*)
 FROM
     (SELECT
         id,
             'OnlyInTruth' AS pipeline,
-    gene, transcript, isCanonical, event, eventIsHighDriver, germline, reported, treatment, onLabel, level, direction,
-    source, sourceEvent, sourceUrls, evidenceType, rangeRank, evidenceUrls
+            gene,
+			transcript,
+			isCanonical,
+			event,
+			eventIsHighDriver,
+			germline,
+			reported,
+			treatment,
+			onLabel,
+			level,
+			direction,
+			source,
+			sourceEvent,
+			sourceUrls,
+			evidenceType,
+			rangeRank,
+			evidenceUrls
     FROM
-        protect
+        VARIABLE_TRUTH_DB_SCHEMA.protect
     WHERE
         sampleId = 'VARIABLE_TRUTH_SAMPLE_ID'
-        UNION SELECT
+            AND reported = 1 UNION SELECT
         id,
             'OnlyInNew' AS pipeline,
-            gene, transcript, isCanonical, event, eventIsHighDriver, germline, reported, treatment, onLabel, level, direction,
-    source, sourceEvent, sourceUrls, evidenceType, rangeRank, evidenceUrls
+            gene,
+			transcript,
+			isCanonical,
+			event,
+			eventIsHighDriver,
+			germline,
+			reported,
+			treatment,
+			onLabel,
+			level,
+			direction,
+			source,
+			sourceEvent,
+			sourceUrls,
+			evidenceType,
+			rangeRank,
+			evidenceUrls
     FROM
-        protect
+        VARIABLE_NEW_DB_SCHEMA.protect
     WHERE
-        sampleId = 'VARIABLE_NEW_SAMPLE_ID') AS a
-GROUP BY gene, transcript, isCanonical, event, eventIsHighDriver, germline, reported, treatment, onLabel, level, direction,
-    source, sourceEvent, sourceUrls, evidenceType, rangeRank, evidenceUrls
-    HAVING COUNT(*) != 2
+        sampleId = 'VARIABLE_NEW_SAMPLE_ID'
+            AND reported = 1) AS a
+GROUP BY gene, transcript, isCanonical, event, eventIsHighDriver, germline, reported, treatment, onLabel, level, direction, source, sourceEvent, sourceUrls, evidenceType, rangeRank, evidenceUrls
+HAVING COUNT(*) != 2
 ORDER BY event;
