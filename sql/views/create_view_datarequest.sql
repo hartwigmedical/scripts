7e6aeca23aa5426a07195ecd6658b256a1fe0169
moez-baksi <moez-baksi@hotmail.com>
2022-06-28 10:20:08 +0200

    added wide samples back to dr-view

--- sql/views/create_view_datarequest.sql
+++ sql/views/create_view_datarequest.sql
@@ -8,4 +8,4 @@ FROM
     INNER JOIN snpcheck ON snpcheck.sampleId = clinical_purity.sampleId
 WHERE
 	blacklisted = 0 AND purpleQC = 'PASS' AND metric.sufficientCoverage = 1 AND snpcheck.isPass = 1 AND inHMFDatabase =1 AND outsideEU =1 AND
-	(clinical_purity.sampleId LIKE '%CPCT%' OR clinical_purity.sampleId LIKE '%ACTN%');
+	(clinical_purity.sampleId LIKE '%CPCT%' OR clinical_purity.sampleId LIKE '%ACTN%' OR clinical_purity.sampleId LIKE '%WIDE%');
