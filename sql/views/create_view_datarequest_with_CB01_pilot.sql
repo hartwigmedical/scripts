826a555ef988a3193dd1d75a819924ab265534e3
moez-baksi <moez-baksi@hotmail.com>
2022-07-12 14:12:37 +0200

    added new view for COREDB in DR's

---
+++ sql/views/create_view_datarequest_with_CB01_pilot.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW datarequest_CB01 AS
+
+SELECT
+	clinical_purity.*, metric.sufficientCoverage AS sufficientCoverage
+FROM
+	clinical_purity
+	INNER JOIN metric ON metric.sampleId = clinical_purity.sampleId
+    INNER JOIN snpcheck ON snpcheck.sampleId = clinical_purity.sampleId
+WHERE
+	(blacklisted = 0 AND purpleQC = 'PASS' AND snpcheck.isPass = 1 AND metric.sufficientCoverage = 1) AND
+	((inHMFDatabase =1 AND outsideEU =1 AND clinical_purity.sampleId LIKE '%CPCT%' OR clinical_purity.sampleId LIKE '%WIDE%' OR clinical_purity.sampleId LIKE '%ACTN%')
+    OR (clinical_purity.sampleId LIKE '%COREDB01%')
+	);
