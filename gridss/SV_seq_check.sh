4697238a032a9d193108e4a29cf38e0a7f893af5
TDeger <t.deger@hartwigmedicalfoundation.nl>
2022-09-20 12:21:18 +0200

    test Teoman

--- gridss/SV_seq_check.sh
+++ gridss/SV_seq_check.sh
@@ -12,5 +12,5 @@ while read -r SVnum sample SVTYPE seqnameso starto endo widtho strando REFo ALTo
 do
   Ext_noQuot=${Extension:1:-1}
   Lig_noQuot=${Ligation:1:-1}
-  /opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} |  grep -c \'${Ext_noQuot}\'\[\[:alpha:\]\]\{0,0\}${Lig_noQuot}\'
+  /opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} |  grep -c ${Ext_noQuot}[[:alpha:]]{0,0}${Lig_noQuot}
 done < <(tail -n +2 $INPUT)
