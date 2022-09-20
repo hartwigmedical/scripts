252d7dd8668cc7bc227a1554c4902a1379a28798
TDeger <t.deger@hartwigmedicalfoundation.nl>
2022-09-20 16:37:31 +0200

    Check SV sequence in bam-file of tumor

--- gridss/SV_seq_check.sh
+++ gridss/SV_seq_check.sh
@@ -5,13 +5,32 @@
 # -----------------------
 
 INPUT=~/data/SV-Test.csv
-IFS=','
+IFS=';'
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-#while read line
 while read -r SVnum sample SVTYPE seqnameso starto endo widtho strando REFo ALTo seqnamesh starth endh widthh strandh REFh ALTh svSequence Extension Ligation
 do
   Ext_noQuot=${Extension:1:-1}
   Lig_noQuot=${Ligation:1:-1}
-  #/opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} | grep -c ${Ext_noQuot}[[:alpha:]]\{0,0\}${Lig_noQuot}
-  /opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} | grep -c ${Ext_noQuot}[A-Z]{0,30}${Lig_noQuot}
+  greptxt=${Ext_noQuot}[[:alpha:]]\\{0,0\\}${Lig_noQuot}
+  echo SV-check ${SVnum}: Checking presence of ${SVTYPE} of ${seqnameso}:${starto} and ${seqnamesh}:${starth}
+  FOUND=$(/opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} | grep -c ${greptxt})
+  if [ ${FOUND} -ne 0 ]
+  then
+    echo ${FOUND} perfectly matching sequence reads found
+  else
+    echo No perfectly matching sequence reads found
+    i=0
+    while [ ${FOUND} -eq 0 ]
+    do
+     ((i++))
+     if [[ $i -eq 50 ]]
+     then
+       break
+     fi
+     greptxt=${Ext_noQuot}[[:alpha:]]\\{0,${i}\\}${Lig_noQuot}
+     FOUND=$(/opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} | grep -c ${greptxt})
+    done
+    echo ${FOUND} matching reads found within ${i}nt distance
+  fi
+  echo -e "\n"
 done < <(tail -n +2 $INPUT)
