f30ea2c8db9b85bf51c6910810ca9838604918d4
TDeger <t.deger@hartwigmedicalfoundation.nl>
2022-09-20 11:58:42 +0200

    test Teoman

--- gridss/SV_seq_check.sh
+++ gridss/SV_seq_check.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
+# Purpose: Confirm SV nt-sequences deduced from .purple.sv.vcf file
+# Author: Teoman Deger
+# Version 0.1, 20-09-2022
+# -----------------------
 
-echo Hello Teoman
+INPUT=~/Work/SV-Test.csv
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+#while read line
+while read -r SVnum sample SVTYPE seqnameso starto endo widtho strando REFo ALTo seqnamesh starth endh widthh strandh REFh ALTh svSequence Extension Ligation
+do
+  Ext_noQuot=${Extension:1:-1}
+  Lig_noQuot=${Ligation:1:-1}
+  samtools view \'${seqnameso}:${starto}-${endo}\' \'${seqnamesh}:${starth}-${endh}\' \|  grep -c \'${Ext_noQuot}\'\[\[:alpha:\]\]\{0,0\}${Lig_noQuot}\'
+done < <(tail -n +2 $INPUT)
