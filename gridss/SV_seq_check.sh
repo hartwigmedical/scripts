c76c9a2a3c4dc30e48ca116bc756e67cbe2f6d9d
TDeger <t.deger@hartwigmedicalfoundation.nl>
2022-09-20 12:52:17 +0200

    test Teoman

--- gridss/SV_seq_check.sh
+++ gridss/SV_seq_check.sh
@@ -13,5 +13,5 @@ do
   Ext_noQuot=${Extension:1:-1}
   Lig_noQuot=${Ligation:1:-1}
   #/opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} | grep -c ${Ext_noQuot}[[:alpha:]]\{0,0\}${Lig_noQuot}
-  /opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} | grep -c ${Ext_noQuot}${Lig_noQuot}
+  /opt/tools/samtools/1.14/samtools view -@ 25 COLO829v003T.bam ${seqnameso}:${starto}-${endo} ${seqnamesh}:${starth}-${endh} | grep -c ${Ext_noQuot}[A-Z]${Lig_noQuot}
 done < <(tail -n +2 $INPUT)
