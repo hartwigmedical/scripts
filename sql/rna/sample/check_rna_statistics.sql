dade5cb9c421742fe804d1d5a541a57368bc5058
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-09-30 16:49:49 +0200

    Update query

--- sql/rna/sample/check_rna_statistics.sql
+++ sql/rna/sample/check_rna_statistics.sql
@@ -3,8 +3,10 @@ totalFragments-duplicates AS nonDuplicates,
 duplicates/totalFragments AS duplicateRate,
 splicedPercent, unsplicedPercent, alternateSplicePercent, chimericPercent,
 round(totalFragments*splicedPercent,0) AS splicedFragments,
+round(totalFragments*unsplicedPercent,0) AS unsplicedFragments,
 round(totalFragments*alternateSplicePercent,0) AS alternateSpliceFragments,
 round(totalFragments*chimericPercent,0) AS chimericFragments,
-fragmentLengthPct05, fragmentLengthPct50, fragmentLengthPct95, enrichedGenePercent, medianGCRatio
+enrichedGenePercent,
+fragmentLengthPct05, fragmentLengthPct50, fragmentLengthPct95, medianGCRatio
 FROM rnaStatistics
 WHERE sampleId IN ('XXX');
