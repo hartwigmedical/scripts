56711a98bb8eba33d2db1dc219eab4b452e54445
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-10-10 09:56:43 +0200

    Start script for generate reportable transcript list

---
+++ hmftools/patientreporter/ops-vm/generate_transcriptlist.R
@@ -0,0 +1,6 @@
+ensemblGeneData <- read.csv('~/data/ ensembl_gene_data.csv')
+ensemblTransExonData <- read.csv('~/data/ ensembl_trans_exon_data.csv')
+driverGenePanel <- read.csv('~/data/DriverGenePanel.37.tsv',sep='\t')
+driverGeneData <- ensemblGeneData %>% filter(GeneName %in% driverGenePanel$gene) %>% select(GeneId,GeneName)
+driverTranscripts <- ensemblTransExonData %>% filter(ExonRank==1) %>% filter(CanonicalTranscriptId==TransId | TransName=='ENST00000579755') # manually include the CDKN2A alt transcript
+driverGeneTransData <- merge(driverGeneData,driverTranscripts %>% select(GeneId,TransName),by='GeneId',all.x=T)
