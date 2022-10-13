638f395c4c901b2f003ac6f99c26067d57250736
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-10-13 16:46:57 +0200

    update generate transcripts

--- hmftools/patientreporter/ops-vm/generate_transcriptlist.R
+++ hmftools/patientreporter/ops-vm/generate_transcriptlist.R
@@ -1,9 +1,14 @@
-ensemblGeneData <- read.csv('~/data/ ensembl_gene_data.csv')
-ensemblTransExonData <- read.csv('~/data/ ensembl_trans_exon_data.csv')
-driverGenePanel <- read.csv('~/data/DriverGenePanel.37.tsv',sep='\t')
+
+driverGeneFile <- '/data/resources/public/gene_panel/37/DriverGenePanel.37.tsv'
+ensemblGeneDataFile <- '/data/resources/public/ensembl_data_cache/37/ensembl_gene_data.csv'
+ensemblTransExonDataFile <- '/data/resources/public/ensembl_data_cache/37/ensembl_trans_exon_data.csv'
+
+ensemblGeneData <- read.csv(ensemblGeneDataFile)
+ensemblTransExonData <- read.csv(ensemblTransExonDataFile)
+driverGenePanel <- read.csv(driverGeneFile,sep='\t')
 driverGeneData <- ensemblGeneData %>% filter(GeneName %in% driverGenePanel$gene) %>% select(GeneId,GeneName)
 driverTranscripts <- ensemblTransExonData %>% filter(ExonRank==1) %>% filter(CanonicalTranscriptId==TransId | TransName=='ENST00000579755') # manually include the CDKN2A alt transcript
 driverGeneTransData <- merge(driverGeneData,driverTranscripts %>% select(GeneId,TransName),by='GeneId',all.x=T)
 
-outputFile <-  paste0('hmf_reportable_gene_transcripts.tsv')
-write.table(driverGeneTransData, file = "outputFile", row.names=TRUE, sep="\t")
+# outputFile <-  paste0('hmf_reportable_gene_transcripts.tsv')
+# write.table(driverGeneTransData, file = "outputFile", row.names=TRUE, sep="\t")
