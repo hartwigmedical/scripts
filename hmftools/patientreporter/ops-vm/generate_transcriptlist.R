7eac7e6ee5ea9af970aa494c187423fa7bf706a8
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-10-13 17:09:21 +0200

    update generate transcripts

--- hmftools/patientreporter/ops-vm/generate_transcriptlist.R
+++ hmftools/patientreporter/ops-vm/generate_transcriptlist.R
@@ -6,6 +6,7 @@ driverGenePanel <- read.csv('/data/resources/public/gene_panel/37/DriverGenePane
 driverGeneData <- ensemblGeneData %>% filter(GeneName %in% driverGenePanel$gene) %>% select(GeneId,GeneName)
 driverTranscripts <- ensemblTransExonData %>% filter(ExonRank==1) %>% filter(CanonicalTranscriptId==TransId | TransName=='ENST00000579755') # manually include the CDKN2A alt transcript
 driverGeneTransData <- merge(driverGeneData,driverTranscripts %>% select(GeneId,TransName),by='GeneId',all.x=T)
+driverGeneTransDataSort <- driverGeneTransData[order[GeneName]]
 
 outputFile <-  paste0('hmf_reportable_gene_transcripts.tsv')
-write.table(driverGeneTransData, file = "outputFile", row.names=TRUE, sep="\t")
+write.table(driverGeneTransDataSort, file = outputFile, row.names=TRUE, sep="\t")
