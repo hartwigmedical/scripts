69dd0bd7d48f007186bcd598a88b177e64c3871e
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2022-10-17 16:12:27 +0200

    update generate transript list

--- hmftools/patientreporter/ops-vm/generate_transcriptlist.R
+++ hmftools/patientreporter/ops-vm/generate_transcriptlist.R
@@ -3,9 +3,9 @@ library(dplyr)
 ensemblGeneData <- read.csv('/data/resources/public/ensembl_data_cache/37/ensembl_gene_data.csv')
 ensemblTransExonData <- read.csv('/data/resources/public/ensembl_data_cache/37/ensembl_trans_exon_data.csv')
 driverGenePanel <- read.csv('/data/resources/public/gene_panel/37/DriverGenePanel.37.tsv',sep='\t')
-driverGeneData <- ensemblGeneData %>% filter(GeneName %in% driverGenePanel$gene) %>% select(GeneId,GeneName)
+driverGeneData <- ensemblGeneData %>% filter(GeneName %in% driverGenePanel$gene) %>% select(GeneName)
 driverTranscripts <- ensemblTransExonData %>% filter(ExonRank==1) %>% filter(CanonicalTranscriptId==TransId | TransName=='ENST00000579755') # manually include the CDKN2A alt transcript
-driverGeneTransData <- merge(driverGeneData,driverTranscripts %>% select(GeneId,TransName),by='GeneId',all.x=T)
+driverGeneTransData <- merge(driverGeneData,driverTranscripts %>% select(TransName),by='GeneId',all.x=T)
 
 outputFile <-  paste0('/data/resources/report_resources/oncoact/hmf_reportable_gene_transcripts.tsv')
 write.table(driverGeneTransData, file = outputFile, row.names=TRUE, sep="\t")
