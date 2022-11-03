235c763f0caf5a5b3ac5b44d1786d12c84057dc6
SandravdBroek <vandenbroek.alexandra@gmail.com>
2022-11-03 17:19:24 +0100

    script to get reports

---
+++ hmftools/patientreporter/ops-vm/reports_to_nc.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+source message_functions || exit 1
+
+sample=$1
+
+if [[ -z ${sample} ]]; then
+    error "No sample name provided"
+fi
+
+run_id=$( hmf_api_get "reports/created" | jq '.[] | select(.sample_name == "'${sample}'") | .run_id' | tail -1 )
+set_name=$( hmf_api_get 'runs/'${run_id}'' | jq -r '.set | .name' )
+
+mkdir temp
+mkdir temp_cup
+gsutil cp gs://patient-reporter-final-prod-1/${sample}*report.pdf ~/temp
+gsutil cp gs://patient-reporter-final-prod-1/${sample}*oncoact.json ~/temp
+gsutil cp gs://diagnostic-pipeline-output-prod-1/${set_name}/orange/${sample}*.orange.pdf ~/temp
+gsutil cp gs://diagnostic-pipeline-output-prod-1/${set_name}/cuppa/${sample}*_cup_report.pdf ~/temp_cup
+
+echo ""
+echo "Do you want to upload the final OncoAct report and json? y or n"
+read answer_report
+if [[ ${answer_report} == "y" ]]; then
+    upload_file_to_nc_for_viewing ~/temp/${sample}*report.pdf
+    upload_file_to_nc_for_viewing ~/temp/${sample}*.json
+    echo "The OncoAct report + json for ${sample} are uploaded to nextcloud STAGING/New-Reports-Viewing"
+fi
+
+echo ""
+echo "Do you want to upload the ORANGE report? y or n"
+read answer_orange
+if [[ ${answer_orange} == "y" ]]; then
+    upload_file_to_nc_for_viewing ~/temp/${sample}*report.pdf ~/temp/${sample}*orange.pdf
+    echo "The ORANGE report for ${sample} is uploaded to nextcloud STAGING/New-Reports-Viewing"
+fi
+
+echo ""
+echo "Do you want to upload the CUPPA RUO report? y or n"
+read answer_cuppa_ruo
+if [[ ${answer_cuppa_ruo} == "y" ]]; then
+    upload_file_to_nc_for_viewing ~/temp/${sample}*report.pdf ~/temp_cup/${sample}*_cup_report.pdf
+    echo "The CUPPA RUO report for ${sample} is uploaded to nextcloud STAGING/New-Reports-Viewing"
+fi
+
+rm -r ~/temp/ 2>&1
+rm -r ~/temp_cup/ 2>&1
