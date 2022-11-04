e76ad2886e608037eb0aeb895d2e1a4ce7abee0f
SandravdBroek <vandenbroek.alexandra@gmail.com>
2022-11-04 08:52:07 +0100

    script to get reports

--- hmftools/patientreporter/ops-vm/reports_to_nc.sh
+++ hmftools/patientreporter/ops-vm/reports_to_nc.sh
@@ -13,15 +13,14 @@ set_name=$( hmf_api_get 'runs/'${run_id}'' | jq -r '.set | .name' )
 
 mkdir temp
 mkdir temp_cup
-gsutil cp gs://patient-reporter-final-prod-1/${sample}*report.pdf ~/temp
-gsutil cp gs://patient-reporter-final-prod-1/${sample}*oncoact.json ~/temp
-gsutil cp gs://diagnostic-pipeline-output-prod-1/${set_name}/orange/${sample}*.orange.pdf ~/temp
-gsutil cp gs://diagnostic-pipeline-output-prod-1/${set_name}/cuppa/${sample}*_cup_report.pdf ~/temp_cup
+
 
 echo ""
 echo "Do you want to upload the final OncoAct report and json? y or n"
 read answer_report
 if [[ ${answer_report} == "y" ]]; then
+    gsutil cp gs://patient-reporter-final-prod-1/${sample}*report.pdf ~/temp
+    gsutil cp gs://patient-reporter-final-prod-1/${sample}*oncoact.json ~/temp
     upload_file_to_nc_for_viewing ~/temp/${sample}*report.pdf
     upload_file_to_nc_for_viewing ~/temp/${sample}*.json
     echo "The OncoAct report + json for ${sample} are uploaded to nextcloud STAGING/New-Reports-Viewing"
@@ -31,6 +30,7 @@ echo ""
 echo "Do you want to upload the ORANGE report? y or n"
 read answer_orange
 if [[ ${answer_orange} == "y" ]]; then
+    gsutil cp gs://diagnostic-pipeline-output-prod-1/${set_name}/orange/${sample}*.orange.pdf ~/temp
     upload_file_to_nc_for_viewing ~/temp/${sample}*orange.pdf
     echo "The ORANGE report for ${sample} is uploaded to nextcloud STAGING/New-Reports-Viewing"
 fi
@@ -39,6 +39,7 @@ echo ""
 echo "Do you want to upload the CUPPA RUO report? y or n"
 read answer_cuppa_ruo
 if [[ ${answer_cuppa_ruo} == "y" ]]; then
+    gsutil cp gs://diagnostic-pipeline-output-prod-1/${set_name}/cuppa/${sample}*_cup_report.pdf ~/temp_cup
     upload_file_to_nc_for_viewing ~/temp_cup/${sample}*_cup_report.pdf
     echo "The CUPPA RUO report for ${sample} is uploaded to nextcloud STAGING/New-Reports-Viewing"
 fi
