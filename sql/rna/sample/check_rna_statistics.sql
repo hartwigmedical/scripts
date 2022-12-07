71eb61f6c84a4f9850f8c8569faf9b50e125e6dd
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2022-12-07 10:28:22 +0100

    Update rna statistics query

--- sql/rna/sample/check_rna_statistics.sql
+++ sql/rna/sample/check_rna_statistics.sql
@@ -1,6 +1,5 @@
-SELECT sampleId, readLength, qcStatus, totalFragments, duplicates,
-totalFragments-duplicates AS nonDuplicates,
-duplicates/totalFragments AS duplicateRate,
+SELECT a.sampleId, readLength, qcStatus, IF(genesWithSplicedFragments>17000,TRUE,FALSE) AS sufficientGenesWithSplicedFragments,
+totalFragments, duplicates, totalFragments-duplicates AS nonDuplicates, duplicates/totalFragments AS duplicateRate,
 splicedPercent, unsplicedPercent, alternateSplicePercent, chimericPercent,
 round(totalFragments*splicedPercent,0) AS splicedFragments,
 round(totalFragments*unsplicedPercent,0) AS unsplicedFragments,
@@ -8,5 +7,6 @@ round(totalFragments*alternateSplicePercent,0) AS alternateSpliceFragments,
 round(totalFragments*chimericPercent,0) AS chimericFragments,
 enrichedGenePercent,
 fragmentLengthPct05, fragmentLengthPct50, fragmentLengthPct95, medianGCRatio
-FROM rnaStatistics
-WHERE sampleId IN ('XXX');
+FROM rnaStatistics r
+INNER JOIN (SELECT sampleId, count(*) AS genesWithSplicedFragments FROM geneExpression WHERE splicedFragments>0 AND sampleId IN ('XXX') GROUP BY 1) AS a
+ON r.sampleId=a.sampleId;
