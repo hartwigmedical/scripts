4faffcb60c288819ca95b83c033aff86804d3249
moez-baksi <moez-baksi@hotmail.com>
2022-12-29 14:23:54 +0100

    dr-views updates

--- sql/views/create_view_datarequest.sql
+++ sql/views/create_view_datarequest.sql
@@ -6,6 +6,14 @@ FROM
 	clinical_purity
 	INNER JOIN metric ON metric.sampleId = clinical_purity.sampleId
     INNER JOIN snpcheck ON snpcheck.sampleId = clinical_purity.sampleId
+    INNER JOIN consentsLAMA ON SUBSTRING_INDEX(SUBSTRING_INDEX(clinical_purity.setName, '_', - 2), '_', 1)=consentsLAMA.barcode
 WHERE
-	blacklisted = 0 AND purpleQC = 'PASS' AND metric.sufficientCoverage = 1 AND snpcheck.isPass = 1 AND inHMFDatabase =1 AND outsideEU =1 AND
-	(clinical_purity.sampleId LIKE '%CPCT%' OR clinical_purity.sampleId LIKE '%ACTN%' OR clinical_purity.sampleId LIKE '%WIDE%');
+	blacklisted = 0
+	AND purpleQC = 'PASS'
+	AND metric.sufficientCoverage = 1
+	AND snpcheck.isPass = 1
+	AND inHMFDatabase =1
+	AND outsideEU =1
+	AND consentsLAMA.allowExternalUseWithoutCheck = 'true'
+    AND clinical_purity.sampleId NOT LIKE '%DRUP%'
+	;
--- sql/views/create_view_datarequest_with_CB01.sql
+++ sql/views/create_view_datarequest_with_CB01.sql
@@ -6,14 +6,12 @@ FROM
 	clinical_purity
 	INNER JOIN metric ON metric.sampleId = clinical_purity.sampleId
     INNER JOIN snpcheck ON snpcheck.sampleId = clinical_purity.sampleId
+    INNER JOIN consentsLAMA ON SUBSTRING_INDEX(SUBSTRING_INDEX(clinical_purity.setName, '_', - 2), '_', 1)=consentsLAMA.barcode
 WHERE
-    blacklisted = 0 AND
-    purpleQC = 'PASS' AND
-    snpcheck.isPass = 1 AND
-    metric.sufficientCoverage = 1 AND
-    (
-    (inHMFDatabase =1 AND outsideEU =1 AND clinical_purity.sampleId LIKE '%CPCT%') OR
-    (inHMFDatabase =1 AND outsideEU =1 AND clinical_purity.sampleId LIKE '%WIDE%') OR
-    (inHMFDatabase =1 AND outsideEU =1 AND clinical_purity.sampleId LIKE '%ACTN%') OR
-    (clinical_purity.sampleId LIKE '%COREDB01%')
-    );
+	blacklisted = 0
+	AND purpleQC = 'PASS'
+	AND metric.sufficientCoverage = 1
+	AND snpcheck.isPass = 1
+	AND consentsLAMA.allowExternalUseWithCheck = 'true'
+    AND clinical_purity.sampleId NOT LIKE '%DRUP%'
+	;
