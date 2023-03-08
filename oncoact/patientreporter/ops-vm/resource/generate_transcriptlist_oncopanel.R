3cef548f13365e1d2f375d441f4d79b634b0272b
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-03-08 15:34:22 +0100

    OPR-074: Update public resources into bucket

--- oncoact/patientreporter/ops-vm/resource/generate_transcriptlist_oncopanel.R
+++ oncoact/patientreporter/ops-vm/resource/generate_transcriptlist_oncopanel.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 library(dplyr)
 
 ensemblGeneData <- read.csv('/data/resources/public/ensembl_data_cache/38/ensembl_gene_data.csv')
@@ -7,5 +9,5 @@ driverGeneData <- ensemblGeneData %>% filter(GeneName %in% driverGenePanel$gene)
 driverTranscripts <- ensemblTransExonData %>% filter(ExonRank==1) %>% filter(CanonicalTranscriptId==TransId | TransName=='ENST00000579755') # manually include the CDKN2A alt transcript
 driverGeneTransData <- merge(driverGeneData,driverTranscripts %>% select(GeneId,TransName),by='GeneId',all.x=T)
 
-outputFile <-  paste0('/data/resources/report_resources/oncopanel/hmf_reportable_gene_transcripts.tsv')
+outputFile <-  paste0('/data/resources/report_resources/oncoact_panel_gene_transcripts.tsv')
 write.table(driverGeneTransData, file = outputFile, row.names=TRUE, sep="\t")
