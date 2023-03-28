f411e0a8c9ebbfe05332d36650b9c8974e7b4e52
moez-baksi <moez-baksi@hotmail.com>
2023-03-28 16:47:45 +0200

    added internal use filter for clinical view

--- sql/views/create_view_clinical.sql
+++ sql/views/create_view_clinical.sql
@@ -31,3 +31,5 @@ FROM sample
         FROM drug INNER JOIN treatment ON drug.treatmentId = treatment.id GROUP BY biopsyId)
         biopsyDrugs ON biopsy.id = biopsyDrugs.biopsyId
     LEFT JOIN firstMatchedTreatmentResponse ON treatment.id = firstMatchedTreatmentResponse.treatmentId
+    INNER JOIN consentsLAMA ON SUBSTRING_INDEX(SUBSTRING_INDEX(sample.setName, '_', - 2), '_', 1)=consentsLAMA.barcode
+ WHERE consentsLAMA.allowInternalUse = 'true'
