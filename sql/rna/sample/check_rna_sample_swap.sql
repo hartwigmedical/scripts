a9f2a897bd1eeda498f94a7c41b9bb6fa1d1447f
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2023-06-06 15:52:49 +0200

    Rename sql script which checks for RNA swap

--- sql/rna/sample/check_rna_sample_swap.sql
+++ sql/rna/sample/check_rna_sample_swap.sql
@@ -3,5 +3,5 @@ SELECT
 WHERE sampleId='XXX' AND filter="PASS" AND type="SNP" AND gene != '' AND rnaTotalReadCount>=10 AND rnaAlleleReadCount>=1)
 /
 (SELECT COUNT(*) FROM germlineVariant
-WHERE sampleId='XXX' AND filter="PASS" AND type ="SNP" AND gene != '' AND rnaTotalReadCount>=10)
+WHERE sampleId='XXX' AND filter="PASS" AND type="SNP" AND gene != '' AND rnaTotalReadCount>=10)
 AS fraction;
