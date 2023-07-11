27f0fbbe14d2aa36eda27e4ee7474364ad10d3bf
Yannick Bijl <y.bijl@hartwigmedicalfoundation.nl>
2023-07-11 16:36:23 +0200

    Ensure r script for calculating tat's is correct and is called correct

--- oncoact/patientreporter/ops-vm/oncoact/determine_tat_for_barcode.R
+++ oncoact/patientreporter/ops-vm/oncoact/determine_tat_for_barcode.R
@@ -1,37 +1,56 @@
-# Get dates per sample
+#!/usr/bin/env Rscript
 
-source message_functions || exit 1
-source locate_reporting_api || exit 1
-source lims_functions || exit 1
+suppressPackageStartupMessages(library(bizdays)) # Calculations of working days TATs
 
-barcode=$1
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("At least one argument must be supplied (tumor sample barcode).n", call.=FALSE)
+}
 
-sampleId=$( find_name_for_barcode ${barcode} )
+make_business_calendar <- function() {
+  non_business_weekdays <- c('saturday','sunday')
+  non_business_holidays <- c("2023-01-01", "2023-04-07", "2023-04-09",
+                             "2023-04-10", "2023-04-27", "2023-05-18",
+                             "2023-05-29", "2023-12-25", "2023-12-26",
+                             "2024-01-01", "2024-03-29", "2024-04-01",
+                             "2024-04-26", "2024-05-09", "2024-05-20",
+                             "2024-12-25", "2024-12-26")
+  calendar <- create.calendar('nl_business_calendar',
+                              weekdays=non_business_weekdays,
+                              holidays=non_business_holidays)
+  return(calendar)
+}
 
-temp_nr=$( lama patients/tumorsamples ${sampleId} | head -n1 | grep -o '^.*arrivalHmf' | wc -w )
-tumor_arrival=$( lama patients/tumorsamples ${sampleId} | awk '{ print $'${temp_nr}' }' | grep -v arrivalHmf )
+barcode <- args[1]
+business_calendar <- make_business_calendar()
 
-sampleId_ref=$( echo $(echo ${sampleId} | cut -c1-12)R )
-temp_nr=$( lama patients/bloodsamples ${sampleId_ref} | head -n1 | grep -o '^.*arrivalHmf' | wc -w )
-ref_arrival=$( lama patients/bloodsamples ${sampleId_ref} | awk '{ print $'${temp_nr}' }' | grep -v arrivalHmf )
+cmd <- paste("bash gather_dates_for_tat.sh", barcode)
+barcode_dates <- system(command=cmd, intern=T)
 
+if (barcode_dates[2] == "NA") {
+  print("Tumor material not yet received")
+  q()
+}
+if (barcode_dates[3] == "NA") {
+  print("Reference material not yet received")
+  q()
+}
 
-if [ $(echo ${tumor_arrival} | wc -w) == 0 ]; then
-    tumor_arrival="NA"
-fi
+if (as.Date(barcode_dates[2]) > as.Date(barcode_dates[3])) {
+  received_date <- as.Date(barcode_dates[2]) # tumor
+} else {
+  received_date <- as.Date(barcode_dates[3]) # reference
+}
 
-if [ $(echo ${ref_arrival} | wc -w) == 0 ]; then
-    ref_arrival="NA"
-fi
-
-report_created_id=$( extract_first_time_reporting_id_on_barcode $barcode )
-reported=$( hmf_api_get reports/shared?report_created_id=${report_created_id}  | jq .[] | jq -r '.share_time' | tr 'T' ' ' | sed 's/\s.*$//' )
-
-if [ $(echo $reported | wc -w ) == 0 ]; then
-    reported="NA"
-fi
-
-echo ${sampleId}
-echo ${tumor_arrival}
-echo ${ref_arrival}
-echo ${reported}
+if (barcode_dates[4] == "NA") {
+  reported_date <- format(Sys.Date(), "%Y-%m-%d")
+  print("This sample is still in process!")
+  print("The current TAT is:")
+} else {
+  reported_date <- as.Date(barcode_dates[4])
+  print("This OncoAct has been reported on:")
+  print(reported_date)
+  print("The TAT of reporting was:")
+}
+tat <- bizdays(received_date, reported_date, cal=business_calendar) + 1
+print(tat)
