8c97c8eae97992612cc2b8d29bf6287314ffbdea
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-09-25 14:16:04 +0200

    DEV-3592 copy_report_to_final_gcp.py now also copies additional files

--- oncoact/patientreporter/copy_report_to_final_gcp.py
+++ oncoact/patientreporter/copy_report_to_final_gcp.py
@@ -1,13 +1,13 @@
 import requests
 import re
 import argparse
-from api_util import get_report_created, API_BASE_URL
+from api_util import get_report_created, API_BASE_URL, get_set
 from google.cloud import storage
 
 # Constants
-
+PIPELINE_OUTPUT_BUCKET = 'diagnostic-pipeline-output-prod-1'
 FINAL_BUCKET_NAME = "patient-reporter-final-prod-1"
-PORTAL_BUCKET_NAME = ""
+PORTAL_BUCKET_NAME = "hmf-customer-portal-report-shared-prod"
 
 # Regex for gs file locations: gs:://<bucket-name>/<blob-name>
 GS_PATH_REGEX = re.compile(r'^gs://([a-z0-9._-]+)((?:/[a-zA-Z0-9_.-]+)*)$')
@@ -25,25 +25,32 @@ def main(sample_barcode):
     """
     report_created = get_report_created(sample_barcode)
     report_files = report_created["report_files"]
-
     reports = [file for file in report_files if file['datatype'] in {'report_pdf', 'report_xml', 'report_json'}]
-    # driver_catalog = next(file for file in report_files if file['path'].endswith('Driver.catalog.somatic.tsv'))
-    # purple_somatic = next(file for file in report_files if file['path'].endswith('purple.somatic.vcf'))
-    # purple_sv = next(file for file in report_files if file['path'].endswith('purple.sv.vcf'))
-    # orange = next(file for file in report_files if file['path'].endswith('Orange.pdf'))
-    # TODO we also need Linx fusion output. Unfortunately, I do not know the file name
 
     storage_client = storage.Client()
     target_bucket_portal: storage.Bucket = storage_client.bucket(PORTAL_BUCKET_NAME)
     target_bucket_final: storage.Bucket = storage_client.bucket(FINAL_BUCKET_NAME)
 
-    for file in reports:
-        (bucket, blob) = get_bucket_and_blob_from_gs_path(file['path'])
+    for report in reports:
+        (bucket, blob) = get_bucket_and_blob_from_gs_path(report['path'])
         bucket_instance: storage.Bucket = storage_client.bucket(bucket)
-        print(f"Copying '{file}' to '{target_bucket_portal}'...")
-        bucket_instance.copy_blob(blob, target_bucket_portal)
-        print(f"Copying '{file}' to '{target_bucket_final}'...")
-        bucket_instance.copy_blob(blob, target_bucket_final)
+        copy_and_log(bucket_instance, target_bucket_portal, blob)
+        copy_and_log(bucket_instance, target_bucket_final, blob)
+
+    sample_name = report_created['sample_name']
+    sample_set = get_set(sample_name)
+    set_name = sample_set['name']
+
+    orange_pdf = f'{set_name}/orange/{sample_name}.orange.pdf'
+    purple_sv_vcf = f'{set_name}/purple/{sample_name}.purple.sv.vcf.gz'
+    purple_somatic_vcf = f'{set_name}/purple/{sample_name}.purple.somatic.vcf.gz'
+    purple_catalog = f'{set_name}/purple/{sample_name}.driver.catalog.somatic.tsv'
+    linx_fusion = f'{set_name}/linx/{sample_name}.linx.fusion.tsv'
+    linx_catalog = f'{set_name}/linx/{sample_name}.linx.driver.catalog.tsv'
+
+    pipline_output_bucket: storage.Bucket = storage_client.bucket(PIPELINE_OUTPUT_BUCKET)
+    for blob in [orange_pdf, purple_sv_vcf, purple_somatic_vcf, purple_catalog, linx_fusion, linx_catalog]:
+        copy_and_log(pipline_output_bucket, target_bucket_portal, blob)
 
     print('Updating report shared status in the API...')
     params = {
@@ -55,6 +62,12 @@ def main(sample_barcode):
     print('API updated!')
     print('all Done (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧ !')
     exit(0)
+
+
+def copy_and_log(source_bucket: storage.Bucket, target_bucket: storage.Bucket, blob_name: str):
+    blob = storage.Blob(blob_name, source_bucket)
+    print(f"Copying '{blob_name}' to '{target_bucket}'...")
+    source_bucket.copy_blob(blob=blob, destination_bucket=target_bucket)
 
 
 def get_bucket_and_blob_from_gs_path(gs_path: str) -> (str, str):
