5b614bcbf8bb006e000341c94eb20f9825c3900b
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-09-26 13:43:54 +0200

    DEV-3592 update fail_report.py

--- oncoact/patientreporter/fail_report.py
+++ oncoact/patientreporter/fail_report.py
@@ -2,20 +2,20 @@ import json
 import argparse
 from google.cloud import pubsub
 
-TOPIC = ''
-
 
 def main():
     parser = argparse.ArgumentParser(description="Emit a QC fail event for a given report.")
-    parser.add_argument('run_id')
-    parser.add_argument('--topic', default=TOPIC)
+    parser.add_argument('tumor_sample_barcode')
+    parser.add_argument('--profile', choices=['pilot', 'prod'], default='pilot')
     args = parser.parse_args()
-    run_id = int(args.run_id)
-    topic = args.topic
-
+    if args.profile == 'prod':
+        prod_warn = input("Warning: you are running in prod. Type 'y' to continue.")
+        if prod_warn.lower() != 'y':
+            print('Program aborted')
+            exit(1)
+    tumor_sample_barcode = args.tumor_sample_barcode
     choices = ['wgs_processing_issue', 'wgs_isolation_fail', 'wgs_tcp_shallow_fail', 'wgs_preparation_fail',
                'wgs_tumor_processing_issue', 'wgs_pipeline_fail', 'wgs_tcp_fail']
-
     print('Please provide a reason for the failure, the possible reasons are:')
     print('\n'.join(choices))
     reason = input()
@@ -25,23 +25,25 @@ def main():
             f" This might cause an exception (y/n)")
         if cont.lower() != 'y':
             exit(1)
-
-    emit_fail_report(pubsub.PublisherClient(), topic, reason, run_id)
-    print('event emitted!')
+    client = pubsub.PublisherClient()
+    project = 'hmf-pipeline-development'  # TODO make dynamic
+    topic_path = client.topic_path(project, 'report.fail')
+    res = emit_fail_report(client, topic_path, reason, tumor_sample_barcode)
+    print(f"event emitted! (message id: '{res}')")
     exit(0)
 
 
-async def emit_fail_report(client: pubsub.PublisherClient,
-                           topic: str,
-                           reason: str,
-                           run_id: int):
+def emit_fail_report(client: pubsub.PublisherClient,
+                     topic_path: str,
+                     reason: str,
+                     tumor_sample_barcode: str):
     data = {
         'reason': reason,
-        'runId': run_id
+        'tumorSampleBarcode': tumor_sample_barcode
     }
     json_data = json.dumps(data)
     encoded_message = json_data.encode('utf-8')
-    await client.publish(topic=topic, data=encoded_message, subject='report', event='fail')
+    return client.publish(topic=topic_path, data=encoded_message).result()
 
 
 if __name__ == '__main__':
