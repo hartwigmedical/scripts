36c9f8cc5a3d458641d77d04f87fce7d5de46e9e
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-10-04 16:51:12 +0200

    DEV-3592 add more elaborate error handling

--- oncoact/patientreporter/check_report_status.py
+++ oncoact/patientreporter/check_report_status.py
@@ -19,6 +19,7 @@ def main():
 def check_report_status(storage_client: Client, profile: str):
     rest_client = RestClient(profile)
 
+    print("Fetching reports and runs...")
     all_reports = pd.DataFrame(rest_client.get_all_reports_created())
     # these if checks are to ensure that if the dataframe is empty,
     # it still has the required columns to prevent KeyErrors.
@@ -43,9 +44,10 @@ def check_report_status(storage_client: Client, profile: str):
     validated_reports_and_shared = all_shared_reports[all_shared_reports['run_id'].isin(validated_runs['id'])].iloc[:5]
     validated_reports_but_not_shared = reports_not_shared[reports_not_shared['run_id'].isin(validated_runs['id'])].iloc[
                                        :5]
-    validated_warnings_and_errors = check_errors_for_validated_reports(storage_client, pd.concat(
+
+    validated_warnings_and_errors = check_warnings_for_validated_reports(storage_client, pd.concat(
         [validated_reports_and_shared, validated_reports_but_not_shared], ignore_index=True),
-                                                                       validated_runs, profile)
+                                                                         validated_runs, profile)
 
     finished_reports_not_shared = reports_not_shared[reports_not_shared['run_id'].isin(finished_runs)]
 
@@ -54,9 +56,9 @@ def check_report_status(storage_client: Client, profile: str):
     runs_without_report = all_runs[~all_runs['id'].isin(all_reports['run_id'])]
     reports_without_runs = all_reports[~all_reports['run_id'].isin(all_runs['id'])]
 
-    failed_executions = rest_client.get_failed_executions()
-    failed_sample_barcodes_with_reason = {rest_client.get_tumor_sample_barcode_from_run_id(run_id): errors for
-                                          (run_id, errors) in failed_executions.items()}
+    # failed_executions = rest_client.get_failed_executions()
+    # failed_sample_barcodes_with_reason = {rest_client.get_tumor_sample_barcode_from_run_id(run_id): errors for
+    #                                       (run_id, errors) in failed_executions.items()}
 
     print('----------')
     print("Failed reports that have not yet been shared:\n")
@@ -90,30 +92,49 @@ def check_report_status(storage_client: Client, profile: str):
         print(f"{i + 1} - {report['sample_barcode']}")
         print(f"\tset name: {get_set_name_from_report(report, validated_runs)}")
         report_id = report['id']
+        print("\tWarnings:")
         if validated_warnings_and_errors[report_id]:
-            print(f"\tWarnings: {validated_warnings_and_errors[report_id]}")
+            for warning in validated_warnings_and_errors[report_id]:
+                print(f'\t{warning}')
+        else:
+            print("\t\tNone, Ready for sharing :)!")
 
     print('----------')
     print("Reports without a run:\n")
     for i, report in enumerate(reports_without_runs['sample_barcode']):
         print(f"{i + 1} - {report}")
 
-    # Runs that failed in the patient reporter:
-    print('----------')
-    print("Runs whose execution failed in the reporting pipeline")
-    for i, (tumor_barcode, errors) in enumerate(failed_sample_barcodes_with_reason):
-        print(f"{i + 1} - {tumor_barcode} : {errors}")
+    # # Runs that failed in the patient reporter:
+    # print('----------')
+    # print("Runs whose execution failed in the reporting pipeline")
+    # for i, (tumor_barcode, errors) in enumerate(failed_sample_barcodes_with_reason):
+    #     print(f"{i + 1} - {tumor_barcode} : {errors}")
+    #
+    # print('----------')
+    # print('Runs without a report')
+    # for i, run_id in enumerate(runs_without_report):
+    #     sample_barcode = rest_client.get_tumor_sample_barcode_from_run_id(run_id)
+    #     print(f'{i + 1} - {sample_barcode}')
+    #     print(f'\trun_id: {run_id}')
 
-    print('----------')
-    print('Runs without a report')
-    for i, run_id in enumerate(runs_without_report):
-        sample_barcode = rest_client.get_tumor_sample_barcode_from_run_id(run_id)
-        print(f'{i + 1} - {sample_barcode}')
-        print(f'\trun_id: {run_id}')
+
+class Warning:
+    def __init__(self, warning, action):
+        self._warning = warning
+        self._action = action
+
+    def __str__(self):
+        return f"""
+        -----
+        Warning: {self._warning}
+        Recommended action: 
+            - {self._action}
+        -----
+        """
 
 
-def check_errors_for_validated_reports(client: Client, validated_reports: pd.DataFrame,
-                                       validated_runs: pd.DataFrame, profile):
+def check_warnings_for_validated_reports(client: Client, validated_reports: pd.DataFrame,
+                                         validated_runs: pd.DataFrame, profile):
     res = {}
     for i, report in validated_reports.iterrows():
         print('Checking errors and warnings for validated runs...', i, '/', len(validated_reports))
@@ -134,19 +155,22 @@ def check_errors_for_validated_reports(client: Client, validated_reports: pd.Dat
         set_name = run['set'].values[0]['name']
         health_error = get_health_error_validated_run(set_name)
 
-        errors = []
+        warnings = []
 
         if has_rose_error(client, set_name, report['sample_name'], profile):
-            errors.append("Summary was incorrect (rose error)")
+            rose_location = get_rose_location_as_gs_string(profile, set_name, report['sample_name'])
+            warnings.append(
+                Warning('Summary was incorrect (rose error).', f"Check for errors 'gsutil cat {rose_location}'"))
         if "WARN" in log_content:
-            errors.append("Warn was present in log file")
+            warnings.append(
+                Warning('Warn was present in log file.', f"Check for warns 'gsutil cat {log['path']} | grep WARN'"))
         if (
                 "Consent" in log_content or "Mismatching ref sample name" in log_content or "do not match" in log_content or "Missing or invalid hospital" in log_content):
-            errors.append("Lims error")
+            warnings.append(Warning('Lims error.', f"Check for lims problems 'gsutil cat {log['path']}'"))
         if "WARN" in health_error:
-            errors.append("Health check error")
+            warnings.append(Warning(f'Health check error.', f"Hint: [{health_error}]"))
 
-        res[report['id']] = errors
+        res[report['id']] = warnings
     return res
 
 
@@ -171,6 +195,11 @@ def has_rose_error(storage_client: Client, set_name: str, sample_name: str, prof
 
     content = blob.download_as_string().decode()
     return content.find('AMP') > 0
+
+
+def get_rose_location_as_gs_string(profile, set_name, sample_name):
+    bucket_name = 'diagnostic-pipeline-output-prod-1' if profile == 'prod' else 'diagnostic-pipeline-output-pilot-1'
+    return f'gs://{bucket_name}/{set_name}/purple/{sample_name}.driver.catalog.somatic.tsv'
 
 
 def get_set_name_from_report(report, runs):
