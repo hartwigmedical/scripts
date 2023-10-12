8333046c65f07023f5186467a2722283d8ca3862
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-10-12 12:41:56 +0200

    DEV-3592 remove useless methods

--- oncoact/patientreporter/share_report.py
+++ oncoact/patientreporter/share_report.py
@@ -40,7 +40,7 @@ def copy_report_to_final_gcp(sample_barcode, profile, portal_bucket, final_bucke
     """
     rest_client = RestClient(profile)
     report_created = rest_client.get_report_created(sample_barcode)
-    report_files = report_created["report_files"]
+
     run_id = report_created['run_id']
     run = rest_client.get_run(run_id)
     if not run:
@@ -56,22 +56,19 @@ def copy_report_to_final_gcp(sample_barcode, profile, portal_bucket, final_bucke
     storage_client = Client()
     target_bucket_portal: Bucket = storage_client.bucket(portal_bucket,
                                                          user_project='hmf-customer-portal')
+
+    delete_old_report_artifacts_in_portal(target_bucket_portal, sample_barcode)
+
+    report_files = report_created["report_files"]
+    relevant_report_files = [file for file in report_files if
+                             file['datatype'] in {'report_pdf', 'report_xml', 'report_json'}]
     target_bucket_final: Bucket = storage_client.bucket(final_bucket)
-
-    delete_old_report(target_bucket_portal, sample_barcode)
-
-    reports = [file for file in report_files if file['datatype'] in {'report_pdf', 'report_xml', 'report_json'}]
-
-    for report in reports:
-        (bucket, blob) = get_bucket_and_blob_from_gs_path(report['path'])
+    for report_file in relevant_report_files:
+        (bucket, blob) = get_bucket_and_blob_from_gs_path(report_file['path'])
         file_name = get_file_name_from_blob(blob)
-        bucket_instance: Bucket = storage_client.bucket(bucket)
-        copy_and_print(bucket_instance, target_bucket_portal, blob, f'{sample_barcode}/{file_name}')
-        # copy_and_print(bucket_instance, target_bucket_final, blob, blob)
-
-    sample_name = report_created['sample_name']
-    sample_set = rest_client.get_sample_set_by_sample_name(sample_name)
-    set_name = sample_set['name']
+        source_bucket: Bucket = storage_client.bucket(bucket)
+        source_bucket.copy_blob(blob, destination_bucket=target_bucket_portal, new_name=f'{sample_barcode}/{file_name}')
+        source_bucket.copy_blob(blob, destination_bucket=target_bucket_final, new_name=f'{sample_barcode}/{file_name}')
 
     run_files = rest_client.get_run_files(run_id)
     run_file_types_to_upload = {'purple_somatic_driver_catalog',
@@ -82,7 +79,6 @@ def copy_report_to_final_gcp(sample_barcode, profile, portal_bucket, final_bucke
                                 'orange_output_pdf'}
 
     relevant_run_files = [file for file in run_files if file['datatype'] in run_file_types_to_upload]
-
     for file in relevant_run_files:
         source_bucket: Bucket = storage_client.bucket(file['bucket'])
         file_blob: Blob = source_bucket.get_blob(blob_name=f'{file["directory"]}/{file["filename"]}')
@@ -97,34 +93,9 @@ def copy_report_to_final_gcp(sample_barcode, profile, portal_bucket, final_bucke
     exit(0)
 
 
-def copy_and_print(source_bucket: Bucket, target_bucket: Bucket, blob_name: str, new_blob_name: str):
-    """
-    Copies the blob from the source bucket to the target bucket and prints this in the stdout.
-
-    :param source_bucket: the bucket to copy from.
-    :param target_bucket: the bucket to copy to.
-    :param blob_name: the name of the blob.
-    :param new_blob_name: the new name of the blob within the target bucket.
-    """
-    print(f"Copying '{blob_name}' from '{source_bucket}' to '{target_bucket}'...")
-    blob = Blob(blob_name, source_bucket)
-    source_bucket.copy_blob(blob=blob, destination_bucket=target_bucket, new_name=new_blob_name)
-
-
-def delete_old_report(portal_bucket: Bucket, sample_barcode):
-    """
-    Prompts the user if they want to delete any old report artifacts for the same sample_barcode.
-
-    :param portal_bucket: the portal bucket where all the report artifacts are copied to.
-    :param sample_barcode: the sample barcode for this report.
-    """
+def delete_old_report_artifacts_in_portal(portal_bucket: Bucket, sample_barcode):
     blobs_old_run = list(portal_bucket.list_blobs(prefix=sample_barcode))
-    if len(blobs_old_run) > 0:
-        print(f"Old report artifacts found for report '{sample_barcode}':", [blob.name for blob in blobs_old_run])
-        delete = input(f"Do you want to delete these? If you choose 'n' the program will exit now (y/n)\n")
-        if delete.lower() != 'y':
-            exit(1)
-        portal_bucket.delete_blobs(blobs=blobs_old_run)
+    portal_bucket.delete_blobs(blobs=blobs_old_run)
 
 
 if __name__ == "__main__":
