32e826d5b8177f9cb2fb8eb8aa8011d65ae4f9f1
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-10-12 13:21:55 +0200

    DEV-3592 big refactor

--- oncoact/patientreporter/share_report.py
+++ oncoact/patientreporter/share_report.py
@@ -14,88 +14,120 @@ def main():
     argument_parser.add_argument('--notify-users', default=False, action='store_true',
                                  help='whether to notify the users of the share event')
     args = argument_parser.parse_args()
-
     profile = args.profile
     perform_prod_test(profile)
 
-    final_bucket = f"patient-reporter-final-{profile}-1"
-    portal_bucket = f'hmf-customer-portal-report-shared-{profile}'
-
-    copy_report_to_final_gcp(sample_barcode=args.sample_barcode,
-                             profile=profile,
-                             final_bucket=final_bucket,
-                             portal_bucket=portal_bucket,
-                             publish_to_portal=args.publish,
-                             notify_users=args.notify_users)
+    ReportShare(sample_barcode=args.sample_barcode, profile=args.profile).share_report(publish_to_portal=args.publish,
+                                                                                       notify_users=args.notify_users)
 
 
-def copy_report_to_final_gcp(sample_barcode, profile, portal_bucket, final_bucket,
-                             publish_to_portal, notify_users):
-    """
-    This program does the following:
+class ReportShare:
 
-    - Copy the relevant report files from the run bucket to the final bucket.
-    - Copy the relevant report files from the run bucket to the portal bucket.
-    - Update shared status of the report in the HMF API.
-    """
-    rest_client = RestClient(profile)
-    report_created = rest_client.get_report_created(sample_barcode)
+    def __init__(self, sample_barcode, profile='pilot'):
+        self.sample_barcode = sample_barcode
+        self.rest_client: RestClient = RestClient(profile)
+        self.storage_client: Client = Client()
 
-    run_id = report_created['run_id']
-    run = rest_client.get_run(run_id)
-    if not run:
+        self.archive_bucket = self.storage_client.bucket(f'patient-reporter-final-{profile}-1')
+        self.portal_bucket = self.storage_client.bucket(f'hmf-customer-portal-report-shared-{profile}')
+
+        self.report_created_record = self.rest_client.get_report_created(self.sample_barcode)
+        self.run_id = self.report_created_record['run_id']
+        self.run = self.rest_client.get_run(self.run_id) if self.run_id else None
+
+    def share_report(self, publish_to_portal, notify_users):
+        """
+        Shares the report by updating the remote portal bucket and the remote archive bucket. It also updates the API.
+
+        :param publish_to_portal: whether to publish a pub/sub message to portal.
+        :param notify_users: setting this to true will email the users regarding the portal update.
+        """
+        if not self.run:
+            self._prompt_user_no_run()
+        elif self.run['status'] != 'Validated':
+            self._prompt_user_non_validated_run()
+
+        self._delete_old_artifacts_in_portal()
+        self._copy_files_to_remote_buckets()
+
+        self.rest_client.post_report_shared(report_created_id=self.report_created_record['id'],
+                                            publish_to_portal=publish_to_portal,
+                                            notify_users=notify_users)
+
+    def _prompt_user_no_run(self):
         cont = input(
-            f"No associated run found for tumor barcode '{sample_barcode}'. Are you sure you want to continue? (y/n)\n")
+            f"No associated run found for tumor barcode '{self.sample_barcode}'. "
+            f"Are you sure you want to continue? (y/n)\n")
         if cont.lower() != 'y':
             exit(1)
-    if run and run['status'] != 'Validated':
-        cont = input(f"Run status for tumor barcode '{sample_barcode}' is not yet 'Validated' "
-                     f"(actual status: {run['status']}). Are you sure you want to continue? (y/n)\n")
+
+    def _prompt_user_non_validated_run(self):
+        cont = input(f"Run status for tumor barcode '{self.sample_barcode}' is not yet 'Validated' "
+                     f"(actual status: {self.run['status']}). Are you sure you want to continue? (y/n)\n")
         if cont.lower() != 'y':
             exit(1)
-    storage_client = Client()
-    target_bucket_portal: Bucket = storage_client.bucket(portal_bucket,
-                                                         user_project='hmf-customer-portal')
 
-    delete_old_report_artifacts_in_portal(target_bucket_portal, sample_barcode)
+    def _delete_old_artifacts_in_portal(self):
+        blobs_old_run = list(self.portal_bucket.list_blobs(prefix=self.sample_barcode))
+        self.portal_bucket.delete_blobs(blobs=blobs_old_run)
 
-    report_files = report_created["report_files"]
-    relevant_report_files = [file for file in report_files if
-                             file['datatype'] in {'report_pdf', 'report_xml', 'report_json'}]
-    target_bucket_final: Bucket = storage_client.bucket(final_bucket)
-    for report_file in relevant_report_files:
-        (bucket, blob) = get_bucket_and_blob_from_gs_path(report_file['path'])
+    def _copy_files_to_remote_buckets(self):
+        run_blobs = self._get_run_files_as_blobs()
+        report_blobs = self._get_report_files_as_blobs()
+
+        for blob in run_blobs:
+            self._copy_blob_to_portal_bucket(blob=blob, target_folder='RUO')
+
+        for blob in report_blobs:
+            self._copy_blob_to_portal_bucket(blob=blob, target_folder='')
+            self._copy_blob_to_archive_bucket(blob=blob)
+
+    def _copy_blob_to_portal_bucket(self, blob, target_folder):
+        if target_folder != '' and target_folder[-1] != '/':
+            target_folder += '/'
         file_name = get_file_name_from_blob(blob)
-        source_bucket: Bucket = storage_client.bucket(bucket)
-        source_bucket.copy_blob(blob, destination_bucket=target_bucket_portal, new_name=f'{sample_barcode}/{file_name}')
-        source_bucket.copy_blob(blob, destination_bucket=target_bucket_final, new_name=f'{sample_barcode}/{file_name}')
+        source_bucket = blob.bucket
+        source_bucket.copy_blob(blob=blob,
+                                destination_bucket=self.portal_bucket,
+                                new_name=f'{self.sample_barcode}/{target_folder}{file_name}')
 
-    run_files = rest_client.get_run_files(run_id)
-    run_file_types_to_upload = {'purple_somatic_driver_catalog',
-                                'linx_driver_catalog',
-                                'somatic_variants_purple',
-                                'structural_variants_purple',
-                                'linx_fusions',
-                                'orange_output_pdf'}
+    def _copy_blob_to_archive_bucket(self, blob):
+        file_name = get_file_name_from_blob(blob.name)
+        source_bucket = blob.bucket
+        source_bucket.copy_blob(blob=blob,
+                                destination_bucket=self.archive_bucket,
+                                new_name=file_name)
 
-    relevant_run_files = [file for file in run_files if file['datatype'] in run_file_types_to_upload]
-    for file in relevant_run_files:
-        source_bucket: Bucket = storage_client.bucket(file['bucket'])
-        file_blob: Blob = source_bucket.get_blob(blob_name=f'{file["directory"]}/{file["filename"]}')
-        new_blob_name = f'{sample_barcode}/RUO/{file["filename"]}'
-        source_bucket.copy_blob(blob=file_blob, destination_bucket=target_bucket_portal, new_name=new_blob_name)
+    def _get_run_files_as_blobs(self):
+        all_run_files = self.rest_client.get_run_files(self.run_id)
+        run_file_types = {'purple_somatic_driver_catalog',
+                          'linx_driver_catalog',
+                          'somatic_variants_purple',
+                          'structural_variants_purple',
+                          'linx_fusions',
+                          'orange_output_pdf'}
+        run_files_to_upload = [file for file in all_run_files if file['datatype'] in run_file_types]
+        result = []
+        for file in run_files_to_upload:
+            blob = self._get_blob_from_gs_path_and_project(gs_path=self.storage_client.bucket(file['filepath']),
+                                                           user_project='hmf-pipeline-prod')
+            result.append(blob)
+        return result
 
-    print('Updating report shared status in the API...')
-    rest_client.post_report_shared(report_created_id=report_created['id'], publish_to_portal=publish_to_portal,
-                                   notify_users=notify_users)
-    print('API updated!')
-    print('All done・ﾟ✧ !')
-    exit(0)
+    def _get_report_files_as_blobs(self):
+        all_report_files = self.report_created_record["report_files"]
+        report_files_to_upload = [file for file in all_report_files if
+                                  file['datatype'] in {'report_pdf', 'report_xml', 'report_json'}]
+        result = []
+        for file in report_files_to_upload:
+            blob = self._get_blob_from_gs_path_and_project(gs_path=file['path'], user_project='hmf-ops')
+            result.append(blob)
+        return result
 
-
-def delete_old_report_artifacts_in_portal(portal_bucket: Bucket, sample_barcode):
-    blobs_old_run = list(portal_bucket.list_blobs(prefix=sample_barcode))
-    portal_bucket.delete_blobs(blobs=blobs_old_run)
+    def _get_blob_from_gs_path_and_project(self, gs_path, user_project):
+        bucket_name, blob_name = get_bucket_and_blob_from_gs_path(gs_path)
+        bucket: Bucket = self.storage_client.bucket(bucket_name=bucket_name, user_project=user_project)
+        return bucket.get_blob(blob_name)
 
 
 if __name__ == "__main__":
