aceb6ff87efe7f3e10dac8603d0dcea33832aac5
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-10-20 14:01:06 +0200

    Update script for update OncoAct resources into bucket

--- oncoact/patientreporter/ops-vm/resource/generate_transcriptlist_oncopanel.R
+++ oncoact/patientreporter/ops-vm/resource/generate_transcriptlist_oncopanel.R
@@ -2,9 +2,9 @@
 
 library(dplyr)
 
-ensemblGeneData <- read.csv('/data/resources/public/ensembl_data_cache/38/ensembl_gene_data.csv')
-ensemblTransExonData <- read.csv('/data/resources/public/ensembl_data_cache/38/ensembl_trans_exon_data.csv')
-driverGenePanel <- read.csv('/data/resources/public/gene_panel/38/DriverGenePanel.38.tsv',sep='\t')
+ensemblGeneData <- read.csv('/data/resources/report_resources/38/ensembl_gene_data.csv')
+ensemblTransExonData <- read.csv('/data/resources/report_resources/38/ensembl_trans_exon_data.csv')
+driverGenePanel <- read.csv('/data/resources/report_resources/38/DriverGenePanel.38.tsv',sep='\t')
 driverGeneData <- ensemblGeneData %>% filter(GeneName %in% driverGenePanel$gene) %>% select(GeneId,GeneName)
 driverTranscripts <- ensemblTransExonData %>% filter(ExonRank==1) %>% filter(CanonicalTranscriptId==TransId | TransName=='ENST00000579755') # manually include the CDKN2A alt transcript
 driverGeneTransData <- merge(driverGeneData,driverTranscripts %>% select(GeneId,TransName),by='GeneId',all.x=T)
--- sql/ckb/views/trials_view.sql
+++ sql/ckb/views/trials_view.sql
@@ -1,6 +1,20 @@
 CREATE OR REPLACE VIEW trials AS (
 
-SELECT DISTINCT
-	clinicalTrial.id, profileName AS tumorProfile, clinicalTrial.updateDate AS trialUpdateDate,
-	clinicalTrial.nctId, title, phase, recruitment, gender
-FROM ckbEntry INNER JOIN clinicalTrial ON clinicalTrial.ckbEntryId = ckbEntry.id);
+SELECT
+    ckbEntry.id as ckbEntryId, ckbEntry.ckbProfileId, profileName AS tumorProfile, requirementType,
+	clinicalTrial.nctId, clinicalTrial.title, clinicalTrial.phase, clinicalTrial.recruitment, clinicalTrial.gender, clinicalTrial.sponsors,clinicalTrial.variantRequirement,
+    group_concat(distinct ageGroup.ageGroup) as ageGroups, group_concat(distinct location.country) as countries,
+    therapyName,indication.name as cancerType
+--    treatmentApproachEvidence.treatmentApproachEvidenceId, treatmentApproachDrugClass.treatmentApproachId, group_concat(distinct drugClass) as drugClasses
+FROM ckbEntry
+LEFT JOIN clinicalTrial ON clinicalTrial.ckbEntryId = ckbEntry.id
+LEFT JOIN variantRequirementDetail ON variantRequirementDetail.clinicalTrialId = clinicalTrial.id
+LEFT JOIN ageGroup ON ageGroup.clinicalTrialId = clinicalTrial.id
+LEFT JOIN location ON location.clinicalTrialId = clinicalTrial.id
+LEFT JOIN therapyClinicalTrial ON therapyClinicalTrial.clinicalTrialId = clinicalTrial.id
+LEFT JOIN therapy ON therapyClinicalTrial.therapyId = therapy.id
+LEFT JOIN indicationClinicalTrial ON indicationClinicalTrial.clinicalTrialId = clinicalTrial.id
+LEFT JOIN indication ON indicationClinicalTrial.indicationId = indication.id
+--LEFT JOIN treatmentApproachEvidence ON treatmentApproachEvidence.evidenceId = clinicalTrial.id
+--LEFT JOIN treatmentApproachDrugClass ON treatmentApproachDrugClass.treatmentApproachId=treatmentApproachEvidence.treatmentApproachEvidenceId
+group by clinicalTrial.nctId, ckbEntry.ckbProfileId, therapyClinicalTrial.therapyId, indicationClinicalTrial.indicationId);
