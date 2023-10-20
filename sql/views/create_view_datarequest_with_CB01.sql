5d615df1de4064c7c7f7b4f3eacedf43578c24bc
moez-baksi <moez-baksi@hotmail.com>
2023-10-20 11:18:41 +0200

    snpcheck is deprecated, removed from DR related views

--- sql/views/create_view_datarequest_with_CB01.sql
+++ sql/views/create_view_datarequest_with_CB01.sql
@@ -1,16 +1,14 @@
+use hmfpatients;
 CREATE OR REPLACE VIEW datarequest_CB01 AS
-
 SELECT
 	clinical_purity.*, metric.sufficientCoverage AS sufficientCoverage
 FROM
 	clinical_purity
 	INNER JOIN metric ON metric.sampleId = clinical_purity.sampleId
-    INNER JOIN snpcheck ON snpcheck.sampleId = clinical_purity.sampleId
     INNER JOIN consentsLAMA ON clinical_purity.sampleId=consentsLAMA.sampleId
 WHERE
 	blacklisted = 0
 	AND purpleQC = 'PASS'
 	AND metric.sufficientCoverage = 1
-	AND snpcheck.isPass = 1
 	AND consentsLAMA.allowExternalUseWithCheck = 'true'
 	;
