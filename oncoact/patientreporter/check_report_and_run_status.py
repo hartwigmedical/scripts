7a0116b0f4ca1a901f2c03d7744b767c7db2f841
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-10-23 12:35:14 +0200

    DEV-3592 add health checker warning

--- oncoact/patientreporter/check_report_and_run_status.py
+++ oncoact/patientreporter/check_report_and_run_status.py
@@ -188,8 +188,8 @@ class StatusChecker:
         return section
 
     def _get_all_report_associated_warnings(self, report_record):
-        return (self._get_patient_reporter_log_related_warnings(report_record))
-                # self._get_health_checker_related_warnings(report_record) +
+        return (self._get_patient_reporter_log_related_warnings(report_record) +
+                self._get_health_checker_related_warnings(report_record))
 
     def _get_patient_reporter_log_related_warnings(self, report_record):
         warnings = []
