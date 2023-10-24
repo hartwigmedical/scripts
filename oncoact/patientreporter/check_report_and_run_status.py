35152d083f61bac382088c49a737241a396e7299
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-10-24 11:53:25 +0200

    use temp deleted genomische pipeline runs instead validated runs

--- oncoact/patientreporter/check_report_and_run_status.py
+++ oncoact/patientreporter/check_report_and_run_status.py
@@ -225,13 +225,13 @@ class StatusChecker:
 
     def _get_health_checker_related_warnings(self, report_record):
         warnings = []
-        run_record = self._get_run_from_report_record(report_record)
-        set_name = run_record['set']['name']
-
-        health_checker_log = subprocess.check_output(['health_check_validated_run', set_name, '2>&1']).decode()
-        if 'WARN' in health_checker_log:
-            warnings.append('A warning was found in the health checker log. '
-                            f'See: {health_checker_log}')
+#         run_record = self._get_run_from_report_record(report_record)
+#         set_name = run_record['set']['name']
+#
+#         health_checker_log = subprocess.check_output(['health_check_validated_run', set_name, '2>&1']).decode()
+#         if 'WARN' in health_checker_log:
+#             warnings.append('A warning was found in the health checker log. '
+#                             f'See: {health_checker_log}')
         return warnings
 
     def _get_doid_warnings(self, report_record):
