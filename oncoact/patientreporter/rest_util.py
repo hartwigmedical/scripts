423129bd2993d95c0e865cc130a90b2672c330ed
KasperWolsink99 <k.wolsink@hartwigmedicalfoundation.nl>
2023-10-27 10:36:54 +0200

    DEV-3592 fix bug where from_report_create_time was misplaced

--- oncoact/patientreporter/rest_util.py
+++ oncoact/patientreporter/rest_util.py
@@ -28,7 +28,7 @@ class RestClient:
         :param lookback_days the amount of days to look back for the report creation time.
         """
         from_date = datetime.today().date() - timedelta(days=lookback_days)
-        return _get_as_json(url=self._report_created_url, params={'from_create_date': from_date})
+        return _get_as_json(url=self._report_created_url, params={'from_report_create_time': from_date})
 
     def get_report_created(self, sample_barcode):
         """
@@ -59,7 +59,7 @@ class RestClient:
         :param lookback_days the amount of days to look back for the report creation time.
         """
         from_date = datetime.today().date() - timedelta(days=lookback_days)
-        return _get_as_json(url=self._report_shared_url, params={'from_create_date': from_date})
+        return _get_as_json(url=self._report_shared_url, params={'from_report_create_time': from_date})
 
     def get_all_relevant_runs(self, lookback_days=90):
         """
