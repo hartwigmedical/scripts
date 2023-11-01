df26e001d759c091162fd7bd4fab52b7a2466393
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-11-01 12:23:30 +0100

    Remove reference to actual sample

--- sql/genomic/indel_repeat_context_analysis.sql
+++ sql/genomic/indel_repeat_context_analysis.sql
@@ -4,5 +4,5 @@ count(*) AS totalCount,
 sum(IF(repeatCount>=4,1,0)) AS repeatCount,
 sum(IF(repeatCount<4 AND microhomology <>'' AND microhomology <> '.',1,0)) AS MHCount,
 sum(IF(repeatCount<4 AND (microhomology ='' OR microhomology='.'),1,0)) AS nonRepeatNonMHCount
-FROM somaticVariant WHERE sampleId = 'WIDE01010200T' AND type = 'INDEL' AND filter = 'PASS' GROUP BY 1,2;
+FROM somaticVariant WHERE sampleId = 'XXX' AND type = 'INDEL' AND filter = 'PASS' GROUP BY 1,2;
 
