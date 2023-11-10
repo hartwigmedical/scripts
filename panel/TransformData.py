3c3de0996fe43eb23637dd19281b2f0988392dbe
edk360 <van.dijk.erik@gmail.com>
2023-11-10 16:32:56 +0100

    Update to improve normalisation and add gene annotations

--- panel/TransformData.py
+++ panel/TransformData.py
@@ -15,6 +15,23 @@ vcf_header = '''##fileformat=VCFv4.2
 
 AmpsDels=dict()
 
+def overlaps(x, y):
+    return max(x[0],y[0]) < min(x[1],y[1])
+
+@dataclass(frozen=True)
+class geneLoc:
+    gene : str
+    chr : str
+    start : int
+    end : int
+
+def weightedAverage(values,weights):
+    weightedSum=0
+    for i in range(0,len(values)):
+        weightedSum+= values[i]*weights[i]
+    return weightedSum/sum(weights)
+
+
 def readDriverCatalog(purple_driver):
     first = True
     with open(purple_driver,'rt') as fh:
@@ -51,7 +68,7 @@ def getPurityPloidy(purple_purity):
             res['purity'] = float(arr[header.index('purity')])
             res['ploidy'] = float(arr[header.index('ploidy')])
     return res
-def transformRatioFile(cobalt, sampleId):
+def transformRatioFile(cobalt, sampleId,geneLocation):
 ## xx
     ft=open('transformed_data/'+sampleId+'.transformed.cnr','w')
     first = True
@@ -63,11 +80,11 @@ def transformRatioFile(cobalt, sampleId):
                 header= lines.split(tsvSplit)
                 header.append("end")
                 first = False
-                ft.write(lines+tsvSplit+'start' + tsvSplit + 'end' + tsvSplit + 'log2\n')
+                ft.write(lines+tsvSplit+'start' + tsvSplit + 'end' + tsvSplit + 'log2' + tsvSplit+'gene\n')
                 continue
             else:
-                data = lines.split(tsvSplit)
                 lines = lines[3:]
+                data = lines.split(tsvSplit)
                 start = data[header.index('position')]
                 tumorGCRatio = float(data[header.index('tumorGCRatio')])
                 if tumorGCRatio < 0:
@@ -75,10 +92,17 @@ def transformRatioFile(cobalt, sampleId):
                 else:
                     logR = math.log2(tumorGCRatio)
                 end = str(int (start) + binSize -1)
-                ft.write(lines+tsvSplit+start+tsvSplit+end+tsvSplit+str(logR)+'\n')
-
-def transformGeneFile(purple_gene, sampleId):
+                chr = data[0]
+                gene = [geneLoc.gene for geneLoc in geneLocation if geneLoc.chr==data[0] and overlaps([geneLoc.start,geneLoc.end],[int(start),int(end)])]
+                if gene:
+                    ft.write(lines+tsvSplit+start+tsvSplit+end+tsvSplit+str(logR)+tsvSplit+';'.join(gene)+'\n')
+                else:
+                    ft.write(lines+tsvSplit+start+tsvSplit+end+tsvSplit+str(logR)+tsvSplit+'NA'+'\n')
+def transformGeneFile(purple_gene, sampleId, panelGenes):
     first=True
+    geneLocation = []
+
+
     ft=open('transformed_data/'+sampleId+'.transformed.genemetrics.cns','w')
     with open(purple_gene,'rt') as fh:
         for lines in fh:
@@ -86,13 +110,19 @@ def transformGeneFile(purple_gene, sampleId):
             if first == True:
                 ft.write(lines+tsvSplit+'log2\n')
                 first = False
+                continue
             lines = lines[3:]
             arr = lines.split(tsvSplit)
+            if arr[3] in panelGenes:
+                geneLocation.append(geneLoc(arr[3],arr[0],int(arr[1]),int(arr[2])))
+
             if arr[3] in AmpsDels:
                 if AmpsDels[arr[3]] == 'DEL':
-                    ft.write(lines+tsvSplit+'-1\n') 
+                    ft.write(lines+tsvSplit+'-1\n')
                 else:
                     ft.write(lines+tsvSplit+'1\n')
+    return geneLocation
+
 def genVCFfile(amber, sampleId):
 #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  SampleName
     ft = open('transformed_data/'+sampleId+'.transformed.vcf','w')
@@ -130,13 +160,29 @@ def genVCFfile(amber, sampleId):
             SampleName = 'NA'
             ft.write(CHROM+tsvSplit+POS + tsvSplit+ID+tsvSplit+REF+tsvSplit+ALT+tsvSplit+QUAL+tsvSplit+FILTER+tsvSplit+INFO+tsvSplit+FORMAT+tsvSplit+SampleName+'\n')
 
-def transformSegmentedFile(purple_segmented,purple_purity, sampleId):
+def getNormCorrection(cobalt_segmented):
+    nProbes = []
+    sValues = []
+    first = True
+    with open(cobalt_segmented,'r') as fh:
+        for lines in fh:
+            lines = lines.rstrip('\n')
+            if first == True:
+                header = lines.split(tsvSplit)
+                first = False
+                continue
+            arr = lines.split(tsvSplit)
+            nProbes.append(float(arr[header.index('n.probes')]))
+            sValues.append(float(arr[header.index('mean')]))
+    return weightedAverage(sValues,nProbes)
+def transformSegmentedFile(purple_segmented,purple_purity, sampleId, normCorrection):
 
 # xx
 #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  SampleName
     ft=open('transformed_data/' + sampleId + '.transformed.cns','w')
     first = True
     header=[]
+
     with open(purple_segmented,'r') as fh:
         for lines in fh:
             lines=lines.rstrip('\n')
@@ -159,50 +205,61 @@ def transformSegmentedFile(purple_segmented,purple_purity, sampleId):
             purity = res['purity']
             ploidy = res['ploidy']
             copyNumber = float(arr[header.index('copyNumber')])
-            FClog = math.log2((purity * copyNumber +2*(1-purity))/(purity*ploidy+2*(1-purity)))
+            FClog = math.log2((purity * copyNumber +2*(1-purity))/(purity*ploidy+2*(1-purity)))+normCorrection
 
             arr.append(str(FClog))
             ft.write(tsvSplit.join(arr)+'\n')
+    ft.close()
+
+def getPanelGenes(panel_file):
+    panelGenes = dict()
+    with open (panel_file,'r') as fh:
+        for lines in fh:
+            lines=lines.rstrip("\n")
+            panelGenes[lines]=1
+    return(panelGenes)
 
 @dataclass(frozen=True)
 class Config:
     cobaltRatio: str
+    cobaltSegmented: str
     amber: str
     purpledriverCatalog: str
     purpleGene: str
     purpleSomatic: str
     purplePurity: str
     sampleId: str
+    panelGenes: str
 
 def parse_args(sys_args):
     parser = argparse.ArgumentParser(prog="TransformData", description="Transform hmftools output to input for reconCNV")
 
     parser.add_argument("--cobaltRatio", "-r", type=str, required=True,  help="ratio file - ends with cobalt.ratio.tsv.gz")
+    parser.add_argument("--cobaltSegmented","-c",type=str,required=True, help="cobalt segment file -ends with cobalt.ratio.pcf")
     parser.add_argument("--amber", "-a", type=str, required=True, help="amber file - end with amber.baf.tsv.gz")
     parser.add_argument("--purpledriverCatalog", "-d", type=str, required=True, help="driver catalog - ends with driver.catalog.somatic.tsv")
     parser.add_argument("--purpleGene", "-g", type=str, required=True, help="gene file - ends with cnv.gene.tsv")
     parser.add_argument("--purpleSomatic", "-s", type=str, required=True, help="somatic file - ends with cnv.somatic.tsv")
     parser.add_argument("--purplePurity", "-p", type=str, required=True, help="purity file - ends with purple.purity.tsv")
     parser.add_argument("--sampleId","-i", type = str, required =True, help="sample Id - used for output file names")
+    parser.add_argument("--panelGenes","-t", type = str, required = True, help="panel genes - used to annotate genes")
     args = parser.parse_args(sys_args)
-    return Config(args.cobaltRatio, args.amber, args.purpledriverCatalog, args.purpleGene, args.purpleSomatic, args.purplePurity, args.sampleId)
+    return Config(args.cobaltRatio, args.cobaltSegmented, args.amber, args.purpledriverCatalog, args.purpleGene, args.purpleSomatic, args.purplePurity, args.sampleId, args.panelGenes)
 
 
 def main(args):
     print(args)
     sampleId=args.sampleId
-
-    transformRatioFile(args.cobaltRatio, sampleId)
+    panelGenes = getPanelGenes(args.panelGenes)
+    normCorrection=getNormCorrection(args.cobaltSegmented)
+    geneLocation=transformGeneFile(args.purpleGene,sampleId, panelGenes)
+    transformRatioFile(args.cobaltRatio, sampleId, geneLocation)
     genVCFfile(args.amber, sampleId)
     readDriverCatalog(args.purpledriverCatalog)
-    transformGeneFile(args.purpleGene, sampleId)
-    transformSegmentedFile(args.purpleSomatic,args.purplePurity,sampleId)
+    transformSegmentedFile(args.purpleSomatic,args.purplePurity,sampleId,normCorrection)
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s - [%(levelname)-8s] - %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
     )
 
     main(parse_args(sys.argv[1:]))
-
-
-
