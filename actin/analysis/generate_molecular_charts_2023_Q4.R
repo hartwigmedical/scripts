4dbba7e60e25dba05d7833fb21286db7bc6c9704
Simon de Leeuw <s.deleeuw@hartwigmedicalfoundation.nl>
2024-01-03 15:59:13 +0100

    modified:   actin/analysis/generate_molecular_charts_2023_Q4.R

--- actin/analysis/generate_molecular_charts_2023_Q4.R
+++ actin/analysis/generate_molecular_charts_2023_Q4.R
@@ -1,4 +1,4 @@
-wd <- paste0(Sys.getenv("HOME"), "/hmf/R/2023/Q4/")
+wd <- paste0(Sys.getenv("HOME"), "/hmf/tmp/")
 library(tidyr)
 library(dplyr)
 library(tidyverse)
@@ -9,6 +9,7 @@ library(DBI)
 library(RMySQL)
 dbConnect <- dbConnect(MySQL(), dbname='actin_pilot', groups="RAnalysis")
 
+## Count of Altered Genes in entire cohort (Reportable)
 queryGeneCount <- "select 
 gene, 
 count(distinct m.patientId) as gene_count,
@@ -21,11 +22,12 @@ where containsTumorCells and isReportable and primaryTumorSubLocation is not nul
 group by gene"
 queryGeneCountResult <- dbGetQuery(dbConnect, queryGeneCount)
 
+## Count of Altered Genes in CUP Cases (Reportable)
 queryGeneCountCUP <- "select 
 gene, 
+count(distinct case when driverLikelihood = 'High' then m.patientId end) as high_driver_likelihood_count,
 count(distinct m.patientId) as gene_count,
-count(distinct case when isHotspot then m.patientId end) as hotspot_count,
-count(distinct case when driverLikelihood = 'High' then m.patientId end) as high_driver_likelihood_count
+count(distinct case when isHotspot then m.patientId end) as hotspot_count
 from tumor t 
 inner join molecular m on m.patientId=t.patientId 
 left join variant v on v.sampleId=m.sampleId
@@ -33,11 +35,12 @@ where containsTumorCells and isReportable and primaryTumorSubLocation='CUP'
 group by gene"
 queryGeneCountCUPResult <- dbGetQuery(dbConnect, queryGeneCountCUP)
 
+## Count of Altered Genes in LR Cases (Reportable)
 queryGeneCountLR <- "select
 gene, 
+count(distinct case when driverLikelihood = 'High' then m.patientId end) as high_driver_likelihood_count,
 count(distinct m.patientId) as gene_count,
-count(distinct case when isHotspot then m.patientId end) as hotspot_count,
-count(distinct case when driverLikelihood = 'High' then m.patientId end) as high_driver_likelihood_count
+count(distinct case when isHotspot then m.patientId end) as hotspot_count
 from tumor t 
 inner join molecular m on m.patientId=t.patientId 
 left join variant v on v.sampleId=m.sampleId
@@ -45,50 +48,174 @@ where containsTumorCells and isReportable and primaryTumorSubLocation != 'CUP' a
 group by gene"
 queryGeneCountLRResult <- dbGetQuery(dbConnect, queryGeneCountLR)
 
-queryTMLCount <- "select hasHighTumorMutationalLoad, count(distinct sampleId) as TML_count
+## Overview of Copy Number alterations - CUP
+queryCopyNumberCUP <- "select 
+event, count(distinct m.patientId) as Count
+from tumor t 
+inner join molecular m on m.patientId=t.patientId 
+left join copyNumber c on c.sampleId=m.sampleId
+where containsTumorCells and isReportable and primaryTumorSubLocation = 'CUP'
+group by event;"
+queryCopyNumberCUPResult <- dbGetQuery(dbConnect, queryCopyNumberCUP)
+
+## Overview of Copy Number alterations - LR
+queryCopyNumberLR <- "select 
+event, count(distinct m.patientId) as Count
+from tumor t 
+inner join molecular m on m.patientId=t.patientId 
+left join copyNumber c on c.sampleId=m.sampleId
+where containsTumorCells and isReportable and primaryTumorSubLocation != 'CUP' and primaryTumorSubLocation is not null
+group by event;"
+queryCopyNumberLRResult <- dbGetQuery(dbConnect, queryCopyNumberLR)
+
+## Overview of variant treatment evidence in CUP Cases (Reportable and High driver)
+queryTreatmentCountCUP <- "select 
+treatment, count(distinct m.patientId) as treatment_count
+from variant v
+inner join variantEvidence e on e.variantId=v.id 
+inner join molecular m on m.sampleId=v.sampleId
+inner join tumor t on t.patientId = m.patientId
+where containsTumorCells and isReportable and driverLikelihood = 'High' and primaryTumorSubLocation = 'CUP'
+group by treatment"
+queryTreatmentCountCUPResult <- dbGetQuery(dbConnect, queryTreatmentCountCUP)
+
+## Overview of variant treatment evidence in LR Cases (Reportable and High driver)
+queryTreatmentCountLR <- "select 
+treatment, count(distinct m.patientId) as treatment_count
+from variant v
+inner join variantEvidence e on e.variantId=v.id 
+inner join molecular m on m.sampleId=v.sampleId
+inner join tumor t on t.patientId = m.patientId
+where containsTumorCells and isReportable and driverLikelihood = 'High' and primaryTumorSubLocation != 'CUP' and primaryTumorSubLocation is not null
+group by treatment"
+queryTreatmentCountLRResult <- dbGetQuery(dbConnect, queryTreatmentCountLR)
+
+## Average amount of high driver likelihood variants per sampleId 
+queryHighDriverPerSample <- "select 
+round(avg(highDriverCount), 1) as average_variant_per_sampleId
+from (
+    select sampleId, count(sampleId) as highDriverCount
+    from variant 
+    where driverLikelihood = 'High'
+    group by sampleId
+) as subquery
+"
+queryHighDriverPerSampleResult <- dbGetQuery(dbConnect, queryHighDriverPerSample)
+
+## Average amount of variant treatment evidence (for high driver likelihood variants) per sampleId
+queryVariantEvidencePerSample <- "
+select round(avg(variantEvidenceCount), 1) as average_evidence_per_sampleId
+    from ( 
+		select sampleId, count(distinct treatment) as variantEvidenceCount
+		from variant v
+		inner join variantEvidence e on e.variantId = v.id
+		where driverLikelihood = 'High'
+		group by sampleId
+	) as subquery
+"
+queryVariantEvidencePerSampleResult <- dbGetQuery(dbConnect, queryVariantEvidencePerSample)
+
+## Tumor mutational load - entire cohort
+queryTMLCount <- "select hasHighTumorMutationalLoad, count(distinct patientId) as TML_count
 from molecular
+where containsTumorCells
 group by hasHighTumorMutationalLoad"
 queryTMLCountResult <- dbGetQuery(dbConnect, queryTMLCount)
 
-queryTMBCount <- "select hasHighTumorMutationalBurden, count(distinct sampleId) as TMB_count
+
+## Tumor mutational burden - entire cohort
+queryTMBCount <- "select hasHighTumorMutationalBurden, count(distinct patientId) as TMB_count
 from molecular
+where containsTumorCells
 group by hasHighTumorMutationalBurden"
 queryTMBCountResult <- dbGetQuery(dbConnect, queryTMBCount)
 
-queryHRDCount <- "select isHomologousRepairDeficient, count(distinct sampleId) as HRD_count
+
+## Tumor homologous repair deficiency - entire cohort
+queryHRDCount <- "select isHomologousRepairDeficient, count(distinct patientId) as HRD_count
 from molecular
+where containsTumorCells
 group by isHomologousRepairDeficient"
 queryHRDCountResult <- dbGetQuery(dbConnect, queryHRDCount)
 
+## Tumor purity overview
 queryPurity <- "select purity from molecular"
 queryPurityResult <- dbGetQuery(dbConnect, queryPurity)
 
 dbDisconnect(dbConnect)
-View(queryGeneCountResult)
-View(queryGeneCountCUPResult)
-View(queryGeneCountLRResult)
-View(queryTMLCountResult)
-View(queryTMBCountResult)
-View(queryHRDCountResult)
-View(queryPurityResult)
-
-# Predominantly Altered Genes in Last Resort Cases (Reportable) ----------------
-LR_sorted <- queryGeneCountLRResult %>% arrange(desc(gene_count))
-LR_gene_10 <- head(LR_sorted, 10)
-
-pdf(file= paste0(wd,"Genes_LR_Reportable.pdf"), width = 10, height = 7)
-LR_table <- tableGrob(LR_gene_10, rows = NULL)
-grid.draw(LR_table)
-invisible(dev.off())
+#View(queryGeneCountResult)
+#View(queryGeneCountCUPResult)
+#View(queryGeneCountLRResult)
+#View(queryCopyNumberCUPResult)
+#View(queryCopyNumberLRResult)
+#View(queryTreatmentCountCUPResult)
+#View(queryTreatmentCountLRResult)
+#View(queryHighDriverPerSampleResult)
+#View(queryVariantEvidencePerSampleResult)
+#View(queryTMLCountResult)
+#View(queryTMBCountResult)
+#View(queryHRDCountResult)
+#View(queryPurityResult)
 
 # Predominantly Altered Genes in CUP Cases (Reportable) -----------------------
-CUP_sorted <- queryGeneCountCUPResult %>% arrange(desc(gene_count))
-CUP_gene_10 <- head(CUP_sorted, 10)
+CUP_gene_sorted <- queryGeneCountCUPResult %>% arrange(desc(high_driver_likelihood_count)) %>% rename("Gene" = gene, "Gene Count" = gene_count, "Hotspot Count" = hotspot_count, "High Driver Likelihood Count" = high_driver_likelihood_count)
+CUP_gene_10 <- head(CUP_gene_sorted, 10)
 
 pdf(file= paste0(wd,"Genes_CUP_Reportable.pdf"), width = 10, height = 7)
-CUP_table <- tableGrob(CUP_gene_10, rows = NULL)
-grid.draw(CUP_table)
+CUP_gene_table <- tableGrob(CUP_gene_10, rows = NULL)
+grid.draw(CUP_gene_table)
 invisible(dev.off())
+
+# Predominantly Altered Genes in Last Resort Cases (Reportable) ----------------
+LR_gene_sorted <- queryGeneCountLRResult %>% arrange(desc(high_driver_likelihood_count)) %>% rename("Gene" = gene, "Gene Count" = gene_count, "Hotspot Count" = hotspot_count, "High Driver Likelihood Count" = high_driver_likelihood_count)
+LR_gene_10 <- head(LR_gene_sorted, 10)
+
+pdf(file= paste0(wd,"Genes_LR_Reportable.pdf"), width = 10, height = 7)
+LR_gene_table <- tableGrob(LR_gene_10, rows = NULL)
+grid.draw(LR_gene_table)
+invisible(dev.off())
+
+# Predominant Copy Number Alterations in CUP Cases (Reportable) ----------------
+CUP_CN_sorted <- queryCopyNumberCUPResult %>% arrange(desc(Count)) %>% rename("Event" = event)
+CUP_CN_10 <- head(CUP_CN_sorted, 10)
+
+pdf(file= paste0(wd,"Copy_Number_CUP.pdf"), width = 10, height = 7)
+CUP_CN_table <- tableGrob(CUP_CN_10, rows = NULL)
+grid.draw(CUP_CN_table)
+invisible(dev.off())
+
+# Predominantly Copy Number Alterations in Last Resort Cases (Reportable) ----------------
+LR_CN_sorted <- queryCopyNumberLRResult %>% arrange(desc(Count)) %>% rename("Event" = event)
+LR_CN_10 <- head(LR_CN_sorted, 10)
+
+pdf(file= paste0(wd,"Copy_Number_LR.pdf"), width = 10, height = 7)
+LR_CN_table <- tableGrob(LR_CN_10, rows = NULL)
+grid.draw(LR_CN_table)
+invisible(dev.off())
+
+# Most frequent variant treatment evidence in CUP Cases (Reportable and High driver) ----------------
+CUP_treatment_sorted <- queryTreatmentCountCUPResult %>% arrange(desc(treatment_count)) %>% rename("Treatment" = treatment, "Treatment Count" = treatment_count)
+CUP_treatment_10 <- head(CUP_treatment_sorted, 10)
+
+pdf(file= paste0(wd,"Variant_treatment_Evidence_CUP.pdf"), width = 10, height = 7)
+CUP_treatment_table <- tableGrob(CUP_treatment_10, rows = NULL)
+grid.draw(CUP_treatment_table)
+invisible(dev.off())
+
+# Most frequent variant treatment evidence in Last Resort Cases (Reportable and High driver) ----------------
+LR_treatment_sorted <- queryTreatmentCountLRResult %>% arrange(desc(treatment_count)) %>% rename("Treatment" = treatment, "Treatment Count" = treatment_count)
+LR_treatment_10 <- head(LR_treatment_sorted, 10)
+
+pdf(file= paste0(wd,"Variant_treatment_Evidence_LR.pdf"), width = 10, height = 7)
+LR_treatment_table <- tableGrob(LR_treatment_10, rows = NULL)
+grid.draw(LR_treatment_table)
+invisible(dev.off())
+
+## Average amount of high driver likelihood variants per sampleId 
+queryHighDriverPerSampleResult
+
+## Average amount of variant treatment evidence (for high driver likelihood variants) per sampleId
+queryVariantEvidencePerSampleResult
 
 # Tumor Mutational Load - entire cohort ----------------------------------------------
 TMLhigh <- queryTMLCountResult$TML_count[queryTMLCountResult$hasHighTumorMutationalLoad==1]
@@ -100,7 +227,7 @@ lbls <- c(paste0("Yes: ", pct[1], "% (N=", TMLhigh, ")"),
           paste0("No: ", pct[2], "% (N=", TMLlow, ")"))
 
 pdf(file= paste0(wd,"Tumor_Mutational_Load.pdf"), width = 10, height = 7)
-pie(slices, labels = lbls, col=c("red","blue"),main="High Tumor Mutational Load")
+pie(slices, labels = lbls, col=c("green4","red"),main="High Tumor Mutational Load")
 invisible(dev.off())
 
 noquote(paste0(pct[1], "% of patients had a high tumor mutational load"))
@@ -115,7 +242,7 @@ lbls <- c(paste0("Yes: ", pct[1], "% (N=", TMBhigh, ")"),
           paste0("No: ", pct[2], "% (N=", TMBlow, ")"))
 
 pdf(file= paste0(wd,"Tumor_Mutational_Burden.pdf"), width = 10, height = 7)
-pie(slices, labels = lbls, col=c("red","blue"),main="High Tumor Mutational Burden")
+pie(slices, labels = lbls, col=c("green4","red"),main="High Tumor Mutational Burden")
 invisible(dev.off())
 
 noquote(paste0(pct[1], "% of patients had a high tumor mutational burden"))
@@ -125,17 +252,16 @@ HRD <- na.omit(queryHRDCountResult$HRD_count[queryHRDCountResult$isHomologousRep
 HRP <- na.omit(queryHRDCountResult$HRD_count[queryHRDCountResult$isHomologousRepairDeficient==0])
 Undetermined <- queryHRDCountResult$HRD_count[is.na(queryHRDCountResult$isHomologousRepairDeficient)]
 
-slices <- c(HRD, HRP)
-lbls <- c("Yes:", "No:", "Undetermined:")
+slices <- c(HRD, HRP, Undetermined)
+lbls <- c("Yes", "No", "NA")
 pct <- round(slices/sum(slices)*100, 1)
-lbls <- c(paste0("Yes: ", pct[1], "% (N=", HRD, ")"),
-          paste0("No: ", pct[2], "% (N=", HRP, ")"))
+lbls <- paste0(lbls, ": ", pct, "% (N=", slices, ")")
 
 pdf(file= paste0(wd,"Homologous_Repair_Deficiency.pdf"), width = 10, height = 7)
-pie(slices, labels = lbls, col=c("red","blue"),main="Homologous Repair Deficiency")
+pie(slices, labels = lbls, col=c("green4","red", "gray"),main="Homologous Repair Deficiency")
 invisible(dev.off())
 
-noquote(paste0(pct[1], "% of patients were homologous repair deficient (NA for n=", Undetermined,")"))
+noquote(paste0(pct[1], "% of patients were homologous repair deficient"))
 
 # Tumor purity distribution - entrire cohort ----------------------------------------------
 pdf(file= paste0(wd,"Tumor_Purity.pdf"), width = 10, height = 7)
@@ -154,3 +280,4 @@ ggplot(data, aes(x = purity)) +
 invisible(dev.off())
 
 noquote(paste0("Median Tumor purity ", median(data$purity), "% (IQR: ", IQR(data$purity),")"))
+
