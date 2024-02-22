aae32014dfc331e800166c847db79cd9211ede4c
sandravandenbroek <vandenbroek.alexandra@gmail.com>
2024-02-22 15:12:03 +0100

    update scripts for creating datarequest views

--- sql/views/create_view_datarequest_with_CB01.sql
+++ sql/views/create_view_datarequest_with_CB01.sql
@@ -1,14 +1,3 @@
 CREATE OR REPLACE VIEW datarequest_CB01 AS
 
-SELECT
-	clinical_purity.*, metric.sufficientCoverage AS sufficientCoverage
-FROM
-	clinical_purity
-	INNER JOIN metric ON clinical_purity.sampleId = metric.sampleId
-    INNER JOIN consentsLAMA ON clinical_purity.sampleId = consentsLAMA.sampleId
-WHERE
-	blacklisted = 0
-	AND purpleQC = 'PASS'
-	AND metric.sufficientCoverage = 1
-	AND consentsLAMA.allowExternalUseWithCheck = 'true'
-	;
+SELECT * FROM hmfpatients.datarequest_all where allowExternalUseWithCheck = 'true';
