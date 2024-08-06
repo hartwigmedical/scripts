77e5e452cca3c38a82af0de0f3a4495f15da9c22
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-08-06 16:16:02 +0200

    Update analysis script (WIP)

--- actin/analysis/actin_wgts_in_last_resort_analysis.R
+++ actin/analysis/actin_wgts_in_last_resort_analysis.R
@@ -1,28 +1,31 @@
 library(dplyr)
 library(tibble)
+library(RMySQL)
 
 rm(list=ls())
 
 # Retrieve data ------------------------------------------------------------------
-dbActin <- dbConnect(MySQL(), dbname='actin_pilot', groups="RAnalysis")
+dbActin <- dbConnect(MySQL(), dbname='actin_paper', groups="RAnalysis")
 
-query_sample <-"select * from actinLastResortPaperSamples;"
-query_patient <-"select * from patient where patientId in (select patientId from actinLastResortPaperSamples);"
-query_clinical_status <-"select * from clinicalStatus where patientId in (select patientId from actinLastResortPaperSamples);"
-query_molecular <-"select * from molecular where sampleId in (select sampleId from actinLastResortPaperSamples);"
-query_tumor <-"select * from tumor where patientId in (select patientId from actinLastResortPaperSamples);"
-query_drivers <- "select distinct patientId, trialId, inclusionMolecularEvents from trialEvaluation where inclusionMolecularEvents not in ('') and isEligibleTrial and isEligibleCohort and patientId in (select patientId from actinLastResortPaperSamples);"
-query_drivers_2 <- "select gene, count(distinct sampleId) from hmfpatients.driverCatalog where driverLikelihood>0.8 and sampleId in (select sampleId from actinLastResortPaperSamples) and sampleId in (select sampleId from hmfpatients.purity where qcStatus not like '%FAIL%') group by 1;"
-query_drivers_3 <- "select sampleId, count(*) from hmfpatients.driverCatalog where driverLikelihood>0.8 and sampleId in (select sampleId from actinLastResortPaperSamples) and sampleId in (select sampleId from hmfpatients.purity where qcStatus not like '%FAIL%') group by 1;"
+query_sample <-"select * from paperSamples;"
+query_patient <-"select * from patient;"
+query_clinical_status <-"select * from clinicalStatus;"
+query_tumor <-"select * from tumor;"
+query_eligibleCohorts <-"select * from eligibleCohorts union select * from eligibleCohorts_addition;"
+query_molecular <-"select * from molecular;"
+query_drivers <- "select * from molecularDrivers;"
+query_drivers_per_sample <- "select m.sampleId, count(md.driverLikelihood) as highDriverCount from molecular m left join molecularDrivers md on m.sampleId=md.sampleId and driverlikelihood='high' where containsTumorCells=1 group by 1;"
+query_drivers_gene_count <- "select gene, count(distinct sampleId) as sampleCount from molecularDrivers where driverLikelihood='High' and sampleId in (select sampleId from molecular where containsTumorCells=1) group by 1 order by 2 desc;"
 
 sample <- dbGetQuery(dbActin, query_sample)
 patient <- dbGetQuery(dbActin, query_patient)
 clinicalStatus <- dbGetQuery(dbActin, query_clinical_status)
-molecular <- dbGetQuery(dbActin, query_molecular)
 tumor <- dbGetQuery(dbActin, query_tumor)
+eligibleCohorts <- dbGetQuery(dbActin, query_eligibleCohorts)
+molecular <- dbGetQuery(dbActin, query_molecular)
 drivers <- dbGetQuery(dbActin, query_drivers)
-drivers_2 <- dbGetQuery(dbActin, query_drivers_2)
-drivers_3 <- dbGetQuery(dbActin, query_drivers_3)
+drivers_per_sample <- dbGetQuery(dbActin, query_drivers_per_sample)
+drivers_gene_count <- dbGetQuery(dbActin, query_drivers_gene_count)
 
 dbDisconnect(dbActin)
 
@@ -50,7 +53,17 @@ patient %>% group_by(gender) %>% summarise(count=n())
 patient %>% group_by(birthYear) %>% summarise(count=n())
 clinicalStatus %>% group_by(who) %>% summarise(count=n())
 
-# drivers WIP
-driverCuration <- read.csv(paste0(Sys.getenv("HOME"), "/hmf/tmp/Curation - Drivers.csv"), sep = ",")
-driver <- inner_join(drivers, driverCuration, by=c('inclusionMolecularEvents'))
-arranged <- driver %>% group_by(inclusionMolecularEventsCurated) %>% summarise(count=n_distinct(patientId))
+## DRIVERS - WIP
+#driverCuration <- read.csv(paste0(Sys.getenv("HOME"), "/hmf/tmp/Curation - Drivers.csv"), sep = ",")
+#driver <- inner_join(drivers, driverCuration, by=c('inclusionMolecularEvents'))
+#arranged <- driver %>% group_by(inclusionMolecularEventsCurated) %>% summarise(count=n_distinct(patientId))
+
+# Driver events per sample
+median(drivers_per_sample$highDriverCount)
+min(drivers_per_sample$highDriverCount)
+max(drivers_per_sample$highDriverCount)
+drivers_per_sample %>% dplyr::filter(highDriverCount>0) %>% nrow()
+
+# Recurring genes in driver events (excluding fusions and viruses)
+sampleCount <- nrow(drivers_per_sample)
+head(drivers_gene_count %>% add_column(percentage = round(drivers_gene_count$sampleCount/sampleCount*100,1)),3)
