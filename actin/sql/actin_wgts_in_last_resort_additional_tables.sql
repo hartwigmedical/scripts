49b678cd9948e9f2c36098b0292a1619f55b207c
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2024-08-06 16:14:17 +0200

    Add view for ACTIN/WGTS in LR analysis

--- actin/sql/actin_wgts_in_last_resort_additional_tables.sql
+++ actin/sql/actin_wgts_in_last_resort_additional_tables.sql
@@ -14,3 +14,33 @@ CREATE TABLE paperSamples (
 sampleId varchar(50) NOT NULL,
 patientId varchar(50) NOT NULL
 );
+
+CREATE OR REPLACE VIEW molecularDrivers
+AS (
+SELECT * FROM (
+	SELECT sampleId, event, gene, driverLikelihood, "a_variant" as category
+    FROM variant
+    WHERE isReportable
+		UNION
+	SELECT sampleId, event, gene, driverLikelihood, "b_copy_number" as category
+    FROM copyNumber
+    WHERE isReportable
+		UNION
+	SELECT sampleId, event, gene, driverLikelihood, "e_hom_disruptions" as category
+    FROM homozygousDisruption
+    WHERE isReportable
+		UNION
+	SELECT sampleId, event, gene, driverLikelihood, "f_het_disruptions" as category
+    FROM disruption
+    WHERE isReportable
+		UNION
+	SELECT sampleId, event, "NA" as gene, driverLikelihood, "c_fusions" as category
+    FROM fusion
+    WHERE isReportable
+		UNION
+	SELECT sampleId, event, "NA" as gene, driverLikelihood, "d_viruses" as category
+    FROM virus
+    WHERE isReportable)
+    AS a
+ORDER BY 1,4,3,2
+);
