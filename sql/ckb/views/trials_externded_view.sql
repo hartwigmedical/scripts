eba15fc408d24cf0eac10f3b88e9b44a430d1950
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2024-09-02 16:03:54 +0200

    Update trial extended view

--- sql/ckb/views/trials_externded_view.sql
+++ sql/ckb/views/trials_externded_view.sql
@@ -3,15 +3,13 @@ CREATE OR REPLACE VIEW trialsExtended AS (
 SELECT DISTINCT
         clinicalTrial.nctId, clinicalTrial.title, clinicalTrial.phase, clinicalTrial.recruitment, clinicalTrial.gender, group_concat(distinct ageGroup.ageGroup) as ageGroups,
         clinicalTrial.variantRequirement, requirementType, profileName AS tumorProfile,
-		therapyName,indication.name as cancerType, group_concat(distinct drugClass) as drugClasses
+		therapyName,indication.name as cancerType
     FROM ckbEntry
     INNER JOIN clinicalTrial ON clinicalTrial.ckbEntryId = ckbEntry.id
-    INNER JOIN variantRequirementDetail ON variantRequirementDetail.clinicalTrialId = clinicalTrial.id
+    INNER JOIN variantRequirementDetail ON variantRequirementDetail.clinicalTrialId = clinicalTrial.id and variantRequirementDetail.ckbProfileId = ckbEntry.ckbProfileId
     INNER JOIN ageGroup ON ageGroup.clinicalTrialId = clinicalTrial.id
     INNER JOIN therapyClinicalTrial ON therapyClinicalTrial.clinicalTrialId = clinicalTrial.id
     INNER JOIN therapy ON therapyClinicalTrial.therapyId = therapy.id
     INNER JOIN indicationClinicalTrial ON indicationClinicalTrial.clinicalTrialId = clinicalTrial.id
     INNER JOIN indication ON indicationClinicalTrial.indicationId = indication.id
-    LEFT JOIN treatmentApproachEvidence ON treatmentApproachEvidence.evidenceId = clinicalTrial.id
-    LEFT JOIN treatmentApproachDrugClass ON treatmentApproachDrugClass.treatmentApproachId=treatmentApproachEvidence.treatmentApproachEvidenceId
-    group by clinicalTrial.nctId, ckbEntry.ckbProfileId, therapyClinicalTrial.therapyId, indicationClinicalTrial.indicationId);
+    group by clinicalTrial.nctId, ckbEntry.ckbProfileId, therapyClinicalTrial.therapyId, indicationClinicalTrial.indicationId
