d2b0fdf03ad9a0668d8e60429ec3d02c9de6f7dd
kzagranovskyi-hw <k.zagranovskyi@hartwigmedicalfoundation.nl>
2024-09-16 12:39:32 +0200

    INFRA-780 fixed filtering issue

--- oncoact/patientreporter/reporting_pipeline/rest_util.py
+++ oncoact/patientreporter/reporting_pipeline/rest_util.py
@@ -28,7 +28,7 @@ class RestClient:
 
         :param lookback_days the amount of days to look back for the report creation time.
         """
-        from_date = datetime.today().date() - timedelta(days=lookback_days)
+        from_date = (datetime.today().date() - timedelta(days=lookback_days)).strftime('%Y-%m-%dT%H:%M:%S')
         return _get_as_json(url=self._report_created_url, params={'from_report_create_time': from_date})
 
     def get_report_created(self, sample_barcode):
