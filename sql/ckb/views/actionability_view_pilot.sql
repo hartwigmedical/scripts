8d67a0a597fd560a7b30299e5d2f18489b440cac
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2024-10-23 09:07:41 +0200

    OPR-158: Update actionability view of ckb database

--- sql/ckb/views/actionability_view_pilot.sql
+++ sql/ckb/views/actionability_view_pilot.sql
@@ -2,9 +2,11 @@ CREATE OR REPLACE VIEW actionability AS (
 
 SELECT
     ckbEntry.id as ckbEntryId, ckbEntry.ckbProfileId, profileName AS tumorProfile,
-    therapyEvidence.therapyId, therapy.ckbTherapyId, therapy.createDate as therapyCreateDate, therapy.updateDate as therapyUpdateDate, therapyName AS treatment, description,
+    therapyEvidence.therapyId, therapy.ckbTherapyId, therapy.createDate as therapyCreateDate, therapy.updateDate as therapyUpdateDate,
+    therapy.therapyName AS treatment, description,
     indicationEvidence.indicationId, indication.ckbIndicationId, termId AS cancerTypeId, name AS cancerType,
     treatmentApproachEvidence.treatmentApproachEvidenceId, treatmentApproachDrugClass.treatmentApproachId, group_concat(drugClass) as drugClasses,
+    group_concat(treatmentApproachTherapy.therapyName) as therapyNames,
     evidence.ckbEvidenceId, evidenceType, responseType, ampCapAscoEvidenceLevel as level, approvalStatus, efficacyEvidence,
     evidenceReference.ckbReferenceId, group_concat(DISTINCT pubmedId) as pubmeds
 FROM ckbEntry
@@ -16,5 +18,6 @@ INNER JOIN indicationEvidence ON indicationEvidence.evidenceId = evidence.id
 INNER JOIN indication ON indicationEvidence.indicationId = indication.id
 LEFT JOIN treatmentApproachEvidence ON treatmentApproachEvidence.evidenceId = evidence.id
 LEFT JOIN treatmentApproachDrugClass ON treatmentApproachDrugClass.treatmentApproachId=treatmentApproachEvidence.treatmentApproachEvidenceId
+LEFT JOIN treatmentApproachTherapy ON treatmentApproachTherapy.treatmentApproachId=treatmentApproachEvidence.treatmentApproachEvidenceId
 group by ckbEntry.id, ckbEntry.ckbProfileId, therapyEvidence.therapyId, therapy.ckbTherapyId, indicationEvidence.indicationId,
 indication.ckbIndicationId, evidence.ckbEvidenceId);
