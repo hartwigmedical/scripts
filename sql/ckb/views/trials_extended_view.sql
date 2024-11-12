af6289f0128675f7538d69e5ebd0c1efdfd66e77
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-11-12 10:47:22 +0100

    Refactor extended trial view in CKB

--- sql/ckb/views/trials_extended_view.sql
+++ sql/ckb/views/trials_extended_view.sql
@@ -1,15 +1,19 @@
 CREATE OR REPLACE VIEW trialsExtended AS (
 
 SELECT DISTINCT
-        clinicalTrial.nctId, clinicalTrial.title, clinicalTrial.phase, clinicalTrial.recruitment, clinicalTrial.gender, group_concat(distinct ageGroup.ageGroup) as ageGroups,
-        clinicalTrial.variantRequirement, requirementType, profileName AS tumorProfile,
-		therapyName,indication.name as cancerType
-    FROM ckbEntry
-    INNER JOIN clinicalTrial ON clinicalTrial.ckbEntryId = ckbEntry.id
-    INNER JOIN variantRequirementDetail ON variantRequirementDetail.clinicalTrialId = clinicalTrial.id and variantRequirementDetail.ckbProfileId = ckbEntry.ckbProfileId
-    INNER JOIN ageGroup ON ageGroup.clinicalTrialId = clinicalTrial.id
-    INNER JOIN therapyClinicalTrial ON therapyClinicalTrial.clinicalTrialId = clinicalTrial.id
-    INNER JOIN therapy ON therapyClinicalTrial.therapyId = therapy.id
-    INNER JOIN indicationClinicalTrial ON indicationClinicalTrial.clinicalTrialId = clinicalTrial.id
-    INNER JOIN indication ON indicationClinicalTrial.indicationId = indication.id
-    group by clinicalTrial.nctId, ckbEntry.ckbProfileId, therapyClinicalTrial.therapyId, indicationClinicalTrial.indicationId
+        clinicalTrial.nctId, clinicalTrial.title, clinicalTrial.phase, clinicalTrial.recruitment, clinicalTrial.gender,
+        clinicalTrial.variantRequirement, ckbEntry.profileName, variantRequirementDetail.requirementType,
+        group_concat(DISTINCT ageGroup.ageGroup separator ',') AS ageGroups,
+        group_concat(DISTINCT therapy.therapyName separator ',') AS therapyNames,
+        group_concat(DISTINCT indication.name separator ',') AS cancerTypes
+    FROM clinicalTrial
+    INNER JOIN ckbEntry ON clinicalTrial.ckbEntryId = ckbEntry.id
+    INNER JOIN variantRequirementDetail ON
+        variantRequirementDetail.clinicalTrialId = clinicalTrial.id AND variantRequirementDetail.ckbProfileId = ckbEntry.ckbProfileId
+    LEFT JOIN ageGroup ON ageGroup.clinicalTrialId = clinicalTrial.id
+    LEFT JOIN therapyClinicalTrial ON therapyClinicalTrial.clinicalTrialId = clinicalTrial.id
+    LEFT JOIN therapy ON therapyClinicalTrial.therapyId = therapy.id
+    LEFT JOIN indicationClinicalTrial ON indicationClinicalTrial.clinicalTrialId = clinicalTrial.id
+    LEFT JOIN indication ON indicationClinicalTrial.indicationId = indication.id
+    GROUP BY 1,2,3,4,5,6,7,8
+)
