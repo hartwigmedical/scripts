fd75a89a45251fc33d4eb6425509f52ec57be778
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-12-10 13:40:01 +0100

    Remove iclusion views

--- sql/iclusion/view/studyBlacklistedTumorLocations.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW studyBlacklistedTumorLocation AS
-
-select idDB, acronym, title, eudra, nct, ipn, ccmo, blacklistedTumorLocation
-from study
-left join blacklistedTumorLocation
-on study.id= blacklistedTumorLocation.blacklistedTumorLocationId;
--- sql/iclusion/view/studyTumorLocations.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW studyTumorLocation AS
-
-select idDB, acronym, title, eudra, nct, ipn, ccmo, tumorLocation
-from study
-left join tumorLocation
-on study.id= tumorLocation.tumorLocationId;
