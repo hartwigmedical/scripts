bfdc313dc2bc849a30f052871de35d0c8b767371
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2024-12-31 12:27:45 +0100

    Select samples for reporting pipeline verification

--- sql/genomic/select_samples_for_reporting_pipeline_verification.sql
+++ sql/genomic/select_samples_for_reporting_pipeline_verification.sql
@@ -1,23 +1,20 @@
-# low purity + low TMB status
-select modified, sampleId, tmbPerMb, tmbStatus  from purity where tmbStatus = "LOW" and qcStatus = "WARN_LOW_PURITY" order by modified desc;
-
-# high tmb + high msi + somatic mutation
+#1 high tmb + high msi + somatic mutation
 select purity.modified, purity.sampleId, tmbPerMb, tmbStatus, msIndelsPerMb, msStatus from purity
 inner join driverCatalog on purity.sampleId = driverCatalog.sampleId
 where tmbStatus = "HIGH" and msStatus = "MSI" and qcStatus = "PASS" and driver = "MUTATION" order by modified desc;
 
-# hrd + germline mutation
+#2 hrd + germline mutation
 select purity.modified, chord.sampleId, hrStatus, gene, canonicalTranscript, category, driver, likelihoodMethod from chord
 inner join purity on purity.sampleId = chord.sampleId
 inner join driverCatalog on purity.sampleId = driverCatalog.sampleId
 where hrStatus = "HR_DEFICIENT" and driver = "GERMLINE_MUTATION" order by modified desc;
 
-# mss + germline disruption
+#3 mss + germline disruption
 select purity.modified, purity.sampleId, msIndelsPerMb, msStatus, gene, canonicalTranscript, category, driver, likelihoodMethod  from purity
 inner join driverCatalog on purity.sampleId = driverCatalog.sampleId
 where msStatus = "MSS" and driver  = "GERMLINE_DISRUPTION" order by modified desc;
 
-#del, amp, partial amp, disruption, hom_del_disruption
+#4 del, amp, partial amp, disruption, hom_del_disruption
 SELECT t.modified, t.sampleId, t.gene, t.canonicalTranscript, t.category, t.driver, t.likelihoodMethod
 FROM driverCatalog t
 WHERE t.driver IN ('AMP', 'DEL', 'DISRUPTION', 'HOM_DEL_DISRUPTION')
@@ -29,18 +26,18 @@ WHERE t.driver IN ('AMP', 'DEL', 'DISRUPTION', 'HOM_DEL_DISRUPTION')
     HAVING COUNT(DISTINCT driver) = 4
   ) order by t.modified desc;
 
-# germline deletion
+#5 germline deletion
 select *  from  driverCatalog
 inner join chord on driverCatalog.sampleId = chord.sampleId
 where driver  = "GERMLINE_DELETION" and hrStatus = "HR_PROFICIENT" order by modified desc;
 
-#hom dup disruption
+#6 hom dup disruption
 select modified, sampleId,gene, canonicalTranscript, category, driver, likelihoodMethod from driverCatalog where driver = "HOM_DUP_DISRUPTION" order by modified desc;
 
-# virus
+#7 virus
 select modified, sampleId, virusName, qcStatus, integrations, interpretation, likelihood, reported from virusAnnotation where reported and interpretation = "HPV" and likelihood = "HIGH" order by modified desc;
 
-#fusion
+#8 fusion
 SELECT *
 FROM svFusion t
 WHERE t.reportedType IN ('KNOWN_PAIR', 'PROMISCUOUS_3', 'PROMISCUOUS_5', 'EXON_DEL_DUP') and reported
@@ -52,8 +49,19 @@ WHERE t.reportedType IN ('KNOWN_PAIR', 'PROMISCUOUS_3', 'PROMISCUOUS_5', 'EXON_D
     HAVING COUNT(DISTINCT reportedType) = 1
   ) order by modified desc;
 
-# HRD cannot be determined
-select modified, chord.sampleId, hrStatus from chord inner join purity on purity.sampleId = chord.sampleId where hrStatus = "CANNOT_BE_DETERMINED" order by modified desc;
+#9 HRD cannot be determined + # low purity
+select modified, chord.sampleId, hrStatus, qcStatus, purity from chord inner join purity on purity.sampleId = chord.sampleId where hrStatus = "CANNOT_BE_DETERMINED" and qcStatus = "WARN_LOW_PURITY" order by modified desc;
 
-# germline variant without tumor support
+#10 germline variant without tumor support
 select * from germlineVariant where reported and variantCopyNumber <= 0.5 order by modified desc;
+
+#select XX samples for verification specific samples
+
+#select 1 lab failure
+
+#select 1 analyse failure
+select modified, sampleId, qcStatus, purity from purity  where qcStatus = "FAIL_NO_TUMOR" order by modified desc;
+
+#select 1 panel analyse
+
+#select 1 panel lab failure
