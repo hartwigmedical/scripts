e1ed40f5a89f873ac517e606e1b5f0c895b95b88
Iris Stuiver <i.stuiver@hartwigmedicalfoundation.nl>
2025-01-03 15:12:13 +0100

    only show mtap cn info when CDK2NA loss

--- oncoact/patientreporter/reporting_pipeline/check_report_and_run_status_test.py
+++ oncoact/patientreporter/reporting_pipeline/check_report_and_run_status_test.py
@@ -212,9 +212,15 @@ class StatusChecker:
         creds = creds_result.stdout.strip()
         #Construct SQL query
         sql_query = (
-            "SELECT sampleId, gene, minCopyNumber, maxCopyNumber "
-            "FROM geneCopyNumber "
+            "SELECT sampleId, gene, minCopyNumber, maxCopyNumber"
+            "FROM geneCopyNumber"
             f"WHERE sampleId = '{reporting_id}' AND gene = 'MTAP'"
+            "AND EXISTS ("
+            "SELECT 1"
+            "FROM geneCopyNumber AS sub"
+            f"WHERE sub.sampleId = '{reporting_id}'"
+            "AND sub.gene = 'CDKN2A'"
+            "AND (sub.minCopyNumber < 0.5 OR sub.maxCopyNumber < 0.5))"
         )
         query_command = f"do_execute_sql_on_database \"{sql_query}\" hmfpatients '{creds}'"
         output = subprocess.run(query_command, shell=True, text=True, capture_output=True)
