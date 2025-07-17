49fbb8b88ecf8409ab9b2a43d4eb921c379f5d43
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2025-07-17 14:09:57 +0200

    remove flag for purity/ploidy check

--- oncoact/patientreporter/reporting_pipeline/check_report_and_run_status.py
+++ oncoact/patientreporter/reporting_pipeline/check_report_and_run_status.py
@@ -401,7 +401,7 @@ class StatusChecker:
         sql_query = (
             "SELECT sampleId, qcStatus, purity, minPurity,maxPurity, ploidy, minPloidy, maxPloidy, maxPurity-minPurity as range_purity, maxPloidy-minPloidy as range_ploidy "
             "FROM purity "
-            "WHERE sampleId = '{reporting_id}' AND qcStatus NOT LIKE '%FAIL_NO_TUMOR%' AND (maxPurity-minPurity > 0.3 or maxPloidy-minPloidy > 3) "
+            "WHERE sampleId = '{reporting_id}' AND qcStatus NOT LIKE '%FAIL_NO_TUMOR%'"
         )
         query_command = f"do_execute_sql_on_database \"{sql_query}\" hmfpatients '{creds}'"
         output = subprocess.run(query_command, shell=True, text=True, capture_output=True)
