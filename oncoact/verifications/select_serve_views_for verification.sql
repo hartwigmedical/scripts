b5061618b3c99a60ff3da4363bbbc1c2eacffe2f
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2025-07-23 12:24:31 +0200

    extend queries for generating serve output for verification

--- oncoact/verifications/select_serve_views_for verification.sql
+++ oncoact/verifications/select_serve_views_for verification.sql
@@ -6,10 +6,29 @@ from efficacyEvidence
 inner join actionableCharacteristic on actionableCharacteristic.molecularCriteriumId = efficacyEvidence.molecularCriteriumId;
 
 #codons
-#exons
+select  sourceEvent, gene, chromosome, start, end, applicableMutationType, source, sourceUrls, treatment, treatmentApproachesDrugClass, treatmentApproachesTherapy, SUBSTRING_INDEX(indication,"(", 1) as cancerType, REPLACE(SUBSTRING_INDEX(indication,"(", -1),")", "") as doid, evidenceLevel, evidenceDirection
+from efficacyEvidence
+inner join actionableCodon on actionableCodon.molecularCriteriumId = efficacyEvidence.molecularCriteriumId;
+
+# exons
+select  sourceEvent, gene, chromosome, start, end, applicableMutationType, source, sourceUrls, treatment, treatmentApproachesDrugClass, treatmentApproachesTherapy, SUBSTRING_INDEX(indication,"(", 1) as cancerType, REPLACE(SUBSTRING_INDEX(indication,"(", -1),")", "") as doid, evidenceLevel, evidenceDirection
+from efficacyEvidence
+inner join actionableExon on actionableExon.molecularCriteriumId = efficacyEvidence.molecularCriteriumId;
+
 #fusions
+select  sourceEvent, geneUp, minExonUp, maxExonUp, geneDown, minExonDown, maxExonDown source, sourceUrls, treatment, treatmentApproachesDrugClass, treatmentApproachesTherapy, SUBSTRING_INDEX(indication,"(", 1) as cancerType, REPLACE(SUBSTRING_INDEX(indication,"(", -1),")", "") as doid, evidenceLevel, evidenceDirection
+from efficacyEvidence
+inner join actionableFusion on actionableFusion.molecularCriteriumId = efficacyEvidence.molecularCriteriumId;
+
 #genes
+select  sourceEvent, gene, event, source, sourceUrls, treatment, treatmentApproachesDrugClass, treatmentApproachesTherapy, SUBSTRING_INDEX(indication,"(", 1) as cancerType, REPLACE(SUBSTRING_INDEX(indication,"(", -1),")", "") as doid, evidenceLevel, evidenceDirection
+from efficacyEvidence
+inner join actionableGene on actionableGene.molecularCriteriumId = efficacyEvidence.molecularCriteriumId;
+
 #hla
+select  sourceEvent, hlaAllele, treatment, treatmentApproachesDrugClass, treatmentApproachesTherapy, SUBSTRING_INDEX(indication,"(", 1) as cancerType, REPLACE(SUBSTRING_INDEX(indication,"(", -1),")", "") as doid, evidenceLevel, evidenceDirection
+from efficacyEvidence
+inner join actionableHla on actionableHla.molecularCriteriumId = efficacyEvidence.molecularCriteriumId;
 
 
 # CLINICAL STUDIES
