ce4ef00c9169b2d65ca4963eea0ee7de85232cea
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2025-07-25 09:32:47 +0200

    update syntax for check on particular ID

--- oncoact/patientreporter/reporting_pipeline/check_report_and_run_status.py
+++ oncoact/patientreporter/reporting_pipeline/check_report_and_run_status.py
@@ -401,7 +401,8 @@ class StatusChecker:
         sql_query = (
             "SELECT sampleId, qcStatus, purity, minPurity,maxPurity, ploidy, minPloidy, maxPloidy, maxPurity-minPurity as range_purity, maxPloidy-minPloidy as range_ploidy "
             "FROM purity "
-            "WHERE sampleId = '{reporting_id}' AND qcStatus NOT LIKE '%FAIL_NO_TUMOR%'"
+            "WHERE qcStatus NOT LIKE '%FAIL_NO_TUMOR%'"
+            f"AND gc.sampleId = '{reporting_id}' "
         )
         query_command = f"do_execute_sql_on_database \"{sql_query}\" hmfpatients '{creds}'"
         output = subprocess.run(query_command, shell=True, text=True, capture_output=True)
