b881dd40ded19109af3634bd2eccd4b5b47d2ef2
Ned Leitch <ned.leitch@gmail.com>
2025-10-17 20:19:53 -0400

    Improve beta tag generation
    Now generates names with the branch name included for a beta.
    
    No checks added on the branch name yet; it should be of the form
    `project-number`, such as `ACTIN-1234` to be properly picked up by the
    build tooling.

--- actin/local/util/next_semver_tags.sh
+++ actin/local/util/next_semver_tags.sh
@@ -6,35 +6,35 @@ echo
 
 set -e
 
-latest="$(git tag -l | egrep '^v?[0-9]+\.[0-9]+\.[0-9]+(-beta\..+)?$' | sort -V | tail -n1 | sed -e 's/^v//' -e 's/-beta.*$//')"
-IFS='.' read -r a b c <<< "$latest"
-a=${a:-0}; b=${b:-0}; c=${c:-0}
+TAG_REGEX="^v?(([0-9]+)\.([0-9]+)\.([0-9]+)(-((alpha|beta)\.([0-9])+))?(_([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?)$"
+latest="$(git tag -l | egrep "$TAG_REGEX" | sort -V | tail -n1)"
+if [[ $latest =~ $TAG_REGEX ]]; then
+    branch="$(git branch | grep '^*' | cut -d' ' -f2)"
+    a=${BASH_REMATCH[2]}
+    b=${BASH_REMATCH[3]}
+    c=${BASH_REMATCH[4]}
+    beta_version=${BASH_REMATCH[8]}
+    description=${BASH_REMATCH[9]}
+    final_version=${BASH_REMATCH[11]}
+    major="$((a+1)).0.0"
+    minor="${a}.$((b+1)).0"
+    patch="${a}.${b}.$((c+1))"
 
-branch="$(git branch | grep '^*' | cut -d' ' -f2)"
-major="$((a+1)).0.0"
-minor="${a}.$((b+1)).0"
-patch="${a}.${b}.$((c+1))"
+    printf "Next tags:\n\n"
+    if [[ $branch == "master" ]]; then
+        echo "Major/minor/patch: $major / $minor / $patch"
+        echo "Beta branch names: Cannot make beta releases on [master]"
+    else
+        local_beta="$(diff <(git tag --merged master) <(git tag --merged "$branch") | grep '^>' | sort -V | tail -n1 | cut -d' ' -f2)"
+        if [[ -z $local_beta ]]; then
+            beta_suffix="beta.1_${branch}"
+        else
+            beta_suffix="beta.$((beta_version+1))${description}"
+        fi
 
-printf "Next tags:\n\n"
-if [[ $branch == "master" ]]; then
-  echo "Major/minor/patch: $major / $minor / $patch"
-  echo "Beta branch names: Cannot make beta releases on [master]"
-else
-  local_beta="$(diff <(git tag --merged master) <(git tag --merged "$branch") | grep '^>' | sort -V | tail -n1 | cut -d' ' -f2)"
-  if [[ -z $local_beta ]]; then
-    beta_suffix="beta.1"
-  else
-    local_version="${local_beta%-beta.*}"
-    local_beta_version="${local_beta#${local_version}-beta\.}"
-    beta_suffix="beta.$((local_beta_version+1))"
-    next_local_beta_version="${local_version}-${beta_suffix}"
-  fi
-  echo "Major/minor/patch: Cannot make non-beta releases on non-master branch [$branch]"
-  printf "Beta branch names: "
-  if [[ -z $next_local_beta_version ]]; then
-    echo "${major}-${beta_suffix} / ${minor}-${beta_suffix} / ${patch}-${beta_suffix}"
-  else
-    echo "${next_local_beta_version}"
-  fi
+        echo "Major/minor/patch: Cannot make non-beta releases on non-master branch [$branch]"
+        printf "Beta branch names: "
+        echo "${major}-${beta_suffix} / ${minor}-${beta_suffix} / ${patch}-${beta_suffix}"
+    fi
+    echo
 fi
-echo
