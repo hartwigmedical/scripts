82aa77e7b9760cd08ebc7cbcd7473abeb7f5866e
Ned Leitch <48131072+nedleitch@users.noreply.github.com>
2026-05-26 16:02:01 -0400

    ACTIN-3639 Use DNS instead of bastion proxy (#147)
    This change requires enabling the DNS endpoint on the cluster, but once
    that's there we no longer require the proxying via the bastion.
    
    It also increments the local ports for research by 1000. This should help
    with the long-standing problem of not knowing whether the environment 
    you're connected to is the production or the research one.
    
    With this approach, both prod and research can be used simultaneously.
    
    A `username` query parameter has been added to the URL used to open
    the tunnelled application; applications that care about the username can
    pull this out and use it for user identification.
    
    Also switch to `zsh` as the Mac version of `bash` is too old to support
    arrays.
    
    Finally rename the script, it only starts tunnels now.
    
    ---------
    
    Co-authored-by: Ned Leitch <ned.leitch@gmail.com>

---
+++ actin/local/operational/start_tunnel.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env zsh
+
+function print_usage() {
+  echo "$0: establish a connection to an application running in Kubernetes"
+  echo "Use one of these forms:"
+  echo "  $0 prod [app]"
+  echo "  $0 research [app] [namespace]"
+}
+
+[[ $# -ne 2 && $# -ne 3 ]] && print_usage && exit 1
+
+gcv="$(gcloud --version | head -n1 | awk '{print $NF}' | awk -F. '{print $1}')"
+[[ $gcv -lt "496" ]] && echo "Local version of gcloud is not new enough (need >= 496.0.0)" && exit 1
+
+env="$1"
+app="$2"
+ns="${3:-default}"
+
+[[ "$env" != "research" && "$env" != "prod" ]] && print_usage && exit 1
+[[ "$env" == "prod" && "$ns" != "default" ]] && print_usage && exit 1
+[[ "$env" == "research" && "$ns" == "default" ]] && echo "Warning: namespace '$ns' given for research, could be a problem"
+
+declare -A clusters=( ["research"]="research-cluster-patient-processing" ["prod"]="shared-cluster-services" )
+declare -A projects=( ["research"]="actin-research" ["prod"]="actin-shared" )
+declare -A apps=( ["trial-ui"]="actin-trial-ui" ["tracker-ui"]="actin-patient-tracker" ["feed-review"]="actin-feed-review" )
+declare -A ports=( ["trial-ui"]="8082" ["tracker-ui"]="8085" ["feed-review"]="8080" )
+
+set -e
+
+gcloud container clusters get-credentials "${clusters[$env]}" --zone europe-west4 --project "${projects[$env]}" --dns-endpoint
+
+pod="$(kubectl -n "$ns" get pods | grep "^${apps[$app]}-" | awk '{print $1}' | tail -n1)"
+[[ -z "$pod" ]] && echo "No pod found for application '$app'" && exit 1
+remote="${ports[$app]}"
+increment=0
+[[ $env == "research" ]] && increment=1000
+local="$(( $remote + $increment ))"
+username="$(gcloud compute os-login describe-profile --format="json" | jq -r '.posixAccounts[].username')"
+(sleep 3; open "http://localhost:$local/${app}?username=${username}" ) &
+echo "Establishing tunnel, Ctrl-C will close it"
+kubectl -n "$ns" port-forward "pod/${pod}" "$local:$remote"
