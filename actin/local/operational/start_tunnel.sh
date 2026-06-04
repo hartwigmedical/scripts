3f7e361fabb7d3f8e94d2f442ce0bf5c7c199a39
Ned Leitch <48131072+nedleitch@users.noreply.github.com>
2026-06-04 12:35:55 -0400

    ACTIN-3691 Use start_tunnel.sh exclusively (#149)
    * ACTIN-3691 Use start_tunnel.sh exclusively
    
    Eliminate the need for the `manage_tunnel.sh` script by porting the
    namespace-related functionality from it to the `start_tunnel.sh` script.
    
    Key changes and notes:
    
     * Pull in the configurations directory formerly used by
       `manage_tunnel`sh` to allow arbitrary applications to be added easily
     * Provide for more varied use-cases by allowing the configuration
       directory, namespace and local port to be provided on the command
       line
     * The research environment will now be able to use the same script as
       the rest of the infrastructure by providing its own configuration
       directory on invocation
     * Improve usage messaging
    
    Tested with all current applications in EMC's various namespaces, NKI
    and the shared project.
    
    * ACTIN-3691 Use more-robust var exist check
    
    This version of the variable existence check is meant to work on any
    version of BASH.
    
    We are actually using `zsh` in most instances so possible it does not
    properly work there.
    
    * ACTIN-3691 Fix MacOS bad substitution
    
    ---------
    
    Co-authored-by: Ned Leitch <ned.leitch@gmail.com>

--- actin/local/operational/start_tunnel.sh
+++ actin/local/operational/start_tunnel.sh
@@ -1,41 +1,85 @@
 #!/usr/bin/env zsh
 
-function print_usage() {
-  echo "$0: establish a connection to an application running in Kubernetes"
-  echo "Use one of these forms:"
-  echo "  $0 prod [app]"
-  echo "  $0 research [app] [namespace]"
+set -o pipefail
+
+CONFIG_DIR="$(readlink -f "$(dirname "$0")")/.tunnel_configurations"
+
+function print_available_configs() {
+    echo "Known configurations:"
+    for c in "$(ls $CONFIG_DIR)"; do
+        echo "${c}" | sed -e 's/^/  /' -e 's/\.config$//'
+    done
 }
 
-[[ $# -ne 2 && $# -ne 3 ]] && print_usage && exit 1
+
+function print_usage() {
+    cat<<EOM
+
+Establish a connection to an application running in a private Kubernetes cluster    
+USAGE: $0 (application) [-c config_dir] [-n namespace] [-p local_port]
+
+  application - Application to connect to, see below for existing list
+  config_dir  - [optional] directory to use for configuration files instead of the default [.../$(basename $CONFIG_DIR)]
+  namespace   - [optional] namespace overriding any set in configuration or the default value "default"
+  local_port  = [optional] local port to bind, used to avoid conflicts (overrides default in configuragion file)
+
+NOTE: To start multiple tunnels  use `-p` to avoid local port conflicts and multiple terminals or Ctrl-Z to run many instances
+
+EOM
+
+  print_available_configs
+}
 
 gcv="$(gcloud --version | head -n1 | awk '{print $NF}' | awk -F. '{print $1}')"
 [[ $gcv -lt "496" ]] && echo "Local version of gcloud is not new enough (need >= 496.0.0)" && exit 1
+[[ $# -eq 0 ]] && echo "ERROR: Application must be provided!" && print_usage && exit 1
+app="$1" && shift
+config_dir="$CONFIG_DIR"
+while getopts 'c:n:p:' opt; do
+    case "$opt" in
+        c)
+            config_dir="$OPTARG"
+            ;;
+        n)
+            user_ns="$OPTARG"
+            ;;
+        p)
+            local_port="$OPTARG"
+            ;;
+        *)
+            echo "Unknown argument [$opt]"
+            print_usage
+            exit 1
+            ;;
+    esac
+done
 
-env="$1"
-app="$2"
-ns="${3:-default}"
+shift "$(($OPTIND -1))"
 
-[[ "$env" != "research" && "$env" != "prod" ]] && print_usage && exit 1
-[[ "$env" == "prod" && "$ns" != "default" ]] && print_usage && exit 1
-[[ "$env" == "research" && "$ns" == "default" ]] && echo "Warning: namespace '$ns' given for research, could be a problem"
+config_file="${config_dir}/${app}.config"
+if [[ ! -f $config_file ]]; then
+    echo "Unknown app [$app]: [$config_file] does not exist"
+    print_available_configs 
+    exit 1
+fi
 
-declare -A clusters=( ["research"]="research-cluster-patient-processing" ["prod"]="shared-cluster-services" )
-declare -A projects=( ["research"]="actin-research" ["prod"]="actin-shared" )
-declare -A apps=( ["trial-ui"]="actin-trial-ui" ["tracker-ui"]="actin-patient-tracker" ["feed-review"]="actin-feed-review" )
-declare -A ports=( ["trial-ui"]="8082" ["tracker-ui"]="8085" ["feed-review"]="8080" )
+source $config_file
+ns="${user_ns:-$NAMESPACE}"
+ns="${ns:-default}"
 
-set -e
+for var in PROJECT PORT CONTEXT_ROOT CLUSTER; do
+    [[ -z ${(P)var} ]] && echo "$var must be specified in [$config_file]" && exit 1
+done
 
-gcloud container clusters get-credentials "${clusters[$env]}" --zone europe-west4 --project "${projects[$env]}" --dns-endpoint
+local_port="${local_port:-$PORT}"
+pod_name_prefix="${POD:=actin-$app}"
 
-pod="$(kubectl -n "$ns" get pods | grep "^${apps[$app]}-" | awk '{print $1}' | tail -n1)"
-[[ -z "$pod" ]] && echo "No pod found for application '$app'" && exit 1
-remote="${ports[$app]}"
-increment=0
-[[ $env == "research" ]] && increment=1000
-local="$(( $remote + $increment ))"
+gcloud container clusters get-credentials "$CLUSTER" --zone europe-west4 --project "$PROJECT" --dns-endpoint
+
+pod="$(kubectl -n "$ns" get pods | grep "^${pod_name_prefix}-" | awk '{print $1}' | tail -n1)"
+[[ -z "$pod" ]] && echo "No pod found for application '$app', check [$config_file]" && exit 1
 username="$(gcloud compute os-login describe-profile --format="json" | jq -r '.posixAccounts[].username')"
-(sleep 3; open "http://localhost:$local/${app}?username=${username}" ) &
+(sleep 3; open "http://localhost:${local_port}/${CONTEXT_ROOT}?username=${username}" ) &
 echo "Establishing tunnel, Ctrl-C will close it"
-kubectl -n "$ns" port-forward "pod/${pod}" "$local:$remote"
+kubectl -n "$ns" port-forward "pod/${pod}" "$local_port:$PORT"
+
